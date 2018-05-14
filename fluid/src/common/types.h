a00ab6acdc6668dd2d5f3b98404314def7c78f4e
liuruilong <liuruilong@baidu.com>
2018-05-14 16:34:37 +0800

    modify code style

--- fluid/src/common/types.h
+++ fluid/src/common/types.h
@@ -20,15 +20,15 @@ SOFTWARE.
 
 namespace paddle_mobile{
 enum class Precision : int{
-    FP32 = 0
+  FP32 = 0
 };
 
 //! device type
 enum DeviceTypeEnum{
-    eINVALID = -1,
-    eARM   = 0,
-    eCPU   = 1,
-    eFPGA  = 2
+  eINVALID = -1,
+  eARM   = 0,
+  eCPU   = 1,
+  eFPGA  = 2
 };
 
 template <DeviceTypeEnum T>
@@ -60,33 +60,33 @@ struct D1{};
 
 //! data type
 enum DataType {
-    PM_INVALID      =       -1,
-    PM_HALF         =       0,
-    PM_FLOAT        =       1,
-    PM_DOUBLE       =       2,
-    PM_INT8         =       3,
-    PM_INT16        =       4,
-    PM_INT32        =       5,
-    PM_INT64        =       6,
-    PM_UINT8        =       7,
-    PM_UINT16       =       8,
-    PM_UINT32       =       9,
-    PM_STRING       =       10,
-    PM_BOOL         =       11,
-    PM_SHAPE        =       12,
-    PM_TENSOR       =       13
+  PM_INVALID      =       -1,
+  PM_HALF         =       0,
+  PM_FLOAT        =       1,
+  PM_DOUBLE       =       2,
+  PM_INT8         =       3,
+  PM_INT16        =       4,
+  PM_INT32        =       5,
+  PM_INT64        =       6,
+  PM_UINT8        =       7,
+  PM_UINT16       =       8,
+  PM_UINT32       =       9,
+  PM_STRING       =       10,
+  PM_BOOL         =       11,
+  PM_SHAPE        =       12,
+  PM_TENSOR       =       13
 };
 //!
 typedef enum {
-    PMSuccess         = -1,                             /*!< No errors */
-    PMNotInitialized  = 1,                              /*!< Data not initialized. */
-    PMInvalidValue    = (1 << 1) + PMNotInitialized, /*!< Incorrect variable value. */
-    PMMemAllocFailed  = (1 << 2) + PMInvalidValue,   /*!< Memory allocation error. */
-    PMUnKownError     = (1 << 3) + PMMemAllocFailed, /*!< Unknown error. */
-    PMOutOfAuthority  = (1 << 4) + PMUnKownError,    /*!< Try to modified data not your own*/
-    PMOutOfMem        = (1 << 5) + PMOutOfAuthority, /*!< OOM error*/
-    PMUnImplError     = (1 << 6) + PMOutOfMem,       /*!< Unimplement error. */
-    PMWrongDevice     = (1 << 7) + PMUnImplError     /*!< un-correct device. */
+  PMSuccess         = -1,                             /*!< No errors */
+  PMNotInitialized  = 1,                              /*!< Data not initialized. */
+  PMInvalidValue    = (1 << 1) + PMNotInitialized, /*!< Incorrect variable value. */
+  PMMemAllocFailed  = (1 << 2) + PMInvalidValue,   /*!< Memory allocation error. */
+  PMUnKownError     = (1 << 3) + PMMemAllocFailed, /*!< Unknown error. */
+  PMOutOfAuthority  = (1 << 4) + PMUnKownError,    /*!< Try to modified data not your own*/
+  PMOutOfMem        = (1 << 5) + PMOutOfAuthority, /*!< OOM error*/
+  PMUnImplError     = (1 << 6) + PMOutOfMem,       /*!< Unimplement error. */
+  PMWrongDevice     = (1 << 7) + PMUnImplError     /*!< un-correct device. */
 } PMStatus;
 
 }
