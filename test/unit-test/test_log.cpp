1a9981ee159a37029b292b0f7f043083186c88f5
liuruilong <liuruilong@baidu.com>
2018-05-17 15:50:33 +0800

    format files

--- test/unit-test/test_log.cpp
+++ test/unit-test/test_log.cpp
@@ -19,13 +19,13 @@ SOFTWARE.
 #include "common/log.h"
 
 int main() {
-  LOG(paddle_mobile::kLOG_DEBUG) << "test debug"
-                                 << " next log";
-  LOG(paddle_mobile::kLOG_DEBUG1) << "test debug1"
-                                  << " next log";
-  LOG(paddle_mobile::kLOG_DEBUG2) << "test debug2"
-                                  << " next log";
-  DLOG << "test DLOG";
-  LOG(paddle_mobile::kLOG_ERROR) << " error occur !";
-  return 0;
+    LOG(paddle_mobile::kLOG_DEBUG) << "test debug"
+                                   << " next log";
+    LOG(paddle_mobile::kLOG_DEBUG1) << "test debug1"
+                                    << " next log";
+    LOG(paddle_mobile::kLOG_DEBUG2) << "test debug2"
+                                    << " next log";
+    DLOG << "test DLOG";
+    LOG(paddle_mobile::kLOG_ERROR) << " error occur !";
+    return 0;
 }
