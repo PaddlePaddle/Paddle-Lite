253ace53ce0d193b3578c114519550ae1eb4c45c
liuruilong <liuruilong@baidu.com>
2018-05-17 17:46:38 +0800

    update log module

--- test/unit-test/test_log.cpp
+++ test/unit-test/test_log.cpp
@@ -19,13 +19,19 @@ SOFTWARE.
 #include "common/log.h"
 
 int main() {
-    LOG(paddle_mobile::kLOG_DEBUG) << "test debug"
-                                   << " next log";
+    DLOGF("DASJFDAFJ%d -- %f", 12345, 344.234);
+    LOG(paddle_mobile::kLOG_DEBUG) << "test debug" << " next log";
+
     LOG(paddle_mobile::kLOG_DEBUG1) << "test debug1"
                                     << " next log";
+
     LOG(paddle_mobile::kLOG_DEBUG2) << "test debug2"
                                     << " next log";
+
     DLOG << "test DLOG";
+
+
     LOG(paddle_mobile::kLOG_ERROR) << " error occur !";
+
     return 0;
 }
