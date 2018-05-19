e6459329d5d05bdf1a361ed2bcc047475d85a56d
eclipsess <wowchinasuiyang@163.com>
2018-05-19 11:20:10 +0800

    code style

--- test/operators/test_mul_op.cpp
+++ test/operators/test_mul_op.cpp
@@ -17,119 +17,117 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 ==============================================================================*/
 #pragma once
-#include "operators/mul_op.h"
 #include "../test_include.h"
+#include "operators/mul_op.h"
 
 namespace paddle_mobile {
-    namespace framework {
+namespace framework {
 
-        template<typename Dtype>
-        class TestMulOp {
-        public:
-            TestMulOp(const Program <Dtype> p) : program_(p) {
-                if (use_optimize_) {
-                    to_predict_program_ = program_.optimizeProgram;
-                } else {
-                    to_predict_program_ = program_.originProgram;
-                }
+template <typename Dtype> class TestMulOp {
+  public:
+    TestMulOp(const Program<Dtype> p) : program_(p) {
+        if (use_optimize_) {
+            to_predict_program_ = program_.optimizeProgram;
+        } else {
+            to_predict_program_ = program_.originProgram;
+        }
 
-                const std::vector<std::shared_ptr<BlockDesc>> blocks =
-                        to_predict_program_->Blocks();
-                //  DLOG << " **block size " << blocks.size();
-                for (int i = 0; i < blocks.size(); ++i) {
-                    std::shared_ptr<BlockDesc> block_desc = blocks[i];
-                    std::vector<std::shared_ptr<OpDesc>> ops = block_desc->Ops();
-                    //    DLOG << " ops " << ops.size();
-                    for (int j = 0; j < ops.size(); ++j) {
-                        std::shared_ptr<OpDesc> op = ops[j];
-                        //                        if (op->Type() == "mul") {
-                        //                            DLOG << "x_num_col_dims :
-                        //                            "
-                        //                                 << op->GetAttrMap()
-                        //                                        .at("x_num_col_dims")
-                        //                                        .Get<int>();
-                        //                            DLOG << "y_num_col_dims :
-                        //                            "
-                        //                                 << op->GetAttrMap()
-                        //                                        .at("y_num_col_dims")
-                        //                                        .Get<int>();
-                        //                            DLOG << " Input X is : "
-                        //                            << op->Input("X")[0];
-                        //                        }
-                        //                        DLOG << "op:" << op->Type();
-                        if (op->Type() == "mul" &&
-                            op->Input("X")[0] == "pool2d_0.tmp_0") {
-                            DLOG << " mul attr size: " << op->GetAttrMap().size();
-                            DLOG << " inputs size: " << op->GetInputs().size();
-                            DLOG << " outputs size: " << op->GetOutputs().size();
-                            DLOG << " Input X is : " << op->Input("X")[0];
-                            DLOG << " Input Y is : " << op->Input("Y")[0];
-                            DLOG << " Output Out is : " << op->Output("Out")[0];
-                            DLOG << "x_num_col_dims : "
-                                 << op->GetAttrMap().at("x_num_col_dims").Get<int>();
-                            DLOG << "y_num_col_dims : "
-                                 << op->GetAttrMap().at("y_num_col_dims").Get<int>();
+        const std::vector<std::shared_ptr<BlockDesc>> blocks =
+            to_predict_program_->Blocks();
+        //  DLOG << " **block size " << blocks.size();
+        for (int i = 0; i < blocks.size(); ++i) {
+            std::shared_ptr<BlockDesc> block_desc = blocks[i];
+            std::vector<std::shared_ptr<OpDesc>> ops = block_desc->Ops();
+            //    DLOG << " ops " << ops.size();
+            for (int j = 0; j < ops.size(); ++j) {
+                std::shared_ptr<OpDesc> op = ops[j];
+                //                        if (op->Type() == "mul") {
+                //                            DLOG << "x_num_col_dims :
+                //                            "
+                //                                 << op->GetAttrMap()
+                //                                        .at("x_num_col_dims")
+                //                                        .Get<int>();
+                //                            DLOG << "y_num_col_dims :
+                //                            "
+                //                                 << op->GetAttrMap()
+                //                                        .at("y_num_col_dims")
+                //                                        .Get<int>();
+                //                            DLOG << " Input X is : "
+                //                            << op->Input("X")[0];
+                //                        }
+                //                        DLOG << "op:" << op->Type();
+                if (op->Type() == "mul" &&
+                    op->Input("X")[0] == "pool2d_0.tmp_0") {
+                    DLOG << " mul attr size: " << op->GetAttrMap().size();
+                    DLOG << " inputs size: " << op->GetInputs().size();
+                    DLOG << " outputs size: " << op->GetOutputs().size();
+                    DLOG << " Input X is : " << op->Input("X")[0];
+                    DLOG << " Input Y is : " << op->Input("Y")[0];
+                    DLOG << " Output Out is : " << op->Output("Out")[0];
+                    DLOG << "x_num_col_dims : "
+                         << op->GetAttrMap().at("x_num_col_dims").Get<int>();
+                    DLOG << "y_num_col_dims : "
+                         << op->GetAttrMap().at("y_num_col_dims").Get<int>();
 
-                            std::shared_ptr<operators::MulOp<Dtype, float>> add =
-                                    std::make_shared<operators::MulOp<Dtype, float>>(
-                                            op->Type(), op->GetInputs(), op->GetOutputs(),
-                                            op->GetAttrMap(), program_.scope);
-                            ops_of_block_[*block_desc.get()].push_back(add);
-                        }
-                    }
+                    std::shared_ptr<operators::MulOp<Dtype, float>> add =
+                        std::make_shared<operators::MulOp<Dtype, float>>(
+                            op->Type(), op->GetInputs(), op->GetOutputs(),
+                            op->GetAttrMap(), program_.scope);
+                    ops_of_block_[*block_desc.get()].push_back(add);
                 }
             }
+        }
+    }
 
-            std::shared_ptr<Tensor> predict_add(Tensor &t1, Tensor &t2) {
-                // feed
-                auto scope = program_.scope;
-                Variable *x_feed_value = scope->Var("pool2d_0.tmp_0");
-                auto tensor_x = x_feed_value->GetMutable<Tensor>();
-                tensor_x->ShareDataWith(t1);
+    std::shared_ptr<Tensor> predict_add(Tensor &t1, Tensor &t2) {
+        // feed
+        auto scope = program_.scope;
+        Variable *x_feed_value = scope->Var("pool2d_0.tmp_0");
+        auto tensor_x = x_feed_value->GetMutable<Tensor>();
+        tensor_x->ShareDataWith(t1);
 
-                Variable *y_feed_value = scope->Var("fc_0.w_0");
-                auto tensor_y = y_feed_value->GetMutable<Tensor>();
-                tensor_y->ShareDataWith(t2);
+        Variable *y_feed_value = scope->Var("fc_0.w_0");
+        auto tensor_y = y_feed_value->GetMutable<Tensor>();
+        tensor_y->ShareDataWith(t2);
 
-                Variable *con_output = scope->Var("fc_0.tmp_0");
-                Tensor *output_tensor = con_output->GetMutable<Tensor>();
-                output_tensor->mutable_data<float>({3, 3});
-                //  DLOG << typeid(output_tensor).name();
-                //  DLOG << "output_tensor dims: " << output_tensor->dims();
+        Variable *con_output = scope->Var("fc_0.tmp_0");
+        Tensor *output_tensor = con_output->GetMutable<Tensor>();
+        output_tensor->mutable_data<float>({3, 3});
+        //  DLOG << typeid(output_tensor).name();
+        //  DLOG << "output_tensor dims: " << output_tensor->dims();
 
-                std::shared_ptr<Tensor> out_tensor = std::make_shared<LoDTensor>();
-                out_tensor.reset(output_tensor);
+        std::shared_ptr<Tensor> out_tensor = std::make_shared<LoDTensor>();
+        out_tensor.reset(output_tensor);
 
-                predict_add(t1, t2, 0);
-                return out_tensor;
-            }
+        predict_add(t1, t2, 0);
+        return out_tensor;
+    }
 
-        private:
-            const framework::Program<Dtype> program_;
-            std::shared_ptr<ProgramDesc> to_predict_program_;
-            std::map<framework::BlockDesc,
-                    std::vector<std::shared_ptr<OperatorBase < Dtype>>>>
-            ops_of_block_;
-            bool use_optimize_ = false;
+  private:
+    const framework::Program<Dtype> program_;
+    std::shared_ptr<ProgramDesc> to_predict_program_;
+    std::map<framework::BlockDesc,
+             std::vector<std::shared_ptr<OperatorBase<Dtype>>>>
+        ops_of_block_;
+    bool use_optimize_ = false;
 
-            void predict_add(const Tensor &t1, const Tensor &t2, int block_id) {
-                std::shared_ptr<BlockDesc> to_predict_block =
-                        to_predict_program_->Block(block_id);
-                for (int j = 0; j < ops_of_block_[*to_predict_block.get()].size();
-                     ++j) {
-                    auto op = ops_of_block_[*to_predict_block.get()][j];
-                    DLOG << "op -> run()";
-                    op->Run();
-                }
-            }
-        };
+    void predict_add(const Tensor &t1, const Tensor &t2, int block_id) {
+        std::shared_ptr<BlockDesc> to_predict_block =
+            to_predict_program_->Block(block_id);
+        for (int j = 0; j < ops_of_block_[*to_predict_block.get()].size();
+             ++j) {
+            auto op = ops_of_block_[*to_predict_block.get()][j];
+            DLOG << "op -> run()";
+            op->Run();
+        }
+    }
+};
 
-        template
-        class TestMulOp<CPU>;
-    } // namespace framework
-} // namespaece paddle_mobile
+template class TestMulOp<CPU>;
+} // namespace framework
+} // namespace paddle_mobile
 
-int main () {
+int main() {
     DLOG << "----------**********----------";
     DLOG << "begin to run MulOp Test";
     paddle_mobile::Loader<paddle_mobile::CPU> loader;
