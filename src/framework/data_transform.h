0a553481694e574158a08f52ac22a082eba85e3a
zhaojiaying01 <zhaojiaying01@baidu.com>
2018-05-23 21:57:53 +0800

    add copyright

--- src/framework/data_transform.h
+++ src/framework/data_transform.h
@@ -1,20 +1,16 @@
-/* Copyright (c) 2016 Baidu, Inc. All Rights Reserved.
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
-==============================================================================*/
+/* Copyright (c) 2018 PaddlePaddle Authors. All Rights Reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License. */
 
 #pragma once
 
@@ -22,10 +18,10 @@ SOFTWARE.
 #include <utility>
 #include <vector>
 
-#include "op_kernel_type.h"
-#include "selected_rows.h"
-#include "tensor.h"
-#include "variable.h"
+#include "framework/op_kernel_type.h"
+#include "framework/selected_rows.h"
+#include "framework/tensor.h"
+#include "framework/variable.h"
 
 namespace paddle_mobile {
 namespace framework {
@@ -35,7 +31,7 @@ void DataTransform(const OpKernelType &expected_kernel_type,
                    const Tensor &input_tensor, Tensor *out);
 
 void CopyVariableWithTensor(const Variable &in_var, const Tensor &tensor,
-                            Variable &out_var);
+                            Variable *out_var);
 
 }  // namespace framework
 }  // namespace paddle_mobile
