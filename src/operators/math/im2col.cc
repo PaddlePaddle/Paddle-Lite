686330b8b2455a0b5178413753cb88a6f739978c
zhaojiaying01 <zhaojiaying01@baidu.com>
2018-05-23 22:08:02 +0800

    add copyright

--- src/operators/math/im2col.cc
+++ src/operators/math/im2col.cc
@@ -12,8 +12,8 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License. */
 
-#include <vector>
 #include "operators/math/im2col.h"
+#include <vector>
 #include "common/types.h"
 
 namespace paddle_mobile {
