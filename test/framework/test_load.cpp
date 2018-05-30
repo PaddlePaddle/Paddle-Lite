9f48fe1343ed651f71dc6f0ebcbfe790099ac018
wangliu <wangliu@baidu.com>
2018-05-30 11:47:10 +0800

    add timer for debug

--- test/framework/test_load.cpp
+++ test/framework/test_load.cpp
@@ -12,7 +12,7 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License. */
 
-#include "io/io.h"
+#include "common/io.h"
 
 int main() {
   paddle_mobile::Loader<paddle_mobile::CPU> loader;
