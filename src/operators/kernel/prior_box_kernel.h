ad099595a9558106c5169ab628da83c2900db33c
liuruilong <liuruilong@baidu.com>
2018-06-05 21:13:11 +0800

    format files

--- src/operators/kernel/prior_box_kernel.h
+++ src/operators/kernel/prior_box_kernel.h
@@ -18,9 +18,9 @@ limitations under the License. */
 
 #include <vector>
 
-#include "operators/op_param.h"
 #include "framework/operator.h"
 #include "operators/math/transform.h"
+#include "operators/op_param.h"
 
 namespace paddle_mobile {
 namespace operators {
