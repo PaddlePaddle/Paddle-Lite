246b7a2a351e86f591f00a3868cbb954bf829179
liuruilong <liuruilong@baidu.com>
2018-06-05 21:13:11 +0800

    format files

--- src/framework/program/program-optimize/node.cpp
+++ src/framework/program/program-optimize/node.cpp
@@ -12,8 +12,8 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License. */
 
-#include "framework/operator.h"
 #include "framework/program/program-optimize/node.h"
+#include "framework/operator.h"
 
 namespace paddle_mobile {
 
--- src/operators/kernel/pool_kernel.h
+++ src/operators/kernel/pool_kernel.h
@@ -17,8 +17,8 @@ limitations under the License. */
 #pragma once
 
 #include "framework/operator.h"
-#include "operators/op_param.h"
 #include "operators/math/pooling.h"
+#include "operators/op_param.h"
 
 namespace paddle_mobile {
 namespace operators {
