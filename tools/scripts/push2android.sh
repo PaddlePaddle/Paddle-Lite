4ea3d0a57ee28a67fa4f6b9765f1b0e447e54446
liuruilong <liuruilong@baidu.com>
2018-06-06 14:13:47 +0800

    remove unused code, modify build and cmake

--- tools/scripts/push2android.sh
+++ tools/scripts/push2android.sh
@@ -7,7 +7,7 @@ EXE_FILE="../../test/build/*"
 EXE_DIR="data/local/tmp/bin"
 MODELS_DIR="data/local/tmp/models"
 IMAGES_DIR="data/local/tmp/images"
-LIB_PATH="../build/release/arm-v7a/build/*"
+LIB_PATH="../../build/release/arm-v7a/build/*"
 adb push ${EXE_FILE} ${EXE_DIR}
 adb push ${LIB_PATH} ${EXE_DIR}
 adb push ${IMAGE_PATH} ${IMAGES_DIR}
