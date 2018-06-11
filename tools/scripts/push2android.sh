e826ad16af50f0e9c4d6328f5a223f483611cd21
eclipsess <wowchinasuiyang@163.com>
2018-06-11 15:52:23 +0800

    auto mkdir in models

--- tools/scripts/push2android.sh
+++ tools/scripts/push2android.sh
@@ -2,11 +2,20 @@
 
 push_fn () {
 MODELS_PATH="../../test/models/*"
+MODELS_SRC="../../test/models"
 IMAGE_PATH="../../test/images/*"
 EXE_FILE="../../test/build/*"
 EXE_DIR="data/local/tmp/bin"
+adb shell mkdir ${EXE_DIR}
 MODELS_DIR="data/local/tmp/models"
+adb shell mkdir ${MODELS_DIR}
+for file in `ls ${MODELS_SRC}`
+do 
+    adb shell mkdir ${MODELS_DIR}"/"${file}
+done
+
 IMAGES_DIR="data/local/tmp/images"
+adb shell mkdir ${IMAGES_DIR}
 LIB_PATH="../../build/release/arm-v7a/build/*"
 adb push ${EXE_FILE} ${EXE_DIR}
 adb push ${LIB_PATH} ${EXE_DIR}
