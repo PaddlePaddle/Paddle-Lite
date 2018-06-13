92498aba2a67aec8ad106c0bc1bf9f231591628b
eclipsess <wowchinasuiyang@163.com>
2018-06-13 18:59:10 +0800

    optimize lrn kernel

---
+++ tools/run_on_android.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env sh
+
+push_fn () {
+sh build.sh android googlenet
+MODELS_PATH="../test/models/*"
+MODELS_SRC="../../test/models"
+IMAGE_PATH="../test/images/*"
+EXE_FILE="../test/build/*"
+EXE_DIR="data/local/tmp/bin"
+adb shell mkdir ${EXE_DIR}
+MODELS_DIR="data/local/tmp/models"
+adb shell mkdir ${MODELS_DIR}
+for file in `ls ${MODELS_SRC}`
+do 
+    adb shell mkdir ${MODELS_DIR}"/"${file}
+done
+
+IMAGES_DIR="data/local/tmp/images"
+adb shell mkdir ${IMAGES_DIR}
+LIB_PATH="../build/release/arm-v7a/build/*"
+adb push ${EXE_FILE} ${EXE_DIR}
+adb push ${LIB_PATH} ${EXE_DIR}
+adb push ${IMAGE_PATH} ${IMAGES_DIR}
+adb push ${MODELS_PATH} ${MODELS_DIR}
+echo "test-op or test-net below : "
+adb shell ls /data/local/tmp/bin
+echo "**** choose OP or NET to test ****"
+adb shell "cd /data/local/tmp/bin; LD_LIBRARY_PATH=. ./test-lrn-op"
+}
+push_fn
