e4cf2516e6c242a61ff20295840a911bb8642a30
liuruilong <liuruilong@baidu.com>
2018-06-25 20:56:55 +0800

    adjuct project structure

--- metal/paddle-mobile-demo/paddle-mobile-demo/AppDelegate.swift
+++ metal/paddle-mobile-demo/paddle-mobile-demo/AppDelegate.swift
@@ -2,7 +2,7 @@
 //  AppDelegate.swift
 //  paddle-mobile-demo
 //
-//  Created by liuRuiLong on 2018/6/20.
+//  Created by liuRuiLong on 2018/6/25.
 //  Copyright © 2018年 orange. All rights reserved.
 //
 
---
+++ metal/paddle-mobile-demo/paddle-mobile-demo/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  paddle-mobile-demo
+//
+//  Created by liuRuiLong on 2018/6/25.
+//  Copyright © 2018年 orange. All rights reserved.
+//
+
+
+import UIKit
+import paddle_mobile
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
