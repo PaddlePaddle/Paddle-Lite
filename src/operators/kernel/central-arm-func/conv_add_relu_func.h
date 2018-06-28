1b1c20c1ce8cbb08a3bf245934b63a41a7caeded
liuruilong <liuruilong@baidu.com>
2018-06-28 13:56:03 +0800

    add central_arm_func folder

---
+++ src/operators/kernel/central-arm-func/conv_add_relu_func.h
@@ -0,0 +1,8 @@
+//
+// Created by liuRuiLong on 2018/6/28.
+//
+
+#ifndef PADDLE_MOBILE_CONV_ADD_RELU_FUNC_H
+#define PADDLE_MOBILE_CONV_ADD_RELU_FUNC_H
+
+#endif //PADDLE_MOBILE_CONV_ADD_RELU_FUNC_H
