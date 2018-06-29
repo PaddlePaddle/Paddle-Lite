4bef099b6579ca5da165300a7297fd4e839b0c21
eclipsess <wowchinasuiyang@163.com>
2018-06-29 17:44:49 +0800

    conv add bn relu

--- src/operators/fusion_conv_add.h
+++ src/operators/fusion_conv_add.h
@@ -40,7 +40,7 @@ class FusionConvAddMatcher : public framework::FusionOpMatcher {
     vector<std::shared_ptr<framework::OpDesc>> origin_descs =
         node->OpDescs(node_.Depth());
     node->Folder(node_.Depth(), Type(),
-                 {{G_OP_TYPE_ELEMENTWISE_ADD, {"Y", "Y"}}}, removed_nodes);
+                 {{G_OP_TYPE_ELEMENTWISE_ADD, {{"Y", "Y"}}}}, removed_nodes);
   }
 
   std::string Type() { return G_OP_TYPE_CONV_ADD; }
@@ -68,11 +68,13 @@ class FusionConvAddOp : public framework::OperatorWithKernel<
 };
 
 #ifdef PADDLE_MOBILE_CPU
+
 #ifndef CONV_ADD_REGISTER
 static framework::FusionOpRegistrar convadd_registrar(
     new FusionConvAddMatcher());
 #define CONV_ADD_REGISTER
 #endif
+
 #endif
 
 #ifdef PADDLE_MOBILE_MALI_GPU
