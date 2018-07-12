d5d5f672687334fd90955db0473f9ad33ac9acf3
wangliu <wangliu@baidu.com>
2018-07-12 16:10:17 +0800

    move android-demo project to cdn

---
+++ demo/ReadMe.md
@@ -0,0 +1,6 @@
+## 如何运行demo
+在demo目录下执行下载demo的脚本
+```
+sh getDemo.sh
+```
+如果想要运行Android的demo,将PaddleMobile_Android导入到Android-Studio中运行即可
---
+++ demo/getDemo.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+wget http://mms-graph.bj.bcebos.com/paddle-mobile%2FPaddleMobile_Android.zip
+unzip paddle-mobile%2FPaddleMobile_Android.zip
+rm -rf paddle-mobile%2FPaddleMobile_Android.zip
