d1220619a8f6db5f84a396b35696419447538b5a
liuruilong <liuruilong@baidu.com>
2018-07-18 20:06:15 +0800

    format files

--- src/operators/fusion_conv_add.h
+++ src/operators/fusion_conv_add.h
@@ -67,7 +67,7 @@ class FusionConvAddOp : public framework::OperatorWithKernel<
 #ifdef PADDLE_MOBILE_CPU
 
 #ifndef CONV_ADD_REGISTER
- static framework::FusionOpRegistrar convadd_registrar(
+static framework::FusionOpRegistrar convadd_registrar(
     new FusionConvAddMatcher());
 #define CONV_ADD_REGISTER
 #endif
