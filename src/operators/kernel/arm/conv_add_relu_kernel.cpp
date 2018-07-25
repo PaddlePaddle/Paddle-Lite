1f54387987e09a6eaa31af3f3d1686853054906f
zhangyang <zhangyang49@baidu.com>
2018-07-25 13:22:04 +0800

    Unify FUSION_CONVADDRELU_OP usage

--- src/operators/kernel/arm/conv_add_relu_kernel.cpp
+++ src/operators/kernel/arm/conv_add_relu_kernel.cpp
@@ -12,7 +12,7 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License. */
 
-#ifdef FUSION_CONVADD_RELU_OP
+#ifdef FUSION_CONVADDRELU_OP
 
 #include "operators/kernel/conv_add_relu_kernel.h"
 #include "operators/kernel/central-arm-func/conv_add_relu_arm_func.h"
