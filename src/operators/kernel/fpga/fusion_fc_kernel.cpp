906a6e12796ccc18164c56a53e0fb6db08ba6115
qnqinan <qnqinan@163.com>
2018-08-06 16:08:45 +0800

    update tensor.h file to solve the problem that * convert to const

--- src/operators/kernel/fpga/fusion_fc_kernel.cpp
+++ src/operators/kernel/fpga/fusion_fc_kernel.cpp
@@ -41,7 +41,7 @@ bool FusionFcKernel<FPGA, float>::Init(FusionFcParam *param) {
 
   fpga::ConvArgs convArgs;
   convArgs.relu_enabled = relu_enabled;
-  convArgs.bias_address = (void *)input_z_ptr;
+  convArgs.sb_address = (void *)bs_ptr;
   convArgs.filter_address = (void *)input_y_ptr;
   convArgs.filter_num = out->dims()[1];
   convArgs.group_num = 1;
