42aafdfb3c4d590ece473e955c5c4aeeba07a4f9
xiebaiyuan <xiebaiyuan@139.com>
2018-08-18 14:09:49 +0800

    format code in prelu_kernel

--- src/operators/kernel/arm/prelu_kernel.cpp
+++ src/operators/kernel/arm/prelu_kernel.cpp
@@ -48,22 +48,19 @@ void PReluKernel<CPU, float>::Compute(const PReluParam &param) const {
   int i = 0;
   int temp = 0;
   if (mode == "channel") {
-#pragma omp parallel for
-
+    #pragma omp parallel for
     for (i = 0; i < numel; i++) {
       temp = numel / (dim[0] * dim[1]);
       index = (i / temp) % dim[1];
       o_ptr[i] = x_ptr[i] > 0 ? x_ptr[i] : alpha_ptr[index] * x_ptr[i];
     }
   } else if (mode == "element") {
-#pragma omp parallel for
-
+    #pragma omp parallel for
     for (i = 0; i < numel; i++) {
       o_ptr[i] = x_ptr[i] > 0 ? x_ptr[i] : alpha_ptr[i] * x_ptr[i];
     }
   } else {
-#pragma omp parallel for
-
+    #pragma omp parallel for
     for (i = 0; i < numel; i++) {
       o_ptr[i] = x_ptr[i] > 0 ? x_ptr[i] : alpha_ptr[0] * x_ptr[i];
     }
