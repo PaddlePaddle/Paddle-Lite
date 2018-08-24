f5d94bf7068029f701d5cb9f87256c661de5ad70
liuruilong <liuruilong@baidu.com>
2018-08-24 17:16:27 +0800

    fix crash

--- metal/paddle-mobile/paddle-mobile/Operators/Kernels/Texture2DTo2DArrayKernel.swift
+++ metal/paddle-mobile/paddle-mobile/Operators/Kernels/Texture2DTo2DArrayKernel.swift
@@ -15,23 +15,24 @@
 import Foundation
 
 struct Texture2DTo2DArrayParam {
-    let input: MTLTexture
-    let output: MTLTexture
-    let expectDim: Dim
+  let input: MTLTexture
+  let output: MTLTexture
+  let expectDim: Dim
 }
 
 class Texture2DTo2DArrayKernel<P: PrecisionType>: Kernel, Computable{
-    func compute(commandBuffer: MTLCommandBuffer, param: FeedParam<P>) throws {
-        guard let encoder = commandBuffer.makeComputeCommandEncoder() else {
-            throw PaddleMobileError.predictError(message: " encode is nil")
-        }
-        encoder.setTexture(param.input.mtlTexture, index: 0)
-        encoder.setTexture(param.output.metalTexture, index: 1)
-        encoder.dispatch(computePipline: pipline, outTexture: param.input.mtlTexture)
-        encoder.endEncoding()
+  func compute(commandBuffer: MTLCommandBuffer, param: FeedParam<P>) throws {
+    guard let encoder = commandBuffer.makeComputeCommandEncoder() else {
+      throw PaddleMobileError.predictError(message: " encode is nil")
     }
-    
-    required init(device: MTLDevice, param: FeedParam<P>) {
-        super.init(device: device, inFunctionName: "texture2d_to_2d_array")
-    }
+    encoder.setTexture(param.input.mtlTexture, index: 0)
+    encoder.setTexture(param.output.metalTexture, index: 1)
+    encoder.dispatch(computePipline: pipline, outTexture: param.input.mtlTexture)
+    encoder.endEncoding()
+  }
+  
+  required init(device: MTLDevice, param: FeedParam<P>) {
+    param.output.initTexture(device: device, transpose: [0, 2, 3, 1])
+    super.init(device: device, inFunctionName: "texture2d_to_2d_array")
+  }
 }
