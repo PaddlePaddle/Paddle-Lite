1e2f7619e3cced69804088022e5deadda8149257
hjchen2 <chenhoujiangcug@gmail.com>
2018-10-08 05:48:26 +0000

    Refine: implement naive quantize and dequantize kernel

--- src/operators/kernel/arm/dequantize_kernel.cpp
+++ src/operators/kernel/arm/dequantize_kernel.cpp
@@ -12,6 +12,8 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License. */
 
+#ifdef PADDLE_MOBILE_CPU
+
 #include "operators/kernel/dequantize_kernel.h"
 
 namespace paddle_mobile {
@@ -26,7 +28,18 @@ template<>
 void DequantizeKernel<CPU, float>::Compute(
     const DequantizeParam<CPU> &param) const {
   // TODO
+  const Tensor *input = param.input_;
+  Tensor *output = param.out_; 
+  float activation_scale = param.activation_scale_->data<float>()[0];
+  float weight_scale = param.weight_scale_;
+  const int32_t *x = input->data<const int32_t>();
+  float *y = output->mutable_data<float>();
+  for (size_t i = 0; i < output->numel(); ++i) {
+    y[i] = x[i] / activation_scale / weight_scale;
+  }
 }
 
 }  // namespace paddle_mobile
 }  // namespace operators
+
+#endif
