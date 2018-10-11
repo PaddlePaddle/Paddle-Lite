4194380a9598683479a1f56ed022b16570bd430e
liuruilong <liuruilong@baidu.com>
2018-10-11 19:01:18 +0800

    modified build.sh

--- src/framework/cl/cl_scope.h
+++ src/framework/cl/cl_scope.h
@@ -54,7 +54,7 @@ class CLScope {
     }
 
     auto program =
-        CLEngine::Instance()->CreateProgramWith(context_.get(), file_name);
+        CLEngine::Instance()->CreateProgramWith(context_.get(), "./cl_kernel/" + file_name);
     programs_[file_name] = std::move(program);
 
     status_ = clBuildProgram(program.get(), 0, 0, 0, 0, 0);
