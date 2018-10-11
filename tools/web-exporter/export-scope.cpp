3b41d716573a6a855503a3053fb289fd4de859dd
dolphin8 <haipengwang03@163.com>
2018-10-11 21:01:04 +0800

    web-export

--- tools/web-exporter/export-scope.cpp
+++ tools/web-exporter/export-scope.cpp
@@ -10,17 +10,24 @@ void export_scope(ProgramPtr program, ScopePtr scope, const std::string & dirnam
         if (var->Persistable()) {
           auto* v = scope->FindVar(var->Name());
           assert(v != nullptr);
-          const float * p = v->Get<float>();
           int count = 1;
           for (auto n: var->Tensor_desc().Dims()) {
             count *= n;
           }
+
+          auto* tensor = v->GetMutable<paddle_mobile::framework::LoDTensor>();
+          const float * p = tensor->mutable_data<float>();
+
           std::string para_file_name = dirname + '/' + var->Name();
           FILE *para_file = fopen(para_file_name.c_str(), "w");
           assert(p != nullptr);
-          // std::cout << var->Name() << " " << count << "\n";
           fwrite(p, sizeof(float), count, para_file);
           fclose(para_file);
+          // std::cout << "==> " << var->Name() << " " << count << "\n";
+          // for (int i = 0; i < count; i++) {
+          //     std::cout << p[i] << ", ";
+          // }
+          // std::cout << "\n";
         }
     }
   }
