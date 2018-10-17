21c28e13e780a5817e746e076ba69058a988f281
dolphin8 <haipengwang03@163.com>
2018-10-17 16:17:05 +0800

    relu

--- src/operators/kernel/cl/cl_kernel/relu.cl
+++ src/operators/kernel/cl/cl_kernel/relu.cl
@@ -24,6 +24,34 @@ __kernel void relu(__read_only image2d_t input,
                             CLK_FILTER_NEAREST;
 
   half4 in = read_imageh(input, sampler, (int2)(x, y));
-  in = max((half4)(0.0f,0.0f,0.0f,0.0f), in);
+  in = max((half4)(0.0f, 0.0f, 0.0f, 0.0f), in);
+  write_imageh(output, (int2)(x, y), in);
+}
+
+__kernel void relu_p0(__read_only image2d_t input,
+                   __write_only image2d_t output){
+
+  const int x = get_global_id(0);
+  const int y = get_global_id(1);
+
+  const sampler_t sampler = CLK_NORMALIZED_COORDS_TRUE |
+                            CLK_ADDRESS_CLAMP |
+                            CLK_FILTER_NEAREST;
+
+  half4 in = read_imageh(input, sampler, (int2)(x, y));
+  in = max((half4)(0.0f, 0.0f, 0.0f, 0.0f), in);
+  write_imageh(output, (int2)(x, y), in);
+}
+__kernel void relu_p1(__read_only image2d_t input,
+                   __write_only image2d_t output){
+
+  const int x = get_global_id(0);
+  const int y = get_global_id(1);
+
+  const sampler_t sampler = CLK_NORMALIZED_COORDS_TRUE |
+                            CLK_ADDRESS_CLAMP |
+                            CLK_FILTER_NEAREST;
+
+  half4 in = read_imageh(input, sampler, (int2)(x, y));
   write_imageh(output, (int2)(x, y), in);
 }
--- src/operators/kernel/cl/relu_kernel.cpp
+++ src/operators/kernel/cl/relu_kernel.cpp
@@ -21,21 +21,31 @@ namespace operators {
 template <>
 bool ReluKernel<GPU_CL, float>::Init(ReluParam<GPU_CL>* param) {
   this->cl_helper_.AddKernel("relu", "relu.cl");
+  this->cl_helper_.AddKernel("relu_p0", "relu.cl");
+  this->cl_helper_.AddKernel("relu_p1", "relu.cl");
+  const auto dim = const_cast<framework::CLImage*>(param->InputX())->ImageDims();
+  param->getMidImage().InitEmptyImage(this->cl_helper_.CLContext(), dim);
   return true;
 }
 
 template <>
 void ReluKernel<GPU_CL, float>::Compute(const ReluParam<GPU_CL>& param) {
-  auto kernel = this->cl_helper_.KernelAt(0);
+  auto kernel_p0 = this->cl_helper_.KernelAt(1);
+  auto kernel_p1 = this->cl_helper_.KernelAt(2);
   const auto* input = param.InputX();
   auto* output = param.Out();
   auto default_work_size = this->cl_helper_.DefaultWorkSize(*output);
   auto inputImage = input->GetCLImage();
   auto outputImage = output->GetCLImage();
-  clSetKernelArg(kernel, 0, sizeof(cl_mem), &inputImage);
-  clSetKernelArg(kernel, 1, sizeof(cl_mem), &outputImage);
+  auto tImage = const_cast<ReluParam<GPU_CL>&>(param).getMidImage().GetCLImage();
+  clSetKernelArg(kernel_p0, 0, sizeof(cl_mem), &inputImage);
+  clSetKernelArg(kernel_p0, 0, sizeof(cl_mem), &tImage);
+  clSetKernelArg(kernel_p1, 0, sizeof(cl_mem), &tImage);
+  clSetKernelArg(kernel_p1, 1, sizeof(cl_mem), &outputImage);
   const size_t work_size[2] = {input->ImageWidth(), input->ImageHeight()};
-  clEnqueueNDRangeKernel(this->cl_helper_.CLCommandQueue(), kernel, 3, NULL,
+  clEnqueueNDRangeKernel(this->cl_helper_.CLCommandQueue(), kernel_p0, 3, NULL,
+                         work_size, NULL, 0, NULL, NULL);
+  clEnqueueNDRangeKernel(this->cl_helper_.CLCommandQueue(), kernel_p1, 3, NULL,
                          work_size, NULL, 0, NULL, NULL);
 }
 
