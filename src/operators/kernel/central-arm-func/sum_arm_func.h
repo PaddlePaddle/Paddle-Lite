68377adbc84cd13a9911947b3e804c40ea7ff246
suiyang <wowchinasuiyang@163.com>
2018-10-18 10:33:06 +0800

    Update sum_arm_func.h

--- src/operators/kernel/central-arm-func/sum_arm_func.h
+++ src/operators/kernel/central-arm-func/sum_arm_func.h
@@ -12,7 +12,7 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License. */
 
-#include <operators/math/selected_rows_functor.h>
+#include "operators/math/selected_rows_functor.h"
 
 #ifdef SUM_OP
 #pragma once
