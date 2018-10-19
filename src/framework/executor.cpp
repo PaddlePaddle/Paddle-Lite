dc7c145547846b6e86db6b2fcf53d201fd296347
zhaojiaying01 <zhaojiaying01@baidu.com>
2018-10-19 14:41:26 +0800

    update fetch kernel

--- src/framework/executor.cpp
+++ src/framework/executor.cpp
@@ -37,7 +37,7 @@ limitations under the License. */
 #include "framework/cl/cl_image.h"
 #endif
 
-int debug_to = 34;
+int debug_to = 33;
 
 namespace paddle_mobile {
 namespace framework {
@@ -922,7 +922,12 @@ void Executor<GPU_CL, Precision::FP32>::InitMemory() {
       if (var_desc->Persistable()) {
         CLImage *cl_image = nullptr;
         if (var_desc->Name() == "feed" || var_desc->Name() == "fetch") {
-          var->template GetMutable<framework::LoDTensor>();
+          auto tensor = var->template GetMutable<framework::LoDTensor>();
+          if (var_desc->Name() == "fetch") {
+            const framework::TensorDesc &desc = var_desc->Tensor_desc();
+            framework::DDim ddim = framework::make_ddim(desc.Dims());
+            tensor->mutable_data<float>(ddim);
+          }
           continue;
         } else {
           cl_image = var->template GetMutable<framework::CLImage>();
@@ -986,7 +991,12 @@ void Executor<GPU_CL, Precision::FP32>::InitCombineMemory() {
       if (var_desc->Persistable()) {
         CLImage *cl_image = nullptr;
         if (var_desc->Name() == "feed" || var_desc->Name() == "fetch") {
-          var->template GetMutable<framework::LoDTensor>();
+          auto tensor = var->template GetMutable<framework::LoDTensor>();
+          if (var_desc->Name() == "fetch") {
+            const framework::TensorDesc &desc = var_desc->Tensor_desc();
+            framework::DDim ddim = framework::make_ddim(desc.Dims());
+            tensor->mutable_data<float>(ddim);
+          }
           continue;
         } else {
           cl_image = var->template GetMutable<framework::CLImage>();
