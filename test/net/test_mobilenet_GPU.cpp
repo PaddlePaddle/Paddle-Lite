105d4f8f2378cc65b538200f6c15cc8cb1892531
yangfei <yangfei11@baidu.com>
2018-11-06 13:03:28 +0800

    repair bug of test-gpu
    Former-commit-id: 8238e534fcb907a82adf01bf1f0d9b339003a7ed

--- test/net/test_mobilenet_GPU.cpp
+++ test/net/test_mobilenet_GPU.cpp
@@ -15,15 +15,20 @@ limitations under the License. */
 #include <iostream>
 #include "../test_helper.h"
 #include "../test_include.h"
+#include "../../src/common/types.h"
 
 int main() {
-  paddle_mobile::PaddleMobile<paddle_mobile::GPU_CL> paddle_mobile;
+  paddle_mobile::PaddleMobile<paddle_mobile::CPU> paddle_mobile;
   //    paddle_mobile.SetThreadNum(4);
   auto time1 = paddle_mobile::time();
-  //  auto isok = paddle_mobile.Load(std::string(g_mobilenet_detect) + "/model",
-  //                     std::string(g_mobilenet_detect) + "/params", true);
-  paddle_mobile.SetCLPath(".");
-  auto isok = paddle_mobile.Load(std::string(g_mobilenet), true);
+#ifdef PADDLE_MOBILE_CL
+    paddle_mobile.SetCLPath("/data/local/tmp/bin");
+#endif
+
+    auto isok = paddle_mobile.Load(std::string(g_mobilenet_mul) + "/model",
+                       std::string(g_mobilenet_mul) + "/params", true);
+
+//  auto isok = paddle_mobile.Load(std::string(g_mobilenet_mul), true);
   if (isok) {
     auto time2 = paddle_mobile::time();
     std::cout << "load cost :" << paddle_mobile::time_diff(time1, time2) << "ms"
