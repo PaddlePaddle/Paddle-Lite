6956407222f629e5cf7756e7d318d7c8bc2c85a6
ZhenWang <wangzhen31@baidu.com>
2018-12-03 14:26:22 +0800

    update the usage of matmul

--- src/operators/kernel/central-arm-func/mul_arm_func.h
+++ src/operators/kernel/central-arm-func/mul_arm_func.h
@@ -73,13 +73,14 @@ void MulCompute(const MulParam<CPU> &param) {
   }
   if (param.InputX()->type() == typeid(int8_t)) {
     out->mutable_data<int32_t>();
-    math::matmul(x_matrix, false, y_matrix, false, static_cast<float>(1), out,
-                 static_cast<float>(0), false, static_cast<int32_t *>(nullptr));
+    math::matmul<float, int32_t>(x_matrix, false, y_matrix, false,
+                                 static_cast<float>(1), out,
+                                 static_cast<float>(0));
 
   } else {
     out->mutable_data<float>();
-    math::matmul(x_matrix, false, y_matrix, false, static_cast<float>(1), out,
-                 static_cast<float>(0), false, static_cast<float *>(nullptr));
+    math::matmul<float>(x_matrix, false, y_matrix, false, static_cast<float>(1),
+                        out, static_cast<float>(0));
   }
   if (out_dim.size() != 2) {
     out->Resize(out_dim);
