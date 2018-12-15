e4bd938611930612a7e5b328eec9c995e31fc42f
zhangyang <zhangyang49@baidu.com>
2018-12-15 22:35:30 +0800

    remove redundancy for V1 for FPGA track
    Former-commit-id: 1e8dd9384a74cedec14f8254b89e2bd241f18ab4

--- src/fpga/V1/deconv_filter.h
+++ src/fpga/V1/deconv_filter.h
@@ -21,7 +21,6 @@ namespace deconv_filter {
 void deconv_inverse_filter(float** data_in, int num, int channel, int width,
                            int height);
 int deconv_calc_sub_pad(int filter_axis, int pad, int stride);
-int deconv_get_sub_filter_num(int filter_num, int stride);
 int deconv_get_sub_filter_axis(int filter_axis, int stride);
 int deconv_get_sub_out_axis(int image_axis, int sub_pad, int sub_filter_axis);
 int deconv_get_omit(int stride, int filter_width, int pad);
