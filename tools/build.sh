2ed516ea5aa1c2975b3dbd07f85bdd3e049007d9
zhaojiaying01 <zhaojiaying01@baidu.com>
2018-12-26 21:11:40 +0800

    add depthwise_con3x3_add_relu fusion

--- tools/build.sh
+++ tools/build.sh
@@ -162,7 +162,7 @@ build_for_ios() {
     fi
     cd "${BUILD_DIR}"
     make -j 8
-    cp ../../../src/ios_io/PaddleMobileCPU.h ./build/PaddleMobileCPU.h
+    cp ../../../src/io/ios_io/PaddleMobileCPU.h ./build/PaddleMobileCPU.h
     cd ./build
     # 生成符号表
     ranlib *.a
