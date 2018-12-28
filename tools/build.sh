26fdcbef530d1000847c48541a95da84ee26744d
zhaojiaying01 <zhaojiaying01@baidu.com>
2018-12-28 12:23:27 +0800

    add superresoltion model separate compilation

--- tools/build.sh
+++ tools/build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 NETS=""
-declare -a supportedNets=("googlenet" "mobilenet" "yolo" "squeezenet" "resnet" "mobilenetssd" "nlp" "mobilenetfssd" "genet")
+declare -a supportedNets=("googlenet" "mobilenet" "yolo" "squeezenet" "resnet" "mobilenetssd" "nlp" "mobilenetfssd" "genet" "super")
 
 build_for_mac() {
     if [ ! `which brew` ]; then
