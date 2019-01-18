ef7229d122f2aba9184ace1de15b925b416dd250
hjchen2 <chenhoujiangcug@gmail.com>
2019-01-18 11:28:20 +0800

    Add less than implamentation and unit test
    Former-commit-id: bc93bedbc46f0aa1c2349ed769d7b17e186bb1b3

--- src/operators/compare_op.cpp
+++ src/operators/compare_op.cpp
@@ -12,25 +12,23 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License. */
 
-#ifdef LOD_RESET_OP
-
-#include "operators/lod_reset_op.h"
+#include "operators/compare_op.h"
 
 namespace paddle_mobile {
 namespace operators {
 
+#ifdef LESS_THAN_OP
 template <typename Dtype, typename T>
-void LodResetOp<Dtype, T>::InferShape() const {
+void LessThanOp<Dtype, T>::InferShape() const {
   const auto &input_dims = this->param_.input_x_->dims();
   this->param_.output_->Resize(input_dims);
 }
+#endif  // LESS_THAN_OP
 
 }  // namespace operators
 }  // namespace paddle_mobile
 
 namespace ops = paddle_mobile::operators;
-#ifdef PADDLE_MOBILE_CPU
-REGISTER_OPERATOR_CPU(lod_reset, ops::LodResetOp);
-#endif
-
-#endif  // LOD_RESET_OP
+#ifdef LESS_THAN_OP
+REGISTER_OPERATOR_CPU(less_than, ops::LessThanOp);
+#endif  // LESS_THAN_OP
