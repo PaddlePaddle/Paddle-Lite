cb262fadfc0172da4428b0aa8e09dbf9bf59231d
liuruilong <liuruilong@baidu.com>
2019-01-18 15:54:03 +0800

    format files
    Former-commit-id: 2c1feaa5d818ea1035a8f3bbf6d031c78bd6ef44

--- src/io/paddle_mobile.cpp
+++ src/io/paddle_mobile.cpp
@@ -45,8 +45,8 @@ PMStatus PaddleMobile<Device, T>::Load(const std::string &dirname,
 
   if (executor_.get() == nullptr) {
     executor_ = std::make_shared<framework::Executor<Device, T>>(
-        loader_->Load(dirname, optimize, quantification), batch_size, optimize,
-        lod_mode);
+        loader_->Load(dirname, optimize, quantification), config_, batch_size,
+        optimize, lod_mode);
   } else {
     LOG(kLOG_INFO) << "executor inited";
   }
@@ -67,7 +67,7 @@ PMStatus PaddleMobile<Device, T>::Load(const std::string &model_path,
 
   if (executor_.get() == nullptr) {
     executor_ = std::make_shared<framework::Executor<Device, T>>(
-        loader_->Load(model_path, para_path, optimize, quantification),
+        loader_->Load(model_path, para_path, optimize, quantification), config_,
         batch_size, optimize, lod_mode);
   } else {
     LOG(kLOG_INFO) << "executor inited";
