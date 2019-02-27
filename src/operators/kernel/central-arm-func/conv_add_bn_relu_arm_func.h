b3562acb81528323bef0592456206b897d9cbb4a
xiebaiyuan <xiebaiyuan@139.com>
2019-02-27 21:58:14 +0800

    use gemm to s1p0 in conv add bn relu
    Former-commit-id: a42aa4c26abfe875a1edc6bc80ffdf7760e6dee1

--- src/operators/kernel/central-arm-func/conv_add_bn_relu_arm_func.h
+++ src/operators/kernel/central-arm-func/conv_add_bn_relu_arm_func.h
@@ -120,7 +120,8 @@ void ConvAddBNReluCompute(const FusionConvAddBNReluParam<CPU> &param) {
   if (param.Groups() == param.Input()->dims()[1] &&
       param.Input()->dims()[1] == param.Output()->dims()[1] &&
       param.Filter()->dims()[2] == param.Filter()->dims()[3] &&
-      param.Filter()->dims()[2] == 3 && param.Strides()[0] == 1) {
+      param.Filter()->dims()[2] == 3 && param.Strides()[0] == 1 &&
+      param.paddings_[0] == 1) {
     math::DepthwiseConvAddBNRelu3x3s1p1(param.Input(), param.Filter(),
                                         param.Output(), param.NewScale(),
                                         param.NewBias(), true);
