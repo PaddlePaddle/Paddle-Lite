99131efa94a31de0a1778cd0ebbe949c888e1bf7
zhaojiaying01 <zhaojiaying01@baidu.com>
2019-04-25 15:00:37 +0800

    format code style

--- src/operators/kernel/cl/conv_kernel.cpp
+++ src/operators/kernel/cl/conv_kernel.cpp
@@ -37,25 +37,25 @@ bool ConvKernel<GPU_CL, float>::Init(ConvParam<GPU_CL> *param) {
   DLOG << " filter dims: " << param->Filter()->dims();
 
   if (param->Filter()->dims()[2] == 1 && param->Filter()->dims()[3] == 1) {
-	  param->Filter()->InitNImage(cl_helper_.CLContext(),
-	                              cl_helper_.CLCommandQueue());
+    param->Filter()->InitNImage(cl_helper_.CLContext(),
+                                cl_helper_.CLCommandQueue());
     this->cl_helper_.AddKernel("conv_1x1", "conv_kernel.cl");
-	  DLOG << "conv 1x1";
+    DLOG << "conv 1x1";
 
   } else if (param->Filter()->dims()[1] == 1 &&
              param->Input()->dims()[1] == param->Output()->dims()[1] &&
              param->Filter()->dims()[2] == 3) {
-	  param->Filter()->InitDWImage(cl_helper_.CLContext(),
-	                               cl_helper_.CLCommandQueue());
+    param->Filter()->InitDWImage(cl_helper_.CLContext(),
+                                 cl_helper_.CLCommandQueue());
     this->cl_helper_.AddKernel("depth_conv_3x3", "depthwise_conv_kernel.cl");
-	  DLOG << "depth_conv 3x3";
+    DLOG << "depth_conv 3x3";
 
   } else if (param->Filter()->dims()[2] == 3 &&
              param->Filter()->dims()[3] == 3) {
-  	param->Filter()->InitCLImage(cl_helper_.CLContext(),
-	                                 cl_helper_.CLCommandQueue());
-  	this->cl_helper_.AddKernel("conv_3x3", "conv_kernel.cl");
-	  DLOG << "conv 3x3";
+    param->Filter()->InitCLImage(cl_helper_.CLContext(),
+                                 cl_helper_.CLCommandQueue());
+    this->cl_helper_.AddKernel("conv_3x3", "conv_kernel.cl");
+    DLOG << "conv 3x3";
 
   } else {
     PADDLE_MOBILE_THROW_EXCEPTION(" not support ");
