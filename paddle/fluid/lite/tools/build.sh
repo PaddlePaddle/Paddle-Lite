dfa6a148d7f9c5ca416e7ca14015356768e0d413
ZhenWang <wangzhen31@baidu.com>
2019-07-08 15:39:24 +0800

    revert build.sh, test=develop

--- paddle/fluid/lite/tools/build.sh
+++ paddle/fluid/lite/tools/build.sh
@@ -39,7 +39,6 @@ function cmake_opencl {
     # $2: ARM_TARGET_ARCH_ABI in "armv8", "armv7" ,"armv7hf"
     # $3: ARM_TARGET_LANG in "gcc" "clang"
     cmake .. \
-        -DCMAKE_BUILD_TYPE=Debug \
         -DLITE_WITH_OPENCL=ON \
         -DWITH_GPU=OFF \
         -DWITH_MKL=OFF \
@@ -316,7 +315,6 @@ function cmake_arm {
     # $2: ARM_TARGET_ARCH_ABI in "armv8", "armv7" ,"armv7hf"
     # $3: ARM_TARGET_LANG in "gcc" "clang"
     cmake .. \
-        -DCMAKE_BUILD_TYPE=Debug \
         -DWITH_GPU=OFF \
         -DWITH_MKL=OFF \
         -DWITH_LITE=ON \
