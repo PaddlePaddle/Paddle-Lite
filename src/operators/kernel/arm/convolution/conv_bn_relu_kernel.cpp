da20683f5f4cfdbe937b3f9c98916b3cc5a96894
StarryRain <36948762+StarryRain@users.noreply.github.com>
2019-07-17 21:43:36 +0800

    add CPU_ARCH info, improve the performance of GEMM1*1s1 (#1751)
    Former-commit-id: 74a309cb3879783cb1be8ff557b6508abd1eba1f

--- src/operators/kernel/arm/convolution/conv_bn_relu_kernel.cpp
+++ src/operators/kernel/arm/convolution/conv_bn_relu_kernel.cpp
@@ -77,6 +77,9 @@ void ConvBNReluKernel<CPU, float>::Compute(
     case ConvParam<CPU>::EXEC_GEMM_FLOAT:
       GemmConv<float, float>(param);
       break;
+    case ConvParam<CPU>::EXEC_GEMM1x1s1_FLOAT:
+      GemmConv1x1s1<float, float>(param);
+      break;
     case ConvParam<CPU>::EXEC_SLIDINGWINDOW3x3S1_FLOAT:
     case ConvParam<CPU>::EXEC_SLIDINGWINDOW3x3S2_FLOAT:
       SlidingwindowConv3x3<float, float>(param);
