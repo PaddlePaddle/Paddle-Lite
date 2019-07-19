a8b64a985ab73bcb505d58aaff3c3fa90ec344f7
zhaojiaying01 <zhaojiaying01@baidu.com>
2019-07-19 11:12:20 +0800

    adjust gemmlowp

--- src/operators/math/gemm_int8.cpp
+++ src/operators/math/gemm_int8.cpp
@@ -12,6 +12,20 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License. */
 
+// Copyright 2015 The Gemmlowp Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 #include <string.h>
 #include "common/log.h"
 #include "operators/math/gemm.h"
@@ -242,8 +256,8 @@ void Gemm::AddDot4x8(int32_t k, const int8_t *a, const int8_t *b, int32_t *c,
 #endif  // __ARM_NEON
 }
 
-// The core idea of AddDot4x2 function is borrowed from the Google's gemmlowp
-// open source library. The address of gemmlowp is
+// The core idea of AddDot4x2 and AddDot4x4 function is borrowed from the
+// Google's gemmlowp open source library. The address of gemmlowp is
 // https://github.com/google/gemmlowp.
 void Gemm::AddDot4x2(int32_t k, const int8_t *a, const int8_t *b, int32_t *c,
                      int32_t ldc) {
