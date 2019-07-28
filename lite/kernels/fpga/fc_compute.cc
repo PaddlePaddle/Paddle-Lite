a3de26b47b6c7520f97cb14b5265724501c14d87
wangtianfei01 <wangtianfei01@baidu.com>
2019-07-28 16:59:01 +0800

    add fpga fc and softmax op

---
+++ lite/kernels/fpga/fc_compute.cc
@@ -0,0 +1,61 @@
+// Copyright (c) 2019 PaddlePaddle Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "lite/kernels/fpga/fc_compute.h"
+#include "lite/core/op_registry.h"
+#include "lite/core/type_system.h"
+
+namespace paddle {
+namespace lite {
+namespace kernels {
+namespace fpga {
+
+void FcCompute::PrepareForRun() {
+  auto& param = this->Param<param_t>();
+
+  // ====================================================
+  zynqmp::FullyConnectedParam& fc_param = pe_.param();
+
+  input_.share_from_tensorlite(*param.input);
+  output_.share_from_tensorlite(*param.output);
+  filter_.share_from_tensorlite(*param.w);
+  bias_.share_from_tensorlite(*param.bias);
+
+  fc_param.input = &input_;
+  fc_param.output = &output_;
+  fc_param.filter = &filter_;
+  fc_param.bias = &bias_;
+
+  // std::vector<int> kernelSize;
+  pe_.init();
+  pe_.apply();
+}
+
+void FcCompute::Run() {
+  auto& param = this->Param<param_t>();
+  pe_.dispatch();
+}
+
+}  // namespace fpga
+}  // namespace kernels
+}  // namespace lite
+}  // namespace paddle
+
+REGISTER_LITE_KERNEL(
+    conv2d, kFPGA, kFP16, kNHWC, paddle::lite::kernels::fpga::FcCompute, def)
+    .BindInput("Input", {LiteType::GetTensorTy(TARGET(kFPGA))})
+    .BindInput("Bias", {LiteType::GetTensorTy(TARGET(kFPGA))})
+    .BindInput("Filter", {LiteType::GetTensorTy(TARGET(kFPGA))})
+    .BindOutput("Output", {LiteType::GetTensorTy(TARGET(kFPGA))})
+    .Finalize();
---
+++ lite/kernels/fpga/softmax_compute.cc
@@ -0,0 +1,48 @@
+// Copyright (c) 2019 PaddlePaddle Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "lite/kernels/fpga/softmax_compute.h"
+#include "lite/arm/math/funcs.h"
+
+namespace paddle {
+namespace lite {
+namespace kernels {
+namespace fpga {
+
+void SoftmaxCompute::Run() {
+  zynqmp::SoftmaxParam& softmax_param = pe_.param();
+  auto& param = Param<operators::SoftmaxParam>();
+  input_x_.share_from_tensorlite(*param.x);
+  output_.share_from_tensorlite(*param.output);
+  softmax_param.input = &input_x_;
+  softmax_param.output = &output_;
+
+  pe_.init();
+  pe_.apply();
+}
+
+}  // namespace fpga
+}  // namespace kernels
+}  // namespace lite
+}  // namespace paddle
+
+REGISTER_LITE_KERNEL(softmax,
+                     kFPGA,
+                     kFP16,
+                     kNHWC,
+                     paddle::lite::kernels::fpga::SoftmaxCompute,
+                     def)
+    .BindInput("X", {LiteType::GetTensorTy(TARGET(kFPGA))})
+    .BindOutput("Out", {LiteType::GetTensorTy(TARGET(kFPGA))})
+    .Finalize();
