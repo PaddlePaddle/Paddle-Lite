74fa285bc87c6c767d24cc209c78d634fbf04754
tensor-tang <tangjian03@baidu.com>
2019-08-02 02:25:30 +0000

    [NPU] fix and workaround depthwise conv

--- lite/npu/bridge/depthwise_conv_op.cc
+++ lite/npu/bridge/depthwise_conv_op.cc
@@ -12,7 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include "lite/operators/conv_op.h"
 #include "ai_ddk_lib/include/graph/buffer.h"
 #include "ai_ddk_lib/include/graph/graph.h"
 #include "ai_ddk_lib/include/graph/model.h"
@@ -21,25 +20,28 @@
 #include "ai_ddk_lib/include/graph/operator_reg.h"
 #include "lite/npu/bridge/registry.h"
 #include "lite/npu/bridge/utils.h"
+#include "lite/operators/conv_op.h"
 
 namespace paddle {
 namespace lite {
 namespace npu {
 namespace bridge {
 
-node_map_type ConvConverter(const std::shared_ptr<lite::OpLite> conv_op,
-                            const node_map_type& inputs_map) {
+node_map_type DepthwiseConvConverter(
+    const std::shared_ptr<lite::OpLite> conv_op,
+    const node_map_type& inputs_map) {
   LOG(INFO) << "converting DepthwiseConv...";
   lite::Scope* scope = conv_op->scope();
   const lite::OpInfo* op_info = conv_op->op_info();
 
-  auto conv_node = std::make_shared<ge::op::Convolution>(UniqueName("conv2d"));
+  auto conv_node = std::make_shared<ge::op::ConvolutionDepthwise>(
+      UniqueName("depthwise_conv2d"));
   auto input_var_name = op_info->Input("Input").front();
   CHECK(inputs_map.count(input_var_name));
   conv_node->set_input_x(*inputs_map.at(input_var_name));
   OpList::Global().add(inputs_map.at(input_var_name));
 
-  // build filter and bias node
+  // build filter
   auto filter_var_name = op_info->Input("Filter").front();
   CHECK(!inputs_map.count(filter_var_name));
 
@@ -49,31 +51,15 @@ node_map_type ConvConverter(const std::shared_ptr<lite::OpLite> conv_op,
   CHECK_EQ(filter_dims.size(), 4);
   auto filter_const_node = std::make_shared<ge::op::Const>(filter_var_name);
   filter_const_node->set_attr_value(CvtFromLiteTensor(filter));
-  conv_node->set_input_w(*filter_const_node);
+  conv_node->set_input_filter(*filter_const_node);
   OpList::Global().add(filter_const_node);
 
-  if (op_info->HasInput("Bias")) {
-    auto bias_var_name = op_info->Input("Bias").front();
-    LOG(INFO) << "bias_var_name:" << bias_var_name;
-    CHECK(!inputs_map.count(bias_var_name));
-    auto* bias = scope->FindVar(bias_var_name)->GetMutable<lite::Tensor>();
-    LOG(INFO) << "bias dims:" << bias->dims();
-    int n = bias->numel();
-    CHECK_EQ(n, bias->dims().production());
-    auto bias_const_node = std::make_shared<ge::op::Const>(bias_var_name);
-
-    ge::TensorDesc bdesc(
-        ge::Shape({1, n, 1, 1}), ge::FORMAT_NCHW, ge::DT_FLOAT);
-    auto size = bdesc.GetShape().GetShapeSize();
-    CHECK_EQ(size, n);
-    ge::TensorPtr ptensor = std::make_shared<ge::Tensor>();
-    ptensor->SetTensorDesc(bdesc);
-    auto* pdata = reinterpret_cast<uint8_t*>(bias->mutable_data<float>());
-    ptensor->SetData(pdata, size * sizeof(float));
-    bias_const_node->set_attr_value(ptensor);
-    conv_node->set_input_b(*bias_const_node);
-    OpList::Global().add(bias_const_node);
-  }
+  auto bias_var_name = op_info->Input("Bias").front();
+  LOG(INFO) << "bias_var_name:" << bias_var_name;
+  CHECK(!inputs_map.count(bias_var_name));
+  auto* bias = scope->FindVar(bias_var_name)->GetMutable<lite::Tensor>();
+  LOG(INFO) << "bias dims:" << bias->dims();
+  CHECK(!op_info->HasInput("Bias")) << "Depthwise conv do not support bias";
 
   // set attributes
   std::vector<int> strides = op_info->GetAttr<std::vector<int>>("strides");
@@ -90,12 +76,14 @@ node_map_type ConvConverter(const std::shared_ptr<lite::OpLite> conv_op,
   conv_node->set_attr_kernel(
       ge::AttrValue::LIST_INT({filter_dims[2], filter_dims[3]}));
 
+  conv_node->set_attr_mode(1);
+  conv_node->set_attr_format(1);  // NCHW
+
   auto output_var_name = op_info->Output("Output").front();
   lite::Tensor* output =
       scope->FindVar(output_var_name)->GetMutable<lite::Tensor>();
   int oc = output->dims()[1];
   conv_node->set_attr_num_output(oc);
-  conv_node->set_attr_mode(1);
 
   node_map_type outputs_map;
   if (op_info->GetAttr<bool>("fuse_relu")) {
@@ -119,6 +107,5 @@ node_map_type ConvConverter(const std::shared_ptr<lite::OpLite> conv_op,
 }  // namespace lite
 }  // namespace paddle
 
-REGISTER_NPU_BRIDGE(conv2d, paddle::lite::npu::bridge::ConvConverter);
-
-REGISTER_NPU_BRIDGE(depthwise_conv2d, paddle::lite::npu::bridge::ConvConverter);
+REGISTER_NPU_BRIDGE(depthwise_conv2d,
+                    paddle::lite::npu::bridge::DepthwiseConvConverter);
