de9fa64e8d2054adfdc779afc639a230fba8220d
sangoly <sangoly@aliyun.com>
2019-08-21 12:53:12 +0000

    add test ci2 test=develop

--- lite/api/cxx_api.h
+++ lite/api/cxx_api.h
@@ -61,7 +61,7 @@ class LITE_API Predictor {
       GenRuntimeProgram();
     }
     program_->Run();
-    LOG(INFO) << "running 11";
+    LOG(INFO) << "running 11111";
   }
 
   // Get offset-th col of feed inputs.
