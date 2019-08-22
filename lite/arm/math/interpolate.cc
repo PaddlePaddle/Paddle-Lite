cf489683a981579c4ec34aa63f28f2d9f06cf399
superjomn <yanchunwei@outlook.com>
2019-08-22 14:40:08 +0800

    test=develop

--- lite/arm/math/interpolate.cc
+++ lite/arm/math/interpolate.cc
@@ -22,6 +22,24 @@ namespace lite {
 namespace arm {
 namespace math {
 
+// The following function bilinear_interp is partially base on
+// https://github.com/Tencent/ncnn/blob/master/src/layer/arm/interp_arm.cpp
+// Tencent is pleased to support the open source community by making ncnn
+// available.
+//
+// Copyright (C) 2019 THL A29 Limited, a Tencent company. All rights reserved.
+//
+// Licensed under the BSD 3-Clause License (the "License"); you may not use this
+// file except
+// in compliance with the License. You may obtain a copy of the License at
+//
+// https://opensource.org/licenses/BSD-3-Clause
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed
+// under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+// CONDITIONS OF ANY KIND, either express or implied. See the License for the
+// specific language governing permissions and limitations under the License.
 void bilinear_interp(const float* src,
                      int w_in,
                      int h_in,
