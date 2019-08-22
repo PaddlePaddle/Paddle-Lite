a531a688b8e32bc8bf7d78d5f47c17b61e351abf
hong19860320 <9973393+hong19860320@users.noreply.github.com>
2019-08-22 14:16:41 +0800

    add license (#1820)
    test=develop

--- lite/arm/math/packed_sgemm.cc
+++ lite/arm/math/packed_sgemm.cc
@@ -262,6 +262,33 @@ void sgemm_prepack(bool is_transB,
 }
 
 #ifdef __aarch64__
+/*
+ * The following function prepackA_8x12 is base on
+ * https://github.com/ARM-software/ComputeLibrary/
+ *
+ * Copyright (c) 2017-2019 ARM Limited.
+ *
+ * SPDX-License-Identifier: MIT
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to
+ * deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 void prepackA_8x12(float *dout,
                    const float *inptr,
                    float alpha,
