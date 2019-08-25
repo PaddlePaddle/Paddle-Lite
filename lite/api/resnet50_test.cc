9efeeb5e11664df60689f8d8a626b94b8a1731c5
superjomn <yanchunwei@outlook.com>
2019-08-25 14:55:34 +0800

    use high api
    test=develop

--- lite/api/resnet50_test.cc
+++ lite/api/resnet50_test.cc
@@ -28,27 +28,30 @@ namespace lite {
 #ifdef LITE_WITH_ARM
 void TestModel(const std::vector<Place>& valid_places,
                const Place& preferred_place) {
-  DeviceInfo::Init();
-  DeviceInfo::Global().SetRunMode(lite_api::LITE_POWER_HIGH, FLAGS_threads);
-  lite::Predictor predictor;
+  using namespace lite_api;  // NOLINT
+  MobileConfig config(preferred_place);
+  config.set_model_dir(FLAGS_model_dir);
+  config.set_power_mode(LITE_POWER_HIGH);
+  config.set_threads(FLAGS_threads);
 
-  predictor.Build(FLAGS_model_dir, preferred_place, valid_places);
+  auto predictor = CreatePaddlePredictor(config);
 
-  auto* input_tensor = predictor.GetInput(0);
-  input_tensor->Resize(DDim(std::vector<DDim::value_type>({1, 3, 224, 224})));
+  auto input_tensor = predictor->GetInput(0);
+  std::vector<DDim::value_type> shape({1, 3, 224, 224});
+  input_tensor->Resize(shape);
   auto* data = input_tensor->mutable_data<float>();
-  auto item_size = input_tensor->dims().production();
+  auto item_size = shape[0] * shape[1] * shape[2] * shape[3];
   for (int i = 0; i < item_size; i++) {
     data[i] = 1;
   }
 
   for (int i = 0; i < FLAGS_warmup; ++i) {
-    predictor.Run();
+    predictor->Run();
   }
 
   auto start = GetCurrentUS();
   for (int i = 0; i < FLAGS_repeats; ++i) {
-    predictor.Run();
+    predictor->Run();
   }
 
   LOG(INFO) << "================== Speed Report ===================";
@@ -65,15 +68,15 @@ void TestModel(const std::vector<Place>& valid_places,
        0.00072681636, 8.531815e-05,  0.0002129998,  0.0021168243,
        0.006387163,   0.0037145028,  0.0012812682,  0.00045948103,
        0.00013535398, 0.0002483765,  0.00076759676, 0.0002773295}));
-  auto* out = predictor.GetOutput(0);
-  ASSERT_EQ(out->dims().size(), 2);
-  ASSERT_EQ(out->dims()[0], 1);
-  ASSERT_EQ(out->dims()[1], 1000);
+  auto out = predictor->GetOutput(0);
+  ASSERT_EQ(out->shape().size(), 2);
+  ASSERT_EQ(out->shape()[0], 1);
+  ASSERT_EQ(out->shape()[1], 1000);
 
   int step = 50;
   for (int i = 0; i < results.size(); ++i) {
     for (int j = 0; j < results[i].size(); ++j) {
-      EXPECT_NEAR(out->data<float>()[j * step + (out->dims()[1] * i)],
+      EXPECT_NEAR(out->data<float>()[j * step + (out->shape()[1] * i)],
                   results[i][j],
                   1e-6);
     }
