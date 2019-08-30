029971b446cd37b910a91a0e79c3026f243f5f12
Pei Yang <peiyang@baidu.com>
2019-08-30 16:28:48 +0800

    add nearest_interp_cuda kernel, test=develop (#1920)
    add nearest_interp cuda kernel for Paddle-Lite

--- lite/api/_paddle_use_kernels.h
+++ lite/api/_paddle_use_kernels.h
@@ -155,6 +155,7 @@ USE_LITE_KERNEL(io_copy, kCUDA, kAny, kAny, device_to_host);
 USE_LITE_KERNEL(io_copy_once, kCUDA, kAny, kAny, host_to_device);
 USE_LITE_KERNEL(io_copy_once, kCUDA, kAny, kAny, device_to_host);
 USE_LITE_KERNEL(leaky_relu, kCUDA, kFloat, kNCHW, def);
+USE_LITE_KERNEL(nearest_interp, kCUDA, kFloat, kNCHW, def);
 USE_LITE_KERNEL(yolo_box, kCUDA, kFloat, kNCHW, def);
 #endif
 
