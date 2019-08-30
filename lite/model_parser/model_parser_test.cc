d616363056b8a6507bcfa566bbe829140490c625
DannyIsFunny <912790387@qq.com>
2019-08-30 15:45:31 +0000

    adding model from memory test=develop

--- lite/model_parser/model_parser_test.cc
+++ lite/model_parser/model_parser_test.cc
@@ -119,7 +119,7 @@ TEST(ModelParser, SaveModelNaive) {
   SaveModelNaive(save_pb_model_path, scope, prog);
 }
 
-TEST(ModelParser, LoadModelNaiveFromMemory) {
+TEST(ModelParser, LoadNaiveModelFromMemory) {
   CHECK(!FLAGS_model_dir.empty());
   cpp::ProgramDesc prog;
   Scope scope;
