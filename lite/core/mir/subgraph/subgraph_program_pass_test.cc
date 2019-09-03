45596d90210191aedc92e4b07e52199c1287d8d1
tensor-tang <tangjian03@baidu.com>
2019-09-03 07:54:54 +0000

    fix test
    test=develop

--- lite/core/mir/subgraph/subgraph_program_pass_test.cc
+++ lite/core/mir/subgraph/subgraph_program_pass_test.cc
@@ -32,12 +32,13 @@ namespace lite {
 TEST(SubgraphTest, models) {
   cpp::ProgramDesc program_desc;
   auto scope = std::make_shared<Scope>();
-  LoadModelPb(FLAGS_model_dir,
-              FLAGS_model_dir + "/model",
-              FLAGS_model_dir + "/params",
-              scope.get(),
-              &program_desc,
-              true);
+  // LoadModelPb(FLAGS_model_dir,
+  //             FLAGS_model_dir + "/model",
+  //             FLAGS_model_dir + "/params",
+  //             scope.get(),
+  //             &program_desc,
+  //             true);
+  LoadModelPb(FLAGS_model_dir, "", "", scope.get(), &program_desc);
   std::vector<Place> valid_places({
       Place{TARGET(kHost), PRECISION(kFloat)},
 #ifdef LITE_WITH_ARM
