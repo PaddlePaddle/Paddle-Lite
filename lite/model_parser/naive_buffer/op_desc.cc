abe5239130673927872a78b7a9653d1b107de6a2
DannyIsFunny <912790387@qq.com>
2019-09-09 03:14:57 +0000

    test=develop

--- lite/model_parser/naive_buffer/op_desc.cc
+++ lite/model_parser/naive_buffer/op_desc.cc
@@ -12,133 +12,120 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include "lite/model_parser/pb/op_desc.h"
+#include "lite/model_parser/naive_buffer/op_desc.h"
+#include <set>
+#include <utility>
 
 namespace paddle {
 namespace lite {
-namespace pb {
+namespace naive_buffer {
 
-google::protobuf::internal::RepeatedPtrIterator<framework::proto::OpDesc_Attr>
-FindAttr(framework::proto::OpDesc *desc, const std::string &name) {
-  auto &xs = *desc->mutable_attrs();
-  auto it = std::find_if(
-      xs.begin(), xs.end(), [&](const framework::proto::OpDesc_Attr &x) {
-        return x.name() == name;
+proto::OpDesc::Attr* FindAttr(proto::OpDesc* desc, const std::string& name) {
+  CHECK(desc);
+  auto& xs = *desc->GetMutableField<ListBuilder<proto::OpDesc::Attr>>("attrs");
+  auto it =
+      std::find_if(xs.begin(), xs.end(), [&](const proto::OpDesc::Attr& x) {
+        auto& builder = x.GetField<StringBuilder>("name");
+        return builder.data() == name;
       });
   if (it == xs.end()) {
-    auto *attr = xs.Add();
-    attr->set_name(name);
-    it = std::find_if(
-        xs.begin(), xs.end(), [&](const framework::proto::OpDesc_Attr &x) {
-          return x.name() == name;
-        });
+    auto* attr_builder = xs.New();
+    auto* name_builder = attr_builder->GetMutableField<StringBuilder>("name");
+    CHECK(name_builder);
+    name_builder->set(name);
+    return attr_builder;
   }
-  return it;
+  return &(*it);
 }
 
-#define SET_IMPL_ONE(T, ty__, pb_f__)                            \
-  template <>                                                    \
-  void OpDesc::SetAttr<T>(const std::string &name, const T &v) { \
-    auto it = FindAttr(desc_, name);                             \
-    it->set_type(framework::proto::ty__);                        \
-    it->set_##pb_f__(v);                                         \
+#define SET_ATTR_IMPL(T, ty__, bd__, pb_f__)                               \
+  template <>                                                              \
+  void OpDesc::SetAttr<T>(const std::string& name, const T& v) {           \
+    auto* it = FindAttr(desc_, name);                                      \
+    auto* type_builder =                                                   \
+        it->GetMutableField<EnumBuilder<proto::OpDesc::AttrType>>("type"); \
+    CHECK(type_builder);                                                   \
+    type_builder->set(proto::OpDesc::AttrType::ty__);                      \
+    auto* f_builder = it->GetMutableField<bd__##Builder>(#pb_f__);         \
+    CHECK(f_builder);                                                      \
+    f_builder->set(v);                                                     \
   }
-SET_IMPL_ONE(int, INT, i);
-SET_IMPL_ONE(float, FLOAT, f);
-SET_IMPL_ONE(bool, BOOLEAN, b);
-SET_IMPL_ONE(int64_t, LONG, l);
+SET_ATTR_IMPL(int, INT, Int32, i);
+SET_ATTR_IMPL(float, FLOAT, Float32, f);
+SET_ATTR_IMPL(bool, BOOLEAN, Bool, b);
+SET_ATTR_IMPL(std::string, STRING, String, s);
+SET_ATTR_IMPL(int64_t, LONG, Int64, l);
+#undef SET_ATTR_IMPL
 
-template <>
-void OpDesc::SetAttr<std::vector<int>>(const std::string &name,
-                                       const std::vector<int> &v) {
-  auto it = FindAttr(desc_, name);
-  it->set_type(framework::proto::INTS);
-  it->clear_ints();
-  for (auto &i : v) {
-    it->add_ints(i);
+#define SET_ATTRS_IMPL(T, ty__, bd__, pb_f__)                              \
+  template <>                                                              \
+  void OpDesc::SetAttr<std::vector<T>>(const std::string& name,            \
+                                       const std::vector<T>& v) {          \
+    auto* it = FindAttr(desc_, name);                                      \
+    auto* type_builder =                                                   \
+        it->GetMutableField<EnumBuilder<proto::OpDesc::AttrType>>("type"); \
+    CHECK(type_builder);                                                   \
+    type_builder->set(proto::OpDesc::AttrType::ty__);                      \
+    auto* vec_builder =                                                    \
+        it->GetMutableField<ListBuilder<bd__##Builder>>(#pb_f__);          \
+    CHECK(vec_builder);                                                    \
+    vec_builder->Clear();                                                  \
+    for (auto& i : v) {                                                    \
+      auto* builder = vec_builder->New();                                  \
+      builder->set(i);                                                     \
+    }                                                                      \
   }
-}
+SET_ATTRS_IMPL(int, INTS, Int32, ints);
+SET_ATTRS_IMPL(float, FLOATS, Float32, floats);
+SET_ATTRS_IMPL(std::string, STRINGS, String, strings);
+SET_ATTRS_IMPL(int64_t, LONGS, Int64, longs); 
+#undef SET_ATTRS_IMPL
 
-template <>
-void OpDesc::SetAttr<std::string>(const std::string &name,
-                                  const std::string &v) {
-  auto it = FindAttr(desc_, name);
-  it->set_type(framework::proto::STRING);
-  it->set_s(v.c_str());
-}
-
-template <>
-void OpDesc::SetAttr<std::vector<float>>(const std::string &name,
-                                         const std::vector<float> &v) {
-  auto it = FindAttr(desc_, name);
-  it->set_type(framework::proto::FLOATS);
-  it->clear_floats();
-  for (auto &i : v) {
-    it->add_floats(i);
-  }
-}
-
-template <>
-void OpDesc::SetAttr<std::vector<std::string>>(
-    const std::string &name, const std::vector<std::string> &v) {
-  auto it = FindAttr(desc_, name);
-  it->set_type(framework::proto::STRINGS);
-  it->clear_strings();
-  for (auto &i : v) {
-    it->add_strings(i);
-  }
-}
-
-template <>
-void OpDesc::SetAttr<std::vector<int64_t>>(const std::string &name,
-                                           const std::vector<int64_t> &v) {
-  auto it = FindAttr(desc_, name);
-  it->set_type(framework::proto::LONGS);
-  it->clear_longs();
-  for (auto &i : v) {
-    it->add_longs(i);
-  }
-}
-
-google::protobuf::internal::RepeatedPtrIterator<
-    const framework::proto::OpDesc_Attr>
-GetFindAttr(const framework::proto::OpDesc &desc, const std::string &name) {
-  auto &xs = desc.attrs();
-  auto it = std::find_if(
-      xs.begin(), xs.end(), [&](const framework::proto::OpDesc_Attr &x) {
-        return x.name() == name;
+const proto::OpDesc::Attr& GetFindAttr(const proto::OpDesc& desc,
+                                       const std::string& name) {
+  auto& xs = desc.GetField<ListBuilder<proto::OpDesc::Attr>>("attrs");
+  auto it =
+      std::find_if(xs.begin(), xs.end(), [&](const proto::OpDesc::Attr& x) {
+        auto& builder = x.GetField<StringBuilder>("name");
+        return builder.data() == name;
       });
-  return it;
+  CHECK(it != xs.end());
+  return *it;
 }
 
-#define GET_ATTR_IMPL(T, pb_f__)                        \
-  template <>                                           \
-  T OpDesc::GetAttr<T>(const std::string &name) const { \
-    auto it = GetFindAttr(*desc_, name);                \
-    return it->pb_f__();                                \
+#define GET_ATTR_IMPL(T, bd__, pb_f__)                   \
+  template <>                                            \
+  T OpDesc::GetAttr<T>(const std::string& name) const {  \
+    auto& it = GetFindAttr(*desc_, name);                \
+    auto& builder = it.GetField<bd__##Builder>(#pb_f__); \
+    return builder.data();                               \
   }
+GET_ATTR_IMPL(int32_t, Int32, i);
+GET_ATTR_IMPL(int16_t, Int32, block_idx);
+GET_ATTR_IMPL(float, Float32, f);
+GET_ATTR_IMPL(bool, Bool, b);
+GET_ATTR_IMPL(int64_t, Int64, l);
+GET_ATTR_IMPL(std::string, String, s);
+#undef GET_ATTR_IMPL
 
-#define GET_ATTRS_IMPL(T, pb_f__)                       \
-  template <>                                           \
-  T OpDesc::GetAttr<T>(const std::string &name) const { \
-    auto it = GetFindAttr(*desc_, name);                \
-    T res;                                              \
-    for (const auto &v : it->pb_f__()) {                \
-      res.push_back(v);                                 \
-    }                                                   \
-    return res;                                         \
+#define GET_ATTRS_IMPL(T, bd__, pb_f__)                                    \
+  template <>                                                              \
+  std::vector<T> OpDesc::GetAttr<std::vector<T>>(const std::string& name)  \
+      const {                                                              \
+    auto& it = GetFindAttr(*desc_, name);                                  \
+    std::vector<T> res;                                                    \
+    auto& list_builder = it.GetField<ListBuilder<bd__##Builder>>(#pb_f__); \
+    for (size_t i = 0; i < list_builder.size(); ++i) {                     \
+      res.push_back(list_builder.Get(i).data());                           \
+    }                                                                      \
+    return res;                                                            \
   }
-GET_ATTR_IMPL(int32_t, i);
-GET_ATTR_IMPL(int16_t, block_idx);
-GET_ATTR_IMPL(float, f);
-GET_ATTR_IMPL(bool, b);
-GET_ATTR_IMPL(int64_t, l);
-GET_ATTRS_IMPL(std::vector<int>, ints);
-GET_ATTRS_IMPL(std::vector<float>, floats);
-GET_ATTRS_IMPL(std::vector<std::string>, strings);
-GET_ATTR_IMPL(std::string, s);
-GET_ATTRS_IMPL(std::vector<int64_t>, longs);
+GET_ATTRS_IMPL(int, Int32, ints);
+GET_ATTRS_IMPL(float, Float32, floats);
+GET_ATTRS_IMPL(std::string, String, strings);
+GET_ATTRS_IMPL(int64_t, Int64, longs);
+#undef GET_ATTRS_IMPL
 
-}  // namespace pb
+}  // namespace naive_buffer
 }  // namespace lite
 }  // namespace paddle
