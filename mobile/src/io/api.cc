a2a5c8b0d71c2c53b99f837ccfdf27cb1f652000
Yanzhan Yang <yangyanzhan@gmail.com>
2019-09-16 18:57:06 +0800

    refine paddle_inference_api.h test=develop (#2048)

--- mobile/src/io/api.cc
+++ mobile/src/io/api.cc
@@ -12,6 +12,7 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License. */
 
+#include "common/type_define.h"
 #include "cstring"
 #include "io/paddle_inference_api.h"
 
