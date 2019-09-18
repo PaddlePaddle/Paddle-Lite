81dffbe8f1ceeeeb4c0e35c6a3140121bdf94ce6
Xiaoyang LI <lixiaoyang05@baidu.com>
2019-09-18 16:55:26 +0800

    fix bias quantize error && fix clang build error (#2049)
    * fix gemm_int8, gemv-int8 and conv-int8 math function, add float bias
    
    * change conv impl
    
    * neon int8 kernel support float bias
    
    * arm compute kernel support float bias
    
    * add math_test target
    
    * add tensor utils for testing, fix sgemm ut error
    
    * add gemm_int8 unit test, support float bias
    
    * fix build script
    
    * add conv compute unit test for arm
    
    * fix build script, test=develop
    
    * fix fp32 dw conv3x3s1, test=develop
    
    * add fp32 dw conv3x3s1, test=develop
    
    * add armv7 fp32 dw conv3x3s1, test=develop
    
    * add fp32 depthwise conv3x3s2, test=develop
    
    * fix fp32 conv3x3 depthwise build error, test=develop
    
    * fix gemm_like conv trans weights error, test=develop
    
    * fix int8 depthwise conv3x3 error, test=develop
    
    * turn on all test for arm fp32 conv, test=develop
    
    * fix int8 conv1x1 error
    
    * fix int8 direct conv3x3s1 error, test=develop
    
    * fix int8 direct conv3x3s2, test=develop
    
    * turn on all test for arm int8 conv, test=develop
    
    * fix int8 fc error, change mobilenetv1-int8 ground-truth result to fluid, test=develop
    
    * remove debug info, strip ut binary, test=develop
    
    * fix conv compute error, test=develop
    
    * change Init() to ReInitWhenNeeded(), test=develop
    
    * fix code style, test=develop
    
    * remote engine_test, test=develop
    
    * fix building server tests error, test=develop
    
    * fix sdot clang build error, test=develop
    
    * fix sgemm ut timeout error, test=develop
    
    * fix clang build error, test=develop
    
    * turn off math basic test due to ci time out, test=develop
    
    * fix conv_int8 ut error, test=develop

---
+++ lite/backends/arm/math/gemm_s8.cc
@@ -0,0 +1,80 @@
+// Copyright (c) 2019 PaddlePaddle Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "lite/backends/arm/math/gemm_s8.h"
+
+namespace paddle {
+namespace lite {
+namespace arm {
+namespace math {
+
+template <typename Dtype>
+void gemm_s8(bool is_transA,
+             bool is_transB,
+             int M,
+             int N,
+             int K,
+             const int8_t* A,
+             const int8_t* B,
+             Dtype* C,
+             const float* bias,
+             bool is_bias,
+             bool is_relu,
+             const float* scale,
+             ARMContext* ctx) {
+  int hblock = get_hblock_int8(ctx);
+  int m_roundup = hblock * ((M + hblock - 1) / hblock);
+  auto packed_A = static_cast<int8_t*>(
+      TargetMalloc(TargetType::kARM, m_roundup * K * sizeof(int8_t)));
+
+  int lda = is_transA ? M : K;
+  prepackA_int8(packed_A, A, lda, 0, M, 0, K, is_transA, ctx);
+
+  gemm_prepack_int8(
+      packed_A, B, bias, C, M, N, K, is_bias, is_relu, is_transB, scale, ctx);
+  TargetFree(TargetType::kARM, packed_A);
+}
+
+template void gemm_s8<float>(bool is_transA,
+                             bool is_transB,
+                             int M,
+                             int N,
+                             int K,
+                             const int8_t* A,
+                             const int8_t* B,
+                             float* C,
+                             const float* bias,
+                             bool is_bias,
+                             bool is_relu,
+                             const float* scale,
+                             ARMContext* ctx);
+
+template void gemm_s8<int8_t>(bool is_transA,
+                              bool is_transB,
+                              int M,
+                              int N,
+                              int K,
+                              const int8_t* A,
+                              const int8_t* B,
+                              int8_t* C,
+                              const float* bias,
+                              bool is_bias,
+                              bool is_relu,
+                              const float* scale,
+                              ARMContext* ctx);
+
+}  // namespace math
+}  // namespace arm
+}  // namespace lite
+}  // namespace paddle
