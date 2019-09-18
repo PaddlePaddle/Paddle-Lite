66d7398e40672b148800048581618a6f69d45d77
石晓伟 <39303645+Shixiaowei02@users.noreply.github.com>
2019-09-18 20:47:53 +0800

    modify the device binding logic of the pass, test=develop (#2061)

--- lite/core/mir/fusion/shuffle_channel_fuse_pass.cc
+++ lite/core/mir/fusion/shuffle_channel_fuse_pass.cc
@@ -36,4 +36,5 @@ void ShuffleChannelFusePass::Apply(const std::unique_ptr<SSAGraph>& graph) {
 
 REGISTER_MIR_PASS(lite_shuffle_channel_fuse_pass,
                   paddle::lite::mir::ShuffleChannelFusePass)
-    .BindTargets({TARGET(kAny)});
+    .BindTargets({TARGET(kAny)})
+    .BindKernel("shuffle_channel");
