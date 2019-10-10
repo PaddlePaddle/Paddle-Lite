94ead828c5376937ae5d6bc429fb8947421758f2
ysh329 <ysh329@sina.com>
2019-10-10 12:18:44 -0400

    add macro of CONVERT_TYPE_TO and READ/WRITE image. test=develop

--- lite/backends/opencl/cl_kernel/cl_common.h
+++ lite/backends/opencl/cl_kernel/cl_common.h
@@ -16,9 +16,23 @@ limitations under the License. */
 
 #pragma OPENCL EXTENSION cl_khr_fp16 : enable
 
+// Note: macro name replacement need twice parser
 #define GET_VEC_TYPE(type__, size__) type__##size__
 #define VECTORIZED_TYPE(type__, size__) GET_VEC_TYPE(type__, size__)
 #define CL_DTYPE4 VECTORIZED_TYPE(CL_DTYPE, 4)
+
+#define _CONVERT_TYPE_TO(value, type) convert_##type(value)
+#define CONVERT_TYPE_TO(value, type) _CONVERT_TYPE_TO(value, type)
+
+#define _WRITE_IMG_TYPE(type_char, img, pos, value) \
+  write_image##type_char(img, pos, value)
+#define WRITE_IMG_TYPE(type_char, img, pos, value) \
+  _WRITE_IMG_TYPE(type_char, img, pos, value)
+
+#define _READ_IMG_TYPE(type_char, img, pos, sampler) \
+  read_image##type_char(img, sampler, pos)
+#define READ_IMG_TYPE(type_char, img, pos, sampler) \
+  _READ_IMG_TYPE(type_char, img, pos, sampler)
 
 inline CL_DTYPE activation(CL_DTYPE in
 #ifdef PRELU
