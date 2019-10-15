6fb0efdd5e93885ea44b3e3def1adbc0d8921e53
HappyAngel <chenjiaobuaa@126.com>
2019-10-15 20:18:08 +0800

    only support conv_dw pad=0, 1

--- lite/backends/arm/math/conv_impl.cc
+++ lite/backends/arm/math/conv_impl.cc
@@ -572,20 +572,7 @@ void conv_depthwise_3x3_fp32(const void* din,
   int pad = pad_w;
   bool flag_relu = param.fuse_relu;
   bool flag_bias = param.bias != nullptr;
-  if (stride == 1) {
-//     conv_3x3s1_depthwise_fp32(reinterpret_cast<const float*>(din),
-//                               reinterpret_cast<float*>(dout),
-//                               num,
-//                               ch_out,
-//                               h_out,
-//                               w_out,
-//                               ch_in,
-//                               h_in,
-//                               w_in,
-//                               reinterpret_cast<const float*>(weights),
-//                               bias,
-//                               param,
-//                               ctx);
+  if (stride == 1  && pad < 2) {
       conv_depthwise_3x3s1_fp32(reinterpret_cast<const float*>(din),
                               reinterpret_cast<float*>(dout),
                               num,
@@ -601,20 +588,7 @@ void conv_depthwise_3x3_fp32(const void* din,
                               flag_bias,
                               flag_relu,
                               ctx);
-  } else if (stride == 2) {
-//     conv_3x3s2_depthwise_fp32(reinterpret_cast<const float*>(din),
-//                               reinterpret_cast<float*>(dout),
-//                               num,
-//                               ch_out,
-//                               h_out,
-//                               w_out,
-//                               ch_in,
-//                               h_in,
-//                               w_in,
-//                               reinterpret_cast<const float*>(weights),
-//                               bias,
-//                               param,
-//                               ctx);
+  } else if (stride == 2  && pad < 2) {
       conv_depthwise_3x3s2_fp32(reinterpret_cast<const float*>(din),
                               reinterpret_cast<float*>(dout),
                               num,
@@ -631,7 +605,7 @@ void conv_depthwise_3x3_fp32(const void* din,
                               flag_relu,
                               ctx);
   } else {
-    LOG(FATAL) << "fp32 depthwise conv3x3 stride: " << stride << " unsupported";
+      LOG(FATAL) << "fp32 depthwise conv3x3 stride: " << stride << " or pad(<2): " << pad << " unsupported";
   }
 #if 0
   if (pad == 1) {
