c6cf184d27d1a7157b928a5edf8b8eaa973a7251
FrostML <380185688@qq.com>
2019-10-23 15:10:57 +0800

    add forget test_helper.h, test=develop

--- lite/api/test_googlenet_lite.cc
+++ lite/api/test_googlenet_lite.cc
@@ -20,6 +20,7 @@
 #include "lite/api/paddle_use_kernels.h"
 #include "lite/api/paddle_use_ops.h"
 #include "lite/api/paddle_use_passes.h"
+#include "lite/api/test_helper.h"
 #include "lite/core/op_registry.h"
 #include "lite/core/tensor.h"
 
