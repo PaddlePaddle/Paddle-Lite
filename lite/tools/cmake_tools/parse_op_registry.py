b16917a47ae678083d565315f97be33cbdd10ec8
huzhiqiang <912790387@qq.com>
2019-10-27 13:04:50 +0800

    model  dynamic library tailoring (#2256)
    * add shell file to automatically build and collect publish result test=develop
    * modify API inference of model_optimize_tool and add option for tiny&full publish test=develop

--- lite/tools/cmake_tools/parse_op_registry.py
+++ lite/tools/cmake_tools/parse_op_registry.py
@@ -19,7 +19,8 @@ from ast import RegisterLiteOpParser
 
 ops_list_path = sys.argv[1]
 dest_path = sys.argv[2]
-
+minops_list_path = sys.argv[3]
+tailored = sys.argv[4]
 out_lines = [
     '#pragma once',
     '#include "paddle_lite_factory_helper.h"',
@@ -30,6 +31,11 @@ paths = set()
 for line in open(ops_list_path):
     paths.add(line.strip())
 
+if tailored == "ON":
+    minlines = set()
+    with open(minops_list_path) as fd:
+        for line in fd:
+            minlines.add(line.strip())
 for path in paths:
     str_info = open(path.strip()).read()
     op_parser = RegisterLiteOpParser(str_info)
@@ -37,6 +43,8 @@ for path in paths:
     for op in ops:
         if "_grad" in op: 
             continue
+        if tailored == "ON":
+            if op not in minlines: continue
         out = "USE_LITE_OP(%s);" % op
         out_lines.append(out)
 
