46bb9703455a60da91da10c1b387cf874c437f3f
Wilber <jiweibo@baidu.com>
2019-11-13 11:58:23 +0800

    add sequence_concat op kernel and test test=develop (#2414)
    - add sequence_concat op
    
    - add sequence_concat kernel for x86 and cuda
    
    - add sequence_concat_test for x86 and cuda

--- lite/operators/op_params.h
+++ lite/operators/op_params.h
@@ -751,6 +751,11 @@ struct SequenceExpandAsParam {
   lite::Tensor* out{nullptr};
 };
 
+struct SequenceConcatParam {
+  std::vector<lite::Tensor*> X{};
+  lite::Tensor* Out{};
+};
+
 struct ReduceMaxParam {
   const lite::Tensor* X{};
   lite::Tensor* Out{};
