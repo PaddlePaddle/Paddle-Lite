ce1bd5dd2c6f4a4f147587931a75d88633272e75
DannyIsFunny <912790387@qq.com>
2019-11-29 03:02:07 +0000

    fix code_style test=develop

--- lite/tools/ci_build.sh
+++ lite/tools/ci_build.sh
@@ -709,8 +709,8 @@ function build_test_arm_subtask_android {
        adb_devices=$(adb devices |grep -v devices |grep device | awk -F " " '{print $1}')
        local portname_armv8=${adb_devices[0]}
        local portname_armv7=${adb_devices[0]}
-       abd shell -s $portname_armv8 'rm -rf /data/local/tmp/*'
-       abd shell -s $portname_armv7 'rm -rf /data/local/tmp/*'
+       adb shell -s $portname_armv8 'rm -rf /data/local/tmp/*'
+       adb shell -s $portname_armv7 'rm -rf /data/local/tmp/*'
     fi
 
     # job 1
@@ -751,8 +751,8 @@ function build_test_arm_subtask_armlinux {
        adb_devices=$(adb devices |grep -v devices | grep device | awk -F " " '{print $1}')
        local portname_armv8=${adb_devices[0]}
        local portname_armv7=${adb_devices[0]}
-       abd shell -s $portname_armv8 'rm -rf /data/local/tmp/*'
-       abd shell -s $portname_armv7 'rm -rf /data/local/tmp/*'
+       adb shell -s $portname_armv8 'rm -rf /data/local/tmp/*'
+       adb shell -s $portname_armv7 'rm -rf /data/local/tmp/*'
     fi
 
     cur=$PWD
@@ -798,7 +798,7 @@ function build_test_arm_subtask_model {
     else
        adb_devices=$(adb devices |grep -v devices | grep device | awk -F " " '{print $1}')
        local portname_armv8=${adb_devices[0]}
-       abd shell -s $portname_armv8 'rm -rf /data/local/tmp/*'
+       adb shell -s $portname_armv8 'rm -rf /data/local/tmp/*'
     fi
     # just test the model on armv8
     test_arm_model $test_name $portname_armv8 "./third_party/install/$model_name"
@@ -859,7 +859,7 @@ function build_test_npu {
     else
        adb_devices=$(adb devices |grep -v devices | grep device | awk -F " " '{print $1}')
        local portname_armv8=${adb_devices[0]}
-       abd shell -s $portname_armv8 'rm -rf /data/local/tmp/*'
+       adb shell -s $portname_armv8 'rm -rf /data/local/tmp/*'
     fi
 
 
