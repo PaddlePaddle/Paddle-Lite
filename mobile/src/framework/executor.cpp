003b98bd3f194861dd05c3eea58b2ecffbe570d6
Jiaying Zhao <zhaojiaying01@baidu.com>
2019-12-26 11:34:20 +0800

    [Mobile][ARM]Fix memory leaks when using input tensor created with external pointer in lod_mode. (#2681)

--- mobile/src/framework/executor.cpp
+++ mobile/src/framework/executor.cpp
@@ -311,7 +311,10 @@ void Executor<Device, T>::InitNoPersistableMemory(const Tensor &input_tensor) {
         DLOG << "InitNoPersistableMemory var " << var_desc->Name();
         auto tensor = var->template GetMutable<LoDTensor>();
         if (tensor->IsInitialized() && tensor->dims().size() == 4) {
-          DLOG << "var's tensor is Initialized or dims size != 4";
+          // don't change user's input and avoid memory leaks
+          if (feed_indices_.find(var_desc->Name()) != feed_indices_.end()) {
+            break;
+          }
           DDim tensor_dim = tensor->dims();
           DDim new_dim =
               make_ddim({tensor_dim[0], tensor_dim[1], input_tensor.dims()[2],
