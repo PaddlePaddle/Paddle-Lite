4300ef75fff50d4a0ab4554d620f658567f960d6
huzhiqiang <912790387@qq.com>
2019-12-28 10:17:56 +0800

    Upgrade of Model_optimize_tool (#2624)

--- lite/tools/cmake_tools/create_fake_kernel_registry.py
+++ lite/tools/cmake_tools/create_fake_kernel_registry.py
@@ -18,6 +18,9 @@ import logging
 from ast import RegisterLiteKernelParser
 from utils import *
 
+if len(sys.argv) != 4:
+    print("Error: create_fake_kernel_registry.py requires three inputs!")
+    exit(1)
 ops_list_path = sys.argv[1]
 dest_path = sys.argv[2]
 kernelmap_path = sys.argv[3]
