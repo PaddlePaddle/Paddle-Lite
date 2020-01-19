f91eaa03e23f2d24a60cfecdf6b5f09b31280f10
zhupengyang <1165938320@qq.com>
2020-01-19 02:09:55 +0000

    [NPU] reshape x,y,out node in elementwise ops
    test=develop

--- lite/kernels/npu/bridges/elementwise_ops.cc
+++ lite/kernels/npu/bridges/elementwise_ops.cc
@@ -30,18 +30,31 @@ void CvtYShape(std::vector<int64_t>* x_shape,
     axis = x_shape->size() - y_shape->size();
   }
 
-  if (x_shape->size() < 4) {
-    x_shape->insert(x_shape->begin(), 1);
-    axis++;
+  // only support:
+  // (n,c,h,w) * (n,c,h,w)
+  // (n,c,h,w) * (1,c,1,1)
+  // (n,c,h,w) * (1,c,h,1)
+  // (n,c,h,w) * (1,c,h,w)
+  int y_shape_size = y_shape->size();
+  if (y_shape_size == 1) {
+    y_shape->insert(y_shape->begin(), 1);
+    y_shape->insert(y_shape->end(), 2, 1);
+  } else if (y_shape_size == 2) {
+    y_shape->insert(y_shape->begin(), 1);
+    y_shape->insert(y_shape->end(), 1);
+  } else if (y_shape_size == 3) {
+    y_shape->insert(y_shape->begin(), 1);
   }
-  while (x_shape->size() < 4) {
-    x_shape->push_back(1);
+  if (y_shape_size < 4) {
+    int n = 1;
+    for (int i = 0; i < axis; i++) {
+      n *= x_shape->at(i);
+    }
+    x_shape->erase(x_shape->begin(), x_shape->begin() + axis);
+    x_shape->insert(x_shape->begin(), n);
+    x_shape->insert(x_shape->end(), 4 - x_shape->size(), 1);
   }
 
-  y_shape->insert(y_shape->begin(), axis, 1);
-  while (y_shape->size() < 4) {
-    y_shape->push_back(1);
-  }
   CHECK_EQ(x_shape->size(), 4UL);
   CHECK_EQ(y_shape->size(), 4UL);
 }
@@ -74,6 +87,8 @@ int ElementwiseConverter(void* ctx, OpLite* op, KernelBase* kernel) {
   auto out_type = kernel->GetOutputDeclType("Out");
   CHECK(out_type->precision() == PRECISION(kFloat));
   CHECK(out_type->layout() == DATALAYOUT(kNCHW));
+  auto out = scope->FindMutableTensor(out_name);
+  auto out_dims = out->dims();
 
   auto axis = op_info->GetAttr<int>("axis");
 
@@ -85,6 +100,15 @@ int ElementwiseConverter(void* ctx, OpLite* op, KernelBase* kernel) {
   std::shared_ptr<Node> x_node = nullptr;
   if (graph->Has(x_name)) {
     x_node = graph->Get(x_name);
+    if (x_dims.Vectorize() != x_new_shape) {
+      auto reshaped_x_node = graph->Add<ge::op::Reshape>(x_name + "/reshape");
+      auto reshaped_x_op = reshaped_x_node->data<ge::op::Reshape>();
+      reshaped_x_op->set_input_tensor(*x_node->data());
+      reshaped_x_op->set_attr_shape(
+          ge::AttrValue::LIST_INT(x_new_shape.begin(), x_new_shape.end()));
+      reshaped_x_op->set_attr_axis(0);
+      x_node = reshaped_x_node;
+    }
   } else {
     x_node = graph->Add(x_name, *x, x_new_shape);
   }
@@ -93,6 +117,15 @@ int ElementwiseConverter(void* ctx, OpLite* op, KernelBase* kernel) {
   std::shared_ptr<Node> y_node = nullptr;
   if (graph->Has(y_name)) {
     y_node = graph->Get(y_name);
+    if (y_dims.Vectorize() != y_new_shape) {
+      auto reshaped_y_node = graph->Add<ge::op::Reshape>(y_name + "/reshape");
+      auto reshaped_y_op = reshaped_y_node->data<ge::op::Reshape>();
+      reshaped_y_op->set_input_tensor(*y_node->data());
+      reshaped_y_op->set_attr_shape(
+          ge::AttrValue::LIST_INT(y_new_shape.begin(), y_new_shape.end()));
+      reshaped_y_op->set_attr_axis(0);
+      y_node = reshaped_y_node;
+    }
   } else {
     y_node = graph->Add(y_name, *y, y_new_shape);
   }
@@ -128,6 +161,17 @@ int ElementwiseConverter(void* ctx, OpLite* op, KernelBase* kernel) {
     return FAILED;
   }
 
+  if (out_dims.Vectorize() != x_new_shape) {
+    auto reshaped_elt_node = graph->Add<ge::op::Reshape>(out_name);
+    auto reshaped_elt_op = reshaped_elt_node->data<ge::op::Reshape>();
+    reshaped_elt_op->set_input_tensor(*elt_node->data());
+    auto out_shape = out_dims.Vectorize();
+    reshaped_elt_op->set_attr_shape(
+        ge::AttrValue::LIST_INT(out_shape.begin(), out_shape.end()));
+    reshaped_elt_op->set_attr_axis(0);
+    elt_node = reshaped_elt_node;
+  }
+
   // Act node
   if (op_type == "fusion_elementwise_add_activation" ||
       op_type == "fusion_elementwise_sub_activation" ||
@@ -141,6 +185,7 @@ int ElementwiseConverter(void* ctx, OpLite* op, KernelBase* kernel) {
     // clipped_relu etc.
     act_op->set_attr_mode(CvtActMode(act_type));
   }
+
   return REBUILD_WHEN_SHAPE_CHANGED;
 }
 
