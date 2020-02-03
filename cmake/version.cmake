95b6a0d17b0134fea55bab1c62681cc30b6592fb
DannyIsFunny <912790387@qq.com>
2020-02-03 09:36:13 +0000

    add Lite version test=develop

--- cmake/version.cmake
+++ cmake/version.cmake
@@ -1,45 +1,44 @@
-message(STATUS "GetInto version.cmake")
+# Copyright (c) 2019 PaddlePaddle Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+
+# This code is used for getting current version, the lastest 
+# tag is used as Paddle-lite version. If the lasted commit doesn't 
+# match any existed tag, we will use the lasted commit id instead.
+
 # Get the latest git tag.
 set(PADDLE_VERSION $ENV{PADDLE_VERSION})
-set(tmp_version "HEAD")
 set(TAG_VERSION_REGEX "[0-9]+\\.[0-9]+\\.[0-9]+(\\.(a|b|rc)\\.[0-9]+)?")
-set(COMMIT_VERSION_REGEX "[0-9a-f]+[0-9a-f]+[0-9a-f]+[0-9a-f]+[0-9a-f]+")
-set(LATEST_PADDLE_VERSION "0.0.0")
 
 while ("${PADDLE_VERSION}" STREQUAL "")
-  # Check current branch name
-  execute_process(
-    COMMAND ${GIT_EXECUTABLE} rev-parse --abbrev-ref ${tmp_version}
-    WORKING_DIRECTORY ${PADDLE_SOURCE_DIR}
-    OUTPUT_VARIABLE GIT_BRANCH_NAME
-    RESULT_VARIABLE GIT_BRANCH_RESULT
-    ERROR_QUIET OUTPUT_STRIP_TRAILING_WHITESPACE)
-
-message(STATUS "GIT_BRANCH_NAME:${GIT_BRANCH_NAME}")
-  if (NOT ${GIT_BRANCH_RESULT})
-    # get the lasted commit id
-    execute_process(
-      COMMAND ${GIT_EXECUTABLE} rev-list --tags --max-count=1
-      WORKING_DIRECTORY ${PADDLE_SOURCE_DIR}
-      OUTPUT_VARIABLE GIT_TAG_NUM
-      RESULT_VARIABLE GIT_RESULT
-      ERROR_QUIET OUTPUT_STRIP_TRAILING_WHITESPACE)
-message(STATUS "Lasted commit ID: ${GIT_TAG_NUM}")
-    # Get the latested tag corresponding to the lasted commit
-    execute_process(
-      COMMAND ${GIT_EXECUTABLE} describe --tags ${GIT_TAG_NUM}
-      WORKING_DIRECTORY ${PADDLE_SOURCE_DIR}
-      OUTPUT_VARIABLE GIT_TAG_NAME
-      RESULT_VARIABLE GIT_RESULT
-      ERROR_QUIET OUTPUT_STRIP_TRAILING_WHITESPACE)
-
-    if (NOT ${GIT_RESULT} AND ${GIT_TAG_NAME} MATCHES "v${TAG_VERSION_REGEX}")
-      string(REPLACE "v" "" PADDLE_VERSION ${GIT_TAG_NAME})
-    else()
-      set(PADDLE_VERSION "0.0.0")
-      message(WARNING "Cannot add paddle version from git tag")
-    endif()
-  endif()
+   # Get current tag corresponding to the lastest commit
+   execute_process(
+     COMMAND git describe --tags --exact-match
+     WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
+     OUTPUT_VARIABLE PADDLE_LITE_TAG
+     RESULT_VARIABLE LITE_TAG_RESULT
+     ERROR_QUIET OUTPUT_STRIP_TRAILING_WHITESPACE
+   )
+   if (NOT ${LITE_TAG_RESULT})
+      if(${PADDLE_LITE_TAG} MATCHES "v${TAG_VERSION_REGEX}")
+         string(REPLACE "v" "" PADDLE_VERSION ${PADDLE_LITE_TAG})
+      endif()
+   endif()
+   if ("${PADDLE_VERSION}" STREQUAL "")
+      # If the lastest commit doesn't match any release tag, we get the lastest commit id in short-format instead.
+      execute_process(
+         COMMAND git log -1 --format=%h
+         WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
+         OUTPUT_VARIABLE PADDLE_LITE_COMMIT
+         OUTPUT_STRIP_TRAILING_WHITESPACE
+      )
+      set(PADDLE_VERSION ${PADDLE_LITE_COMMIT})
+      message(WARNING "This project is not based on a release branch, we can't get a stable Lite version.")
+   endif()
 endwhile()
 
 add_definitions(-DPADDLE_VERSION=${PADDLE_VERSION})
