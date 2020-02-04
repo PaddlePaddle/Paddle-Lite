87be1ae69798ec04eba8b9cb37186ce52b81f0e2
chenjiaoAngel <chenjiaobuaa@126.com>
2020-02-04 02:35:50 -0500

    fix format, test=develop

--- lite/tests/math/conv_compute_test.cc
+++ lite/tests/math/conv_compute_test.cc
@@ -307,7 +307,7 @@ void test_conv_fp32(const std::vector<DDim>& input_dims,
 #endif  // LITE_WITH_ARM
 
 // TODO(chenjiaoAngel): fix 4 threds, diff: 3x3 depthwise conv
-#if 1   /// 3x3dw
+#if 1   /// 3x3dw 
 TEST(TestConv3x3DW, test_conv3x3_depthwise) {
   if (FLAGS_basic_test) {
     for (auto& stride : {1, 2}) {
