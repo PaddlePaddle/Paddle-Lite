2bed71e4f5e468afaf0fb4dd63c502c66eabeedd
DannyIsFunny <912790387@qq.com>
2020-02-14 12:16:54 +0000

    test=develop

--- lite/model_parser/model_parser.cc
+++ lite/model_parser/model_parser.cc
@@ -802,7 +802,7 @@ void LoadModelNaiveFromFile(const std::string &filename,
   // version.
   const std::string paddle_version = version();
   const std::string opt_version_str = opt_version;
-  if (paddle_version == opt_version_str) {
+  if (paddle_version != opt_version_str) {
     LOG(WARNING) << "warning: the version of opt that transformed this model "
                     "is not consistent with current Paddle-Lite version."
                     "\n      version of opt:"
