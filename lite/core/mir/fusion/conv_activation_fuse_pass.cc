5d0d80c94aa9cd82972c06b6095269b523c38a64
chenjiaoAngel <chenjiaobuaa@126.com>
2020-02-21 16:26:30 +0800

    fix conv+relu6 int8 fusion error, test=develop

--- lite/core/mir/fusion/conv_activation_fuse_pass.cc
+++ lite/core/mir/fusion/conv_activation_fuse_pass.cc
@@ -29,10 +29,12 @@ void ConvActivationFusePass::Apply(const std::unique_ptr<SSAGraph>& graph) {
       act_types.push_back("leaky_relu");
       break;
     }
-    if (place.target == TARGET(kARM) && place.precision == PRECISION(kFloat)) {
-      act_types.push_back("relu6");
-      act_types.push_back("leaky_relu");
-      break;
+    if (place.target == TARGET(kARM)) {
+      if (place.precision != PRECISION(kInt8)) {
+        act_types.push_back("relu6");
+        act_types.push_back("leaky_relu");
+        break;
+      }
     }
   }
   for (auto conv_type : {"conv2d", "depthwise_conv2d", "conv2d_transpose"}) {
