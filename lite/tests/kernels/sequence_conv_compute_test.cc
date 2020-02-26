07d5460f0e1a8e53f17d9ba50228169a08a7331b
mapingshuo <mps2012@yeah.net>
2020-02-26 20:49:28 +0800

    test=develop

--- lite/tests/kernels/sequence_conv_compute_test.cc
+++ lite/tests/kernels/sequence_conv_compute_test.cc
@@ -20,8 +20,8 @@
 namespace paddle {
 namespace lite {
 
-class SequenceConvComputeTester: public arena::TestCase{
-public:
+class SequenceConvComputeTester : public arena::TestCase {
+ public:
   SequenceConvComputeTester(const Place& place,
                             const std::string& alias,
                             LoD lod,
@@ -30,36 +30,42 @@ public:
                             const int& contextStride,
                             const int& contextLength,
                             const int& kernel_num)
-      : TestCase(place, alias), lod_(lod), dims_(dims),
-        contextStart_(contextStart), contextStride_(contextStride), 
-        contextLength_(contextLength), kernel_num_(kernel_num) {}
+      : TestCase(place, alias),
+        lod_(lod),
+        dims_(dims),
+        contextStart_(contextStart),
+        contextStride_(contextStride),
+        contextLength_(contextLength),
+        kernel_num_(kernel_num) {}
 
   void PrepareOpDesc(cpp::OpDesc* op_desc) {
-    op_desc -> SetType("sequence_conv");
-    op_desc -> SetInput("X", {input_name_});
-    op_desc -> SetInput("Filter", {filter_name_});
-    op_desc -> SetOutput("Out", {output_name_});
-    op_desc -> SetAttr("contextStart", contextStart_);
-    op_desc -> SetAttr("contextStride", contextStride_);
-    op_desc -> SetAttr("contextLength", contextLength_);
+    op_desc->SetType("sequence_conv");
+    op_desc->SetInput("X", {input_name_});
+    op_desc->SetInput("Filter", {filter_name_});
+    op_desc->SetOutput("Out", {output_name_});
+    op_desc->SetAttr("contextStart", contextStart_);
+    op_desc->SetAttr("contextStride", contextStride_);
+    op_desc->SetAttr("contextLength", contextLength_);
   }
 
   void PrepareData() override {
     std::vector<float> din(dims_.production());
-    for(int i = 0; i < dims_[0]; i++ ){
-      for (int j = 0; j < dims_[1]; j++){
-        din[i * dims_[1] + j] = 
-          (2.0 * i + 3.0 * j) / (2.0 * dims_[0] + 3.0 * dims_[1]) - 0.5;
+    for (int i = 0; i < dims_[0]; i++) {
+      for (int j = 0; j < dims_[1]; j++) {
+        din[i * dims_[1] + j] =
+            (2.0 * i + 3.0 * j) / (2.0 * dims_[0] + 3.0 * dims_[1]) - 0.5;
       }
     }
     SetCommonTensor(input_name_, dims_, din.data(), lod_);
 
-    DDim filter_dims(std::vector<int64_t>{contextLength_ * dims_[1], kernel_num_});
+    DDim filter_dims(
+        std::vector<int64_t>{contextLength_ * dims_[1], kernel_num_});
     std::vector<float> dfilter(filter_dims.production());
-    for(int i = 0; i < filter_dims[0]; i++ ){
-      for (int j = 0; j < filter_dims[1]; j++){
-        dfilter[i * filter_dims[0] + j] = 
-          (1.5 * i + 2.0 * j) / (1.5 * filter_dims[0] + 2 * filter_dims[1]) - 0.5;
+    for (int i = 0; i < filter_dims[0]; i++) {
+      for (int j = 0; j < filter_dims[1]; j++) {
+        dfilter[i * filter_dims[0] + j] =
+            (1.5 * i + 2.0 * j) / (1.5 * filter_dims[0] + 2 * filter_dims[1]) -
+            0.5;
       }
     }
     SetCommonTensor(filter_name_, filter_dims, dfilter.data(), lod_);
@@ -75,46 +81,46 @@ public:
     output->Resize(DDim(output_shape));
     auto output_dims = output->dims();
     auto output_data = output->mutable_data<float>();
-    std::vector<std::vector<float>> res({{0.194508,    0.05720823, -0.08009153},
-                           {0.73512584,  0.5749428,   0.41475973},
-                           {0.5635012,   0.49485126,  0.42620137},
-                           {0.2517162,   0.23646072,  0.22120519}});
+    std::vector<std::vector<float>> res({{0.194508, 0.05720823, -0.08009153},
+                                         {0.73512584, 0.5749428, 0.41475973},
+                                         {0.5635012, 0.49485126, 0.42620137},
+                                         {0.2517162, 0.23646072, 0.22120519}});
 
-    for(int i = 0; i < output_shape[0]; i++){
-        for(int j = 0; j < output_shape[1]; j++){
-            output_data[i * output_shape[0] + j] = res[i][j];
-        }
-    }    
+    for (int i = 0; i < output_shape[0]; i++) {
+      for (int j = 0; j < output_shape[1]; j++) {
+        output_data[i * output_shape[0] + j] = res[i][j];
+      }
+    }
   }
-protected:
+
+ protected:
   std::string input_name_ = "x";
   std::string filter_name_ = "filter";
   std::string output_name_ = "out";
+  LoD lod_;
+  DDim dims_;
   int contextStart_;
   int contextStride_;
   int contextLength_;
   int kernel_num_;
-  DDim dims_;
-  LoD lod_;
 };
 
 void TestNormalCase(Place place, float abs_error = 2e-5) {
-    std::vector<std::vector<uint64_t>> lod{{0, 4}};
-    std::vector<int64_t> dims({4, 5});
-    std::unique_ptr<arena::TestCase> tester(new SequenceConvComputeTester(
-            place, "def", lod, DDim(dims), -1, 1, 3, 3));
-    arena::Arena arena(std::move(tester), place, abs_error);
-    arena.TestPrecision();
+  std::vector<std::vector<uint64_t>> lod{{0, 4}};
+  std::vector<int64_t> dims({4, 5});
+  std::unique_ptr<arena::TestCase> tester(new SequenceConvComputeTester(
+      place, "def", lod, DDim(dims), -1, 1, 3, 3));
+  arena::Arena arena(std::move(tester), place, abs_error);
+  arena.TestPrecision();
 }
 
 TEST(sequence_conv, precision) {
 #ifdef LITE_WITH_ARM
   float abs_error = 2e-5;
-  Place place;
+  Place place(TARGET(kARM));
 
   TestNormalCase(place, abs_error);
 #endif
-
 }
 
 }  // namespace lite
