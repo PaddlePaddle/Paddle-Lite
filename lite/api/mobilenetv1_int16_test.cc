246568e479887d13ad5f4476a7aa9ffdba7a8177
juncaipeng <13006307475@163.com>
2020-03-03 02:06:35 +0000

    up, test=develop

--- lite/api/mobilenetv1_int16_test.cc
+++ lite/api/mobilenetv1_int16_test.cc
@@ -32,42 +32,7 @@ DEFINE_string(model_dir,
               "",
               "the path of the model, the model and param files is under "
               "model_dir.");
-DEFINE_string(model_filename,
-              "",
-              "the filename of model file. When the model is combined formate, "
-              "please set model_file.");
-DEFINE_string(param_filename,
-              "",
-              "the filename of param file, set param_file when the model is "
-              "combined formate.");
-DEFINE_string(input_shape,
-              "1,3,224,224",
-              "set input shapes according to the model, "
-              "separated by colon and comma, "
-              "such as 1,3,244,244");
 DEFINE_string(input_img_path, "", "the path of input image");
-DEFINE_int32(warmup, 0, "warmup times");
-DEFINE_int32(repeats, 1, "repeats times");
-DEFINE_int32(power_mode,
-             3,
-             "arm power mode: "
-             "0 for big cluster, "
-             "1 for little cluster, "
-             "2 for all cores, "
-             "3 for no bind");
-DEFINE_int32(threads, 1, "threads num");
-DEFINE_string(result_filename,
-              "result.txt",
-              "save benchmark "
-              "result to the file");
-DEFINE_bool(run_model_optimize,
-            false,
-            "if set true, apply model_optimize_tool to "
-            "model and use optimized model to test. ");
-DEFINE_bool(is_quantized_model,
-            false,
-            "if set true, "
-            "test the performance of the quantized model. ");
 
 namespace paddle {
 namespace lite_api {
@@ -81,17 +46,9 @@ inline double GetCurrentUS() {
 void OutputOptModel(const std::string& save_optimized_model_dir) {
   lite_api::CxxConfig config;
   config.set_model_dir(FLAGS_model_dir);
-  if (!FLAGS_model_filename.empty() && !FLAGS_param_filename.empty()) {
-    config.set_model_file(FLAGS_model_dir + "/" + FLAGS_model_filename);
-    config.set_param_file(FLAGS_model_dir + "/" + FLAGS_param_filename);
-  }
   std::vector<Place> vaild_places = {
       Place{TARGET(kARM), PRECISION(kFloat)},
   };
-  if (FLAGS_is_quantized_model) {
-    vaild_places.insert(vaild_places.begin(),
-                        Place{TARGET(kARM), PRECISION(kInt8)});
-  }
   config.set_valid_places(vaild_places);
   auto predictor = lite_api::CreatePaddlePredictor(config);
 
@@ -113,9 +70,9 @@ void Run(const std::vector<int64_t>& input_shape,
          const std::string model_name) {
   // set config and create predictor
   lite_api::MobileConfig config;
-  config.set_threads(FLAGS_threads);
-  config.set_power_mode(static_cast<PowerMode>(FLAGS_power_mode));
+  config.set_threads(1);
   config.set_model_from_file(model_dir + ".nb");
+  LOG(INFO) << "load model from " << model_dir + ".nb";
 
   auto predictor = lite_api::CreatePaddlePredictor(config);
 
@@ -173,31 +130,11 @@ TEST(mobilenetv1_post_quant_nodata_int16, test_arm) {
   std::size_t found = FLAGS_model_dir.find_last_of("/");
   std::string model_name = FLAGS_model_dir.substr(found + 1);
   std::string save_optimized_model_dir = FLAGS_model_dir + "_opt2";
-
-  auto get_shape = [](const std::string& str_shape) -> std::vector<int64_t> {
-    std::vector<int64_t> shape;
-    std::string tmp_str = str_shape;
-    while (!tmp_str.empty()) {
-      int dim = atoi(tmp_str.data());
-      shape.push_back(dim);
-      size_t next_offset = tmp_str.find(",");
-      if (next_offset == std::string::npos) {
-        break;
-      } else {
-        tmp_str = tmp_str.substr(next_offset + 1);
-      }
-    }
-    return shape;
-  };
-  std::vector<int64_t> input_shape = get_shape(FLAGS_input_shape);
+  std::vector<int64_t> input_shape = {1, 3, 224, 224};
 
   // Output optimized model if needed
-  if (FLAGS_run_model_optimize) {
-    paddle::lite_api::OutputOptModel(save_optimized_model_dir);
-  }
+  paddle::lite_api::OutputOptModel(save_optimized_model_dir);
 
   // Run inference using optimized model
-  std::string run_model_dir =
-      FLAGS_run_model_optimize ? save_optimized_model_dir : FLAGS_model_dir;
-  paddle::lite_api::Run(input_shape, run_model_dir, model_name);
+  paddle::lite_api::Run(input_shape, save_optimized_model_dir, model_name);
 }
