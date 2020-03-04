ab62e6df5ca3a86414f83b250b468cf08547836f
ysh329@sina.com <ysh329@sina.com>
2020-03-04 08:50:26 +0000

    add opencl kernel build list; set num cores to build opencl in ci_build.sh and build.sh. test=develop

--- lite/tools/ci_build.sh
+++ lite/tools/ci_build.sh
@@ -118,6 +118,7 @@ function cmake_opencl {
         -DLITE_WITH_LIGHT_WEIGHT_FRAMEWORK=ON \
         -DWITH_TESTING=ON \
         -DLITE_BUILD_EXTRA=ON \
+        -DLITE_WITH_CV=OFF \
         -DARM_TARGET_OS=$1 -DARM_TARGET_ARCH_ABI=$2 -DARM_TARGET_LANG=$3
 }
 
@@ -189,7 +190,7 @@ function build_opencl {
     prepare_opencl_source_code $cur_dir $build_dir
 
     cmake_opencl ${os} ${abi} ${lang}
-    make opencl_clhpp
+    make opencl_clhpp -j$NUM_CORES_FOR_COMPILE
     build $TESTS_FILE
 
     # test publish inference lib
