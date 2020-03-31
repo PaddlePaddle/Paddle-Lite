845c0a45c7569372eee5f26959b08ecd8a3969a8
xiebaiyuan <xiebaiyuan@139.com>
2020-03-31 18:17:51 +0800

    [LITE][OPENCL][Image] add lws turn &  close cl check when shutdownlog , test=develop

--- lite/backends/opencl/cl_context.h
+++ lite/backends/opencl/cl_context.h
@@ -48,8 +48,8 @@ class CLContext {
   cl::NDRange LocalWorkSizeTurn(cl::NDRange global_work_size,
                                 size_t max_work_size,
                                 int divitor = 2);
-  cl::NDRange LocalWorkSizeConv1x1(cl::NDRange global_work_size,
-                                   size_t max_work_size);
+  //  cl::NDRange LocalWorkSizeConv1x1(cl::NDRange global_work_size,
+  //                                   size_t max_work_size);
 
  private:
   std::unordered_map<std::string, std::unique_ptr<cl::Program>> programs_;
