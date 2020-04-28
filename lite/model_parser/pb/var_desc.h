059fa0cc51340ef705032877604b6944a6895b1d
DannyIsFunny <912790387@qq.com>
2020-04-28 12:59:13 +0000

    test=develop

--- lite/model_parser/pb/var_desc.h
+++ lite/model_parser/pb/var_desc.h
@@ -84,7 +84,7 @@ class VarDesc : public VarDescAPI {
 
   std::vector<std::vector<int64_t>> GetShapes() const;
 
-  void SetDataType(framework::proto::VarType::Type data_type);
+  void SetDataType(VarDescAPI::VarDataType data_type);
 
   void SetDataTypes(
       const std::vector<framework::proto::VarType::Type> &multiple_data_type);
