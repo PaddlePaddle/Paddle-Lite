5fdbc37eba2423d29df882383ad25c0fd2c499f2
huzhiqiang <912790387@qq.com>
2020-05-18 20:22:12 +0800

    [Python Compiling] Add option for python version (#3654)

--- lite/tools/build.sh
+++ lite/tools/build.sh
@@ -395,6 +395,7 @@ function make_x86 {
             -DLITE_WITH_XTCL=$BUILD_XTCL \
             -DXPU_SDK_ROOT=$XPU_SDK_ROOT \
             -DCMAKE_BUILD_TYPE=Release \
+            -DPY_VERSION=$PY_VERSION \
             $PYTHON_EXECUTABLE_OPTION
 
   make publish_inference -j$NUM_PROC
@@ -528,6 +529,10 @@ function main {
                 ;;
             --python_executable=*)
                 PYTHON_EXECUTABLE_OPTION="-DPYTHON_EXECUTABLE=${i#*=}"
+                shift
+                ;;
+            --python_version=*)
+                PY_VERSION="${i#*=}"
                 shift
                 ;;
             --build_apu=*)
