a8ee1095a503735085e673bd8b6e4e91d76eefae
DannyIsFunny <912790387@qq.com>
2020-05-21 11:18:03 +0000

    test=develop

--- lite/core/program.cc
+++ lite/core/program.cc
@@ -157,8 +157,8 @@ void RuntimeProgram::UpdateVarsOfProgram(cpp::ProgramDesc* desc) {
         if (type->IsTensor()) {
           auto tensor = scope->FindVar(out_name)->GetMutable<Tensor>();
           v->SetPersistable(tensor->persistable());
-          if (in_name != "feed" && in_name != "fetch") {
-            LOG(INFO) << "update var" << in_name;
+          if (out_name != "feed" && out_name != "fetch") {
+            LOG(INFO) << "update var" << out_name;
             v->SetShape(tensor->dims().data());
             switch (tensor->precision()) {
 #define SET_DATATYPE(precision__, data_type)                    \
@@ -177,174 +177,175 @@ void RuntimeProgram::UpdateVarsOfProgram(cpp::ProgramDesc* desc) {
               default:
                 LOG(WARNING) << "unknown precision type";
             }
-          } else {
-            CHECK(false) << "unsupported var type";
-          }
-        }
-      }
-    }
-  }
-  void RuntimeProgram::Run() {
-#ifdef LITE_WITH_PRECISION_PROFILE
-    auto inst_precision_profiler = paddle::lite::profile::PrecisionProfiler();
-    std::string precision_profiler_summary =
-        inst_precision_profiler.GetSummaryHeader();
-#endif
-
-    for (auto& inst : instructions_) {
-#ifndef LITE_WITH_FPGA
-      if (inst.is_feed_fetch_op()) continue;
-#endif
-#ifdef LITE_WITH_CUDA
-      if (inst.need_sync()) {
-        inst.Sync();
-      }
-#endif
-      inst.Run();
-#ifdef LITE_WITH_PRECISION_PROFILE
-#ifndef LITE_WITH_FPGA
-      precision_profiler_summary +=
-          inst_precision_profiler.GetInstPrecision(&inst);
-#endif
-#endif  // LITE_WITH_PRECISION_PROFILE
-    }
-#ifdef LITE_WITH_PROFILE
-    LOG(INFO) << "\n" << profiler_.Summary(profile::Type::kDispatch, false, 0);
-#endif
-#ifdef LITE_WITH_PRECISION_PROFILE
-    LOG(INFO) << "\n" << precision_profiler_summary;
-#endif
-  }
-
-  void Program::Build(const cpp::ProgramDesc& prog) {
-    CHECK(ops_.empty()) << "Executor duplicate Build found";
-
-    // Create operators.
-    auto program = prog;
-    CHECK(program.BlocksSize());
-    auto& main_block = *program.GetBlock<cpp::BlockDesc>(0);
-    for (size_t i = 0; i < main_block.OpsSize(); ++i) {
-      auto& op_desc = *main_block.GetOp<cpp::OpDesc>(i);
-      auto op_type = op_desc.Type();
-      // if (op_type == "feed" || op_type == "fetch") continue;
-      VLOG(4) << "create Op [" << op_type << "]";
-      auto op = LiteOpRegistry::Global().Create(op_type);
-      CHECK(op) << "no Op found for " << op_type;
-      if (op_type == "while" || op_type == "conditional_block" ||
-          op_type == "subgraph") {
-        auto sub_block_idx = op_desc.GetAttr<int32_t>("sub_block");
-        CHECK(sub_block_idx >= 0 && sub_block_idx < program.BlocksSize())
-            << "Invalid attribute sub_block(" << sub_block_idx << ") for "
-            << op_type;
-        auto sub_block_desc =
-            const_cast<cpp::ProgramDesc&>(prog).GetBlock<cpp::BlockDesc>(
-                sub_block_idx);
-        CHECK(sub_block_desc);
-        if (op_type == "while") {
-          static_cast<operators::WhileOpLite*>(op.get())->SetSubBlock(
-              sub_block_desc);
-        } else if (op_type == "conditional_block") {
-          static_cast<operators::ConditionalBlockOpLite*>(op.get())
-              ->SetSubBlock(sub_block_desc);
-        } else if (op_type == "subgraph") {
-          static_cast<operators::SubgraphOp*>(op.get())->SetSubBlock(
-              sub_block_desc);
-        }
-      }
-      ops_.emplace_back(std::move(op));
-      ops_.back()->Attach(op_desc, exec_scope_);
-    }
-  }
-
-  void Program::PrepareWorkspace(const cpp::ProgramDesc& prog) {
-    CHECK(!exec_scope_) << "Duplicate PrepareWorkspace found";
-    exec_scope_ = &scope_->NewScope();
-    // Create Feed and Fetch var.
-    scope_->Var("feed")->GetMutable<std::vector<lite::Tensor>>();
-    scope_->Var("fetch")->GetMutable<std::vector<lite::Tensor>>();
-    tmp_vars_.push_back("feed");
-    tmp_vars_.push_back("fetch");
-
-    auto VarPrecision2KernlPrecision =
-        [](const lite::VarDescAPI::Type& type) -> PrecisionType {
-      switch (type) {
-        case lite::VarDescAPI::Type::FP32:
-          return PRECISION(kFloat);
-        case lite::VarDescAPI::Type::FP16:
-          return PRECISION(kFP16);
-        case lite::VarDescAPI::Type::INT8:
-          return PRECISION(kInt8);
-        case lite::VarDescAPI::Type::INT16:
-          return PRECISION(kInt16);
-        case lite::VarDescAPI::Type::INT32:
-          return PRECISION(kInt32);
-        case lite::VarDescAPI::Type::INT64:
-          return PRECISION(kInt64);
-        default:
-          // LOG(FATAL) << "not supported type: " << static_cast<int>(type);
-          return PRECISION(kUnk);
-      }
-    };
-
-    auto program = prog;
-    CHECK(program.BlocksSize());
-    for (size_t b = 0; b < program.BlocksSize(); ++b) {
-      auto& main_block = *program.GetBlock<cpp::BlockDesc>(b);
-      for (size_t i = 0; i < main_block.VarsSize(); ++i) {
-        auto& var_desc = *main_block.GetVar<cpp::VarDesc>(i);
-        if (!var_desc.Persistable()) {
-          if (var_desc.GetType() == lite::VarDescAPI::Type::LOD_TENSOR &&
-              VarPrecision2KernlPrecision(var_desc.GetDataType()) !=
-                  PRECISION(kUnk)) {
-            var_data_type_[var_desc.Name()] =
-                VarPrecision2KernlPrecision(var_desc.GetDataType());
-          }
-          tmp_vars_.push_back(var_desc.Name());
-          VLOG(4) << "var name: " << var_desc.Name() << " type is "
-                  << static_cast<int>(var_desc.GetType()) << " data type is "
-                  << static_cast<int>(var_desc.GetDataType());
-          exec_scope_->Var(var_desc.Name());
-          if (b > 0) {
-            VLOG(4) << "var: " << var_desc.Name();
           }
         } else {
-          if (var_desc.Name() == "feed" || var_desc.Name() == "fetch") continue;
-          weights_.push_back(var_desc.Name());
-          if (var_desc.Persistable()) scope_->Var(var_desc.Name());
+          CHECK(false) << "unsupported var type";
         }
       }
     }
   }
-
-  void Instruction::Run() {
-#ifdef LITE_WITH_PROFILE
-    CHECK(profiler_) << "Profiler pointer of kernel can not be nullptr. "
-                        "When LITE_WITH_PROFILE is defined, please set a "
-                        "Profiler for Instruction.";
-    profiler_->StartTiming(
-        profile::Type::kCreate, profile_id_, kernel_->mutable_context());
+}
+void RuntimeProgram::Run() {
+#ifdef LITE_WITH_PRECISION_PROFILE
+  auto inst_precision_profiler = paddle::lite::profile::PrecisionProfiler();
+  std::string precision_profiler_summary =
+      inst_precision_profiler.GetSummaryHeader();
 #endif
-    CHECK(op_) << "op null";
-    CHECK(kernel_) << "kernel null";
 
-    if (first_epoch_) {
-      first_epoch_ = false;
-      CHECK(op_->CheckShape());
+  for (auto& inst : instructions_) {
+#ifndef LITE_WITH_FPGA
+    if (inst.is_feed_fetch_op()) continue;
+#endif
+#ifdef LITE_WITH_CUDA
+    if (inst.need_sync()) {
+      inst.Sync();
     }
+#endif
+    inst.Run();
+#ifdef LITE_WITH_PRECISION_PROFILE
+#ifndef LITE_WITH_FPGA
+    precision_profiler_summary +=
+        inst_precision_profiler.GetInstPrecision(&inst);
+#endif
+#endif  // LITE_WITH_PRECISION_PROFILE
+  }
+#ifdef LITE_WITH_PROFILE
+  LOG(INFO) << "\n" << profiler_.Summary(profile::Type::kDispatch, false, 0);
+#endif
+#ifdef LITE_WITH_PRECISION_PROFILE
+  LOG(INFO) << "\n" << precision_profiler_summary;
+#endif
+}
 
-    if (op_->run_once() && has_run_) {
-      return;
+void Program::Build(const cpp::ProgramDesc& prog) {
+  CHECK(ops_.empty()) << "Executor duplicate Build found";
+
+  // Create operators.
+  auto program = prog;
+  CHECK(program.BlocksSize());
+  auto& main_block = *program.GetBlock<cpp::BlockDesc>(0);
+  for (size_t i = 0; i < main_block.OpsSize(); ++i) {
+    auto& op_desc = *main_block.GetOp<cpp::OpDesc>(i);
+    auto op_type = op_desc.Type();
+    // if (op_type == "feed" || op_type == "fetch") continue;
+    VLOG(4) << "create Op [" << op_type << "]";
+    auto op = LiteOpRegistry::Global().Create(op_type);
+    CHECK(op) << "no Op found for " << op_type;
+    if (op_type == "while" || op_type == "conditional_block" ||
+        op_type == "subgraph") {
+      auto sub_block_idx = op_desc.GetAttr<int32_t>("sub_block");
+      CHECK(sub_block_idx >= 0 && sub_block_idx < program.BlocksSize())
+          << "Invalid attribute sub_block(" << sub_block_idx << ") for "
+          << op_type;
+      auto sub_block_desc =
+          const_cast<cpp::ProgramDesc&>(prog).GetBlock<cpp::BlockDesc>(
+              sub_block_idx);
+      CHECK(sub_block_desc);
+      if (op_type == "while") {
+        static_cast<operators::WhileOpLite*>(op.get())->SetSubBlock(
+            sub_block_desc);
+      } else if (op_type == "conditional_block") {
+        static_cast<operators::ConditionalBlockOpLite*>(op.get())->SetSubBlock(
+            sub_block_desc);
+      } else if (op_type == "subgraph") {
+        static_cast<operators::SubgraphOp*>(op.get())->SetSubBlock(
+            sub_block_desc);
+      }
     }
+    ops_.emplace_back(std::move(op));
+    ops_.back()->Attach(op_desc, exec_scope_);
+  }
+}
 
-    op_->InferShape();
-    kernel_->Launch();
-    has_run_ = true;
+void Program::PrepareWorkspace(const cpp::ProgramDesc& prog) {
+  CHECK(!exec_scope_) << "Duplicate PrepareWorkspace found";
+  exec_scope_ = &scope_->NewScope();
+  // Create Feed and Fetch var.
+  scope_->Var("feed")->GetMutable<std::vector<lite::Tensor>>();
+  scope_->Var("fetch")->GetMutable<std::vector<lite::Tensor>>();
+  tmp_vars_.push_back("feed");
+  tmp_vars_.push_back("fetch");
+
+  auto VarPrecision2KernlPrecision =
+      [](const lite::VarDescAPI::Type& type) -> PrecisionType {
+    switch (type) {
+      case lite::VarDescAPI::Type::FP32:
+        return PRECISION(kFloat);
+      case lite::VarDescAPI::Type::FP16:
+        return PRECISION(kFP16);
+      case lite::VarDescAPI::Type::INT8:
+        return PRECISION(kInt8);
+      case lite::VarDescAPI::Type::INT16:
+        return PRECISION(kInt16);
+      case lite::VarDescAPI::Type::INT32:
+        return PRECISION(kInt32);
+      case lite::VarDescAPI::Type::INT64:
+        return PRECISION(kInt64);
+      default:
+        // LOG(FATAL) << "not supported type: " << static_cast<int>(type);
+        return PRECISION(kUnk);
+    }
+  };
+
+  auto program = prog;
+  CHECK(program.BlocksSize());
+  for (size_t b = 0; b < program.BlocksSize(); ++b) {
+    auto& main_block = *program.GetBlock<cpp::BlockDesc>(b);
+    for (size_t i = 0; i < main_block.VarsSize(); ++i) {
+      auto& var_desc = *main_block.GetVar<cpp::VarDesc>(i);
+      if (!var_desc.Persistable()) {
+        if (var_desc.GetType() == lite::VarDescAPI::Type::LOD_TENSOR &&
+            VarPrecision2KernlPrecision(var_desc.GetDataType()) !=
+                PRECISION(kUnk)) {
+          var_data_type_[var_desc.Name()] =
+              VarPrecision2KernlPrecision(var_desc.GetDataType());
+        }
+        tmp_vars_.push_back(var_desc.Name());
+        VLOG(4) << "var name: " << var_desc.Name() << " type is "
+                << static_cast<int>(var_desc.GetType()) << " data type is "
+                << static_cast<int>(var_desc.GetDataType());
+        exec_scope_->Var(var_desc.Name());
+        if (b > 0) {
+          VLOG(4) << "var: " << var_desc.Name();
+        }
+      } else {
+        if (var_desc.Name() == "feed" || var_desc.Name() == "fetch") continue;
+        weights_.push_back(var_desc.Name());
+        if (var_desc.Persistable()) scope_->Var(var_desc.Name());
+      }
+    }
+  }
+}
+
+void Instruction::Run() {
+#ifdef LITE_WITH_PROFILE
+  CHECK(profiler_) << "Profiler pointer of kernel can not be nullptr. "
+                      "When LITE_WITH_PROFILE is defined, please set a "
+                      "Profiler for Instruction.";
+  profiler_->StartTiming(
+      profile::Type::kCreate, profile_id_, kernel_->mutable_context());
+#endif
+  CHECK(op_) << "op null";
+  CHECK(kernel_) << "kernel null";
+
+  if (first_epoch_) {
+    first_epoch_ = false;
+    CHECK(op_->CheckShape());
   }
 
-  STL::ostream& operator<<(STL::ostream& os, const Instruction& other) {
-    os << other.kernel_->summary() << "\t(" << other.kernel_->doc() << ")";
-    return os;
+  if (op_->run_once() && has_run_) {
+    return;
   }
+
+  op_->InferShape();
+  kernel_->Launch();
+  has_run_ = true;
+}
+
+STL::ostream& operator<<(STL::ostream& os, const Instruction& other) {
+  os << other.kernel_->summary() << "\t(" << other.kernel_->doc() << ")";
+  return os;
+}
 
 }  // namespace lite
 }  // namespace paddle
