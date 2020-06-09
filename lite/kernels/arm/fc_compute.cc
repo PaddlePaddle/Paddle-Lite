82f5f4399f6df675d63413c9b31bafc03daee4d1
DannyIsFunny <912790387@qq.com>
2020-06-09 07:00:51 +0000

    test=develop

--- lite/kernels/arm/fc_compute.cc
+++ lite/kernels/arm/fc_compute.cc
@@ -88,7 +88,7 @@ void FcCompute<PRECISION(kFloat), PRECISION(kFloat)>::Run() {
 
   auto i_data = param.input->data<float>();
   auto o_data = param.output->mutable_data<float>();
-  auto w_data = flag_gemm_ ? param.w->data<float>() : weights_.data<float>();
+  auto w_data = param.w->data<float>();
   const float* b_data = param.bias ? param.bias->data<float>() : nullptr;
   if (flag_trans_bias_) {
     b_data = bias_.data<float>();
@@ -130,7 +130,7 @@ void FcCompute<PRECISION(kFloat), PRECISION(kFloat)>::Run() {
       lite::arm::math::sgemv(w_data,
                              i_data_batch,
                              o_data_batch,
-                             false,
+                             true,
                              n_,
                              k_,
                              param.bias != nullptr,
@@ -149,8 +149,7 @@ void FcCompute<PRECISION(kInt8), PRECISION(kFloat)>::Run() {
 
   auto i_data = param.input->data<int8_t>();
   auto o_data = param.output->mutable_data<float>();
-  auto w_data =
-      flag_trans_weights_ ? weights_.data<int8_t>() : param.w->data<int8_t>();
+  auto w_data = param.w->data<int8_t>();
   const float* b_data = param.bias ? param.bias->data<float>() : nullptr;
   if (flag_trans_bias_) {
     b_data = bias_.data<float>();
@@ -188,7 +187,7 @@ void FcCompute<PRECISION(kInt8), PRECISION(kFloat)>::Run() {
       lite::arm::math::gemv_int8(w_data,
                                  i_data_batch,
                                  o_data_batch,
-                                 false,
+                                 true,
                                  n_,
                                  k_,
                                  scale_.data(),
@@ -208,8 +207,7 @@ void FcCompute<PRECISION(kInt8), PRECISION(kInt8)>::Run() {
 
   auto i_data = param.input->data<int8_t>();
   auto o_data = param.output->mutable_data<int8_t>();
-  auto w_data =
-      flag_trans_weights_ ? weights_.data<int8_t>() : param.w->data<int8_t>();
+  auto w_data = param.w->data<int8_t>();
   const float* b_data = param.bias ? param.bias->data<float>() : nullptr;
   if (flag_trans_bias_) {
     b_data = bias_.data<float>();
@@ -247,7 +245,7 @@ void FcCompute<PRECISION(kInt8), PRECISION(kInt8)>::Run() {
       lite::arm::math::gemv_int8(w_data,
                                  i_data_batch,
                                  o_data_batch,
-                                 false,
+                                 true,
                                  n_,
                                  k_,
                                  scale_.data(),
