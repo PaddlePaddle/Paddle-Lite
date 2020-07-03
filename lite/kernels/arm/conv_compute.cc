3137f18b0154e6faf4d79288c1dde3c501392dda
HappyAngel <chenjiaobuaa@126.com>
2020-07-03 02:14:41 -0500

    [cherry-pick] fix xiaodu crash and profiler (#3859)
    * [arm]add 2x2s2p1 pooling  (#3705)
    
    * fix pooling bug and speed
    
    * add 2x2s2p1 pooling. test=develop
    
    * fix conflict, test=develop
    
    * fix conflict in wino
    
    * [arm] add 3x3s1 Winograd int8 (#3767)
    
    * fix: winograd support unsame pad
    test=develop
    
    * feat: add winograd int8 kernel
    test=develop
    
    * fix winograd_int8 ut sgement default. test=develop
    
    * close basic_test, test=develop
    
    Co-authored-by: MyPandaShaoxiang <txg4794@163.com>
    
    * fix xiaodu crash in gemm prepacked
    
    * in huwen phone, 3x3s2p0 avg pooling will rand crash, other phone does not have this feature
    
    * [arm] update con int8 kernel choose (#3834)
    
    * fix conv int8 kernel choose and sooftmax compute bug
    
    * change axis_size = 4 kernel choose, test=develop
    
    * fix wino_int8 computte erroor. test=develop

--- lite/kernels/arm/conv_compute.cc
+++ lite/kernels/arm/conv_compute.cc
@@ -73,7 +73,6 @@ void ConvCompute<PRECISION(kFloat), PRECISION(kFloat)>::PrepareForRun() {
     // VLOG(3) << "invoking dw conv";
   } else if (param.groups == 1 && kw == 3 && stride == 1 && ks_equal &&
              no_dilation) {
-    // TODO(MyPandaShaoxiang): winograd conv support any pad
     impl_ = new WinogradConv<PRECISION(kFloat), PRECISION(kFloat)>;
     // VLOG(3) << "invoking winograd conv";
   } else if (param.groups == 1 && kw == 3 && stride == 2 &&
@@ -122,10 +121,14 @@ void ConvCompute<PRECISION(kInt8), PRECISION(kFloat)>::PrepareForRun() {
       no_dilation && flag_dw) {
     impl_ = new DepthwiseConv<PRECISION(kInt8), PRECISION(kFloat)>;
     // VLOG(3) << "Run DepthwiseConv Int8";
-  } else if (param.groups == 1 && kw == 3 && (sw == 1 || sw == 2) &&
-             ic * oc < 4 * hin * win && kps_equal && no_dilation) {
+  } else if (param.groups == 1 && kw == 3 && sw == 2 && no_dilation &&
+             pads_equal) {
     impl_ = new DirectConv<PRECISION(kInt8), PRECISION(kFloat)>;
     // VLOG(3) << "Run DirectConv Int8";
+  } else if (param.groups == 1 && kw == 3 && sw == 1 && no_dilation &&
+             pads_equal) {
+    impl_ = new WinogradConv<PRECISION(kInt8), PRECISION(kFloat)>;
+    // VLOG(3) << "Run WinogradConv Int8";
   } else {
     impl_ = new GemmLikeConv<PRECISION(kInt8), PRECISION(kFloat)>;
     // VLOG(3) << "Run GemmLikeConvInt8";
@@ -169,10 +172,14 @@ void ConvCompute<PRECISION(kInt8), PRECISION(kInt8)>::PrepareForRun() {
       no_dilation && flag_dw) {
     impl_ = new DepthwiseConv<PRECISION(kInt8), PRECISION(kInt8)>;
     // VLOG(3) << "Run DepthwiseConv Int8";
-  } else if (param.groups == 1 && kw == 3 && (sw == 1 || sw == 2) &&
-             ic * oc < 4 * hin * win && kps_equal && no_dilation) {
+  } else if (param.groups == 1 && kw == 3 && sw == 2 && no_dilation &&
+             pads_equal) {
     impl_ = new DirectConv<PRECISION(kInt8), PRECISION(kInt8)>;
     // VLOG(3) << "Run DirectConv Int8";
+  } else if (param.groups == 1 && kw == 3 && sw == 1 && no_dilation &&
+             pads_equal) {
+    impl_ = new WinogradConv<PRECISION(kInt8), PRECISION(kInt8)>;
+    // VLOG(3) << "Run WinogradConv Int8";
   } else {
     impl_ = new GemmLikeConv<PRECISION(kInt8), PRECISION(kInt8)>;
     // VLOG(3) << "Run GemmLikeConvInt8";
