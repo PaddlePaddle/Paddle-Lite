afe6bd01d5acb8957273c3ab03aad66254daaef0
jiweibo <jiweibo@baidu.com>
2020-07-07 09:26:47 +0000

    update code format. test=develop

--- lite/kernels/cuda/fc_compute.cu
+++ lite/kernels/cuda/fc_compute.cu
@@ -11,12 +11,12 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+#include "lite/kernels/cuda/fc_compute.h"
 
 #include <string>
 
 #include "lite/backends/cuda/cuda_utils.h"
 #include "lite/core/op_registry.h"
-#include "lite/kernels/cuda/fc_compute.h"
 
 namespace paddle {
 namespace lite {
