ba05522c5543c2405a1fb6156b612e82ffc15a94
DannyIsFunny <912790387@qq.com>
2020-07-08 05:24:42 +0000

    test=develop

--- lite/core/mir/subgraph/subgraph_pass_test.cc
+++ lite/core/mir/subgraph/subgraph_pass_test.cc
@@ -17,8 +17,6 @@
 #include <cmath>
 
 #include "lite/api/paddle_api.h"
-#include "lite/api/paddle_use_kernels.h"
-#include "lite/api/paddle_use_ops.h"
 #include "lite/api/test_helper.h"
 #include "lite/utils/cp_logging.h"
 #include "lite/utils/string.h"
