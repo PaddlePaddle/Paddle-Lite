491f7f5e4e50560d36f03f1d372f49242c0e39a1
ysh329@sina.com <ysh329@sina.com>
2020-07-13 09:51:12 +0000

    test=develop

--- lite/kernels/opencl/conv_image_compute.cc
+++ lite/kernels/opencl/conv_image_compute.cc
@@ -467,9 +467,11 @@ void ConvImageCompute::PrepareForRun() {
 void ConvImageCompute::ReInitWhenNeeded() {
   conv_param_ = param_.get_mutable<param_t>();
   auto x_dims = conv_param_->x->dims();
+#ifdef LITE_WITH_LOG
   LOG(INFO) << "is_first_epoch_for_run_:" << is_first_epoch_for_run_
             << ", last_input_dims_:" << last_input_dims_
             << ", x_dims:" << x_dims;
+#endif
 
   if (is_first_epoch_for_run_ || last_input_dims_ != x_dims) {
     is_first_epoch_for_run_ = false;
