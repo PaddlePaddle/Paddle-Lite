6e9fe911f0515a70a38e1d9b667b9e97d64300ac
jiweibo <jiweibo@baidu.com>
2020-08-03 11:23:48 +0000

    update test=develop

--- lite/kernels/cuda/softmax_compute.cu
+++ lite/kernels/cuda/softmax_compute.cu
@@ -12,6 +12,7 @@ limitations under the License. */
 #pragma once
 #include <limits>
 #include <vector>
+
 #include "lite/backends/cuda/cuda_utils.h"
 #include "lite/core/op_registry.h"
 #include "lite/kernels/cuda/softmax_compute.h"
@@ -72,6 +73,69 @@ __global__ void sharemem_softmax_kernel(int total_size,
   }
 }
 
+template <>
+__global__ void sharemem_softmax_kernel(int total_size,
+                                        const half* in_data,
+                                        half* out_data,
+                                        int inner_num,
+                                        int outer_num,
+                                        int axis_size) {
+  half* data = reinterpret_cast<half*>(tile) + threadIdx.x;
+  //! compute thread index and real data index
+  int idx = blockIdx.x * blockDim.x + threadIdx.x;
+  if (idx < total_size) {
+    int idx_inner = idx % inner_num;
+    int idx_outer = (idx / inner_num) * axis_size;
+    int blocksize = blockDim.x;
+    int real_index = idx_outer * inner_num + idx_inner;
+    int loop_idx = real_index;
+//! read all data to sharemem in softmax channel
+#pragma unroll
+    for (int i = 0; i < axis_size; ++i) {
+      data[i * blocksize] = in_data[loop_idx];
+      loop_idx += inner_num;
+    }
+    //! get maximum value in softmax channel
+    half max_data = data[0];
+#pragma unroll
+    for (int i = 1; i < axis_size; ++i) {
+      half dt = data[i * blocksize];
+#if __CUDA_ARCH__ >= 530
+      if (__hlt(max_data, dt)) {
+#else
+      if (__half2float(max_data) < __half2float(dt)) {
+#endif
+        max_data = dt;
+      }
+    }
+    //! subtract then summarize
+    half sum = 0;
+#pragma unroll
+    for (int i = 0; i < axis_size; ++i) {
+      half* dt = data + i * blocksize;
+#if __CUDA_ARCH__ >= 530
+      *dt = hexp(__hsub(*dt, max_data));
+      sum = __hadd(sum, *dt);
+#else
+      *dt = __float2half(expf(__half2float(*dt) - __half2float(max_data)));
+      sum = __float2half(__half2float(sum) + __half2float(*dt));
+#endif
+    }
+    //! write back result
+    loop_idx = real_index;
+#pragma unroll
+    for (int i = 0; i < axis_size; ++i) {
+#if __CUDA_ARCH__ >= 530
+      out_data[loop_idx] = __hdiv(data[i * blocksize], sum);
+#else
+      out_data[loop_idx] =
+          __float2half(__half2float(data[i * blocksize]) / __half2float(sum));
+#endif
+      loop_idx += inner_num;
+    }
+  }
+}
+
 //! general kernel for softmax
 template <typename dtype>
 __global__ void softmax_max_kernel(int total_size,
@@ -92,6 +156,38 @@ __global__ void softmax_max_kernel(int total_size,
     for (int i = 0; i < axis_size; ++i) {
       max_data =
           in_data[real_index] > max_data ? in_data[real_index] : max_data;
+      real_index += inner_num;
+    }
+    out_data[idx] = max_data;
+  }
+}
+
+template <>
+__global__ void softmax_max_kernel(int total_size,
+                                   const half* in_data,
+                                   half* out_data,
+                                   half min_data,
+                                   int inner_num,
+                                   int outer_num,
+                                   int axis_size) {
+  //! compute data index
+  int idx = blockIdx.x * blockDim.x + threadIdx.x;
+  if (idx < total_size) {
+    int idx_inner = idx % inner_num;
+    int idx_outer = (idx / inner_num) * axis_size;
+    int real_index = idx_outer * inner_num + idx_inner;
+    //! get maximum data across softmax axis
+    half max_data = min_data;
+    for (int i = 0; i < axis_size; ++i) {
+#if __CUDA_ARCH__ >= 530
+      max_data =
+          __hgt(in_data[real_index], max_data) ? in_data[real_index] : max_data;
+#else
+      float a = __half2float(in_data[real_index]);
+      float b = __half2float(max_data);
+      float res = a > b ? a : b;
+      max_data = __float2half(res);
+#endif
       real_index += inner_num;
     }
     out_data[idx] = max_data;
@@ -121,6 +217,44 @@ __global__ void softmax_sub_exp_sum_kernel(int total_size,
       dtype sub_data = in_data[real_index] - max_data_cur;
       sub_data = expf(sub_data);
       sum_data_cur += sub_data;
+      out_data[real_index] = sub_data;
+      real_index += inner_num;
+    }
+    sum_data[idx] = sum_data_cur;
+  }
+}
+
+template <>
+__global__ void softmax_sub_exp_sum_kernel(int total_size,
+                                           const half* in_data,
+                                           half* out_data,
+                                           const half* max_data,
+                                           half* sum_data,
+                                           int inner_num,
+                                           int outer_num,
+                                           int axis_size) {
+  //! compute data index
+  int idx = blockIdx.x * blockDim.x + threadIdx.x;
+  if (idx < total_size) {
+    int idx_inner = idx % inner_num;
+    int idx_outer = (idx / inner_num) * axis_size;
+
+    half max_data_cur = max_data[idx];
+    half sum_data_cur = 0;
+    int real_index = idx_outer * inner_num + idx_inner;
+    //! compute exp and summarize across the softmax axis
+    for (int i = 0; i < axis_size; ++i) {
+#if __CUDA_ARCH__ >= 530
+      half sub_data = __hsub(in_data[real_index], max_data_cur);
+      sub_data = hexp(sub_data);
+      sum_data_cur = __hadd(sum_data_cur, sub_data);
+#else
+      half sub_data = __float2half(__half2float(in_data[real_index]) -
+                                   __half2float(max_data_cur));
+      sub_data = __float2half(expf(__half2float(sub_data)));
+      sum_data_cur =
+          __float2half(__half2float(sum_data_cur) + __half2float(sub_data));
+#endif
       out_data[real_index] = sub_data;
       real_index += inner_num;
     }
@@ -162,11 +296,20 @@ __global__ void softmax_divid_output_kernel(int total_size,
   if (idx < total_size) {
     int idx_inner = idx % inner_num;
     int idx_outer = (idx / inner_num) * axis_size;
+#if __CUDA_ARCH__ >= 530
     half sum_data_cur = __hdiv(__float2half(1.f), sum_data[idx]);
+#else
+    half sum_data_cur = __float2half(1.f / __half2float(sum_data[idx]));
+#endif
     int real_index = idx_outer * inner_num + idx_inner;
     //! compute final result
     for (int i = 0; i < axis_size; ++i) {
-      io_data[real_index] = io_data[real_index] * sum_data_cur;
+#if __CUDA_ARCH__ >= 530
+      io_data[real_index] = __hmul(io_data[real_index], sum_data_cur);
+#else
+      io_data[real_index] = __float2half(__half2float(io_data[real_index]) *
+                                         __half2float(sum_data_cur));
+#endif
       real_index += inner_num;
     }
   }
