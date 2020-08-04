737fedebe9286bcd10b1c2723b79b40e5e3e598e
Qi Li <selmalq@gmail.com>
2020-08-04 09:44:24 +0800

    [ASCEND] fix conv op, test=develop (#4041)

--- lite/tests/kernels/interp_compute_test.cc
+++ lite/tests/kernels/interp_compute_test.cc
@@ -451,7 +451,7 @@ TEST(Interp, precision) {
   abs_error = 1e-2;  // use fp16 in npu
 #elif defined(LITE_WITH_HUAWEI_ASCEND_NPU)
   place = TARGET(kHuaweiAscendNPU);
-  abs_error = 1e-2;  // use fp16 in npu
+  abs_error = 1e-2;  // precision_mode default is force_fp16
 #elif defined(LITE_WITH_ARM)
   place = TARGET(kARM);
 #else
