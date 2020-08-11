3a5798fb664e0af9890c68ee127626dd902e5136
Wilber <jiweibo@baidu.com>
2020-08-11 13:30:45 +0800

    [CUDA] [Fuse] Fuse relu. (#4090)

--- lite/core/mir/fusion/match_matrix_activation_fuse_pass.cc
+++ lite/core/mir/fusion/match_matrix_activation_fuse_pass.cc
@@ -1,4 +1,4 @@
-// Copyright (c) 2019 PaddlePaddle Authors. All Rights Reserved.
+// Copyright (c) 2020 PaddlePaddle Authors. All Rights Reserved.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -12,40 +12,27 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include "lite/core/mir/fusion/fc_fuse_pass.h"
+#include "lite/core/mir/fusion/match_matrix_activation_fuse_pass.h"
+
 #include <memory>
 #include <vector>
-#include "lite/core/mir/fusion/fc_fuser.h"
+
+#include "lite/core/mir/fusion/match_matrix_activation_fuser.h"
 #include "lite/core/mir/pass_registry.h"
 
 namespace paddle {
 namespace lite {
 namespace mir {
 
-void FcFusePass::Apply(const std::unique_ptr<SSAGraph>& graph) {
-#if defined(LITE_WITH_X86) || defined(LITE_WITH_CUDA)
-#ifdef LITE_WITH_MLU
-  fusion::FcFuser fuser(false);
+void MatchMatrixActFusePass::Apply(const std::unique_ptr<SSAGraph>& graph) {
+  fusion::MatchMatrixActFuser fuser("relu");
   fuser(graph.get());
-#else
-  fusion::FcFuser fuser(true);
-  fuser(graph.get());
-#endif
-#endif
-
-  fusion::FcFuser fuser2(false);
-  fuser2(graph.get());
 }
 
 }  // namespace mir
 }  // namespace lite
 }  // namespace paddle
 
-REGISTER_MIR_PASS(lite_fc_fuse_pass, paddle::lite::mir::FcFusePass)
-    .BindTargets({TARGET(kAny)})
-    .ExcludeTargets({TARGET(kXPU)})
-#if (!defined(LITE_WITH_MLU) && !defined(LITE_WITH_HUAWEI_ASCEND_NPU))
-    .ExcludeTargets({TARGET(kX86)})
-#endif
-    .ExcludeTargets({TARGET(kBM)})
-    .BindKernel("fc");
+REGISTER_MIR_PASS(lite_match_matrix_activation_fuse_pass,
+                  paddle::lite::mir::MatchMatrixActFusePass)
+    .BindTargets({TARGET(kCUDA)});
