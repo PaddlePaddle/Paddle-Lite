a4c8f9b7188312c162d03a6ca2bf8bfdb1725866
chenjiaoAngel <chenjiaobuaa@126.com>
2020-08-24 18:14:34 +0800

    fix format, test=develop

--- lite/backends/arm/math/conv5x5s1_depthwise_fp32.cc
+++ lite/backends/arm/math/conv5x5s1_depthwise_fp32.cc
@@ -4353,6 +4353,7 @@ inline void compute_all_padding_mid_leakyRelu(float* dout,
     }
     *dout++ = sum > 0.f ? sum : sum * scale[0];
   }
+// clang-format off
   if (cnt > 0) {
 #ifdef __aarch64__
     asm volatile(COMPUTE_FIVE_LINE_S1 RESULT_S1_LEAKY_RELU
@@ -4415,6 +4416,7 @@ inline void compute_all_padding_mid_leakyRelu(float* dout,
                    "q14",
                    "q15");
 #endif
+// clang-format on
     din_ptr_arr[0] -= 4;
   }
   // clang-format on
@@ -4487,6 +4489,7 @@ inline void compute_all_padding_mid_leakyRelu_out2(float* dout0,
     *dout0++ = sum > 0.f ? sum : sum * scale[0];
     *dout1++ = sum1 > 0.f ? sum1 : sum1 * scale[0];
   }
+// clang-format off
   if (cnt > 0) {
 #ifdef __aarch64__
     asm volatile(COMPUTE_FIVE_LINE_S1_OUT2 RESULT_S1_LEAKY_RELU_OUT2
@@ -4556,6 +4559,7 @@ inline void compute_all_padding_mid_leakyRelu_out2(float* dout0,
                    "q14",
                    "q15");
 #endif
+// clang-format on
     din_ptr_arr[0] -= 4;
   }
   // clang-format on
