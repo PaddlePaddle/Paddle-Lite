20c7b582f1902f23ac43a45e7654afce96650825
Wangzheee <634486483@qq.com>
2020-08-25 12:09:51 +0800

    test=develop

--- lite/backends/bm/bm_demo/ci.sh
+++ lite/backends/bm/bm_demo/ci.sh
@@ -1,13 +1,13 @@
 wget http://paddle-inference-dist.bj.bcebos.com/mobilenet_v1.tar.gz
 tar -xvf mobilenet_v1.tar.gz
 
-mkdir lib
-cp -r ./build.lite.bm/lite/api/libpaddle_full_api_shared.so \
-./third-party/bmlibs/bm_sc3_libs/lib/bmcompiler \
-./third-party/bmlibs/bm_sc3_libs/lib/bmnn/pcie ./lib
+mkdir ./lib
+cp -r /paddlelite/build.lite.bm/lite/api/libpaddle_full_api_shared.so \
+/paddlelite/third-party/bmlibs/bm_sc3_libs/lib/bmcompiler \
+/paddlelite/third-party/bmlibs/bm_sc3_libs/lib/bmnn/pcie ./lib
 
-mkdir include
-cp -r ./lite/api/paddle_place.h ./lite/api/paddle_api.h ./include
+mkdir ./include
+cp -r /paddlelite/lite/api/paddle_place.h /paddlelite/lite/api/paddle_api.h ./include
 
 mkdir ./build
 cd ./build
