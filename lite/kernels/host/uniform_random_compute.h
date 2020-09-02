46321ec6718e062900f62db6c7f1d935426acff3
Wangzheee <634486483@qq.com>
2020-09-02 21:58:23 +0800

    Update uniform_random_compute.h

--- lite/kernels/host/uniform_random_compute.h
+++ lite/kernels/host/uniform_random_compute.h
@@ -11,10 +11,11 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
+#pragma once
 #include <random>
 #include "lite/core/kernel.h"
 #include "lite/core/op_registry.h"
+
 namespace paddle {
 namespace lite {
 namespace kernels {
