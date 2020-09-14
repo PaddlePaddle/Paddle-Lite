44608749e8596077d67678637b4145d56e4fa918
chonwhite <arthurbrown@163.com>
2020-09-14 16:51:46 +0800

    ssd works

--- lite/core/mir/fpga_kernel_place_correct_pass.h
+++ lite/core/mir/fpga_kernel_place_correct_pass.h
@@ -16,7 +16,9 @@
 #include <map>
 #include <memory>
 #include <string>
+#include <utility>
 #include <vector>
+
 #include "lite/core/mir/pass.h"
 #include "lite/core/target_wrapper.h"
 
@@ -126,86 +128,26 @@ class KernelPlaceCorrectPass : public DebugPass {
         }
       }
 
-      // if (inst.op_type() == "elementwise_mul") {
-
-      //   for (auto* x_in : x->inlinks) {
-
-      //     std::string in_name = get_argname(x_in->AsArg().name,
-      //     inst.op_info()->inputs());
-      //     std::cout << "name: " << x_in->AsArg().name  << std::endl;
-      //     std::cout << "in_name: " << in_name  << std::endl;
-      //     if (in_name == "Y") {
-      //       in = x_in;
-      //       std::cout << "found y \n";
-      //       // exit(-1);
-      //     }
-      //   }
-
-      //   if ( p != PrecisionType::kFP16) {
-      //     UpdateTarget(inst, TargetType::kARM);
-      //     UpdateTensor(inst, in, out, TargetType::kARM);
-      //   }
-      // }
-
-      std::vector<TargetType> in_types;
-      std::vector<TargetType> out_types;
-      for (auto* x_in : x->inlinks) {
-        std::string node_name = x_in->AsArg().name;
-        std::string arg_name = get_argname(node_name, inst.op_info()->inputs());
-        CHECK(arg_name.size() > 0) << "can not found op arguments for node "
-                                   << node_name;
-        VLOG(4) << "-- input arg_name:" << arg_name << " "
-                << "-- node name:" << node_name;
-
-        auto type = inst.picked_kernel().GetInputDeclType(arg_name);
-        if (!x_in->AsArg().is_weight) {
-          auto p = x_in->AsArg().type->precision();
-          auto t = x_in->AsArg().type->target();
-          auto l = x_in->AsArg().type->layout();
-        }
-
-        if (!x_in->AsArg().type) {
-          need_correct_place &= false;
-        } else {
-          if (in_types.empty()) {
-            in_types.push_back(x_in->AsArg().type->target());
-          } else {
-            if (in_types[0] != x_in->AsArg().type->target()) {
-              need_correct_place &= false;
+      if (inst.op_type() == "elementwise_mul") {
+        UpdateTarget(inst, TargetType::kFPGA);
+        for (auto* in : x->inlinks) {
+          std::string in_name =
+              get_argname(in->AsArg().name, inst.op_info()->inputs());
+          if (in_name == "Y") {
+            in = in;
+            p = in->AsArg().type->precision();
+            std::unique_ptr<KernelBase> best_match;
+            for (auto& k : inst.kernels()) {
+              auto kp = k->GetInputDeclType(in_name)->precision();
+              if (kp == p) {
+                best_match = std::move(k);
+              }
             }
+            inst.kernels().clear();
+            inst.kernels().emplace_back(std::move(best_match));
+            break;
           }
         }
-      }
-
-      for (auto* x_out : x->outlinks) {
-        std::string node_name = x_out->AsArg().name;
-        std::string arg_name =
-            get_argname(node_name, inst.op_info()->outputs());
-        CHECK(arg_name.size() > 0) << "can not found op arguments for node "
-                                   << node_name << " in Inst "
-                                   << inst.op_type();
-        VLOG(4) << "-- output arg_name " << arg_name;
-
-        auto type = inst.picked_kernel().GetOutputDeclType(arg_name);
-        if (!x_out->AsArg().type) {
-          need_correct_place &= false;
-        } else {
-          if (out_types.empty()) {
-            out_types.push_back(x_out->AsArg().type->target());
-          } else {
-            if (out_types[0] != x_out->AsArg().type->target()) {
-              need_correct_place &= false;
-            }
-          }
-        }
-      }
-
-      auto this_type = inst.picked_kernel().target();
-      bool io_target_same = (in_types[0] == out_types[0]);
-      need_correct_place &= (io_target_same && (in_types[0] != this_type));
-      if (need_correct_place) {
-        // update this kernel's valid place;
-        // UpdateTarget(inst, in_types[0]);
       }
     }
   }
