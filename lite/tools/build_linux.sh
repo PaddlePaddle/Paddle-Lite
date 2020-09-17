2a27b83881069d0183330c54d17600d51f386446
Jiansong Wang <james.wjs@hotmail.com>
2020-09-17 13:47:29 +0800

    Set LITE_WITH_IMAGINATION to OFF by defaults in build_linux.sh.

--- lite/tools/build_linux.sh
+++ lite/tools/build_linux.sh
@@ -28,8 +28,8 @@ WITH_OPENCL=OFF
 WITH_ROCKCHIP_NPU=OFF
 ROCKCHIP_NPU_SDK_ROOT="$(pwd)/rknpu_ddk"  # Download RKNPU SDK from https://github.com/airockchip/rknpu_ddk.git
 # options of compiling imagination NNA lib
-WITH_IMAGINATION_NNA=ON
-IMAGINATION_NNA_SDK_ROOT="$(pwd)/imagination_nna_sdk/"
+WITH_IMAGINATION_NNA=OFF
+IMAGINATION_NNA_SDK_ROOT="$(pwd)/imagination_nna_sdk/"  # the '/' at the end is required
 # options of compiling baidu XPU lib.
 WITH_BAIDU_XPU=OFF
 BAIDU_XPU_SDK_ROOT=""
