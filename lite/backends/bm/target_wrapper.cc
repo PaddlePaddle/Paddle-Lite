c03f96128f3182af7ac6252596d1989831d55f74
anbaolei1984 <anbaolei1984@163.com>
2020-09-18 17:43:51 +0800

    create

--- lite/backends/bm/target_wrapper.cc
+++ lite/backends/bm/target_wrapper.cc
@@ -23,10 +23,12 @@ int TargetWrapperBM::device_id_ = 0;
 std::map<int, void*> TargetWrapperBM::bm_hds_;
 
 size_t TargetWrapperBM::num_devices() {
-  int count = 0;
+  int count = 1;
+#if 0
   bm_status_t ret = bm_dev_getcount(&count);
   CHECK_EQ(ret, BM_SUCCESS) << "Failed with error code: "
                             << static_cast<int>(ret);
+#endif
   return count;
 }
 
@@ -36,6 +38,7 @@ void TargetWrapperBM::SetDevice(int id) {
     LOG(FATAL) << "Failed with invalid device id " << id;
   }
   device_id_ = id;
+#if 0
   if (bm_hds_.find(id) == bm_hds_.end()) {
     bm_handle_t bm_handle;
     bm_status_t ret = bm_dev_request(&bm_handle, id);
@@ -43,6 +46,7 @@ void TargetWrapperBM::SetDevice(int id) {
                               << static_cast<int>(ret);
     bm_hds_.insert(std::pair<int, bm_handle_t>(id, bm_handle));
   }
+#endif
   return;
 }
 
