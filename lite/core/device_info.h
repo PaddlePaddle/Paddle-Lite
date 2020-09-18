26acf380a2fd61b385c7ee1710f2cb381bd26029
DannyIsFunny <912790387@qq.com>
2020-09-18 14:20:03 +0000

    test=develop

--- lite/core/device_info.h
+++ lite/core/device_info.h
@@ -67,11 +67,11 @@ class DeviceInfo {
   int l3_cache_size() const { return L3_cache_[active_ids_[0]]; }
   // Methods for allocating L3Cache on Arm platform
   // enum class L3CacheSetMethod {
-  //  kDeviceL3Cache = 0, // Use the system L3 Cache size, best performancei.
-  //  kDeviceL2Cache = 1, // Use the system L2 Cache size, trade off performance
-  //                      // with less memory consumption.
-  //  kAbsolute = 2,      // Use the external setting.
-  //  // kAutoGrow = 3,   // Not supported yet, least memory consumption.
+  //  kDeviceL3Cache = 0, use the system L3 Cache size, best performancei.
+  //  kDeviceL2Cache = 1, use the system L2 Cache size, trade off performance
+  //                      with less memory consumption.
+  //  kAbsolute = 2,      use the external setting.
+  //  // kAutoGrow = 3,   not supported yet, least memory consumption.
   // };
   void SetArmL3CacheSize(int method = 0, int absolute_val = -1) {
     l3_cache_method_ = method;
