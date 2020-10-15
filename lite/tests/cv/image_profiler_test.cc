be06ebb72bba378a38f05d2df0812f0b90a99db3
chenjiaoAngel <chenjiaobuaa@126.com>
2020-10-15 17:36:06 +0800

    open basic test. test=develop

--- lite/tests/cv/image_profiler_test.cc
+++ lite/tests/cv/image_profiler_test.cc
@@ -26,7 +26,7 @@ DEFINE_int32(cluster, 3, "cluster id");
 DEFINE_int32(threads, 1, "threads num");
 DEFINE_int32(warmup, 0, "warmup times");
 DEFINE_int32(repeats, 10, "repeats times");
-DEFINE_bool(basic_test, false, "do all tests");
+DEFINE_bool(basic_test, true, "do all tests");
 DEFINE_bool(check_result, true, "check the result");
 
 DEFINE_int32(srcFormat, 12, "input image format NV12");
