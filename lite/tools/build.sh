bf36d80eb08aac48e1127da178344227f961c9f7
dingweihao <leo_ding30@163.com>
2020-10-19 14:01:33 +0000

    add fp32 4x4_3x3 winograd and add lto link option to speed up compile time

--- lite/tools/build.sh
+++ lite/tools/build.sh
@@ -275,7 +275,7 @@ function make_all_tests {
   cmake $root_dir \
       ${CMAKE_COMMON_OPTIONS} \
       -DWITH_TESTING=ON \
-      -DLITE_WITH_PROFILE=OFF \
+      -DLITE_WITH_PROFILE=${WITH_PROFILE} \
       -DLITE_WITH_LTO=${WITH_LTO} \
       -DLITE_WITH_PRECISION_PROFILE=OFF \
       -DLITE_BUILD_EXTRA=$BUILD_EXTRA \
