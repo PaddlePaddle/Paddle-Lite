cee709751f01d57ef62920ef5d193fbb5f8324f9
石晓伟 <39303645+Shixiaowei02@users.noreply.github.com>
2020-10-21 13:47:43 +0800

    add a move assignment buffer api, test=develop (#4531) (#4565)
    * new ModelBuffer, test=develop
    
    * fix bugs, test=develop
    
    * rename the model buffer, test=develop
    
    * not release the model buffer, test=develop
    
    * fix the ignored-qualifiers warning, test=develop
    
    * rename ModelBuffer to CxxModelBuffer, test=develop

--- lite/api/paddle_api.cc
+++ lite/api/paddle_api.cc
@@ -279,6 +279,40 @@ void ConfigBase::set_threads(int threads) {
 #endif
 }
 
+CxxModelBuffer::CxxModelBuffer(const char *program_buffer,
+                               size_t program_buffer_size,
+                               const char *params_buffer,
+                               size_t params_buffer_size) {
+  program_ = std::string(program_buffer, program_buffer + program_buffer_size);
+  params_ = std::string(params_buffer, params_buffer + params_buffer_size);
+}
+
+CxxModelBuffer::CxxModelBuffer(std::string &&program_buffer,
+                               std::string &&params_buffer) {
+  program_ = std::forward<std::string>(program_buffer);
+  params_ = std::forward<std::string>(params_buffer);
+}
+
+const std::string &CxxModelBuffer::get_program() const {
+  CHECK(!program_.empty());
+  return program_;
+}
+
+const std::string &CxxModelBuffer::get_params() const {
+  CHECK(!params_.empty());
+  return params_;
+}
+
+bool CxxModelBuffer::is_empty() const {
+  CHECK(program_.empty() == params_.empty());
+  return program_.empty();
+}
+
+const CxxModelBuffer &CxxConfig::get_model_buffer() const {
+  CHECK(model_buffer_) << "Cannot get an empty model buffer.";
+  return *model_buffer_;
+}
+
 #ifdef LITE_WITH_MLU
 void CxxConfig::set_mlu_core_version(lite_api::MLUCoreVersion core_version) {
   mlu_core_version_ = core_version;
