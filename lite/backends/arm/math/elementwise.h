8f770319ebf299cd7a8fdeda40afb4bbc2d359a3
juncaipeng <13006307475@163.com>
2020-10-21 12:11:11 +0000

    add sin cos elementwise_pow, test=develop

--- lite/backends/arm/math/elementwise.h
+++ lite/backends/arm/math/elementwise.h
@@ -263,6 +263,13 @@ template <typename T>
 void elementwise_mod_broadcast(
     const T* dinx, const T* diny, T* dout, int batch, int channels, int num);
 
+template <typename T>
+void elementwise_pow(const T* dinx, const T* diny, T* dout, int num);
+
+template <typename T>
+void elementwise_pow_broadcast(
+    const T* dinx, const T* diny, T* dout, int batch, int channels, int num);
+
 }  // namespace math
 }  // namespace arm
 }  // namespace lite
---
+++ lite/backends/arm/math/sin.cc
@@ -0,0 +1,34 @@
+// Copyright (c) 2019 PaddlePaddle Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "lite/backends/arm/math/sin.h"
+#include <algorithm>
+#include <cmath>
+#include "lite/backends/arm/math/funcs.h"
+
+namespace paddle {
+namespace lite {
+namespace arm {
+namespace math {
+
+void sin_func(const float* input, float* output, const int num) {
+  for (int n = 0; n < num; n++) {
+    output[n] = sin(input[n]);
+  }
+}
+
+}  // namespace math
+}  // namespace arm
+}  // namespace lite
+}  // namespace paddle
