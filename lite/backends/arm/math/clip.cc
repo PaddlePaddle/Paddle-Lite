a49c6f12895e27c8cab72d0417b9601f97bc004c
dingweihao <leo_ding30@163.com>
2020-10-22 09:24:36 +0000

    [ARM] optimize clip op with arm neon intrinsic code,test=develop

--- lite/backends/arm/math/clip.cc
+++ lite/backends/arm/math/clip.cc
@@ -26,13 +26,13 @@ namespace math {
 
 void clip_kernel_fp32(
     const float* input, int64_t num, float min, float max, float* output) {
+  const float* din_ptr = input;
+  float* dout_ptr = output;
   int64_t cnt = num >> 4;
   int remain = num % 16;
   float32x4_t max_val = vdupq_n_f32(max);
   float32x4_t min_val = vdupq_n_f32(min);
   for (int64_t n = 0; n < cnt; n++) {
-    const float* din_ptr = input + (n << 4);
-    float* dout_ptr = output + (n << 4);
     float32x4_t tmp0 =
         vminq_f32(vmaxq_f32(vld1q_f32(din_ptr), min_val), max_val);
     float32x4_t tmp1 =
@@ -45,9 +45,9 @@ void clip_kernel_fp32(
     vst1q_f32(dout_ptr + 4, tmp1);
     vst1q_f32(dout_ptr + 8, tmp2);
     vst1q_f32(dout_ptr + 12, tmp3);
+    din_ptr += 16;
+    dout_ptr += 16;
   }
-  const float* din_ptr = input + (cnt << 4);
-  float* dout_ptr = output + (cnt << 4);
   for (int i = 0; i < remain; i++) {
     float tmp = din_ptr[i] > min ? din_ptr[i] : min;
     dout_ptr[i] = tmp < max ? tmp : max;
