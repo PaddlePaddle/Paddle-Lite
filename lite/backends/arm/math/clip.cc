5759e3657684813061c3a88758f155f50628c8e2
Leonardo-Ding <48182083+Leonardo-Ding@users.noreply.github.com>
2020-10-26 15:50:21 +0800

    [ARM] optimize clip op with arm neon intrinsic code (#4583)
    * [ARM] optimize clip op with arm neon intrinsic code
    
    * [ARM] optimize clip op with arm neon intrinsic code,test=develop

--- lite/backends/arm/math/clip.cc
+++ lite/backends/arm/math/clip.cc
@@ -26,13 +26,31 @@ namespace math {
 
 void clip_kernel_fp32(
     const float* input, int64_t num, float min, float max, float* output) {
-  float tmp;
-  for (int64_t i = 0; i < num; i++) {
-    tmp = *input;
-    tmp = tmp > min ? tmp : min;
-    *output = tmp < max ? tmp : max;
-    input++;
-    output++;
+  const float* din_ptr = input;
+  float* dout_ptr = output;
+  int64_t cnt = num >> 4;
+  int remain = num % 16;
+  float32x4_t max_val = vdupq_n_f32(max);
+  float32x4_t min_val = vdupq_n_f32(min);
+  for (int64_t n = 0; n < cnt; n++) {
+    float32x4_t tmp0 =
+        vminq_f32(vmaxq_f32(vld1q_f32(din_ptr), min_val), max_val);
+    float32x4_t tmp1 =
+        vminq_f32(vmaxq_f32(vld1q_f32(din_ptr + 4), min_val), max_val);
+    float32x4_t tmp2 =
+        vminq_f32(vmaxq_f32(vld1q_f32(din_ptr + 8), min_val), max_val);
+    float32x4_t tmp3 =
+        vminq_f32(vmaxq_f32(vld1q_f32(din_ptr + 12), min_val), max_val);
+    vst1q_f32(dout_ptr, tmp0);
+    vst1q_f32(dout_ptr + 4, tmp1);
+    vst1q_f32(dout_ptr + 8, tmp2);
+    vst1q_f32(dout_ptr + 12, tmp3);
+    din_ptr += 16;
+    dout_ptr += 16;
+  }
+  for (int i = 0; i < remain; i++) {
+    float tmp = din_ptr[i] > min ? din_ptr[i] : min;
+    dout_ptr[i] = tmp < max ? tmp : max;
   }
 }
 
