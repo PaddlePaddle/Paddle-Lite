5d661c0017bc39268a431e68d5642e15a67d32ba
zhangwen31 <edimetia3d@gmail.com>
2020-11-02 14:53:29 +0000

    [op][arm][kernel] fix: fix_roi_align  updated to paddle fluid 1.8 (#4615)

--- lite/operators/op_params.h
+++ lite/operators/op_params.h
@@ -1446,6 +1446,7 @@ struct AssignParam : ParamBase {
 struct RoiAlignParam : ParamBase {
   lite::Tensor* X{};
   lite::Tensor* ROIs{};
+  lite::Tensor* RoisLod{};
   lite::Tensor* Out{};
   float spatial_scale{1.0};
   int pooled_height{1};
