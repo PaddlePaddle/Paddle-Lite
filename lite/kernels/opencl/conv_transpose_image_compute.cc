abb6cb83d7925f9f58a8ec1cf9549d472ac51ea0
zhaoyang-star <zhaoyangstar@foxmail.com>
2020-11-10 03:42:15 +0000

    v1

---
+++ lite/kernels/opencl/conv_transpose_image_compute.cc
@@ -0,0 +1,475 @@
+// Copyright (c) 2019 PaddlePaddle Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "lite/kernels/opencl/conv_transpose_image_compute.h"
+
+#include <iomanip>
+#include <sstream>
+#include "lite/backends/opencl/cl_image_converter.h"
+#include "lite/backends/opencl/cl_include.h"
+#include "lite/core/op_registry.h"
+#include "lite/kernels/opencl/image_helper.h"
+#include "lite/operators/op_params.h"
+
+// #undef LITE_WITH_LOG
+
+namespace paddle {
+namespace lite {
+namespace kernels {
+namespace opencl {
+
+void ConvTransposeImageCompute::PrepareForRun() {
+  auto& context = ctx_->As<OpenCLContext>();
+  CHECK(context.cl_context() != nullptr);
+  const bool is_mali = context.cl_context()->IsArmMali();
+
+  use_tune_ = CLRuntime::Global()->auto_tune();
+  if (!is_mali) {
+    use_tune_ = false;
+  }
+#ifdef LITE_WITH_LOG
+  LOG(INFO) << "use_tune_" << use_tune_;
+#endif
+
+  conv_param_ = param_.get_mutable<param_t>();
+  auto x_dims = conv_param_->x->dims();
+  auto o_dims = conv_param_->output->dims();
+  auto filter_dims = conv_param_->filter->dims();
+  filter_tensor_c_ = filter_dims[1];
+  filter_tensor_h_ = filter_dims[2];
+  filter_tensor_w_ = filter_dims[3];
+
+  auto paddings = *conv_param_->paddings;
+  pad_up_ = paddings[0];
+  pad_down_ = paddings[1];
+  pad_left_ = paddings[2];
+  pad_right_ = paddings[3];
+
+  auto dilations = *conv_param_->dilations;
+  dilation_h_ = dilations[0];
+  dilation_w_ = dilations[1];
+
+  stride_h_ = conv_param_->strides[0];
+  stride_w_ = conv_param_->strides[1];
+
+  groups_ = conv_param_->groups;
+  relu_fused_ = conv_param_->fuse_relu;
+  has_bias_ = (conv_param_->bias) != nullptr;
+
+  bool pad_equal = (pad_left_ == pad_right_) && (pad_up_ == pad_down_);
+  bool pad_all_equal = pad_equal * (pad_left_ == pad_up_);
+  bool stride_equal = stride_h_ == stride_w_;
+  bool dilation_equal = dilation_h_ == dilation_w_;
+
+  CHECK(pad_equal && stride_equal &&
+        dilation_equal);  // no need to check this!!!
+
+  /*********************************************
+   * Upload filter, bias to opencl device
+   *********************************************/
+  if (filter_tensor_c_ == 1 && x_dims[1] == o_dims[1]) {
+    impl_ = &ConvTransposeImageCompute::DepthwiseConv2dTranspose;
+    // kernel_func_names_.push_back("depthwise_conv2d_transpose");
+
+    CLImageConverterDWBlock converter;
+    float* filter_cpu = conv_param_->filter->mutable_data<float>();
+    filter_gpu_image_ = std::unique_ptr<Tensor>(new Tensor);
+    tensor_hold_filter_image_ = std::unique_ptr<Tensor>(new Tensor);
+    tensor_hold_bias_image_ = std::unique_ptr<Tensor>(new Tensor);
+
+    const DDim& filter_image_dims = converter.InitImageDimInfoWith(filter_dims);
+    filter_image_w_ = filter_image_dims[0];
+    filter_image_h_ = filter_image_dims[1];
+    tensor_hold_filter_image_->Resize({1, filter_image_w_, filter_image_h_, 4});
+
+    half_t* filter_image_data =
+        tensor_hold_filter_image_->mutable_data<half_t>();
+    converter.NCHWToImage(filter_cpu, filter_image_data, filter_dims);
+    filter_gpu_image_->mutable_data<half_t, cl::Image2D>(
+        filter_image_w_, filter_image_h_, filter_image_data);
+  } else if (filter_tensor_w_ == 3 && filter_tensor_h_ == 3 && stride_w_ == 2 &&
+             stride_h_ == 2) {
+    impl_ = &ConvTransposeImageCompute::Conv2dTranspose3x3s2;
+    // kernel_func_names_.push_back("conv2d_transpose3x3s2");
+
+    CLImageConverterConv2dTransposeTransWeight converter;
+    float* filter_cpu = conv_param_->filter->mutable_data<float>();
+    filter_gpu_image_ = std::unique_ptr<Tensor>(new Tensor);
+    tensor_hold_filter_image_ = std::unique_ptr<Tensor>(new Tensor);
+    tensor_hold_bias_image_ = std::unique_ptr<Tensor>(new Tensor);
+
+    const DDim& filter_image_dims = converter.InitImageDimInfoWith(filter_dims);
+    filter_image_w_ = filter_image_dims[0];
+    filter_image_h_ = filter_image_dims[1];
+    tensor_hold_filter_image_->Resize({1, filter_image_w_, filter_image_h_, 4});
+
+    half_t* filter_image_data =
+        tensor_hold_filter_image_->mutable_data<half_t>();
+    converter.NCHWToImage(filter_cpu, filter_image_data, filter_dims);
+    filter_gpu_image_->mutable_data<half_t, cl::Image2D>(
+        filter_image_w_, filter_image_h_, filter_image_data);
+  } else {
+    LOG(INFO) << "IN conv2d_transpose";
+    impl_ = &ConvTransposeImageCompute::Conv2dTranspose;
+    kernel_func_names_.push_back("conv2d_transpose");
+
+    CLImageConverterConv2dTransposeTransWeight converter;
+    float* filter_cpu = conv_param_->filter->mutable_data<float>();
+    filter_gpu_image_ = std::unique_ptr<Tensor>(new Tensor);
+    tensor_hold_filter_image_ = std::unique_ptr<Tensor>(new Tensor);
+    tensor_hold_bias_image_ = std::unique_ptr<Tensor>(new Tensor);
+
+    const DDim& filter_image_dims = converter.InitImageDimInfoWith(filter_dims);
+    filter_image_w_ = filter_image_dims[0];
+    filter_image_h_ = filter_image_dims[1];
+    LOG(INFO) << "FILTER image w h:" << filter_image_dims;
+    tensor_hold_filter_image_->Resize({1, filter_image_w_, filter_image_h_, 4});
+
+    half_t* filter_image_data =
+        tensor_hold_filter_image_->mutable_data<half_t>();
+    converter.NCHWToImage(filter_cpu, filter_image_data, filter_dims);
+    filter_gpu_image_->mutable_data<half_t, cl::Image2D>(
+        filter_image_w_, filter_image_h_, filter_image_data);
+  }
+  kernel_func_paths_.push_back("image/conv2d_transpose_kernel.cl");
+
+  VLOG(1) << "kernel_func_names_[0]:" << kernel_func_names_[0]
+          << " kernel_func_paths_[0]:" << kernel_func_paths_[0];
+
+  // build options
+  std::string build_options_single(" -DCL_DTYPE_half");
+  // relu options
+  VLOG(3) << "relu_fused_:" << relu_fused_
+          << " conv_param_->activation_param.active_type:"
+          << static_cast<int>(conv_param_->activation_param.active_type)
+          << " conv_param_->activation_param.has_active:"
+          << conv_param_->activation_param.has_active;
+  if (conv_param_->activation_param.has_active) {
+    if (conv_param_->activation_param.active_type ==
+        lite_api::ActivationType::kRelu) {
+      build_options_single += " -DRELU";
+    } else if (conv_param_->activation_param.active_type ==
+               lite_api::ActivationType::kRelu6) {
+      build_options_single += " -DRELU6";
+    } else if (conv_param_->activation_param.active_type ==
+               lite_api::ActivationType::kLeakyRelu) {
+      std::string leaky_relu_alpha_str =
+          std::to_string(conv_param_->activation_param.Leaky_relu_alpha);
+      build_options_single +=
+          " -DLEAKY_RELU -DLEAKY_RELU_ALPHA=" + leaky_relu_alpha_str;
+    } else {
+      LOG(FATAL) << "Unsupported activation type:"
+                 << static_cast<int>(conv_param_->activation_param.active_type);
+    }
+  }
+
+  // bias options
+  if (has_bias_) {
+    bias_gpu_image_ = std::unique_ptr<Tensor>(new Tensor);
+    const bool is_element_wise_bias =
+        has_bias_ && conv_param_->output->dims() == conv_param_->bias->dims();
+    build_options_single +=
+        is_element_wise_bias ? " -DBIASE_ELE" : " -DBIASE_CH";
+
+    // convert cpu buffer bias --> gpu image
+    CLImageConverterFolder bias_converter;
+    const DDim& bias_image_dims =
+        bias_converter.InitImageDimInfoWith(conv_param_->bias->dims());
+    tensor_hold_bias_image_->Resize(
+        {1, bias_image_dims[0], bias_image_dims[1], 4});
+
+    half_t* bias_image_data = tensor_hold_bias_image_->mutable_data<half_t>();
+
+    float* bias_cpu_data = conv_param_->bias->mutable_data<float>();
+    bias_converter.NCHWToImage(
+        bias_cpu_data, bias_image_data, conv_param_->bias->dims());
+    bias_gpu_image_->mutable_data<half_t, cl::Image2D>(
+        bias_image_dims[0], bias_image_dims[1], bias_image_data);
+    // convert cpu buffer bias --> gpu image --- end ----
+  } else {  // can delete this!!!!
+    bias_gpu_image_ = std::unique_ptr<Tensor>(new Tensor);
+    CLImageConverterFolder bias_converter;
+    tensor_hold_bias_image_->Resize({1, 1, 1, 4});
+    half_t* bias_image_data = tensor_hold_bias_image_->mutable_data<half_t>();
+    this->bias_gpu_image_->mutable_data<half_t, cl::Image2D>(
+        1, 1, bias_image_data);
+  }
+
+  // define image pointer for filter, bias
+  filter_image_p_ = filter_gpu_image_->data<half_t, cl::Image2D>();
+  bias_image_p_ = bias_gpu_image_->data<half_t, cl::Image2D>();
+
+  build_options_.push_back(build_options_single);
+
+  for (size_t i = 0; i < kernel_func_names_.size(); i++) {
+    context.cl_context()->AddKernel(kernel_func_names_[i],
+                                    kernel_func_paths_[i],
+                                    build_options_[i],
+                                    time_stamp_);
+  }
+
+  GetGlobalWorkSize();
+
+  VLOG(4) << "global_work_size_[3D]: {" << global_work_size_[0] << ","
+          << global_work_size_[1] << "," << global_work_size_[2] << "}";
+
+  std::stringstream kernel_key;
+  kernel_key << kernel_func_names_[0] << build_options_[0] << time_stamp_;
+  kernel_ = context.cl_context()->GetKernel(kernel_key.str());
+  VLOG(4) << "kernel_key: " << kernel_key.str();
+}
+
+void ConvTransposeImageCompute::ReInitWhenNeeded() {
+  conv_param_ = param_.get_mutable<param_t>();
+  auto x_dims = conv_param_->x->dims();
+#ifdef LITE_WITH_LOG
+  LOG(INFO) << "is_first_epoch_for_run_:" << is_first_epoch_for_run_
+            << ", last_input_dims_:" << last_input_dims_
+            << ", x_dims:" << x_dims;
+#endif
+
+  if (is_first_epoch_for_run_ || last_input_dims_ != x_dims) {
+    is_first_epoch_for_run_ = false;
+    last_input_dims_ = x_dims;
+
+    input_tensor_n_ = x_dims[0];
+    input_tensor_c_ = x_dims[1];
+    input_tensor_h_ = x_dims[2];
+    input_tensor_w_ = x_dims[3];
+    auto x_image_shape = InitImageDimInfoWith(x_dims);
+    input_image_h_ = x_image_shape["height"];
+    input_image_w_ = x_image_shape["width"];
+
+    auto output_dims = conv_param_->output->dims();
+    output_tensor_n_ = output_dims[0];
+    output_tensor_c_ = output_dims[1];
+    output_tensor_h_ = output_dims[2];
+    output_tensor_w_ = output_dims[3];
+    auto output_image_shape = InitImageDimInfoWith(output_dims);
+    output_image_h_ = output_image_shape["height"];
+    output_image_w_ = output_image_shape["width"];
+
+    CHECK_GE(x_dims.size(), 4);
+    CHECK_GE(output_dims.size(), 4);
+
+    // define image pointer for input, output
+    input_image_p_ = conv_param_->x->data<half_t, cl::Image2D>();
+    output_image_p_ = conv_param_->output->mutable_data<half_t, cl::Image2D>(
+        output_image_w_, output_image_h_);
+
+    GetGlobalWorkSize();
+  }
+}
+
+void ConvTransposeImageCompute::GetGlobalWorkSize() {
+  if (kernel_func_names_.size() <= 0) return;
+  // general input_c_block
+  input_c_block_ = static_cast<int>(input_image_w_ / input_tensor_w_);
+
+  // general gws
+  auto output_dims = conv_param_->output->dims();
+  const std::vector<size_t>& default_work_size =
+      DefaultWorkSize(output_dims,
+                      DDim(std::vector<DDim::value_type>{
+                          static_cast<int64_t>(output_image_w_),
+                          static_cast<int64_t>(output_image_h_)}));
+  default_c_blk_ = default_work_size[0];
+  default_w_blk_ = default_work_size[1];
+  default_nh_blk_ = default_work_size[2];
+  c_blk_ = default_c_blk_;
+  w_blk_ = default_w_blk_;
+  nh_blk_ = default_nh_blk_;
+  global_work_size_ = cl::NDRange{static_cast<size_t>(c_blk_),
+                                  static_cast<size_t>(w_blk_),
+                                  static_cast<size_t>(nh_blk_)};
+}
+
+void ConvTransposeImageCompute::Conv2dTranspose(bool enable_tune) {
+#ifdef LITE_WITH_LOG
+  PrintConvInfo();
+#endif
+  auto& context = ctx_->As<OpenCLContext>();
+
+  cl_int status;
+  int arg_idx = 0;
+  status = kernel_.setArg(arg_idx++, c_blk_);
+  CL_CHECK_FATAL(status);
+  status = kernel_.setArg(arg_idx++, w_blk_);
+  CL_CHECK_FATAL(status);
+  status = kernel_.setArg(arg_idx++, nh_blk_);
+  CL_CHECK_FATAL(status);
+  status = kernel_.setArg(arg_idx++, *input_image_p_);
+  CL_CHECK_FATAL(status);
+  status = kernel_.setArg(arg_idx++, *filter_image_p_);
+  CL_CHECK_FATAL(status);
+  if (has_bias_) {
+    status = kernel_.setArg(arg_idx++, *bias_image_p_);
+    CL_CHECK_FATAL(status);
+  }
+  status = kernel_.setArg(arg_idx++, *output_image_p_);
+  CL_CHECK_FATAL(status);
+  status = kernel_.setArg(arg_idx++, stride_w_);
+  CL_CHECK_FATAL(status);
+  status = kernel_.setArg(arg_idx++, pad_left_);
+  CL_CHECK_FATAL(status);
+  status = kernel_.setArg(arg_idx++, dilation_w_);
+  CL_CHECK_FATAL(status);
+  status = kernel_.setArg(arg_idx++, input_c_block_);
+  CL_CHECK_FATAL(status);
+  status = kernel_.setArg(arg_idx++, input_tensor_w_);
+  CL_CHECK_FATAL(status);
+  status = kernel_.setArg(arg_idx++, input_tensor_h_);
+  CL_CHECK_FATAL(status);
+  status = kernel_.setArg(arg_idx++, output_tensor_w_);
+  CL_CHECK_FATAL(status);
+  status = kernel_.setArg(arg_idx++, output_tensor_h_);
+  CL_CHECK_FATAL(status);
+  status = kernel_.setArg(arg_idx++, filter_tensor_w_);
+  CL_CHECK_FATAL(status);
+  status = kernel_.setArg(arg_idx++, filter_tensor_h_);
+  CL_CHECK_FATAL(status);
+
+  status = EnqueueNDRangeKernel(context,
+                                kernel_,
+                                cl::NullRange,
+                                global_work_size_,
+                                cl::NullRange,
+                                nullptr,
+                                event_);
+  CL_CHECK_FATAL(status);
+}
+
+void ConvTransposeImageCompute::Conv2dTranspose3x3s2(bool enable_tune) {
+#ifdef LITE_WITH_LOG
+  PrintConvInfo();
+#endif
+  auto& context = ctx_->As<OpenCLContext>();
+}
+
+void ConvTransposeImageCompute::DepthwiseConv2dTranspose(bool enable_tune) {
+#ifdef LITE_WITH_LOG
+  PrintConvInfo();
+#endif
+  auto& context = ctx_->As<OpenCLContext>();
+}
+
+void ConvTransposeImageCompute::Run() { (this->*impl_)(false); }
+
+#ifdef LITE_WITH_LOG
+void ConvTransposeImageCompute::PrintConvInfo() {
+  const bool is_element_wise_bias =
+      has_bias_ && conv_param_->output->dims() == conv_param_->bias->dims();
+
+  VLOG(4) << "input_image_shape: " << input_image_w_ << "," << input_image_h_;
+  //  VLOG(4) << "input_image: " << input_image_p_;
+  VLOG(4) << "input_dims: " << conv_param_->x->dims();
+  VLOG(4) << "filter_dims: " << conv_param_->filter->dims();
+  //  VLOG(4) << "filter_image: " << filter_image;
+  VLOG(4) << "output_dims: " << conv_param_->output->dims();
+  VLOG(4) << "out_image_shape: " << output_image_w_ << ", " << output_image_h_;
+  VLOG(4) << "paddings: " << pad_left_ << "," << pad_up_;
+  VLOG(4) << "has bias: " << has_bias_;
+  VLOG(4) << "is_element_wise_bias : " << is_element_wise_bias;
+  VLOG(4) << "strides: " << stride_h_ << "," << stride_w_;
+  VLOG(4) << "offset: ";
+  VLOG(4) << "dilations.size : " << conv_param_->dilations->size();
+  VLOG(4) << "dilations: " << dilation_h_ << ", " << dilation_w_;
+  VLOG(4) << "global_work_size_[3D]: {" << global_work_size_[0] << ","
+          << global_work_size_[1] << "," << global_work_size_[2] << "}";
+  VLOG(4) << "groups_:" << groups_;
+
+  LOG(INFO) << "================================";
+  LOG(INFO) << "c_blk_=" << c_blk_ << ", w_blk_=" << w_blk_
+            << ",nh_blk_=" << nh_blk_;
+
+  LOG(INFO) << "stride_h_:" << stride_h_;
+  LOG(INFO) << "stride_w_:" << stride_w_;
+
+  LOG(INFO) << "dilation_h_:" << dilation_h_;
+  LOG(INFO) << "dilation_w_:" << dilation_w_;
+
+  LOG(INFO) << "pad_up_:" << pad_up_;
+  LOG(INFO) << "pad_down_:" << pad_down_;
+  LOG(INFO) << "pad_left_:" << pad_left_;
+  LOG(INFO) << "pad_right_:" << pad_right_;
+
+  LOG(INFO) << "groups_:" << groups_;
+  LOG(INFO) << "relu_fused_:" << relu_fused_;
+  LOG(INFO) << "has_bias_:" << has_bias_;
+
+  LOG(INFO) << "input_tensor_n_:" << input_tensor_n_;
+  LOG(INFO) << "input_tensor_c_:" << input_tensor_c_;
+  LOG(INFO) << "input_tensor_h_:" << input_tensor_h_;
+  LOG(INFO) << "input_tensor_w_:" << input_tensor_w_;
+  LOG(INFO) << "input_image_h_:" << input_image_h_;
+  LOG(INFO) << "input_image_w_:" << input_image_w_;
+  LOG(INFO) << "input_c_block_:" << input_c_block_;
+
+  LOG(INFO) << "output_tensor_n_:" << output_tensor_n_;
+  LOG(INFO) << "output_tensor_c_:" << output_tensor_c_;
+  LOG(INFO) << "output_tensor_h_:" << output_tensor_h_;
+  LOG(INFO) << "output_tensor_w_:" << output_tensor_w_;
+  LOG(INFO) << "output_image_h_:" << output_image_h_;
+  LOG(INFO) << "output_image_w_:" << output_image_w_;
+
+  LOG(INFO) << "filter_tensor_n_:" << filter_tensor_n_;
+  LOG(INFO) << "filter_tensor_c_:" << filter_tensor_c_;
+  LOG(INFO) << "filter_tensor_h_:" << filter_tensor_h_;
+  LOG(INFO) << "filter_tensor_w_:" << filter_tensor_w_;
+  LOG(INFO) << "filter_image_h_:" << filter_image_h_;
+  LOG(INFO) << "filter_image_w_:" << filter_image_w_;
+}
+#endif
+
+}  // namespace opencl
+}  // namespace kernels
+}  // namespace lite
+}  // namespace paddle
+
+REGISTER_LITE_KERNEL(conv2d_transpose,
+                     kOpenCL,
+                     kFP16,
+                     kImageDefault,
+                     paddle::lite::kernels::opencl::ConvTransposeImageCompute,
+                     image2d)
+    .BindInput("Input",
+               {LiteType::GetTensorTy(TARGET(kOpenCL),
+                                      PRECISION(kFP16),
+                                      DATALAYOUT(kImageDefault))})
+    .BindInput("Bias", {LiteType::GetTensorTy(TARGET(kARM))})
+    .BindInput("Filter", {LiteType::GetTensorTy(TARGET(kARM))})
+    .BindOutput("Output",
+                {LiteType::GetTensorTy(TARGET(kOpenCL),
+                                       PRECISION(kFP16),
+                                       DATALAYOUT(kImageDefault))})
+    .Finalize();
+
+REGISTER_LITE_KERNEL(depthwise_conv2d_transpose,
+                     kOpenCL,
+                     kFP16,
+                     kImageDefault,
+                     paddle::lite::kernels::opencl::ConvTransposeImageCompute,
+                     image2d)
+    .BindInput("Input",
+               {LiteType::GetTensorTy(TARGET(kOpenCL),
+                                      PRECISION(kFP16),
+                                      DATALAYOUT(kImageDefault))})
+    .BindInput("Bias", {LiteType::GetTensorTy(TARGET(kARM))})
+    .BindInput("Filter", {LiteType::GetTensorTy(TARGET(kARM))})
+    .BindOutput("Output",
+                {LiteType::GetTensorTy(TARGET(kOpenCL),
+                                       PRECISION(kFP16),
+                                       DATALAYOUT(kImageDefault))})
+    .Finalize();
+// #define LITE_WITH_LOG
