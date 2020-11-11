053706b8f2f7df4ac908fc27ee04803f5cae600b
DannyIsFunny <912790387@qq.com>
2020-11-11 04:51:22 +0000

    test=develop

--- lite/demo/cxx/x86_mobilenetv1_light_demo/mem_rec.sh
+++ lite/demo/cxx/x86_mobilenetv1_light_demo/mem_rec.sh
@@ -1,7 +1,7 @@
 #mv 20201106_data.txt 20201106_data.txt_back
-pmap -x 23868 | grep Address >> 20201110_data.txt
+pmap -x $(pidof mobilenet_light_api) | grep Address >> 20201111_data.txt
 while(true)
 do
-    sleep 50
-    pmap -x 23868 | grep total >> 20201110_data.txt
+    sleep 20
+    pmap -x $(pidof mobilenet_light_api) | grep total >> 20201111_data.txt
 done
