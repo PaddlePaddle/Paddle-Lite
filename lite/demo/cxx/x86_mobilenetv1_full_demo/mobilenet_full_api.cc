560a4668122129eb25406e6f25c9bdbd564868d2
DannyIsFunny <912790387@qq.com>
2020-11-20 08:09:44 +0000

    test=develop

--- lite/demo/cxx/x86_mobilenetv1_full_demo/mobilenet_full_api.cc
+++ lite/demo/cxx/x86_mobilenetv1_full_demo/mobilenet_full_api.cc
@@ -15,7 +15,11 @@
 #include <iostream>
 #include <vector>
 #include "paddle_api.h"  // NOLINT
-
+/////////////////////////////////////////////////////////////////////////
+// If this demo is linked to static library:libpaddle_api_full_bundled.a
+// , you should include `paddle_use_ops.h` and `paddle_use_kernels.h` to
+// avoid linking errors such as `unsupport ops or kernels`.
+/////////////////////////////////////////////////////////////////////////
 #ifdef _WIN32
 #include "paddle_use_kernels.h"  // NOLINT
 #include "paddle_use_ops.h"      // NOLINT
