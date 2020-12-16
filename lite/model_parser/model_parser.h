c59310be2251aa26d8e3b1ab5129966ed812f36f
huzhiqiang <912790387@qq.com>
2020-12-16 10:44:51 +0800

    [Model][Framework]update model_version to `meta_version=2` and accelerate loading model period (#4934)

--- lite/model_parser/model_parser.h
+++ lite/model_parser/model_parser.h
@@ -31,11 +31,6 @@
 
 namespace paddle {
 namespace lite {
-namespace model_parser {
-Buffer LoadFile(const std::string& path, size_t offset = 0, size_t size = 0);
-void SaveFile(const std::string& path, const Buffer& cache);
-}  // namespace model_parser
-
 #ifndef LITE_ON_TINY_PUBLISH
 // Read a __model__ file.
 std::unique_ptr<framework::proto::ProgramDesc> LoadProgram(
@@ -123,17 +118,18 @@ void LoadModelNaive(const std::string& model_dir,
 void LoadModelNaiveV0FromFile(const std::string& filename,
                               Scope* scope,
                               cpp::ProgramDesc* cpp_prog);
+void LoadModelNaiveV0FromMemory(const std::string& model_buffer,
+                                Scope* scope,
+                                cpp::ProgramDesc* cpp_prog);
 void LoadModelNaiveFromMemory(const std::string& model_buffer,
                               const std::string& param_buffer,
                               lite::Scope* scope,
                               cpp::ProgramDesc* cpp_prog);
-void LoadModelNaiveV0FromMemory(const std::string& model_buffer,
-                                Scope* scope,
-                                cpp::ProgramDesc* cpp_prog);
 #endif  // LITE_ON_TINY_PUBLISH
-void LoadModelFbsFromFile(const std::string& filename,
+void LoadModelFbsFromFile(model_parser::BinaryFileReader* reader,
                           Scope* scope,
-                          cpp::ProgramDesc* cpp_prog);
+                          cpp::ProgramDesc* cpp_prog,
+                          uint16_t meta_version);
 
 void LoadModelNaiveFromFile(const std::string& filename,
                             lite::Scope* scope,
@@ -142,8 +138,9 @@ void LoadModelNaiveFromFile(const std::string& filename,
 void LoadModelNaiveFromMemory(const std::string& model_buffer,
                               lite::Scope* scope,
                               cpp::ProgramDesc* cpp_prog);
-void LoadModelNaiveV1FromMemory(const std::string& model_buffer,
-                                Scope* scope,
-                                cpp::ProgramDesc* cpp_prog);
+void LoadModelFbsFromMemory(model_parser::StringBufferReader* reader,
+                            Scope* scope,
+                            cpp::ProgramDesc* cpp_prog,
+                            uint16_t meta_version);
 }  // namespace lite
 }  // namespace paddle
