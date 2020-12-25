1b6f22534fa398720e2ed0c9c6c91478112bbab6
szdiao <702368107@qq.com>
2020-12-25 16:18:22 +0800

    add multiclass_nms3 op, test=develop

--- lite/operators/op_params.h
+++ lite/operators/op_params.h
@@ -936,6 +936,8 @@ struct MulticlassNmsParam : ParamBase {
   float nms_eta{1.0f};
   int keep_top_k;
   bool normalized{true};
+  const lite::Tensor* rois_num{};
+  lite::Tensor* nms_rois_num{};
 };
 
 /// ----------------------- matrix_nms operators ----------------------
