e21029e1b72aa7ce9f8f884b9072e7b9a5bb606d
Shixiaowei02 <39303645+Shixiaowei02@users.noreply.github.com>
2020-12-31 03:57:41 +0000

    fix a bug in tests, test=develop

--- lite/model_parser/flatbuffers/opencl/test_cache.cc
+++ lite/model_parser/flatbuffers/opencl/test_cache.cc
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#include <gflags/gflags.h>
+#include <gtest/gtest.h>
 #include "lite/model_parser/flatbuffers/opencl/cache.h"
 
 namespace paddle {
