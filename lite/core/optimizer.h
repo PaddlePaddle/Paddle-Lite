ece82009ee34f81f69f2d5f502d89460b09d904d
huzhiqiang <912790387@qq.com>
2020-12-31 11:42:02 +0800

    [ARM] Register pass about matmul (#5089) (#5116)

--- lite/core/optimizer.h
+++ lite/core/optimizer.h
@@ -93,6 +93,9 @@ class Optimizer {
          "lite_conv_activation_fuse_pass",              //
          "lite_var_conv_2d_activation_fuse_pass",       //
          "lite_match_matrix_activation_fuse_pass",      //
+         "lite_squeeze2_matmul_fuse_pass",              //
+         "lite_reshape2_matmul_fuse_pass",              //
+         "lite_matmul_fuse_pass",                       //
          "lite_fc_fuse_pass",                           //
          "lite_shuffle_channel_fuse_pass",              //
          "lite_transpose_softmax_transpose_fuse_pass",  //
