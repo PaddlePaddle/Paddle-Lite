37ab38ce1e89750109344f07d87ab10fe468ba40
hong19860320 <9973393+hong19860320@users.noreply.github.com>
2021-01-04 16:29:47 +0000

    [RockchipNPU] Add pass to apply the constraints for the quantized ops(such as concat) that the inputs and outputs must have the same scale test=develop

--- lite/api/paddle_use_passes.h
+++ lite/api/paddle_use_passes.h
@@ -65,6 +65,7 @@ USE_MIR_PASS(weight_quantization_preprocess_pass);
 USE_MIR_PASS(post_quant_dynamic_pass);
 USE_MIR_PASS(apu_subgraph_pass);
 USE_MIR_PASS(quantized_op_attributes_inference_pass);
+USE_MIR_PASS(restrict_quantized_op_with_same_input_output_scale_pass);
 USE_MIR_PASS(control_flow_op_unused_inputs_and_outputs_eliminate_pass)
 USE_MIR_PASS(lite_scale_activation_fuse_pass);
 USE_MIR_PASS(__xpu__resnet_fuse_pass);
