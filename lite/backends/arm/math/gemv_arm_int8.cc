fd0b07b5f810d5d437eecefe916c15b04547772b
yongqiangma <xing.wo@163.com>
2021-01-04 03:43:02 +0000

    fix gcc build error.test=develop

--- lite/backends/arm/math/gemv_arm_int8.cc
+++ lite/backends/arm/math/gemv_arm_int8.cc
@@ -1938,9 +1938,8 @@ bool gemv_int8_trans_oth(const int8_t* A,
     [cnt] "+r"(cnt),                                                      \
     [scale] "+r"(scale_ptr),                                              \
     [bias] "+r"(bias_ptr),                                                \
-    [relu] "+r"(act),                                                     \
-    [alpha] "+r"(alpha)                                                   \
-  : [out] "r"(out_ptr)                                                    \
+    [relu] "+r"(act)                                                      \
+  : [out] "r"(out_ptr), [alpha] "r"(alpha)                                \
   : "cc", "memory",                                                       \
     "v0", "v1", "v2", "v3", "v4", "v5", "v6", "v7", "v8", "v9",           \
     "v10", "v11", "v12", "v13", "v14", "v15", "v16", "v17",               \
