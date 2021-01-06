7e8bbc650343b9431f28c28251ccd721e3f2e1f0
hong19860320 <9973393+hong19860320@users.noreply.github.com>
2021-01-06 15:56:39 +0800

    [RockchipNPU] Add pass to apply the constraints for the quantized ops(such as concat) that the inputs and outputs must have the same scale (#5132)
    test=develop

--- lite/core/optimizer.h
+++ lite/core/optimizer.h
@@ -124,12 +124,12 @@ class Optimizer {
          "__xpu__fc_fuse_pass",
          "__xpu__softmax_topk_fuse_pass",
          "__xpu__multi_encoder_slice_link_fuse_pass",
-         "quantized_op_attributes_inference_pass",  // Only for fully
-                                                    // quantized model, infer
-                                                    // the output scale and
-                                                    // fix the attribute
-                                                    // 'enable_int8' for all
-                                                    // of the quantized ops.
+         // Only for fully quantized model, infer the output scale and fix the
+         // attribute 'enable_int8' for all of the quantized ops.
+         "quantized_op_attributes_inference_pass",
+         // Apply the constraints for the quantized ops(such as concat) that the
+         // inputs and outputs must have the same scale.
+         "restrict_quantized_op_with_same_input_output_scale_pass",
          "npu_subgraph_pass",
          "huawei_ascend_npu_subgraph_pass",
          "imagination_nna_subgraph_pass",
