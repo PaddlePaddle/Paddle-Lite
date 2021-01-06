e0f3887998bd044fe22eb3cf36a9a4077aa7199d
DannyIsFunny <912790387@qq.com>
2021-01-06 06:50:30 +0000

    test=develop

--- tools/ci_tools/ci_android_publish.sh
+++ tools/ci_tools/ci_android_publish.sh
@@ -34,38 +34,35 @@ WORKSPACE=${SHELL_FOLDER%tools/ci_tools*}
 #   None
 ####################################################################################################
 function publish_inference_lib {
-    local arch=$1
-    local toolchain=$2
-    local with_extra=$3
-    cd $WORKSPACE
-    # Remove Compiling Cache
-    rm -rf build*
-    # Compiling inference library
-    ./lite/tools/build_android.sh --arch=$arch --toolchain=$toolchain  --with_extra=$with_extra
-    # Checking results: cplus and java inference lib.
-    if [ -d build*/inference*/cxx/lib ] && [ -d build*/inference*/java/so ]; then
-      cxx_results=$(ls build*/inference*/cxx/lib | wc -l)
-      java_results=$(ls build*/inference*/java/so | wc -l)
-          if [ $cxx_results -ge 2 ] && [ $java_results -ge 1 ]; then
-              return 0
-          fi
-    fi
-    # Error message.
-    echo "**************************************************************************************"
-    echo -e "* Android compiling task failed on the following instruction:"
-    echo -e "*     ./lite/tools/build_android.sh --arch=$arch --toolchain=$toolchain  --with_extra=ON"
-    echo "**************************************************************************************"
-    exit 1
+  local arch=$1
+  local toolchain=$2
+  local with_extra=$3
+  cd $WORKSPACE
+  # Remove Compiling Cache
+  rm -rf build*
+  # Compiling inference library
+  ./lite/tools/build_android.sh --arch=$arch --toolchain=$toolchain  --with_extra=$with_extra
+  # Checking results: cplus and java inference lib.
+  if [ -d build*/inference*/cxx/lib ] && [ -d build*/inference*/java/so ]; then
+    cxx_results=$(ls build*/inference*/cxx/lib | wc -l)
+    java_results=$(ls build*/inference*/java/so | wc -l)
+      if [ $cxx_results -ge 2 ] && [ $java_results -ge 1 ]; then
+          return 0
+      fi
+  fi
+  # Error message.
+  echo "**************************************************************************************"
+  echo -e "* Android compiling task failed on the following instruction:"
+  echo -e "*     ./lite/tools/build_android.sh --arch=$arch --toolchain=$toolchain  --with_extra=ON"
+  echo "**************************************************************************************"
+  exit 1
 }
 
 
 # Compiling test
-for arch in ${ARCH[@]}
-do
-    for toolchain in ${TOOLCHAIN[@]}
-    do
-        cd $WORKSPACE
-        publish_inference_lib $arch $toolchain ON
-    done
+for arch in ${ARCH[@]}; do
+  for toolchain in ${TOOLCHAIN[@]}; do
+    cd $WORKSPACE
+    publish_inference_lib $arch $toolchain ON
+  done
 done    
-              
