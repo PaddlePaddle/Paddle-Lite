17df27e19076f2e7c4152723702d672f3cd0a216
DannyIsFunny <912790387@qq.com>
2021-01-08 07:52:52 +0000

    test=develop

--- lite/operators/reduce_all_op.cc
+++ lite/operators/reduce_all_op.cc
@@ -90,8 +90,7 @@ bool ReduceAllOp::InferShapeImpl() const {
 }
 
 bool ReduceAllOp::AttachImpl(const cpp::OpDesc &opdesc, lite::Scope *scope) {
-  param_.X = const_cast<lite::Tensor *>(
-      &scope->FindVar(opdesc.Input("X").front())->Get<lite::Tensor>());
+  param_.X = scope->FindVar(opdesc.Input("X").front())->Get<lite::Tensor>();
   param_.Out =
       scope->FindVar(opdesc.Output("Out").front())->GetMutable<lite::Tensor>();
   param_.dim = opdesc.GetAttr<std::vector<int>>("dim");
