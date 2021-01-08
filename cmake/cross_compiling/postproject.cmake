f74f5fa541b11ec16cf112455baa03f48b10382f
dingweihao <leo_ding30@163.com>
2021-01-08 12:17:50 +0000

    [ARM]fix android ndk version in cmake,test=develop

--- cmake/cross_compiling/postproject.cmake
+++ cmake/cross_compiling/postproject.cmake
@@ -15,24 +15,28 @@
 if(NOT LITE_WITH_LIGHT_WEIGHT_FRAMEWORK)
     return()
 endif()
-
 include(CheckCXXCompilerFlag)
-
 if(ANDROID)
     include(cross_compiling/findar)
-    
     set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -llog -fPIC")
     set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -llog -fPIC")
-
-    if(LITE_WITH_ARM82_FP16 AND (${ANDROID_NDK_MAJOR} GREATER 17))
-        add_definitions(-DENABLE_ARM_FP16)
-        set(CMAKE_C_FLAGS   "${CMAKE_C_FLAGS}   -march=armv8.2-a+fp16")
-        set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -march=armv8.2-a+fp16")
+    if(LITE_WITH_ARM82_FP16)
+        if(${ANDROID_NDK_MAJOR})
+            if(${ANDROID_NDK_MAJOR} GREATER "17")
+                add_definitions(-DENABLE_ARM_FP16)
+                set(CMAKE_C_FLAGS   "${CMAKE_C_FLAGS}   -march=armv8.2-a+fp16")
+                set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -march=armv8.2-a+fp16")
+            endif()
+        endif()
     endif()
 
-    if(LITE_WITH_ARM82_INT8_SDOT AND (${ANDROID_NDK_MAJOR} GREATER 17))
-        set(CMAKE_C_FLAGS   "${CMAKE_C_FLAGS}   -march=armv8.2-a+dotprod")
-        set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -march=armv8.2-a+dotprod")
+    if(LITE_WITH_ARM82_INT8_SDOT)
+        if(${ANDROID_NDK_MAJOR})
+            if(${ANDROID_NDK_MAJOR} GREATER "17")
+                set(CMAKE_C_FLAGS   "${CMAKE_C_FLAGS}   -march=armv8.2-a+dotprod")
+                set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -march=armv8.2-a+dotprod")
+            endif()
+        endif()
     endif()
 
     # Don't re-export libgcc symbols
