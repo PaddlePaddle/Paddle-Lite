c93534ee6bb7f62e0ec6db7df28bc7e045d2d94b
huzhiqiang <912790387@qq.com>
2021-01-10 23:19:17 +0800

    [Op] Register reduce_all op (#5150)

--- lite/operators/op_params.h
+++ lite/operators/op_params.h
@@ -1255,7 +1255,7 @@ struct IsEmptyParam : ParamBase {
 };
 
 struct ReduceParam : ParamBase {
-  lite::Tensor* X{};
+  const lite::Tensor* X{};
   lite::Tensor* Out{};
   std::vector<int> dim{0};
   bool keep_dim{false};
