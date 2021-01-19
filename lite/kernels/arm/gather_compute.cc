f3dbe1c2000ed4c31e0c9e555a63b332ab67409a
chenjiaoAngel <chenjiaobuaa@126.com>
2021-01-19 06:57:03 +0000

    change int64 and int32 queue. test=develop

--- lite/kernels/arm/gather_compute.cc
+++ lite/kernels/arm/gather_compute.cc
@@ -13,14 +13,6 @@
 // limitations under the License.
 #include "lite/kernels/arm/gather_compute.h"
 
-REGISTER_LITE_KERNEL(gather, kARM, kFloat, kNCHW, GatherInt64Int64, int64int64)
-    .BindInput("X", {LiteType::GetTensorTy(TARGET(kARM), PRECISION(kAny))})
-    .BindInput("Index",
-               {LiteType::GetTensorTy(TARGET(kARM), PRECISION(kInt64))})
-    .BindInput("Axis", {LiteType::GetTensorTy(TARGET(kARM), PRECISION(kInt64))})
-    .BindOutput("Out", {LiteType::GetTensorTy(TARGET(kARM), PRECISION(kAny))})
-    .Finalize();
-
 REGISTER_LITE_KERNEL(gather, kARM, kFloat, kNCHW, GatherInt32Int32, int32int32)
     .BindInput("X", {LiteType::GetTensorTy(TARGET(kARM), PRECISION(kAny))})
     .BindInput("Index",
@@ -29,6 +21,13 @@ REGISTER_LITE_KERNEL(gather, kARM, kFloat, kNCHW, GatherInt32Int32, int32int32)
     .BindOutput("Out", {LiteType::GetTensorTy(TARGET(kARM), PRECISION(kAny))})
     .Finalize();
 
+REGISTER_LITE_KERNEL(gather, kARM, kFloat, kNCHW, GatherInt64Int64, int64int64)
+    .BindInput("X", {LiteType::GetTensorTy(TARGET(kARM), PRECISION(kAny))})
+    .BindInput("Index",
+               {LiteType::GetTensorTy(TARGET(kARM), PRECISION(kInt64))})
+    .BindInput("Axis", {LiteType::GetTensorTy(TARGET(kARM), PRECISION(kInt64))})
+    .BindOutput("Out", {LiteType::GetTensorTy(TARGET(kARM), PRECISION(kAny))})
+    .Finalize();
 REGISTER_LITE_KERNEL(gather, kARM, kFloat, kNCHW, GatherInt64Int32, int64int32)
     .BindInput("X", {LiteType::GetTensorTy(TARGET(kARM), PRECISION(kAny))})
     .BindInput("Index",
