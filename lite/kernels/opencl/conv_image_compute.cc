6aedada006802b85e161eb1d841743e41a41f7bb
zhaoyang-star <zhaoyangstar@foxmail.com>
2021-01-20 09:48:37 +0800

    [OpenCL][Kernel] Fix depthwise_conv2d_3x3 stride equal err (#5340)

--- lite/kernels/opencl/conv_image_compute.cc
+++ lite/kernels/opencl/conv_image_compute.cc
@@ -119,8 +119,8 @@ void ConvImageCompute::PrepareForRun() {
   } else if (filter_tensor_c_ == 1 && input_tensor_c_ == output_tensor_c_ &&
              filter_tensor_h_ == 3 && filter_tensor_w_ == 3 && groups_ > 1) {
     // depth_conv2d_3x3s1, depth_conv2d_3x3
-    CHECK(pad_equal && stride_equal && dilation_equal);
-    if (stride_h_ == 1 && dilation_h_ == 1) {
+    CHECK(pad_equal && dilation_equal);
+    if (stride_equal && stride_h_ == 1 && dilation_h_ == 1) {
       kernel_func_names_.push_back("depth_conv2d_3x3s1");
       impl_ = &ConvImageCompute::DepthwiseConv2d3x3s1;
     } else {
@@ -148,7 +148,7 @@ void ConvImageCompute::PrepareForRun() {
 #undef DEPTH_CONV_USE_SPL
              ) {
     // depth_conv2d
-    CHECK(pad_equal && stride_equal && dilation_equal);
+    CHECK(pad_equal && dilation_equal);
     kernel_func_names_.push_back("depth_conv2d");
     kernel_func_paths_.push_back("image/depthwise_conv2d_basic_kernel.cl");
 
@@ -919,19 +919,21 @@ void ConvImageCompute::DepthwiseConv2d3x3() {
   CL_CHECK_FATAL(status_);
   status_ = kernel_.setArg(7, stride_h_);
   CL_CHECK_FATAL(status_);
-  status_ = kernel_.setArg(8, offset_);
+  status_ = kernel_.setArg(8, stride_w_);
   CL_CHECK_FATAL(status_);
-  status_ = kernel_.setArg(9, dilation_h_);
+  status_ = kernel_.setArg(9, offset_);
   CL_CHECK_FATAL(status_);
-  status_ = kernel_.setArg(10, input_c_block_);
+  status_ = kernel_.setArg(10, dilation_h_);
   CL_CHECK_FATAL(status_);
-  status_ = kernel_.setArg(11, input_tensor_w_);
+  status_ = kernel_.setArg(11, input_c_block_);
   CL_CHECK_FATAL(status_);
-  status_ = kernel_.setArg(12, input_tensor_h_);
+  status_ = kernel_.setArg(12, input_tensor_w_);
   CL_CHECK_FATAL(status_);
-  status_ = kernel_.setArg(13, output_tensor_w_);
+  status_ = kernel_.setArg(13, input_tensor_h_);
   CL_CHECK_FATAL(status_);
-  status_ = kernel_.setArg(14, output_tensor_h_);
+  status_ = kernel_.setArg(14, output_tensor_w_);
+  CL_CHECK_FATAL(status_);
+  status_ = kernel_.setArg(15, output_tensor_h_);
   CL_CHECK_FATAL(status_);
 }
 
@@ -953,23 +955,25 @@ void ConvImageCompute::DepthwiseConv2d() {
   CL_CHECK_FATAL(status_);
   status_ = kernel_.setArg(7, stride_h_);
   CL_CHECK_FATAL(status_);
-  status_ = kernel_.setArg(8, offset_);
+  status_ = kernel_.setArg(8, stride_w_);
   CL_CHECK_FATAL(status_);
-  status_ = kernel_.setArg(9, input_c_block_);
+  status_ = kernel_.setArg(9, offset_);
   CL_CHECK_FATAL(status_);
-  status_ = kernel_.setArg(10, dilation_h_);
+  status_ = kernel_.setArg(10, input_c_block_);
   CL_CHECK_FATAL(status_);
-  status_ = kernel_.setArg(11, input_tensor_w_);
+  status_ = kernel_.setArg(11, dilation_h_);
   CL_CHECK_FATAL(status_);
-  status_ = kernel_.setArg(12, input_tensor_h_);
+  status_ = kernel_.setArg(12, input_tensor_w_);
   CL_CHECK_FATAL(status_);
-  status_ = kernel_.setArg(13, output_tensor_w_);
+  status_ = kernel_.setArg(13, input_tensor_h_);
   CL_CHECK_FATAL(status_);
-  status_ = kernel_.setArg(14, output_tensor_h_);
+  status_ = kernel_.setArg(14, output_tensor_w_);
   CL_CHECK_FATAL(status_);
-  status_ = kernel_.setArg(15, filter_tensor_w_);
+  status_ = kernel_.setArg(15, output_tensor_h_);
   CL_CHECK_FATAL(status_);
-  status_ = kernel_.setArg(16, filter_tensor_h_);
+  status_ = kernel_.setArg(16, filter_tensor_w_);
+  CL_CHECK_FATAL(status_);
+  status_ = kernel_.setArg(17, filter_tensor_h_);
   CL_CHECK_FATAL(status_);
 }
 
