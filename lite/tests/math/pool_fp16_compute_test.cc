ad335088c6dfb8de37019fef30bf4bdda757ee58
HappyAngel <chenjiaobuaa@126.com>
2021-02-08 10:31:53 +0800

    [arm fp16] add fp16 pool  OP (#5458)
    * add fp16 pooling op.
    * pool fp16 register problem. test=develop

--- lite/tests/math/pool_fp16_compute_test.cc
+++ lite/tests/math/pool_fp16_compute_test.cc
@@ -15,7 +15,7 @@
 #include "lite/tests/math/pool_ut.h"
 
 #ifdef LITE_WITH_ARM
-void test_pool_fp32(const std::vector<DDim>& input_dims,
+void test_pool_fp16(const std::vector<DDim>& input_dims,
                     const std::vector<int>& ksize,
                     const std::vector<int>& strides,
                     const std::vector<int>& pads,
@@ -32,7 +32,7 @@ void test_pool_fp32(const std::vector<DDim>& input_dims,
 #endif
   PoolParam param;
   param.x = new Tensor;
-  param.x->set_precision(PRECISION(kFloat));
+  param.x->set_precision(PRECISION(kFP16));
   param.ksize = ksize;
 
   param.strides = strides;
@@ -45,12 +45,12 @@ void test_pool_fp32(const std::vector<DDim>& input_dims,
   param.use_quantizer = use_quantizer;
 
   param.output = new Tensor;
-  param.output->set_precision(PRECISION(kFloat));
+  param.output->set_precision(PRECISION(kFP16));
 
   for (auto& cls : power_mode) {
     for (auto& th : thread_num) {
-      paddle::lite::kernels::arm::PoolCompute<PRECISION(kFloat),
-                                              PRECISION(kFloat)>
+      paddle::lite::kernels::arm::PoolCompute<PRECISION(kFP16),
+                                              PRECISION(kFP16)>
           pool;
       std::unique_ptr<paddle::lite::KernelContext> ctx1(
           new paddle::lite::KernelContext);
@@ -70,19 +70,33 @@ void test_pool_fp32(const std::vector<DDim>& input_dims,
         param.x->Resize(dim_in);
         param.output->Resize(dim_out);
 
-        paddle::lite::fill_tensor_rand(*param.x, -1.f, 1.f);
-        //        paddle::lite::fill_tensor_const(*param.x, 1.f);
-        auto din = param.x->data<float>();
+        Tensor x_fp32;
+        x_fp32.Resize(dim_in);
+        x_fp32.set_precision(PRECISION(kFloat));
+        float* a_ptr = x_fp32.mutable_data<float>();
+        float16_t* b_ptr = param.x->mutable_data<float16_t>();
+        fill_data_rand<float16_t>(b_ptr, -1.f, 1.f, param.x->numel());
+        // fill_data_const<float16_t>(b_ptr, -1.f, param.x->numel());
+        fp16_to_float(param.x->data<float16_t>(), a_ptr, param.x->numel());
+        auto din = param.x->data<float16_t>();
+        auto din_fp32 = x_fp32.data<float>();
 
         Tensor tout_basic;
+        Tensor tout_basic_fp16;
+        Tensor tout_basic_fp32;
         if (FLAGS_check_result) {
-          LOG(INFO) << "basic compute";
-          tout_basic.set_precision(PRECISION(kFloat));
+          tout_basic.set_precision(PRECISION(kFP16));
+          tout_basic_fp16.set_precision(PRECISION(kFP16));
+          tout_basic_fp32.set_precision(PRECISION(kFloat));
           tout_basic.Resize(dim_out);
-          fill_tensor_const(tout_basic, 0.f);
-          auto dout_basic = tout_basic.mutable_data<float>();
-          pooling_basic<float, float>(din,
-                                      dout_basic,
+          tout_basic_fp16.Resize(dim_out);
+          tout_basic_fp32.Resize(dim_out);
+          fill_tensor_const(tout_basic_fp32, 0.f);
+          auto dout_basic = tout_basic.mutable_data<float16_t>();
+          auto dout_basic_fp32 = tout_basic_fp32.mutable_data<float>();
+          fill_data_const<float16_t>(dout_basic, 0.f, tout_basic.numel());
+          pooling_basic<float, float>(din_fp32,
+                                      dout_basic_fp32,
                                       dim_in[0],
                                       dim_out[1],
                                       dim_out[2],
@@ -99,8 +113,11 @@ void test_pool_fp32(const std::vector<DDim>& input_dims,
                                       ceil_mode,
                                       use_quantizer,
                                       pooling_type);
+          // fp32 -> fp16
+          auto dout_basic_fp16_ptr = tout_basic_fp16.mutable_data<float16_t>();
+          float_to_fp16(
+              dout_basic_fp32, dout_basic_fp16_ptr, tout_basic_fp16.numel());
         }
-        LOG(INFO) << "lite compute";
         /// warm up
         for (int i = 0; i < FLAGS_warmup; ++i) {
           pool.Launch();
@@ -114,18 +131,30 @@ void test_pool_fp32(const std::vector<DDim>& input_dims,
         }
 
         double gops = 2.0 * dim_out.production() * ksize[0] * ksize[1];
-
-        print_gops_info("pool_fp32", dim_in, dim_out, t0, gops);
+        print_gops_info("pool_fp16", dim_in, dim_out, t0, gops);
         if (FLAGS_check_result) {
           double max_ratio = 0;
           double max_diff = 0;
-          tensor_cmp_host(tout_basic, *param.output, max_ratio, max_diff);
+          auto basic_ptr = tout_basic_fp16.data<float16_t>();
+          auto saber_ptr = param.output->data<float16_t>();
+          Tensor tdiff;
+          tdiff.Resize(tout_basic.dims());
+          tdiff.set_precision(PRECISION(kFP16));
+          auto ptr = tdiff.mutable_data<float16_t>();
+          // paddle::lite::data_diff_kernel(
+          data_diff(basic_ptr,
+                    saber_ptr,
+                    ptr,
+                    tout_basic.numel(),
+                    max_ratio,
+                    max_diff);
           print_diff_info(max_diff, max_ratio);
           if (std::abs(max_ratio) > 1e-3f) {
             if (max_diff > 5e-4f) {
-              print_tensor_info_common(
-                  *param.x, tout_basic, *param.output, true);
-              print_pool_success_or_fail_info("pool_fp32",
+              int64_t size = tout_basic.numel();
+              int64_t width = tout_basic.dims()[tout_basic.dims().size() - 1];
+              print_tensor_info_fp16(basic_ptr, saber_ptr, ptr, size, width);
+              print_pool_success_or_fail_info("pool_fp16",
                                               false,
                                               dim_in,
                                               dim_out,
@@ -141,7 +170,7 @@ void test_pool_fp32(const std::vector<DDim>& input_dims,
             }
           }
         }
-        print_pool_success_or_fail_info("pool_fp32",
+        print_pool_success_or_fail_info("pool_fp16",
                                         true,
                                         dim_in,
                                         dim_out,
@@ -162,7 +191,7 @@ void test_pool_fp32(const std::vector<DDim>& input_dims,
   delete param.output;
 }
 #else
-void test_pool_fp32(const std::vector<DDim>& input_dims,
+void test_pool_fp16(const std::vector<DDim>& input_dims,
                     const std::vector<int>& ksize,
                     const std::vector<int>& strides,
                     const std::vector<int>& pads,
@@ -199,7 +228,7 @@ TEST(TestPoolRand, test_pool_rand) {
                                 dims.push_back(DDim({batch, cin, h, h}));
                               }
                             }
-                            test_pool_fp32(
+                            test_pool_fp16(
                                 dims,
                                 {kh, kw},
                                 {stride, stride},
@@ -229,8 +258,8 @@ TEST(TestPoolRand, test_pool_rand) {
 #endif  /// random param conv
 
 #if 1  /// custom
-TEST(TesPoolCustom, test_pool_fp32_custom_size) {
-  test_pool_fp32(
+TEST(TesPoolCustom, test_pool_fp16_custom_size) {
+  test_pool_fp16(
       {DDim({FLAGS_batch, FLAGS_in_channel, FLAGS_in_height, FLAGS_in_width})},
       {FLAGS_kernel_h, FLAGS_kernel_w},
       {FLAGS_stride_h, FLAGS_stride_w},
