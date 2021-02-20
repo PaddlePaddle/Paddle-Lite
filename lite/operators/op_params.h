a044328ddf802653ffcbf33729aa3557c929b276
zhupengyang <zhu_py@qq.com>
2021-02-20 13:36:56 +0800

    [HOST] add tril_triu; fix expand_as (#5507)

--- lite/operators/op_params.h
+++ lite/operators/op_params.h
@@ -664,6 +664,14 @@ struct TransposeParam : ParamBase {
   }
 };
 
+struct TrilTriuParam : ParamBase {
+  const lite::Tensor* x{nullptr};
+  lite::Tensor* out{nullptr};
+
+  int diagonal{0};
+  bool lower{true};
+};
+
 /// ----------------------- element wise operators ----------------------
 struct ElementwiseParam : ParamBase {
   const lite::Tensor* X{};
