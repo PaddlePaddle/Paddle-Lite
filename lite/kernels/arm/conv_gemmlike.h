bfe5792493c649bf87519354847b44f0fb4797f0
pengjuncai <13006307475@163.com>
2021-03-01 09:28:06 +0000

    fix bug

--- lite/kernels/arm/conv_gemmlike.h
+++ lite/kernels/arm/conv_gemmlike.h
@@ -116,7 +116,7 @@ class GemmLikeConv : public KernelLite<TARGET(kARM), Ptype> {
     ch->kernel_func_name = kernel_func_name_;                               \
   }
 
-#define KERNEL_FUNC_NAME(kernel_func_name) kernel_func_name_ = kernel_func_name
+#define KERNEL_FUNC_NAME(kernel_func_name) kernel_func_name_ = kernel_func_name;
 #else
 #define PROFILE_INFO(dtype1, dtype2)
 #define KERNEL_FUNC_NAME(kernel_func_name)
