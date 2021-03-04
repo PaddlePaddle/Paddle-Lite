c7dc5c0a63121aff632056f376cb6fd264c86f60
HappyAngel <chenjiaobuaa@126.com>
2021-03-04 14:31:58 +0800

    fix conflict

--- lite/operators/op_params.h
+++ lite/operators/op_params.h
@@ -1119,9 +1119,12 @@ struct WhileParam : ParamBase {
 
 struct TopkParam : ParamBase {
   const lite::Tensor* X{};
+  const lite::Tensor* KTensor{};
   lite::Tensor* Out{};
   lite::Tensor* Indices{};
+  bool k_is_tensor{false};
   int K{1};
+  int axis{-1};
 };
 
 struct IncrementParam : ParamBase {
