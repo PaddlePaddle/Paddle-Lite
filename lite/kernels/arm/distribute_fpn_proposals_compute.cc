b3c3b1260afb532b249a944814b6130cacdf6921
cc <52520497+juncaipeng@users.noreply.github.com>
2021-03-05 14:05:05 +0800

    [Support Tinybert ] add int64_t elementwise_sub and fix distribute_fpn_proposals op (#5626)
    * add int64_t elementwise_sub and fix distribute_fpn_proposals op, test=develop

--- lite/kernels/arm/distribute_fpn_proposals_compute.cc
+++ lite/kernels/arm/distribute_fpn_proposals_compute.cc
@@ -174,9 +174,12 @@ REGISTER_LITE_KERNEL(distribute_fpn_proposals,
                      paddle::lite::kernels::arm::DistributeFpnProposalsCompute,
                      def)
     .BindInput("FpnRois", {LiteType::GetTensorTy(TARGET(kARM))})
-    .BindInput("RoisNum", {LiteType::GetTensorTy(TARGET(kARM))})
+    .BindInput("RoisNum",
+               {LiteType::GetTensorTy(TARGET(kARM), PRECISION(kInt32))})
     .BindOutput("MultiFpnRois", {LiteType::GetTensorTy(TARGET(kARM))})
-    .BindOutput("MultiLevelRoIsNum", {LiteType::GetTensorTy(TARGET(kARM))})
-    .BindOutput("RestoreIndex", {LiteType::GetTensorTy(TARGET(kARM))})
+    .BindOutput("MultiLevelRoIsNum",
+                {LiteType::GetTensorTy(TARGET(kARM), PRECISION(kInt32))})
+    .BindOutput("RestoreIndex",
+                {LiteType::GetTensorTy(TARGET(kARM), PRECISION(kInt32))})
     .BindPaddleOpVersion("distribute_fpn_proposals", 1)
     .Finalize();
