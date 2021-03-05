4c8a895b7527dcd5b22274f9c224ae46ec17b2c7
Zhang Jun <ewalker@live.cn>
2021-03-05 13:50:09 +0800

    [x86] avx2 bilinear interp (#5605)

--- lite/backends/x86/math/interpolate.cc
+++ lite/backends/x86/math/interpolate.cc
@@ -26,94 +26,431 @@ void bilinear_interp(const float* input_data,
                      float* output_data,
                      const float ratio_h,
                      const float ratio_w,
-                     const int in_h,
-                     const int in_w,
+                     const int h_in,
+                     const int w_in,
                      const int n,
                      const int c,
-                     const int out_h,
-                     const int out_w,
+                     const int h_out,
+                     const int w_out,
                      const bool align_corners,
                      const bool align_mode) {
-  bool align_flag = (align_mode == 0 && !align_corners);
+  int* buf = static_cast<int*>(lite::host::malloc(
+      sizeof(int) * (w_out + h_out + w_out * 2 + h_out * 2)));
 
-  std::vector<int> vy_n, vy_s;
-  std::vector<float> vd_n, vd_s;
-  vy_n.reserve(out_h);
-  vy_s.reserve(out_h);
-  vd_n.reserve(out_h);
-  vd_s.reserve(out_h);
+  int* xofs = buf;
+  int* yofs = buf + w_out;
+
+  float* alpha = reinterpret_cast<float*>(buf + w_out + h_out);
+  float* beta = reinterpret_cast<float*>(buf + w_out + h_out + w_out * 2);
+
+  float fx = 0.0f;
+  float fy = 0.0f;
+  int sx = 0;
+  int sy = 0;
+  if (align_corners) {
+    // calculate x axis coordinate
+    for (int dx = 0; dx < w_out; dx++) {
+      fx = dx * ratio_w;
+      sx = static_cast<int>(fx);
+      fx -= sx;
+      xofs[dx] = sx;
+      alpha[dx * 2] = 1.f - fx;
+      alpha[dx * 2 + 1] = fx;
+    }
+    // calculate y axis coordinate
+    for (int dy = 0; dy < h_out; dy++) {
+      fy = dy * ratio_h;
+      sy = static_cast<int>(fy);
+      fy -= sy;
+      yofs[dy] = sy;
+      beta[dy * 2] = 1.f - fy;
+      beta[dy * 2 + 1] = fy;
+    }
+  } else {
+    // calculate x axis coordinate
+    for (int dx = 0; dx < w_out; dx++) {
+      fx = align_mode ? ratio_w * dx : ratio_w * (dx + 0.5f) - 0.5f;
+      fx = fx < 0 ? 0.f : fx;
+      sx = static_cast<int>(fx);
+      fx -= sx;
+      xofs[dx] = sx;
+      alpha[dx * 2] = 1.f - fx;
+      alpha[dx * 2 + 1] = fx;
+    }
+    // calculate y axis coordinate
+    for (int dy = 0; dy < h_out; dy++) {
+      fy = align_mode ? ratio_h * dy : ratio_h * (dy + 0.5f) - 0.5f;
+      fy = fy < 0 ? 0.f : fy;
+      sy = static_cast<int>(fy);
+      fy -= sy;
+      yofs[dy] = sy;
+      beta[dy * 2] = 1.f - fy;
+      beta[dy * 2 + 1] = fy;
+    }
+  }
+  // output w , h boundary
+  int w_bound = w_out;
+  int h_bound = h_out;
+  if (ratio_w > 0 && ratio_h > 0) {
+    if (align_corners) {
+      w_bound = ceil((w_in - 1) / ratio_w);
+      h_bound = ceil((h_in - 1) / ratio_h);
+    } else {
+      w_bound = ceil((w_in - 0.5f) / ratio_w - 0.5f);
+      h_bound = ceil((h_in - 0.5f) / ratio_h - 0.5f);
+    }
+  }
+  int in_stride = h_in * w_in;
+  int out_stride = h_out * w_out;
+  int total = n * c;
+
 #ifdef PADDLE_WITH_MKLML
 #pragma omp parallel for
 #endif
-  for (int k = 0; k < out_h; k++) {
-    int y_n = align_flag ? static_cast<int>(ratio_h * (k + 0.5) - 0.5)
-                         : static_cast<int>(ratio_h * k);
-    y_n = (y_n > 0) ? y_n : 0;
-    int y_s = (y_n + 1) < (in_h - 1) ? (y_n + 1) : (in_h - 1);
-    float idx_src_y = ratio_h * (k + 0.5) - 0.5;
-    idx_src_y = (idx_src_y > 0) ? idx_src_y : 0;
-    float d_n = align_flag ? idx_src_y - y_n : ratio_h * k - y_n;
-    float d_s = 1.f - d_n;
-    {
-      vy_n[k] = y_n;
-      vy_s[k] = y_s;
-      vd_n[k] = d_n;
-      vd_s[k] = d_s;
-    }
-  }
+  for (int nc = 0; nc < total; ++nc) {
+    const float* src = input_data + nc * in_stride;
+    float* dst = output_data + nc * out_stride;
+    const float* betap = beta;
 
-  std::vector<int> vx_w, vx_e;
-  std::vector<float> vd_w, vd_e;
-  vx_w.reserve(out_w);
-  vx_e.reserve(out_w);
-  vd_w.reserve(out_w);
-  vd_e.reserve(out_w);
-#ifdef PADDLE_WITH_MKLML
-#pragma omp parallel for
-#endif
-  for (int l = 0; l < out_w; l++) {
-    int x_w = (align_mode == 0 && !align_corners)
-                  ? static_cast<int>(ratio_w * (l + 0.5) - 0.5)
-                  : static_cast<int>(ratio_w * l);
-    x_w = (x_w > 0) ? x_w : 0;
-    int x_e = (x_w + 1) < (in_w - 1) ? (x_w + 1) : (in_w - 1);
-    float idx_src_x = ratio_w * (l + 0.5) - 0.5;
-    idx_src_x = (idx_src_x > 0) ? idx_src_x : 0;
-    float d_w = align_flag ? idx_src_x - x_w : ratio_w * l - x_w;
-    float d_e = 1.f - d_w;
-    {
-      vx_w[l] = x_w;
-      vx_e[l] = x_e;
-      vd_w[l] = d_w;
-      vd_e[l] = d_e;
-    }
-  }
+    float* rowsbuf0 =
+        static_cast<float*>(lite::host::malloc(sizeof(int) * w_out));
+    float* rowsbuf1 =
+        static_cast<float*>(lite::host::malloc(sizeof(int) * w_out));
+    float* rows0 = rowsbuf0;
+    float* rows1 = rowsbuf1;
+    // h_bound loop
+    for (int dy = 0; dy < h_bound; dy++) {
+      int sy = yofs[dy];
 
-  int total_count = n * c;
+      const float* s0 = src + sy * w_in;
+      const float* s1 = src + (sy + 1) * w_in;
 
-#ifdef PADDLE_WITH_MKLML
-#if !defined(WIN32)
-#pragma omp parallel for collapse(3)
-#else
-#pragma omp parallel for
-#endif  // WIN32
-#endif  // PADDLE_WITH_MKLML
-  for (int i = 0; i < total_count; i++) {
-    for (int h = 0; h < out_h; h++) {
-      for (int w = 0; w < out_w; w++) {
-        // bilinear interpolation
-        const float* input_data_ptr = input_data + i * in_h * in_w;
-        float* output_data_ptr =
-            output_data + i * out_h * out_w + h * out_w + w;
-        *output_data_ptr =
-            input_data_ptr[vy_n[h] * in_w + vx_w[w]] * vd_s[h] * vd_e[w] +
-            input_data_ptr[vy_s[h] * in_w + vx_w[w]] * vd_n[h] * vd_e[w] +
-            input_data_ptr[vy_n[h] * in_w + vx_e[w]] * vd_s[h] * vd_w[w] +
-            input_data_ptr[vy_s[h] * in_w + vx_e[w]] * vd_n[h] * vd_w[w];
+      const float* alphap = alpha;
+      float* rows0p = rows0;
+      float* rows1p = rows1;
+
+      int dx = 0;
+// w_bound loop
+#ifdef __AVX__
+      for (; dx + 3 < w_bound; dx += 4) {
+        int x0 = xofs[dx];
+        int x1 = xofs[dx + 1];
+        int x2 = xofs[dx + 2];
+        int x3 = xofs[dx + 3];
+
+        const float* s0p0 = s0 + x0;
+        const float* s0p1 = s0 + x1;
+        const float* s0p2 = s0 + x2;
+        const float* s0p3 = s0 + x3;
+
+        const float* s0p0_1 = s0p0 + 1;
+        const float* s0p1_1 = s0p1 + 1;
+        const float* s0p2_1 = s0p2 + 1;
+        const float* s0p3_1 = s0p3 + 1;
+
+        const float* s1p0 = s1 + x0;
+        const float* s1p1 = s1 + x1;
+        const float* s1p2 = s1 + x2;
+        const float* s1p3 = s1 + x3;
+
+        const float* s1p0_1 = s1p0 + 1;
+        const float* s1p1_1 = s1p1 + 1;
+        const float* s1p2_1 = s1p2 + 1;
+        const float* s1p3_1 = s1p3 + 1;
+
+        __m256 _a = _mm256_loadu_ps(alphap);
+
+        __m256 _s0p0p3 = _mm256_set_ps(
+            *s0p3_1, *s0p3, *s0p2_1, *s0p2, *s0p1_1, *s0p1, *s0p0_1, *s0p0);
+
+        __m256 _ms0 = _mm256_mul_ps(_s0p0p3, _a);
+        __m256 _s1p0p3 = _mm256_set_ps(
+            *s1p3_1, *s1p3, *s1p2_1, *s1p2, *s1p1_1, *s1p1, *s1p0_1, *s1p0);
+        __m256 _ms1 = _mm256_mul_ps(_s1p0p3, _a);
+
+        __m256 _rows0 = _mm256_hadd_ps(_ms0, _ms0);
+        __m256 _rows1 = _mm256_hadd_ps(_ms1, _ms1);
+
+        __m256 _rs0 = _mm256_castpd_ps(
+            _mm256_permute4x64_pd(_mm256_castps_pd(_rows0), 0b11011000));
+        __m256 _rs1 = _mm256_castpd_ps(
+            _mm256_permute4x64_pd(_mm256_castps_pd(_rows1), 0b11011000));
+        _mm_storeu_ps(rows0p + dx, _mm256_castps256_ps128(_rs0));
+        _mm_storeu_ps(rows1p + dx, _mm256_castps256_ps128(_rs1));
+
+        alphap += 8;
       }
-    }
+#endif
+
+      // w_bound remain loop
+      for (; dx < w_bound; ++dx) {
+        int sx = xofs[dx];
+        const float* s0p = s0 + sx;
+        const float* s1p = s1 + sx;
+
+        float a0 = alphap[0];
+        float a1 = alphap[1];
+        rows0p[dx] = s0p[0] * a0 + s0p[1] * a1;
+        rows1p[dx] = s1p[0] * a0 + s1p[1] * a1;
+
+        alphap += 2;
+      }
+
+      float param0 = *(src + sy * w_in + w_in - 1);
+      float param1 = *(src + (sy + 1) * w_in + w_in - 1);
+      const float buffer0[2] = {param0, param0};
+      const float buffer1[2] = {param1, param1};
+#ifdef __AVX__
+      __m256 _s0p0p3 = _mm256_set1_ps(param0);
+      __m256 _s1p0p3 = _mm256_set1_ps(param1);
+      for (; dx + 3 < w_out; dx += 4) {
+        __m256 _a = _mm256_loadu_ps(alphap);
+
+        __m256 _ms0 = _mm256_mul_ps(_s0p0p3, _a);
+        __m256 _ms1 = _mm256_mul_ps(_s1p0p3, _a);
+
+        __m256 _rows0 = _mm256_hadd_ps(_ms0, _ms0);
+        __m256 _rows1 = _mm256_hadd_ps(_ms1, _ms1);
+
+        __m256 _rs0 = _mm256_castpd_ps(
+            _mm256_permute4x64_pd(_mm256_castps_pd(_rows0), 0b11011000));
+        __m256 _rs1 = _mm256_castpd_ps(
+            _mm256_permute4x64_pd(_mm256_castps_pd(_rows1), 0b11011000));
+        _mm_storeu_ps(rows0p + dx, _mm256_castps256_ps128(_rs0));
+        _mm_storeu_ps(rows1p + dx, _mm256_castps256_ps128(_rs1));
+
+        alphap += 8;
+      }
+#endif
+
+      // w_bound - w_out remain loop
+      for (; dx < w_out; dx++) {
+        const float* s0p = buffer0;
+        const float* s1p = buffer1;
+
+        float a0 = alphap[0];
+        float a1 = alphap[1];
+        rows0p[dx] = s0p[0] * a0 + s0p[1] * a1;
+        rows1p[dx] = s1p[0] * a0 + s1p[1] * a1;
+
+        alphap += 2;
+      }
+
+      float b0 = betap[0];
+      float b1 = betap[1];
+
+      // output pos
+      float* dp = dst + dy * w_out;
+
+      int nn = 0;
+
+#ifdef __AVX__
+      // 8 float
+      __m256 _b0 = _mm256_set1_ps(b0);
+      __m256 _b1 = _mm256_set1_ps(b1);
+      // calculate and store results
+      for (; nn + 7 < w_out; nn += 8) {
+        __m256 _rows0 = _mm256_loadu_ps(rows0p);
+        __m256 _rows1 = _mm256_loadu_ps(rows1p);
+
+        __m256 _d = _mm256_add_ps(_mm256_mul_ps(_rows0, _b0),
+                                  _mm256_mul_ps(_rows1, _b1));
+        _mm256_storeu_ps(dp, _d);
+
+        dp += 8;
+        rows0p += 8;
+        rows1p += 8;
+      }
+
+      // 4 float
+      __m128 _c0 = _mm_set1_ps(b0);
+      __m128 _c1 = _mm_set1_ps(b1);
+      for (; nn + 3 < w_out; nn += 4) {
+        __m128 _rows0 = _mm_loadu_ps(rows0p);
+        __m128 _rows1 = _mm_loadu_ps(rows1p);
+
+        __m128 _d =
+            _mm_add_ps(_mm_mul_ps(_rows0, _c0), _mm_mul_ps(_rows1, _c1));
+        _mm_storeu_ps(dp, _d);
+
+        dp += 4;
+        rows0p += 4;
+        rows1p += 4;
+      }
+#endif
+
+      // calculate and store remain resluts
+      for (; nn < w_out; ++nn) {
+        *dp++ = *rows0p++ * b0 + *rows1p++ * b1;
+      }
+      betap += 2;
+    }  // end h_bound loop
+
+    // h_bound - h_out loop
+    for (int dy = h_bound; dy < h_out; dy++) {
+      int sy = h_in - 1;
+      const float* s0 = src + sy * w_in;
+      const float* s1 = s0;
+      const float* alphap = alpha;
+      float* rows0p = rows0;
+      float* rows1p = rows1;
+
+      int dx = 0;
+#ifdef __AVX__
+      // w_bound loop
+      for (; dx + 3 < w_bound; dx += 4) {
+        int x0 = xofs[dx];
+        int x1 = xofs[dx + 1];
+        int x2 = xofs[dx + 2];
+        int x3 = xofs[dx + 3];
+
+        const float* s0p0 = s0 + x0;
+        const float* s0p1 = s0 + x1;
+        const float* s0p2 = s0 + x2;
+        const float* s0p3 = s0 + x3;
+
+        const float* s0p0_1 = s0p0 + 1;
+        const float* s0p1_1 = s0p1 + 1;
+        const float* s0p2_1 = s0p2 + 1;
+        const float* s0p3_1 = s0p3 + 1;
+
+        const float* s1p0 = s1 + x0;
+        const float* s1p1 = s1 + x1;
+        const float* s1p2 = s1 + x2;
+        const float* s1p3 = s1 + x3;
+
+        const float* s1p0_1 = s1p0 + 1;
+        const float* s1p1_1 = s1p1 + 1;
+        const float* s1p2_1 = s1p2 + 1;
+        const float* s1p3_1 = s1p3 + 1;
+
+        __m256 _a = _mm256_loadu_ps(alphap);
+
+        __m256 _s0p0p3 = _mm256_set_ps(
+            *s0p3_1, *s0p3, *s0p2_1, *s0p2, *s0p1_1, *s0p1, *s0p0_1, *s0p0);
+        __m256 _ms0 = _mm256_mul_ps(_s0p0p3, _a);
+        __m256 _s1p0p3 = _mm256_set_ps(
+            *s1p3_1, *s1p3, *s1p2_1, *s1p2, *s1p1_1, *s1p1, *s1p0_1, *s1p0);
+        __m256 _ms1 = _mm256_mul_ps(_s1p0p3, _a);
+
+        __m256 _rows0 = _mm256_hadd_ps(_ms0, _ms0);
+        __m256 _rows1 = _mm256_hadd_ps(_ms1, _ms1);
+
+        __m256 _rs0 = _mm256_castpd_ps(
+            _mm256_permute4x64_pd(_mm256_castps_pd(_rows0), 0b11011000));
+        __m256 _rs1 = _mm256_castpd_ps(
+            _mm256_permute4x64_pd(_mm256_castps_pd(_rows1), 0b11011000));
+        _mm_storeu_ps(rows0p + dx, _mm256_castps256_ps128(_rs0));
+        _mm_storeu_ps(rows1p + dx, _mm256_castps256_ps128(_rs1));
+
+        alphap += 8;
+      }
+#endif
+
+      // w_bound remain loop
+      for (; dx < w_bound; ++dx) {
+        int sx = xofs[dx];
+        const float* s0p = s0 + sx;
+        float a0 = alphap[0];
+        float a1 = alphap[1];
+        rows0p[dx] = s0p[0] * a0 + s0p[1] * a1;
+        rows1p[dx] = rows0p[dx];
+
+        alphap += 2;
+      }
+
+      float param = *(src + sy * w_in + w_in - 1);
+      const float buffer1[2] = {param, param};
+
+#ifdef __AVX__
+      __m256 _s0p0p3 = _mm256_set1_ps(param);
+      __m256 _s1p0p3 = _mm256_set1_ps(param);
+
+      // w_bound - w_out loop
+      for (; dx + 3 < w_out; dx += 4) {
+        __m256 _a = _mm256_loadu_ps(alphap);
+
+        __m256 _ms0 = _mm256_mul_ps(_s0p0p3, _a);
+        __m256 _ms1 = _mm256_mul_ps(_s1p0p3, _a);
+
+        __m256 _rows0 = _mm256_hadd_ps(_ms0, _ms0);
+        __m256 _rows1 = _mm256_hadd_ps(_ms1, _ms1);
+
+        __m256 _rs0 = _mm256_castpd_ps(
+            _mm256_permute4x64_pd(_mm256_castps_pd(_rows0), 0b11011000));
+        __m256 _rs1 = _mm256_castpd_ps(
+            _mm256_permute4x64_pd(_mm256_castps_pd(_rows1), 0b11011000));
+        _mm_storeu_ps(rows0p + dx, _mm256_castps256_ps128(_rs0));
+        _mm_storeu_ps(rows1p + dx, _mm256_castps256_ps128(_rs1));
+
+        alphap += 8;
+      }
+#endif
+
+      // w_bound - wout remain loop
+      for (; dx < w_out; dx++) {
+        const float* s0p = buffer1;
+        float a0 = alphap[0];
+        float a1 = alphap[1];
+        rows0p[dx] = s0p[0] * a0 + s0p[1] * a1;
+        rows1p[dx] = rows0p[dx];
+        alphap += 2;
+      }
+
+      float b0 = betap[0];
+      float b1 = betap[1];
+
+      float* dp = dst + dy * w_out;
+
+      int nn = 0;
+
+#ifdef __AVX__
+      // 8 float
+      __m256 _b0 = _mm256_set1_ps(b0);
+      __m256 _b1 = _mm256_set1_ps(b1);
+      // calculate and store results
+      for (; nn + 7 < w_out; nn += 8) {
+        __m256 _rows0 = _mm256_loadu_ps(rows0p);
+        __m256 _rows1 = _mm256_loadu_ps(rows1p);
+
+        __m256 _d = _mm256_add_ps(_mm256_mul_ps(_rows0, _b0),
+                                  _mm256_mul_ps(_rows1, _b1));
+        _mm256_storeu_ps(dp, _d);
+
+        dp += 8;
+        rows0p += 8;
+        rows1p += 8;
+      }
+
+      // 4 float
+      __m128 _c0 = _mm_set1_ps(b0);
+      __m128 _c1 = _mm_set1_ps(b1);
+      for (; nn + 3 < w_out; nn += 4) {
+        __m128 _rows0 = _mm_loadu_ps(rows0p);
+        __m128 _rows1 = _mm_loadu_ps(rows1p);
+
+        __m128 _d =
+            _mm_add_ps(_mm_mul_ps(_rows0, _c0), _mm_mul_ps(_rows1, _c1));
+        _mm_storeu_ps(dp, _d);
+
+        dp += 4;
+        rows0p += 4;
+        rows1p += 4;
+      }
+#endif
+      // calculate and store remain results
+      for (; nn < w_out; ++nn) {
+        *dp++ = *rows0p++ * b0 + *rows1p++ * b1;
+      }
+
+      betap += 2;
+    }  // end h_bound - h_out loop
+    lite::host::free(rowsbuf0);
+    lite::host::free(rowsbuf1);
   }
+  lite::host::free(buf);
 }
+
 void nearest_interp(const float* input_data,
                     float* output_data,
                     const float ratio_h,
