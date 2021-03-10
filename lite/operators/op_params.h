bfe1479adabdbd5c2f8e40b14ceeecd43d376007
mayang002 <77949147+mayang002@users.noreply.github.com>
2021-03-10 14:58:42 +0800

    [cherry-pick 2.8][XPU] refine xpu_multi_encoder, support transformer encoder(int16) (#5475)

--- lite/operators/op_params.h
+++ lite/operators/op_params.h
@@ -1872,6 +1872,7 @@ struct XPUMultiEncoderParam : ParamBase {
   std::string act_type{};
   std::string precision{};
   bool enable_qkv_fusion{false};
+  bool norm_before{false};
 };
 
 struct XPUEmbeddingWithEltwiseAddParam : ParamBase {
