94b1ab49853c9760dd9a16525e78d74ef5867f1e
zhupengyang <zhu_py@qq.com>
2021-03-26 06:49:31 +0000

    test=develop

--- lite/core/mir/generate_program_pass.h
+++ lite/core/mir/generate_program_pass.h
@@ -40,6 +40,7 @@ class GenerateProgramPass : public ProgramPass {
   std::unique_ptr<RuntimeProgram> GenProgram() {
     LOG(INFO) << "insts.size: " << insts_.size();
 
+#ifdef LITE_WITH_XPU
     // generate RuntimeProgram for sub_block and set RuntimeProgram into
     // sub_block kernel
     // sub_block: while, conditional_block
@@ -76,6 +77,7 @@ class GenerateProgramPass : public ProgramPass {
         }
       }
     }
+#endif
 
     // generate RuntimeProgram for main block
     std::unique_ptr<RuntimeProgram> program(
