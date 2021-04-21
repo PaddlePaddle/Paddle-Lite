0d3476c12d4782c844754cebe7bff2c02e130a0e
zhupengyang <zhu_py@qq.com>
2021-04-21 10:22:52 +0800

    [X86] add elementwise_pow, sqrt (#5928)

--- lite/kernels/x86/activation_compute.h
+++ lite/kernels/x86/activation_compute.h
@@ -307,6 +307,28 @@ class Relu6Compute : public KernelLite<TARGET(kX86), PRECISION(kFloat)> {
   virtual ~Relu6Compute() = default;
 };
 
+template <typename T>
+struct SqrtFunctor : public BaseActivationFunctor<T> {
+  template <typename Device, typename X, typename Out>
+  void operator()(Device d, X x, Out out) const {
+    out.device(d) = x.sqrt();
+  }
+};
+
+template <typename T>
+class SqrtCompute : public KernelLite<TARGET(kX86), PRECISION(kFloat)> {
+ public:
+  using param_t = operators::ActivationParam;
+
+  void Run() override {
+    auto& param = *param_.get_mutable<operators::ActivationParam>();
+    param.Out->template mutable_data<T>();
+    Activate<SqrtFunctor<T>>(param.X, param.Out);
+  }
+
+  virtual ~SqrtCompute() = default;
+};
+
 }  // namespace x86
 }  // namespace kernels
 }  // namespace lite
