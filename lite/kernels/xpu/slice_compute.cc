43a9a4ebf11d10792f361edb98a09313079fa591
weihaoji <68884893+weihaoji@users.noreply.github.com>
2021-05-07 16:26:12 +0800

    add xpu slice int64 kernel (#6026)

--- lite/kernels/xpu/slice_compute.cc
+++ lite/kernels/xpu/slice_compute.cc
@@ -218,3 +218,18 @@ REGISTER_LITE_KERNEL(slice, kXPU, kFloat, kAny, SliceInt32, int32)
                {LiteType::GetTensorTy(TARGET(kHost), PRECISION(kAny))})
     .BindOutput("Out", {LiteType::GetTensorTy(TARGET(kXPU), PRECISION(kInt32))})
     .Finalize();
+
+using SliceInt64 = paddle::lite::kernels::xpu::SliceCompute<int64_t>;
+REGISTER_LITE_KERNEL(slice, kXPU, kFloat, kAny, SliceInt64, int64)
+    .BindInput("Input",
+               {LiteType::GetTensorTy(TARGET(kXPU), PRECISION(kInt64))})
+    .BindInput("StartsTensor",
+               {LiteType::GetTensorTy(TARGET(kHost), PRECISION(kAny))})
+    .BindInput("EndsTensor",
+               {LiteType::GetTensorTy(TARGET(kHost), PRECISION(kAny))})
+    .BindInput("StartsTensorList",
+               {LiteType::GetTensorTy(TARGET(kHost), PRECISION(kAny))})
+    .BindInput("EndsTensorList",
+               {LiteType::GetTensorTy(TARGET(kHost), PRECISION(kAny))})
+    .BindOutput("Out", {LiteType::GetTensorTy(TARGET(kXPU), PRECISION(kInt64))})
+    .Finalize();
