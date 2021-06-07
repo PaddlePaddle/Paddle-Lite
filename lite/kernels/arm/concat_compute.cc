279b71958f5fe50abd0713dd66df9cddf3e49817
HappyAngel <chenjiaobuaa@126.com>
2021-06-07 18:56:57 +0800

    fix concat error when input tensor type is different. test=develop (#6228)

--- lite/kernels/arm/concat_compute.cc
+++ lite/kernels/arm/concat_compute.cc
@@ -19,6 +19,9 @@
 #include "lite/core/op_registry.h"
 #include "lite/core/tensor.h"
 #include "lite/core/type_system.h"
+#ifdef ENABLE_ARM_FP16
+#include "lite/backends/arm/math/fp16/funcs_fp16.h"
+#endif
 
 namespace paddle {
 namespace lite {
@@ -77,6 +80,25 @@ void ConcatCompute::Run() {
       if (type == PRECISION(kUnk)) {
         type = tensor->precision();
       } else {
+        VLOG(4) << "type: " << PrecisionRepr(type)
+                << ", tensor: " << PrecisionRepr(tensor->precision());
+#ifdef ENABLE_ARM_FP16
+        if (type != tensor->precision() &&
+            tensor->precision() == PrecisionType::kFP16 &&
+            type == PrecisionType::kFloat) {
+          // fp16 ==> flaot
+          Tensor tmp;
+          auto shape = tensor->dims();
+          tmp.Resize(shape);
+          tmp.set_precision(PrecisionType::kFloat);
+          auto dout = tmp.mutable_data<float>();
+          auto din = tensor->data<float16_t>();
+          auto size = tmp.numel();
+          lite::arm::math::fp16::fp16_to_fp32(din, dout, tensor->numel());
+          tensor->CopyDataFrom(tmp);
+          continue;
+        }
+#endif
         CHECK(type == tensor->precision()) << "The precision of "
                                            << "concat inputs should be same.";
       }
