fe8d39f4e9fa1bf47a9b61e2261899a5b2d38f03
weihaoji <weihaoji@baidu.com>
2021-06-15 20:18:29 +0800

    [XPU] add xpu op prelu, bugfix on box_clip and cast, test=develop, test=xpu

--- lite/tests/kernels/activation_compute_test.cc
+++ lite/tests/kernels/activation_compute_test.cc
@@ -467,6 +467,8 @@ TEST(Activation_prelu, precision) {
   abs_error = 1e-2;  // Using fp16 in OPENCL
 #elif defined(LITE_WITH_ARM)
   place = TARGET(kARM);
+#elif defined(LITE_WITH_XPU) && defined(LITE_WITH_XTCL)
+  place = TARGET(kXPU);
 #else
   return;
 #endif
