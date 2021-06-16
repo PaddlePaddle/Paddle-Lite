d4329f6fbaeb6f17c8595f2a813692107bfaa716
zhupengyang <zhu_py@qq.com>
2021-06-16 14:06:07 +0800

    [Host] add generate_proposals (#6265)

--- lite/kernels/host/generate_proposals_compute.h
+++ lite/kernels/host/generate_proposals_compute.h
@@ -13,17 +13,16 @@
 // limitations under the License.
 
 #pragma once
-#include <algorithm>
 #include "lite/core/kernel.h"
 #include "lite/operators/generate_proposals_op.h"
 
 namespace paddle {
 namespace lite {
 namespace kernels {
-namespace arm {
+namespace host {
 
 class GenerateProposalsCompute
-    : public KernelLite<TARGET(kARM), PRECISION(kFloat)> {
+    : public KernelLite<TARGET(kHost), PRECISION(kFloat)> {
  public:
   using param_t = operators::GenerateProposalsParam;
 
@@ -32,7 +31,7 @@ class GenerateProposalsCompute
   virtual ~GenerateProposalsCompute() = default;
 };
 
-}  // namespace arm
+}  // namespace host
 }  // namespace kernels
 }  // namespace lite
 }  // namespace paddle
