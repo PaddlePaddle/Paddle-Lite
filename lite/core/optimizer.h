2127551933c8fdc00eee75b8f4f79380e21badd9
zhaoyang-star <zhaoyangstar@foxmail.com>
2021-06-19 10:16:38 +0800

    [Pass][OpenCL] Add conv+elementwise_add tree fuse pass (#6279)

--- lite/core/optimizer.h
+++ lite/core/optimizer.h
@@ -118,6 +118,7 @@ class Optimizer {
          "lite_fc_prelu_fuse_pass",                     //
          "lite_elementwise_activation_fuse_pass",
          "lite_conv_scale_fuse_pass",
+         "lite_conv_elementwise_tree_fuse_pass",
          "identity_dropout_eliminate_pass",
          "__xpu__graph_dedup_pass",
          "__xpu__resnet_fuse_pass",
