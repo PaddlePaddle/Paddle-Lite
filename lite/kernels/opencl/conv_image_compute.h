03d114e48bc14cd1f097b5d5a1422a7be8e0f257
zhaoyang-star <zhaoyangstar@foxmail.com>
2021-06-28 22:14:22 +0800

    .h

--- lite/kernels/opencl/conv_image_compute.h
+++ lite/kernels/opencl/conv_image_compute.h
@@ -78,7 +78,9 @@ class ConvImageCompute : public KernelLite<TARGET(kOpenCL),
   void Conv2d1x1Mali();
   void OIHW2OHWIO4I4(
       void* src, void* dst, size_t O, size_t I, size_t H, size_t W);
+  void OI2IOO4I4(void* src, void* dst, size_t O, size_t I);
   void AssignDataFromCPUToGPU(const Tensor* tensor_cpu_p, Tensor* tensor_gpu_p);
+  bool UseFcReplaceConv();
 
   param_t* conv_param_{nullptr};
 
