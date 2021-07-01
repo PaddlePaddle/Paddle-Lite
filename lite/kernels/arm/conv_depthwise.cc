53f078bb58167ee2ffa0195a85f113685f7a9502
津 <33440396+xingjing1@users.noreply.github.com>
2021-07-01 16:43:33 +0800

    [cherry-pick]Depthwise3x3s1relu6 2.9 (#6364)

--- lite/kernels/arm/conv_depthwise.cc
+++ lite/kernels/arm/conv_depthwise.cc
@@ -106,7 +106,8 @@ void DepthwiseConv<PRECISION(kInt8), PRECISION(kFloat)>::ReInitWhenNeeded() {
   // s2: only support pad=1
   bool support_act_type_s1 =
       (has_act == false) ||
-      (has_act == true && (act_type == lite_api::ActivationType::kRelu));
+      (has_act == true && (act_type == lite_api::ActivationType::kRelu ||
+                           act_type == lite_api::ActivationType::kRelu6));
   bool pads_equal = (paddings[0] == paddings[2]) && (paddings[0] < 2);
   bool support_pad_type_s2 = pads_equal && (paddings[0] == 1);
   bool support_stride_type_s1 = (strides[0] == 1 && strides[1] == 1);
@@ -202,7 +203,8 @@ void DepthwiseConv<PRECISION(kInt8), PRECISION(kInt8)>::ReInitWhenNeeded() {
   // s2: only support pad=1
   bool support_act_type_s1 =
       (has_act == false) ||
-      (has_act == true && (act_type == lite_api::ActivationType::kRelu));
+      (has_act == true && (act_type == lite_api::ActivationType::kRelu ||
+                           act_type == lite_api::ActivationType::kRelu6));
   bool pads_equal = (paddings[0] == paddings[2]) && (paddings[0] < 2);
   bool support_pad_type_s2 = pads_equal && (paddings[0] == 1);
   bool support_stride_type_s1 = (strides[0] == 1 && strides[1] == 1);
