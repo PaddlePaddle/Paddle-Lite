393b721d5c7e5ac5dc473c9d1992544703105880
zhaoyang-star <zhaoyangstar@foxmail.com>
2021-07-01 12:44:41 +0800

    [Cherry-pick] Replace conv1x1 with fc (#6389)
    * [OpenCL][Kernel] Use FC replace conv1x1 (#6365)
    
    * test=develop

--- lite/kernels/opencl/conv_image_compute.h
+++ lite/kernels/opencl/conv_image_compute.h
@@ -76,9 +76,12 @@ class ConvImageCompute : public KernelLite<TARGET(kOpenCL),
   void DepthwiseConv2d();
   void Conv2dCommon();
   void Conv2d1x1Mali();
+  void Conv2d1x1FC();
   void OIHW2OHWIO4I4(
       void* src, void* dst, size_t O, size_t I, size_t H, size_t W);
+  void OI2IOO4I4(void* src, void* dst, size_t O, size_t I);
   void AssignDataFromCPUToGPU(const Tensor* tensor_cpu_p, Tensor* tensor_gpu_p);
+  bool UseFcReplaceConv();
 
   param_t* conv_param_{nullptr};
 
