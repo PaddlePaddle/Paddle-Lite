014b41ece9820dd5407a94c0cf8ec138ded426e4
zhaoyang-star <zhaoyangstar@foxmail.com>
2021-07-08 15:16:41 +0800

    add fc image impl

---
+++ lite/kernels/opencl/fc_image_compute.cc
@@ -0,0 +1,305 @@
+// Copyright (c) 2021 PaddlePaddle Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <vector>
+#include "lite/backends/opencl/cl_include.h"
+#include "lite/core/kernel.h"
+#include "lite/core/op_registry.h"
+#include "lite/kernels/opencl/image_helper.h"
+#include "lite/operators/op_params.h"
+#include "lite/utils/replace_stl/stream.h"
+#include "lite/utils/string.h"
+#ifdef LITE_WITH_PROFILE
+#include "lite/core/profile/profiler.h"
+#endif
+#include "lite/backends/opencl/cl_image_converter.h"
+#include "lite/backends/opencl/cl_utility.h"
+
+namespace paddle {
+namespace lite {
+namespace kernels {
+namespace opencl {
+
+class FcCompute : public KernelLite<TARGET(kOpenCL),
+                                    PRECISION(kFP16),
+                                    DATALAYOUT(kImageDefault)> {
+ public:
+  void PrepareForRun() override {
+    auto& param = this->Param<operators::FcParam>();
+    const auto w_t = param.w;
+    const auto bias_t = param.bias;
+    has_bias_ = (bias_t == nullptr) ? false : true;
+
+    // convert weights from cpu to gpu
+    auto w_cpu_tensor = std::unique_ptr<Tensor>(new Tensor);
+    auto w_gpu_tensor = std::unique_ptr<Tensor>(new Tensor);
+
+    const auto w_dims = w_t->dims();
+    auto w_ext_dims = w_dims;
+    w_ext_dims[0] = ROUND_UP(w_dims[0], 4);
+    w_ext_dims[1] = ROUND_UP(w_dims[1], 4);
+    w_cpu_tensor->Resize(w_ext_dims);
+    auto* w_buffer_data = MUTABLE_DATA_CPU(w_cpu_tensor.get());
+    size_t buf_size = w_cpu_tensor->memory_size();
+
+    auto* w_cpu = param.w->mutable_data<float>();
+    OI2IOO4I4(w_cpu, w_buffer_data, w_dims[0], w_dims[1]);
+
+    auto* w_gpu_data = w_gpu_tensor->mutable_data(TARGET(kOpenCL),
+                                                  w_cpu_tensor->memory_size());
+    TargetWrapperCL::MemcpySync(w_gpu_data,
+                                w_cpu_tensor->raw_data(),
+                                w_cpu_tensor->memory_size(),
+                                IoDirection::HtoD);
+    w_buf_ = GET_BUFFER_GPU(w_gpu_tensor);
+
+    // convert bias from cpu to gpu
+    auto bias_cpu_tensor = std::unique_ptr<Tensor>(new Tensor);
+    auto bias_gpu_tensor = std::unique_ptr<Tensor>(new Tensor);
+    CLImageConverterFolder bias_converter;
+    if (has_bias_) {
+      const DDim& bias_image_dims =
+          bias_converter.InitImageDimInfoWith(param.bias->dims());
+      bias_cpu_tensor->Resize({1, bias_image_dims[0], bias_image_dims[1], 4});
+      auto* bias_image_data = MUTABLE_DATA_CPU(bias_cpu_tensor);
+      auto* bias_cpu = param.bias->mutable_data<float>();
+      bias_converter.NCHWToImage(bias_cpu, bias_image_data, param.bias->dims());
+      MUTABLE_DATA_GPU(bias_gpu_tensor,
+                       bias_image_dims[0],
+                       bias_image_dims[1],
+                       bias_image_data);
+
+      build_options_ += " -DBIASE_CH";
+    } else {
+      bias_cpu_tensor->Resize({1, 1, 1, 4});
+      auto* bias_image_data = DATA_GPU(bias_cpu_tensor);
+      MUTABLE_DATA_GPU(bias_gpu_tensor, 1, 1, bias_image_data);
+    }
+    bias_img_ = DATA_GPU(bias_gpu_tensor);
+
+    if (param.activation_type == "relu") {
+      build_options_ += " -DRELU";
+    } else if (param.activation_type == "relu6") {
+      build_options_ += " -DRELU6";
+    } else if (param.activation_type == "prelu") {
+      std::string prelu_mode = param.Prelu_mode;
+      build_options_ += " -DPRELU";
+      if (prelu_mode == "channel") {
+        build_options_ += " -DPRELU_CH";
+      } else if (prelu_mode == "element") {
+        build_options_ += " -DPRELU_ELE";
+      } else {
+        build_options_ += " -DPRELU_ALL";
+      }
+
+      // convert alpha from cpu to gpu
+      auto alpha_cpu_tensor = std::unique_ptr<Tensor>(new Tensor);
+      auto alpha_gpu_tensor = std::unique_ptr<Tensor>(new Tensor);
+      CLImageConverterFolder alpha_converter;
+      const DDim& alpha_image_dims =
+          alpha_converter.InitImageDimInfoWith(param.Prelu_alpha->dims());
+      alpha_cpu_tensor->Resize(
+          {1, alpha_image_dims[0], alpha_image_dims[1], 4});
+      auto* alpha_image_data = MUTABLE_DATA_CPU(alpha_cpu_tensor);
+      auto* alpha_cpu = param.Prelu_alpha->mutable_data<float>();
+      alpha_converter.NCHWToImage(
+          alpha_cpu, alpha_image_data, param.Prelu_alpha->dims());
+      MUTABLE_DATA_GPU(alpha_gpu_tensor,
+                       alpha_image_dims[0],
+                       alpha_image_dims[1],
+                       alpha_image_data);
+      alpha_img_ = DATA_GPU(alpha_gpu_tensor);
+    }
+  }
+
+  void ReInitWhenNeeded() override {
+    auto& param = this->Param<operators::FcParam>();
+    const auto x_dims = param.input->dims();
+    if ((!first_epoch_for_reinit_ && x_dims != last_x_dims_) ||
+        first_epoch_for_reinit_) {
+      last_x_dims_ = x_dims;
+      first_epoch_for_reinit_ = false;
+
+      // compute m,n,k
+      const auto w_dims = param.w->dims();
+      CHECK_GE(x_dims.size(), 2UL);
+      CHECK_GE(w_dims.size(), 2UL);
+      CHECK_EQ(param.output->dims().size(), 2UL);
+
+      m_ = x_dims.Slice(0, param.in_num_col_dims).production();
+      k_ = x_dims.Slice(param.in_num_col_dims, x_dims.size()).production();
+      n_ = w_dims[1];
+      CHECK_EQ(k_, static_cast<int>(w_dims[0]));
+      k_blks_ = UP_DIV(k_, 4);
+      n_blks_ = UP_DIV(n_, 4);
+
+      kernel_func_name_ = "conv2d_1x1_fc";
+#ifdef LITE_WITH_LOG
+      VLOG(1) << "kernel_func_name_:" << kernel_func_name_;
+      VLOG(4) << "x_dims:" << x_dims;
+      VLOG(4) << "w_dims:" << w_dims;
+      VLOG(4) << "m_: " << m_ << " n_: " << n_ << " k_: " << k_;
+#endif
+
+      auto& context = ctx_->As<OpenCLContext>();
+      context.cl_context()->AddKernel(kernel_func_name_,
+                                      "image/conv2d_1x1_opt_kernel.cl",
+                                      build_options_,
+                                      time_stamp_);
+      STL::stringstream kernel_key;
+      kernel_key << kernel_func_name_ << build_options_ << time_stamp_;
+      kernel_ = context.cl_context()->GetKernel(kernel_key.str());
+
+      SetGlobalLocalWorkSize();
+    }
+  }
+
+  void Run() override {
+    auto& param = this->Param<operators::FcParam>();
+
+    x_img_ = DATA_GPU(param.input);
+    out_img_ = MUTABLE_DATA_GPU(param.output,
+                                UP_DIV(param.output->dims()[1], 4),
+                                param.output->dims()[0],
+                                nullptr);
+
+    auto& kernel = kernel_;
+    cl_int status;
+    int arg_idx = 0;
+    status = kernel.setArg(arg_idx++, *x_img_);
+    CL_CHECK_FATAL(status);
+    status = kernel.setArg(arg_idx++, *out_img_);
+    CL_CHECK_FATAL(status);
+    status = kernel.setArg(arg_idx++, *w_buf_);
+    CL_CHECK_FATAL(status);
+    if (has_bias_) {
+      status = kernel.setArg(arg_idx++, *bias_img_);
+      CL_CHECK_FATAL(status);
+    }
+    if (alpha_img_) {
+      status = kernel.setArg(arg_idx++, *alpha_img_);
+      CL_CHECK_FATAL(status);
+    }
+    status = kernel.setArg(arg_idx++, k_blks_);
+    CL_CHECK_FATAL(status);
+    status = kernel.setArg(arg_idx++, n_blks_);
+    CL_CHECK_FATAL(status);
+
+    auto& context = ctx_->As<OpenCLContext>();
+    CHECK(context.cl_context() != nullptr);
+
+    status = EnqueueNDRangeKernel(context,
+                                  kernel,
+                                  cl::NullRange,
+                                  global_work_size_,
+                                  local_work_size_,
+                                  nullptr,
+                                  event_);
+    CL_CHECK_FATAL(status);
+  }
+
+#ifdef LITE_WITH_PROFILE
+  void SetProfileRuntimeKernelInfo(paddle::lite::profile::OpCharacter* ch) {
+    ch->kernel_func_name = kernel_func_name_;
+    ch->global_work_size = global_work_size_;
+    ch->local_work_size = local_work_size_;
+    ch->cl_event =
+        event_;  // `event_` defined in `kernel.h`, valid after kernel::Run
+  }
+#endif
+
+  void SetGlobalLocalWorkSize() {
+    local_work_size_ = cl::NDRange(32, 4);
+    global_work_size_ = cl::NDRange(
+        ROUND_UP(UP_DIV(n_, 4), local_work_size_[0]), local_work_size_[1]);
+  }
+
+  void OI2IOO4I4(void* src, void* dst, size_t O, size_t I) {
+    bool fp16_support =
+        CLRuntime::Global()->get_precision() == lite_api::CL_PRECISION_FP16;
+    size_t padded_I = ROUND_UP(I, 4);
+    size_t padded_O = ROUND_UP(O, 4);
+
+    float* dst_fp32 = static_cast<float*>(dst);
+    half_t* dst_fp16 = static_cast<half_t*>(dst);
+    float* p_src = static_cast<float*>(src);
+
+    for (int block_y = 0; 4 * block_y < padded_O; block_y++) {
+      for (int y_in_block = 0; y_in_block < 4; y_in_block++) {
+        for (int block_x = 0; 4 * block_x < padded_I; block_x++) {
+          for (int x_in_block = 0; x_in_block < 4; x_in_block++) {
+            int y = 4 * block_y + y_in_block;
+            int x = 4 * block_x + x_in_block;
+            // Consider destination as an array with extents
+            // [padded_src_channels/4][padded_dst_channels/4][4][4]
+            int dst_index = block_x * padded_O * 4 + block_y * 16 +
+                            x_in_block * 4 + y_in_block;
+            if (x < I && y < O) {
+              fp16_support ? dst_fp16[dst_index] = Float2Half(p_src[I * y + x])
+                           : dst_fp32[dst_index] = p_src[I * y + x];
+            } else {
+              fp16_support ? dst_fp16[dst_index] = Float2Half(0.f)
+                           : dst_fp32[dst_index] = 0.f;
+            }
+          }
+        }
+      }
+    }
+  }
+
+ private:
+  int m_, n_, k_, k_blks_, n_blks_;
+  std::string kernel_func_name_{};
+  std::string build_options_{""};
+  std::string time_stamp_{GetTimeStamp()};
+  bool first_epoch_for_reinit_{true};
+  DDim last_x_dims_;
+
+  cl::NDRange global_work_size_;
+  cl::NDRange local_work_size_;
+
+  cl::Kernel kernel_;
+
+  cl::Image2D* x_img_{nullptr};
+  cl::Image2D* out_img_{nullptr};
+  const cl::Buffer* w_buf_{nullptr};
+  cl::Image2D* bias_img_{nullptr};
+  cl::Image2D* alpha_img_{nullptr};
+
+  bool has_bias_{false};
+};
+
+}  // namespace opencl
+}  // namespace kernels
+}  // namespace lite
+}  // namespace paddle
+
+REGISTER_LITE_KERNEL(fc,
+                     kOpenCL,
+                     kFP16,
+                     kImageDefault,
+                     paddle::lite::kernels::opencl::FcCompute,
+                     def)
+    .BindInput("Input",
+               {LiteType::GetTensorTy(TARGET(kOpenCL),
+                                      PRECISION(kFP16),
+                                      DATALAYOUT(kImageDefault))})
+    .BindInput("Bias", {LiteType::GetTensorTy(TARGET(kHost))})
+    .BindInput("W", {LiteType::GetTensorTy(TARGET(kHost))})
+    .BindInput("Alpha", {LiteType::GetTensorTy(TARGET(kHost))})
+    .BindOutput("Out",
+                {LiteType::GetTensorTy(TARGET(kOpenCL),
+                                       PRECISION(kFP16),
+                                       DATALAYOUT(kImageDefault))})
+    .Finalize();
