f3bd9f5bde70e93e8ed7cb4bfe50a707372e9aa5
root <root@gzbh-qianmo-com-162-69-147.gzbh.baidu.com>
2021-07-22 12:36:17 +0000

    update kernels

--- cmake/functions.cmake
+++ cmake/functions.cmake
@@ -1,6 +1,7 @@
 # add a kernel for some specific device
 # device: one of (Host, ARM, X86, NPU, MLU, HUAWEI_ASCEND_NPU, APU, FPGA, OPENCL, CUDA, BM, RKNPU IMAGINATION_NNA)
 # level: one of (basic, extra)
+set(KERNELS_SRC CACHE INTERNAL "kernels")
 function(add_kernel TARGET device level)
     set(options "")
     set(oneValueArgs "")
@@ -32,219 +33,18 @@ function(add_kernel TARGET device level)
         return()
     endif()
 
-
-    if ("${device}" STREQUAL "Host")
-       if (LITE_ON_MODEL_OPTIMIZE_TOOL)
-            foreach(src ${args_SRCS})
-                file(APPEND ${fake_kernels_src_list} "${CMAKE_CURRENT_SOURCE_DIR}/${src}\n")
-            endforeach()
-            return()
-        endif()
-        set(host_kernels "${host_kernels};${TARGET}" CACHE INTERNAL "")
-    endif()
-    if ("${device}" STREQUAL "ARM")
-        if (NOT LITE_WITH_ARM)
-            foreach(src ${args_SRCS})
-                file(APPEND ${fake_kernels_src_list} "${CMAKE_CURRENT_SOURCE_DIR}/${src}\n")
-            endforeach()
-            return()
-        endif()
-        set(arm_kernels "${arm_kernels};${TARGET}" CACHE INTERNAL "")
-    endif()
-    if ("${device}" STREQUAL "X86")
-        if (NOT LITE_WITH_X86 OR LITE_ON_MODEL_OPTIMIZE_TOOL)
-            foreach(src ${args_SRCS})
-                file(APPEND ${fake_kernels_src_list} "${CMAKE_CURRENT_SOURCE_DIR}/${src}\n")
-            endforeach()
-            return()
-        endif()
-        set(x86_kernels "${x86_kernels};${TARGET}" CACHE INTERNAL "")
-    endif()
-    if ("${device}" STREQUAL "NPU")
-        if (NOT LITE_WITH_NPU)
-            foreach(src ${args_SRCS})
-                file(APPEND ${fake_kernels_src_list} "${CMAKE_CURRENT_SOURCE_DIR}/${src}\n")
-            endforeach()
-            return()
-        endif()
-        set(npu_kernels "${npu_kernels};${TARGET}" CACHE INTERNAL "")
-    endif()
-    if ("${device}" STREQUAL "APU")
-        if (NOT LITE_WITH_APU)
-            foreach(src ${args_SRCS})
-                file(APPEND ${fake_kernels_src_list} "${CMAKE_CURRENT_SOURCE_DIR}/${src}\n")
-            endforeach()
-            return()
-        endif()
-        set(apu_kernels "${apu_kernels};${TARGET}" CACHE INTERNAL "")
-    endif()
-    if ("${device}" STREQUAL "XPU")
-        if (NOT LITE_WITH_XPU)
-            foreach(src ${args_SRCS})
-                file(APPEND ${fake_kernels_src_list} "${CMAKE_CURRENT_SOURCE_DIR}/${src}\n")
-            endforeach()
-            return()
-        endif()
-        set(xpu_kernels "${xpu_kernels};${TARGET}" CACHE INTERNAL "")
-    endif()
-    if ("${device}" STREQUAL "FPGA")
-        if (NOT LITE_WITH_FPGA)
-            foreach(src ${args_SRCS})
-                file(APPEND ${fake_kernels_src_list} "${CMAKE_CURRENT_SOURCE_DIR}/${src}\n")
-            endforeach()
-            return()
-        endif()
-        set(fpga_kernels "${fpga_kernels};${TARGET}" CACHE INTERNAL "")
-    endif()
-    if ("${device}" STREQUAL "INTEL_FPGA")
-        if (NOT LITE_WITH_INTEL_FPGA)
-            foreach(src ${args_SRCS})
-                file(APPEND ${fake_kernels_src_list} "${CMAKE_CURRENT_SOURCE_DIR}/${src}\n")
-            endforeach()
-            return()
-        endif()
-        set(intel_fpga_kernels "${intel_fpga_kernels};${TARGET}" CACHE INTERNAL "")
-    endif()
-    if ("${device}" STREQUAL "BM")
-        if (NOT LITE_WITH_BM)
-            foreach(src ${args_SRCS})
-                file(APPEND ${fake_kernels_src_list} "${CMAKE_CURRENT_SOURCE_DIR}/${src}\n")
-            endforeach()
-            return()
-        endif()
-        set(bm_kernels "${bm_kernels};${TARGET}" CACHE INTERNAL "")
-    endif()
-    if ("${device}" STREQUAL "RKNPU")
-        if (NOT LITE_WITH_RKNPU)
-            foreach(src ${args_SRCS})
-                file(APPEND ${fake_kernels_src_list} "${CMAKE_CURRENT_SOURCE_DIR}/${src}\n")
-            endforeach()
-            return()
-        endif()
-        set(rknpu_kernels "${rknpu_kernels};${TARGET}" CACHE INTERNAL "")
-    endif()
-    if ("${device}" STREQUAL "MLU")
-        if (NOT LITE_WITH_MLU)
-            foreach(src ${args_SRCS})
-                file(APPEND ${fake_kernels_src_list} "${CMAKE_CURRENT_SOURCE_DIR}/${src}\n")
-            endforeach()
-            return()
-        endif()
-        set(mlu_kernels "${mlu_kernels};${TARGET}" CACHE INTERNAL "")
-    endif()
-    if ("${device}" STREQUAL "IMAGINATION_NNA")
-        if (NOT LITE_WITH_IMAGINATION_NNA)
-            foreach(src ${args_SRCS})
-                file(APPEND ${fake_kernels_src_list} "${CMAKE_CURRENT_SOURCE_DIR}/${src}\n")
-            endforeach()
-            return()
-        endif()
-        set(imagination_nna_kernels "${imagination_nna_kernels};${TARGET}" CACHE INTERNAL "")
-    endif()
-    if ("${device}" STREQUAL "NNADAPTER")
-        if (NOT LITE_WITH_NNADAPTER)
-            foreach(src ${args_SRCS})
-                file(APPEND ${fake_kernels_src_list} "${CMAKE_CURRENT_SOURCE_DIR}/${src}\n")
-            endforeach()
-            return()
-        endif()
-        set(nnadapter_kernels "${nnadapter_kernels};${TARGET}" CACHE INTERNAL "")
-    endif()
-
-    if ("${device}" STREQUAL "HUAWEI_ASCEND_NPU")
-        if (NOT LITE_WITH_HUAWEI_ASCEND_NPU)
-            foreach(src ${args_SRCS})
-                file(APPEND ${fake_kernels_src_list} "${CMAKE_CURRENT_SOURCE_DIR}/${src}\n")
-            endforeach()
-            return()
-        endif()
-        set(huawei_ascend_npu_kernels "${huawei_ascend_npu_kernels};${TARGET}" CACHE INTERNAL "")
-    endif()
-    if ("${device}" STREQUAL "OPENCL")
-        if (NOT LITE_WITH_OPENCL)
-            foreach(src ${args_SRCS})
-                file(APPEND ${fake_kernels_src_list} "${CMAKE_CURRENT_SOURCE_DIR}/${src}\n")
-            endforeach()
-            return()
-        endif()
-        set(opencl_kernels "${opencl_kernels};${TARGET}" CACHE INTERNAL "")
-    endif()
-
-    if ("${device}" STREQUAL "METAL")
-        if (NOT LITE_WITH_METAL)
-            foreach(src ${args_SRCS})
-                file(APPEND ${fake_kernels_src_list} "${CMAKE_CURRENT_SOURCE_DIR}/${src}\n")
-            endforeach()
-            return()
-        endif()
-        set(metal_kernels "${metal_kernels};${TARGET}" CACHE INTERNAL "")
-    endif()
-
-    if ("${device}" STREQUAL "CUDA")
-        if (NOT LITE_WITH_CUDA)
-            foreach(src ${args_SRCS})
-                file(APPEND ${fake_kernels_src_list} "${CMAKE_CURRENT_SOURCE_DIR}/${src}\n")
-            endforeach()
-            return()
-        endif()
-        set(cuda_kernels "${cuda_kernels};${TARGET}" CACHE INTERNAL "")
-        foreach(src ${args_SRCS})
-          file(APPEND ${kernels_src_list} "${CMAKE_CURRENT_SOURCE_DIR}/${src}\n")
-        endforeach()
-        nv_library(${TARGET} SRCS ${args_SRCS} DEPS ${args_DEPS})
-        return()
-    endif()
-
-    # the source list will collect for paddle_use_kernel.h code generation.
-    foreach(src ${args_SRCS})
-        file(APPEND ${kernels_src_list} "${CMAKE_CURRENT_SOURCE_DIR}/${src}\n")
-    endforeach()
-
-    if (NOT LITE_BUILD_TAILOR)
-      lite_cc_library(${TARGET} SRCS ${args_SRCS}
-              DEPS ${args_DEPS}
-              X86_DEPS ${args_X86_DEPS}
-              CUDA_DEPS ${args_CUDA_DEPS}
-              CL_DEPS ${args_CL_DEPS}
-              METAL_DEPS ${args_METAL_DEPS}
-              ARM_DEPS ${args_ARM_DEPS}
-              FPGA_DEPS ${args_FPGA_DEPS}
-              INTEL_FPGA_DEPS ${args_INTEL_FPGA_DEPS}
-              NPU_DEPS ${args_NPU_DEPS}
-              APU_DEPS ${args_APU_DEPS}
-              XPU_DEPS ${args_XPU_DEPS}
-              RKNPU_DEPS ${args_RKNPU_DEPS}
-              BM_DEPS ${args_BM_DEPS}
-              MLU_DEPS ${args_MLU_DEPS}
-              IMAGINATION_NNA_DEPS ${args_IMAGINATION_NNA_DEPS}
-              HUAWEI_ASCEND_NPU_DEPS ${args_HUAWEI_ASCEND_NPU_DEPS}
-              PROFILE_DEPS ${args_PROFILE_DEPS}
-              LIGHT_DEPS ${args_LIGHT_DEPS}
-              HVY_DEPS ${args_HVY_DEPS})
-    else()
-      lite_cc_library(${TARGET} SRCS ${dst_file}
-              DEPS ${args_DEPS}
-              X86_DEPS ${args_X86_DEPS}
-              CUDA_DEPS ${args_CUDA_DEPS}
-              CL_DEPS ${args_CL_DEPS}
-              METAL_DEPS ${args_METAL_DEPS}
-              ARM_DEPS ${args_ARM_DEPS}
-              FPGA_DEPS ${args_FPGA_DEPS}
-              INTEL_FPGA_DEPS ${args_INTEL_FPGA_DEPS}
-              NPU_DEPS ${args_NPU_DEPS}
-              APU_DEPS ${args_APU_DEPS}
-              XPU_DEPS ${args_XPU_DEPS}
-              RKNPU_DEPS ${args_RKNPU_DEPS}
-              BM_DEPS ${args_BM_DEPS}
-              MLU_DEPS ${args_MLU_DEPS}
-              IMAGINATION_NNA_DEPS ${args_IMAGINATION_NNA_DEPS}
-              NNADAPTER_DEPS ${args_NNADAPTER_DEPS}
-              HUAWEI_ASCEND_NPU_DEPS ${args_HUAWEI_ASCEND_NPU_DEPS}
-              PROFILE_DEPS ${args_PROFILE_DEPS}
-              LIGHT_DEPS ${args_LIGHT_DEPS}
-              HVY_DEPS ${args_HVY_DEPS}
-      )
-    endif()
+    if (LITE_ON_MODEL_OPTIMIZE_TOOL)
+       foreach(src ${args_SRCS})
+           file(APPEND ${fake_kernels_src_list} "${CMAKE_CURRENT_SOURCE_DIR}/${src}\n")
+       endforeach()
+       return()
+   else()
+       # the source list will collect for paddle_use_kernel.h code generation.
+       foreach(src ${args_SRCS})
+           file(APPEND ${kernels_src_list} "${CMAKE_CURRENT_SOURCE_DIR}/${src}\n")
+           set(KERNELS_SRC ${KERNELS_SRC} "${CMAKE_CURRENT_SOURCE_DIR}/${src}" CACHE INTERNAL "kernel source")
+       endforeach() 
+   endif()
 endfunction()
 
 
@@ -284,5 +84,5 @@ function(add_operator TARGET level)
       endif()
       file(APPEND ${ops_src_list} "${CMAKE_CURRENT_SOURCE_DIR}/${src}\n")
       set(OPS_SRC ${OPS_SRC} "${CMAKE_CURRENT_SOURCE_DIR}/${src}" CACHE INTERNAL "source")
-endforeach()
-
+    endforeach()
+endfunction()
