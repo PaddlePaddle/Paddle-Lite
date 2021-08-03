c64008b55b12237e1c9828f9d5effe0c5891f20d
TangYuan-Liu <1131165566@qq.com>
2021-08-03 05:43:42 +0000

    test=develop

--- lite/kernels/nnadapter/bridges/paddle_use_bridges.h
+++ lite/kernels/nnadapter/bridges/paddle_use_bridges.h
@@ -114,10 +114,6 @@ USE_SUBGRAPH_BRIDGE(
     flatten2,
     kNNAdapter,
     "rockchip_npu,mediatek_apu,huawei_kirin_npu,huawei_ascend_npu,amlogic_npu");
-USE_SUBGRAPH_BRIDGE(
-    hard_sigmoid,
-    kNNAdapter,
-    "rockchip_npu,mediatek_apu,huawei_kirin_npu,huawei_ascend_npu");
 USE_SUBGRAPH_BRIDGE(hard_sigmoid, kNNAdapter, "huawei_ascend_npu");
 USE_SUBGRAPH_BRIDGE(split, kNNAdapter, "huawei_kirin_npu,huawei_ascend_npu");
 USE_SUBGRAPH_BRIDGE(cast, kNNAdapter, "huawei_ascend_npu");
