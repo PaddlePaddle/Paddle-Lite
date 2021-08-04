a6fe79f00af491f71bc9dd6c7e880dca0106d573
zhupengyang <zhu_py@qq.com>
2021-08-04 15:48:15 +0800

    [NNAdapter][HuaweiAscendNPU] add fill_constant bridge and fill converter (#6601)

--- lite/kernels/nnadapter/bridges/paddle_use_bridges.h
+++ lite/kernels/nnadapter/bridges/paddle_use_bridges.h
@@ -121,6 +121,7 @@ USE_SUBGRAPH_BRIDGE(assign, kNNAdapter, "huawei_ascend_npu");
 USE_SUBGRAPH_BRIDGE(assign_value, kNNAdapter, "huawei_ascend_npu");
 USE_SUBGRAPH_BRIDGE(abs, kNNAdapter, "huawei_ascend_npu");
 USE_SUBGRAPH_BRIDGE(norm, kNNAdapter, "huawei_ascend_npu");
+USE_SUBGRAPH_BRIDGE(fill_constant, kNNAdapter, "huawei_ascend_npu");
 USE_SUBGRAPH_BRIDGE(deformable_conv, kNNAdapter, "huawei_ascend_npu");
 USE_SUBGRAPH_BRIDGE(conv2d_transpose, kNNAdapter, "amlogic_npu");
 USE_SUBGRAPH_BRIDGE(nearest_interp, kNNAdapter, "huawei_ascend_npu");
