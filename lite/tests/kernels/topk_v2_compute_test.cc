33d00c582b0c03e7a6cc65f90f34db2a1375eec2
zhenlin-work <wuzhenlin456@163.com>
2021-08-05 12:35:38 +0000

    fix bugs in topkv2_host_test test=develop

--- lite/tests/kernels/topk_v2_compute_test.cc
+++ lite/tests/kernels/topk_v2_compute_test.cc
@@ -1,4 +1,4 @@
-// Copyright (c) 2019 PaddlePaddle Authors. All Rights Reserved.
+// Copyright (c) 2021 PaddlePaddle Authors. All Rights Reserved.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -48,11 +48,13 @@ class TopkV2ComputeTester : public arena::TestCase {
   void RunBaseline(Scope* scope) override {
     auto* out_val = scope->NewTensor(out_);
     auto* out_ind = scope->NewTensor(indices_);
+
     DDim out_dims = x_dims_;
     if (axis_ < 0) {
       axis_ += x_dims_.size();
     }
     out_dims[axis_] = k_;
+
     out_val->Resize(out_dims);
     out_ind->Resize(out_dims);
     auto* out_val_data = out_val->template mutable_data<T1>();
@@ -60,24 +62,27 @@ class TopkV2ComputeTester : public arena::TestCase {
 
     auto* x = scope->FindTensor(x_);
     const auto* x_data = x->template data<T1>();
+
     int inner_size = x_dims_.count(axis_ + 1, x_dims_.size());
     int axis_size = x_dims_[axis_];
     int outer_size = x_dims_.count(0, axis_);
-    int out_sum_size = k * inner_size;
-    for (int n = 0; n < outer_size; n++) {
-      const float* in_data = x_data + n * sum_size;
-      float* out_val_data1 = out_val_data + n * out_sum_size;
-      int64_t* out_ind_data1 = out_ind_data + n * out_sum_size;
-      for (int i = 0; i < inner_size; i++) {
-        std::vector<std::pair<float, int>> vec;
-        for (int j = 0; j < axis_size; j++) {
-          vec.push_back(std::make_pair(in_data[j * outer_size + i], j));
-        }
-        std::partial_sort(
+    int out_sum_size = k_ * inner_size;
+    int sum_size = axis_size * inner_size;
+
+    for (int i = 0; i < outer_size; i++) {
+      int glb_in_off = i * sum_size;
+      int glb_out_off = i * out_sum_size;
+      std::vector<std::pair<float, int>> vec;
+      for (int j = 0; j < axis_size; j++) {
+        vec.push_back(std::make_pair(x_data[glb_in_off + j * inner_size], j));
+      }
+      std::partial_sort(
             vec.begin(), vec.begin() + k_, vec.end(), comp_func<T1, T2>);
-        for (int j = 0; j < k_; j++) {
-          out_val_data1[j * outer_size + i] = vec[j].first;
-          out_ind_data1[j * outer_size + i] = vec[j].second;
+      for (int j = 0; j < k_; j++) {
+        for (int k = 0; k < inner_size; k++) {
+          int cur_off = glb_in_off + vec[j].second * inner_size + k;
+          out_val_data[glb_out_off + j * inner_size + k] = x_data[cur_off];
+          out_ind_data[glb_out_off + j * inner_size + k] = vec[j].second;
         }
       }
     }
@@ -101,10 +106,27 @@ class TopkV2ComputeTester : public arena::TestCase {
 
 template <typename T1, typename T2>
 void test_topk_v2(Place place, float abs_error) {
+  int caseNum = 0;
   for (auto x_shape :
        std::vector<std::vector<int64_t>>{{2, 3, 4, 5}, {3, 4, 5}, {4, 5}}) {
     for (int axis : {-1, -2}) {
       for (int k : {2, 5}) {
+        std::cout << "start case " << caseNum++ << ":" << std::endl;
+        auto axis_valid = ((axis >= (-1 * (int)x_shape.size())) && (axis < (int)x_shape.size()));
+        if (!axis_valid) {
+          LOG(INFO) << "the axis of topk_v2 must be ["
+                    << (-1 * x_shape.size()) << ", " << x_shape.size()
+                    << "but you set axis is" << axis;
+          continue;
+        }
+        if (axis < 0) {
+          axis += x_shape.size();
+        }
+        if (x_shape[axis] < k) {
+          LOG(INFO) << "input of topk_v2 op must have >=" << k
+                    << " columns in axis of " << x_shape[axis];
+          continue;
+        }
         std::unique_ptr<arena::TestCase> tester(new TopkV2ComputeTester<T1, T2>(
             place, "def", DDim(x_shape), axis, k));
         arena::Arena arena(std::move(tester), place, abs_error);
