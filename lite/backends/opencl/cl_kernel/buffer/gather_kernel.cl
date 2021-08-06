18349b1a9fda24516ec85d945ebd3e3246ca3ad9
zhenlin-work <wuzhenlin456@163.com>
2021-08-06 06:01:02 +0000

    add gather ocl buffer test=develop

--- lite/backends/opencl/cl_kernel/buffer/gather_kernel.cl
+++ lite/backends/opencl/cl_kernel/buffer/gather_kernel.cl
@@ -14,68 +14,69 @@ limitations under the License. */
 
 #include <cl_common.h>
 
-__kernel void gather_without_axis_nchw( __global const CL_DTYPE* input,
-                                        __global CL_DTYPE* output,
-                                        __global const CL_DTYPE* index,
-                                        int slice_size,
-                                        int batch_size,
-                                        int height,
-                                        int width) {
-    const int h = get_global_id(0); // height
-    const int w = get_global_id(1); // width
-    const int c = get_global_id(2); // channel
+__kernel void gather_without_axis_nchw(__global const CL_DTYPE* input,
+                                       __global CL_DTYPE* output,
+                                       __global const CL_DTYPE* index,
+                                       int slice_size,
+                                       int batch_size,
+                                       int height,
+                                       int width) {
+  const int h = get_global_id(0);  // height
+  const int w = get_global_id(1);  // width
+  const int c = get_global_id(2);  // channel
 
-    for (int i = 0; i < batch_size; ++i) {
-        int ind = (int)index[i]; // for dim n
-        int in_off = ind * slice_size + c * height * width + h * width + w;
-        int out_off = i * slice_size + c * height * width + h * width + w;
-        output[out_off] = input[in_off];
-    }
+  for (int i = 0; i < batch_size; ++i) {
+    int ind = (int)index[i];  // for dim n
+    int in_off = ind * slice_size + c * height * width + h * width + w;
+    int out_off = i * slice_size + c * height * width + h * width + w;
+    output[out_off] = input[in_off];
+  }
 }
 
-__kernel void gather_without_axis_chw( __global const CL_DTYPE* input,
-                                        __global CL_DTYPE* output,
-                                        __global const CL_DTYPE* index,
-                                        int slice_size,
-                                        int index_size,
-                                        int height,
-                                        int width) {
-    const int h = get_global_id(0); // height
-    const int w = get_global_id(1); // width
-    const int c = get_global_id(2); // index_size
+__kernel void gather_without_axis_chw(__global const CL_DTYPE* input,
+                                      __global CL_DTYPE* output,
+                                      __global const CL_DTYPE* index,
+                                      int slice_size,
+                                      int index_size,
+                                      int height,
+                                      int width) {
+  const int h = get_global_id(0);  // height
+  const int w = get_global_id(1);  // width
+  const int c = get_global_id(2);  // index_size
 
-    int ind = (int)index[c]; // for dim c
-    int in_off = ind * height * width + h * width + w;
-    int out_off = c * height * width + h * width + w;
-    output[out_off] = input[in_off];
+  int ind = (int)index[c];  // for dim c
+  int in_off = ind * height * width + h * width + w;
+  int out_off = c * height * width + h * width + w;
+  output[out_off] = input[in_off];
 }
 
-__kernel void gather_without_axis_hw( __global const CL_DTYPE* input,
-                                        __global CL_DTYPE* output,
-                                        __global const CL_DTYPE* index,
-                                        int index_size,
-                                        int width) {
-    const int h = get_global_id(0); // index_size
-    const int w = get_global_id(1); // width
+__kernel void gather_without_axis_hw(__global const CL_DTYPE* input,
+                                     __global CL_DTYPE* output,
+                                     __global const CL_DTYPE* index,
+                                     int index_size,
+                                     int width) {
+  const int h = get_global_id(0);  // index_size
+  const int w = get_global_id(1);  // width
 
-    int ind = (int)index[h];
-    int in_off = ind * width + w;
-    int out_off = h * width + w;
-    output[out_off] = input[in_off];
+  int ind = (int)index[h];
+  int in_off = ind * width + w;
+  int out_off = h * width + w;
+  output[out_off] = input[in_off];
 }
 
-__kernel void gather_with_axis( __global const CL_DTYPE* input,
-                                __global CL_DTYPE* output,
-                                __global const CL_DTYPE* index,
-                                int input_size,
-                                int index_size,
-                                int inner_dim_size,
-                                int outer_dim_size ) {
-    const int in = get_global_id(0); // inner_dim_size
-    const int mid = get_global_id(1); // index_size
-    const int out = get_global_id(2); // outer_dim_size
+__kernel void gather_with_axis(__global const CL_DTYPE* input,
+                               __global CL_DTYPE* output,
+                               __global const CL_DTYPE* index,
+                               int input_size,
+                               int index_size,
+                               int inner_dim_size,
+                               int outer_dim_size) {
+  const int in = get_global_id(0);   // inner_dim_size
+  const int mid = get_global_id(1);  // index_size
+  const int out = get_global_id(2);  // outer_dim_size
 
-    int in_off = in * (input_size / inner_dim_size) + index[mid] * outer_dim_size + out;
-    int out_off = in * index_size * outer_dim_size + mid * outer_dim_size + out;
-    output[out_off] = input[in_off];
+  int in_off =
+      in * (input_size / inner_dim_size) + index[mid] * outer_dim_size + out;
+  int out_off = in * index_size * outer_dim_size + mid * outer_dim_size + out;
+  output[out_off] = input[in_off];
 }
--- lite/kernels/opencl/gather_buffer_compute.cc
+++ lite/kernels/opencl/gather_buffer_compute.cc
@@ -13,10 +13,10 @@
 // limitations under the License.
 
 #include <iostream>
-#include "lite/core/op_registry.h"
 #include "lite/backends/opencl/cl_include.h"
 #include "lite/backends/opencl/cl_runtime.h"
 #include "lite/backends/opencl/cl_utility.h"
+#include "lite/core/op_registry.h"
 #include "lite/kernels/opencl/image_helper.h"
 #ifdef LITE_WITH_PROFILE
 #include "lite/core/profile/profiler.h"
@@ -27,10 +27,9 @@ namespace lite {
 namespace kernels {
 namespace opencl {
 
-class GatherBufferCompute : public KernelLite<TARGET(kOpenCL),
-                                              PRECISION(kFloat),
-                                              DATALAYOUT(kNCHW)> {
-public:
+class GatherBufferCompute
+    : public KernelLite<TARGET(kOpenCL), PRECISION(kFloat), DATALAYOUT(kNCHW)> {
+ public:
   using param_t = operators::GatherParam;
 
   std::string doc() const override { return "Gather using cl::Buffer, kFloat"; }
@@ -49,37 +48,84 @@ public:
     }
   }
 
-  void Run() override { 
+  void Run() override {
     auto& context = ctx_->As<OpenCLContext>();
     CHECK(context.cl_context() != nullptr);
     cl_int status;
 
     auto* x_buf = gather_param_->X->template data<float, cl::Buffer>();
     auto* index_buf = gather_param_->Index->template data<float, cl::Buffer>();
-    auto* out_buf = gather_param_->Out->template mutable_data<float, cl::Buffer>(TARGET(kOpenCL));
+    auto* out_buf =
+        gather_param_->Out->template mutable_data<float, cl::Buffer>(
+            TARGET(kOpenCL));
     auto index_dims = gather_param_->Index->dims();
     auto x_dims = gather_param_->X->dims();
-    
+
     if (gather_param_->Axis != nullptr) {
-        // with axis!
-        VLOG(4) << "gather_param_->Axis is not nullptr!!!";
-        auto* axis_data = gather_param_->Axis->data<int32_t>();
-        int axis_index = axis_data[0];
-        int input_size = x_dims.production();
-        int index_size = index_dims.production();
-        int inner_dim_size = 1;
-        int outer_dim_size = 1;
-        for (int i = 0; i < axis_index; i++) {
-          inner_dim_size *= x_dims[i];
-        }
-        for (int i = axis_index + 1; i < x_dims.size(); i++) {
-          outer_dim_size *= x_dims[i];
-        }
-        global_work_size_ =
-            cl::NDRange{static_cast<cl::size_type>(inner_dim_size),
-                        static_cast<cl::size_type>(index_size),
-                        static_cast<cl::size_type>(outer_dim_size)};
-        kernel_func_names_.push_back("gather_with_axis");
+      // with axis!
+      VLOG(4) << "gather_param_->Axis is not nullptr!!!";
+      auto* axis_data = gather_param_->Axis->data<int32_t>();
+      int axis_index = axis_data[0];
+      int input_size = x_dims.production();
+      int index_size = index_dims.production();
+      int inner_dim_size = 1;
+      int outer_dim_size = 1;
+      for (int i = 0; i < axis_index; i++) {
+        inner_dim_size *= x_dims[i];
+      }
+      for (int i = axis_index + 1; i < x_dims.size(); i++) {
+        outer_dim_size *= x_dims[i];
+      }
+      global_work_size_ =
+          cl::NDRange{static_cast<cl::size_type>(inner_dim_size),
+                      static_cast<cl::size_type>(index_size),
+                      static_cast<cl::size_type>(outer_dim_size)};
+      kernel_func_names_.push_back("gather_with_axis");
+      context.cl_context()->AddKernel(kernel_func_names_[0],
+                                      kernel_func_paths_[0],
+                                      build_options_,
+                                      time_stamp_);
+      STL::stringstream kernel_key;
+      kernel_key.str("");
+      kernel_key << kernel_func_names_[0] << build_options_ << time_stamp_;
+      auto kernel = context.cl_context()->GetKernel(kernel_key.str());
+
+      status = kernel.setArg(0, *x_buf);
+      CL_CHECK_FATAL(status);
+      status = kernel.setArg(1, *out_buf);
+      CL_CHECK_FATAL(status);
+      status = kernel.setArg(2, *index_buf);
+      CL_CHECK_FATAL(status);
+      status = kernel.setArg(3, input_size);
+      CL_CHECK_FATAL(status);
+      status = kernel.setArg(4, index_size);
+      CL_CHECK_FATAL(status);
+      status = kernel.setArg(5, inner_dim_size);
+      CL_CHECK_FATAL(status);
+      status = kernel.setArg(6, outer_dim_size);
+      CL_CHECK_FATAL(status);
+      VLOG(4) << "input_size: " << input_size;
+      VLOG(4) << "index_size: " << index_size;
+      VLOG(4) << "inner_dim_size: " << inner_dim_size;
+      VLOG(4) << "outer_dim_size: " << outer_dim_size;
+      status = EnqueueNDRangeKernel(context,
+                                    kernel,
+                                    cl::NullRange,
+                                    global_work_size_,
+                                    cl::NullRange,
+                                    nullptr,
+                                    event_);
+      CL_CHECK_FATAL(status);
+    } else {
+      // no axis!
+      VLOG(4) << "gather_param_->Axis is nullptr!!!";
+      int batch_size = index_dims[0];
+      int slice_size = 1;
+      for (size_t i = 1; i < x_dims.size(); ++i) {
+        slice_size *= x_dims[i];
+      }
+      if (x_dims.size() == 4) {  // n,c,h,w
+        kernel_func_names_.push_back("gather_without_axis_nchw");
         context.cl_context()->AddKernel(kernel_func_names_[0],
                                         kernel_func_paths_[0],
                                         build_options_,
@@ -88,6 +134,9 @@ public:
         kernel_key.str("");
         kernel_key << kernel_func_names_[0] << build_options_ << time_stamp_;
         auto kernel = context.cl_context()->GetKernel(kernel_key.str());
+        global_work_size_ = cl::NDRange{static_cast<cl::size_type>(x_dims[2]),
+                                        static_cast<cl::size_type>(x_dims[3]),
+                                        static_cast<cl::size_type>(x_dims[1])};
 
         status = kernel.setArg(0, *x_buf);
         CL_CHECK_FATAL(status);
@@ -95,144 +144,93 @@ public:
         CL_CHECK_FATAL(status);
         status = kernel.setArg(2, *index_buf);
         CL_CHECK_FATAL(status);
-        status = kernel.setArg(3, input_size);
+        status = kernel.setArg(3, slice_size);
         CL_CHECK_FATAL(status);
-        status = kernel.setArg(4, index_size);
+        status = kernel.setArg(4, batch_size);
         CL_CHECK_FATAL(status);
-        status = kernel.setArg(5, inner_dim_size);
+        status = kernel.setArg(5, (int)x_dims[2]);  // h
         CL_CHECK_FATAL(status);
-        status = kernel.setArg(6, outer_dim_size);
+        status = kernel.setArg(6, (int)x_dims[3]);  // w
         CL_CHECK_FATAL(status);
-        VLOG(4) << "input_size: " << input_size;
-        VLOG(4) << "index_size: " << index_size;
-        VLOG(4) << "inner_dim_size: " << inner_dim_size;
-        VLOG(4) << "outer_dim_size: " << outer_dim_size;
+
         status = EnqueueNDRangeKernel(context,
-                                        kernel,
-                                        cl::NullRange,
-                                        global_work_size_,
-                                        cl::NullRange,
-                                        nullptr,
-                                        event_);
+                                      kernel,
+                                      cl::NullRange,
+                                      global_work_size_,
+                                      cl::NullRange,
+                                      nullptr,
+                                      event_);
         CL_CHECK_FATAL(status);
-    } else {
-        // no axis!
-        VLOG(4) << "gather_param_->Axis is nullptr!!!";
-        int batch_size = index_dims[0];
-        int slice_size = 1;
-        for (size_t i = 1; i < x_dims.size(); ++i) {
-            slice_size *= x_dims[i];
-        }
-        if (x_dims.size() == 4) { // n,c,h,w
-            kernel_func_names_.push_back("gather_without_axis_nchw");
-            context.cl_context()->AddKernel(kernel_func_names_[0],
-                                            kernel_func_paths_[0],
-                                            build_options_,
-                                            time_stamp_);
-            STL::stringstream kernel_key;
-            kernel_key.str("");
-            kernel_key << kernel_func_names_[0] << build_options_ << time_stamp_;
-            auto kernel = context.cl_context()->GetKernel(kernel_key.str());
-            global_work_size_ =
-                cl::NDRange{static_cast<cl::size_type>(x_dims[2]),
-                            static_cast<cl::size_type>(x_dims[3]),
-                            static_cast<cl::size_type>(x_dims[1])};
+      } else if (x_dims.size() == 3) {  // c,h,w
+        kernel_func_names_.push_back("gather_without_axis_chw");
+        context.cl_context()->AddKernel(kernel_func_names_[0],
+                                        kernel_func_paths_[0],
+                                        build_options_,
+                                        time_stamp_);
+        STL::stringstream kernel_key;
+        kernel_key.str("");
+        kernel_key << kernel_func_names_[0] << build_options_ << time_stamp_;
+        auto kernel = context.cl_context()->GetKernel(kernel_key.str());
+        global_work_size_ = cl::NDRange{static_cast<cl::size_type>(x_dims[1]),
+                                        static_cast<cl::size_type>(x_dims[2]),
+                                        static_cast<cl::size_type>(batch_size)};
 
-            status = kernel.setArg(0, *x_buf);
-            CL_CHECK_FATAL(status);
-            status = kernel.setArg(1, *out_buf);
-            CL_CHECK_FATAL(status);
-            status = kernel.setArg(2, *index_buf);
-            CL_CHECK_FATAL(status);
-            status = kernel.setArg(3, slice_size);
-            CL_CHECK_FATAL(status);
-            status = kernel.setArg(4, batch_size);
-            CL_CHECK_FATAL(status);
-            status = kernel.setArg(5, (int)x_dims[2]); // h 
-            CL_CHECK_FATAL(status);
-            status = kernel.setArg(6, (int)x_dims[3]); // w
-            CL_CHECK_FATAL(status);
+        status = kernel.setArg(0, *x_buf);
+        CL_CHECK_FATAL(status);
+        status = kernel.setArg(1, *out_buf);
+        CL_CHECK_FATAL(status);
+        status = kernel.setArg(2, *index_buf);
+        CL_CHECK_FATAL(status);
+        status = kernel.setArg(3, slice_size);
+        CL_CHECK_FATAL(status);
+        status = kernel.setArg(4, batch_size);
+        CL_CHECK_FATAL(status);
+        status = kernel.setArg(5, (int)x_dims[1]);  // h
+        CL_CHECK_FATAL(status);
+        status = kernel.setArg(6, (int)x_dims[2]);  // w
+        CL_CHECK_FATAL(status);
 
-            status = EnqueueNDRangeKernel(context,
-                                            kernel,
-                                            cl::NullRange,
-                                            global_work_size_,
-                                            cl::NullRange,
-                                            nullptr,
-                                            event_);
-            CL_CHECK_FATAL(status);
-        } else if (x_dims.size() == 3) { // c,h,w
-            kernel_func_names_.push_back("gather_without_axis_chw");
-            context.cl_context()->AddKernel(kernel_func_names_[0],
-                                            kernel_func_paths_[0],
-                                            build_options_,
-                                            time_stamp_);
-            STL::stringstream kernel_key;
-            kernel_key.str("");
-            kernel_key << kernel_func_names_[0] << build_options_ << time_stamp_;
-            auto kernel = context.cl_context()->GetKernel(kernel_key.str());
-            global_work_size_ =
-                cl::NDRange{static_cast<cl::size_type>(x_dims[1]),
-                            static_cast<cl::size_type>(x_dims[2]),
-                            static_cast<cl::size_type>(batch_size)};
+        status = EnqueueNDRangeKernel(context,
+                                      kernel,
+                                      cl::NullRange,
+                                      global_work_size_,
+                                      cl::NullRange,
+                                      nullptr,
+                                      event_);
+        CL_CHECK_FATAL(status);
+      } else if (x_dims.size() == 2) {  // h,w
+        kernel_func_names_.push_back("gather_without_axis_hw");
+        context.cl_context()->AddKernel(kernel_func_names_[0],
+                                        kernel_func_paths_[0],
+                                        build_options_,
+                                        time_stamp_);
+        STL::stringstream kernel_key;
+        kernel_key.str("");
+        kernel_key << kernel_func_names_[0] << build_options_ << time_stamp_;
+        auto kernel = context.cl_context()->GetKernel(kernel_key.str());
+        global_work_size_ = cl::NDRange{static_cast<cl::size_type>(batch_size),
+                                        static_cast<cl::size_type>(x_dims[1])};
 
-            status = kernel.setArg(0, *x_buf);
-            CL_CHECK_FATAL(status);
-            status = kernel.setArg(1, *out_buf);
-            CL_CHECK_FATAL(status);
-            status = kernel.setArg(2, *index_buf);
-            CL_CHECK_FATAL(status);
-            status = kernel.setArg(3, slice_size);
-            CL_CHECK_FATAL(status);
-            status = kernel.setArg(4, batch_size);
-            CL_CHECK_FATAL(status);
-            status = kernel.setArg(5, (int)x_dims[1]); // h 
-            CL_CHECK_FATAL(status);
-            status = kernel.setArg(6, (int)x_dims[2]); // w
-            CL_CHECK_FATAL(status);
+        status = kernel.setArg(0, *x_buf);
+        CL_CHECK_FATAL(status);
+        status = kernel.setArg(1, *out_buf);
+        CL_CHECK_FATAL(status);
+        status = kernel.setArg(2, *index_buf);
+        CL_CHECK_FATAL(status);
+        status = kernel.setArg(3, batch_size);
+        CL_CHECK_FATAL(status);
+        status = kernel.setArg(4, (int)x_dims[1]);  // w
+        CL_CHECK_FATAL(status);
 
-            status = EnqueueNDRangeKernel(context,
-                                            kernel,
-                                            cl::NullRange,
-                                            global_work_size_,
-                                            cl::NullRange,
-                                            nullptr,
-                                            event_);
-            CL_CHECK_FATAL(status);
-        } else if (x_dims.size() == 2) { // h,w
-            kernel_func_names_.push_back("gather_without_axis_hw");
-            context.cl_context()->AddKernel(kernel_func_names_[0],
-                                            kernel_func_paths_[0],
-                                            build_options_,
-                                            time_stamp_);
-            STL::stringstream kernel_key;
-            kernel_key.str("");
-            kernel_key << kernel_func_names_[0] << build_options_ << time_stamp_;
-            auto kernel = context.cl_context()->GetKernel(kernel_key.str());
-            global_work_size_ =
-                cl::NDRange{static_cast<cl::size_type>(batch_size),
-                            static_cast<cl::size_type>(x_dims[1])};
-
-            status = kernel.setArg(0, *x_buf);
-            CL_CHECK_FATAL(status);
-            status = kernel.setArg(1, *out_buf);
-            CL_CHECK_FATAL(status);
-            status = kernel.setArg(2, *index_buf);
-            CL_CHECK_FATAL(status);
-            status = kernel.setArg(3, batch_size);
-            CL_CHECK_FATAL(status);
-            status = kernel.setArg(4, (int)x_dims[1]); // w
-            CL_CHECK_FATAL(status);
-
-            status = EnqueueNDRangeKernel(context,
-                                            kernel,
-                                            cl::NullRange,
-                                            global_work_size_,
-                                            cl::NullRange,
-                                            nullptr,
-                                            event_);
-            CL_CHECK_FATAL(status);
-        }
+        status = EnqueueNDRangeKernel(context,
+                                      kernel,
+                                      cl::NullRange,
+                                      global_work_size_,
+                                      cl::NullRange,
+                                      nullptr,
+                                      event_);
+        CL_CHECK_FATAL(status);
+      }
     }
   }
 
@@ -245,7 +243,7 @@ public:
   }
 #endif
 
-protected:
+ protected:
   std::vector<std::string> kernel_func_names_{};
   std::vector<std::string> kernel_func_paths_{};
   std::string build_options_{"-DCL_DTYPE_float"};
@@ -255,7 +253,7 @@ protected:
   bool first_epoch_for_reinit_{true};
   DDim last_x_dims_;
   cl::NDRange global_work_size_ = cl::NDRange{
-        static_cast<size_t>(1), static_cast<size_t>(1), static_cast<size_t>(1)};
+      static_cast<size_t>(1), static_cast<size_t>(1), static_cast<size_t>(1)};
 };
 
 }  // namespace opencl
@@ -271,12 +269,11 @@ REGISTER_LITE_KERNEL(gather,
                      kNCHW,
                      paddle::lite::kernels::opencl::GatherBufferCompute,
                      def)
-    .BindInput("X",
-                {LiteType::GetTensorTy(TARGET(kOpenCL), PRECISION(kFloat) )})
+    .BindInput("X", {LiteType::GetTensorTy(TARGET(kOpenCL), PRECISION(kFloat))})
     .BindInput("Index",
-                {LiteType::GetTensorTy(TARGET(kOpenCL), PRECISION(kFloat))})
+               {LiteType::GetTensorTy(TARGET(kOpenCL), PRECISION(kFloat))})
     .BindInput("Axis",
-                {LiteType::GetTensorTy(TARGET(kHost), PRECISION(kInt32))})
+               {LiteType::GetTensorTy(TARGET(kHost), PRECISION(kInt32))})
     .BindOutput("Out",
-                {LiteType::GetTensorTy(TARGET(kOpenCL), PRECISION(kFloat) )})
+                {LiteType::GetTensorTy(TARGET(kOpenCL), PRECISION(kFloat))})
     .Finalize();
