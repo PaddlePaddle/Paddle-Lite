fb95a2ca17bb8d00d90c42bbce1ae7c929ed8b85
wangleilei001 <wangll4397@gmail.com>
2021-08-13 15:35:11 +0800

    [XPU] add less_than op, test=develop, test=xpu

--- lite/tests/kernels/compare_compute_test.cc
+++ lite/tests/kernels/compare_compute_test.cc
@@ -235,7 +235,7 @@ TEST(Compare_OP_ARM, precision) {
   TestCompare<int32_t>(place, abs_error, "less_than", {3, 4}, {3, 4}, -1);
   TestCompare<int64_t>(place, abs_error, "less_than", {3, 4}, {3, 4}, -1);
 }
-#elif defined(LITE_WITH_XPU)
+#elif defined(LITE_WITH_XPU) && !defined(LITE_WITH_XTCL)
 TEST(Compare_OP_XPU, precision) {
   Place place{TARGET(kXPU)};
   float abs_error = 1e-5;
