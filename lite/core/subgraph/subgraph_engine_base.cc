e25ae69cf783b66bd3a9f869511c59d5b03573da
DannyIsFunny <912790387@qq.com>
2021-08-17 06:23:43 +0000

    1

--- lite/core/subgraph/subgraph_engine_base.cc
+++ lite/core/subgraph/subgraph_engine_base.cc
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include "lite/core/subgraph_engine_base.h"
+#include "lite/core/subgraph/subgraph_engine_base.h"
 #include <sys/time.h>
 #include <time.h>
 #include <algorithm>
