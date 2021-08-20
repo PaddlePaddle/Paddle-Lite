ef0958eb82397a287c3b14d56ab919923083abad
zhupengyang <zhu_py@qq.com>
2021-08-20 02:45:46 +0000

    test=develop

--- lite/backends/nnadapter/nnadapter/driver/mediatek_apu/converter/pool2d.cc
+++ lite/backends/nnadapter/nnadapter/driver/mediatek_apu/converter/pool2d.cc
@@ -12,6 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#include "core/operation/pool2d.h"
 #include "driver/mediatek_apu/converter.h"
 #include "utility/debug.h"
 #include "utility/logging.h"
@@ -20,81 +21,7 @@ namespace nnadapter {
 namespace mediatek_apu {
 
 int Program::ConvertPool2D(hal::Operation* operation) {
-  auto& input_operands = operation->input_operands;
-  auto& output_operands = operation->output_operands;
-  auto input_count = input_operands.size();
-  auto output_count = output_operands.size();
-  NNADAPTER_CHECK_EQ(input_count, 8);
-  auto operation_type = operation->type;
-  if (operation_type == NNADAPTER_AVERAGE_POOL_2D) {
-    NNADAPTER_CHECK_EQ(output_count, 1);
-  } else if (operation_type == NNADAPTER_MAX_POOL_2D) {
-    NNADAPTER_CHECK_EQ(output_count, 2);
-  } else {
-    NNADAPTER_LOG(FATAL) << "Unsupported pooling operation type "
-                         << OperationTypeToString(operation->type)
-                         << " is found.";
-  }
-
-  // Input
-  auto input_operand = input_operands[0];
-  NNADAPTER_VLOG(5) << "input: " << OperandToString(input_operand);
-  // Auto pad: not support auto_pad.
-  // Pads: Pads are transed according to auto_pad, so pads are used.
-  uint32_t pads_size =
-      input_operands[2]->length / static_cast<uint32_t>(sizeof(int32_t));
-  NNADAPTER_CHECK_EQ(pads_size, 4U);
-  auto pads_buffer = reinterpret_cast<int32_t*>(input_operands[2]->buffer);
-  auto pad_height_top = pads_buffer[0];
-  auto pad_height_bottom = pads_buffer[1];
-  auto pad_width_left = pads_buffer[2];
-  auto pad_width_right = pads_buffer[3];
-  NNADAPTER_VLOG(5) << "paddings = [" << pad_height_top << ", "
-                    << pad_height_bottom << ", " << pad_width_left << ", "
-                    << pad_width_right << "]";
-  // Kernel shape
-  uint32_t kernel_shape_size =
-      input_operands[3]->length / static_cast<uint32_t>(sizeof(int32_t));
-  NNADAPTER_CHECK_EQ(kernel_shape_size, 2U);
-  auto kernel_buffer = reinterpret_cast<int32_t*>(input_operands[3]->buffer);
-  auto kernel_height = kernel_buffer[0];
-  auto kernel_width = kernel_buffer[1];
-  NNADAPTER_VLOG(5) << "kernel = [" << kernel_height << ", " << kernel_width
-                    << "]";
-  bool global_pooling = kernel_height == input_operand->type.dimensions[2] &&
-                        kernel_width == input_operand->type.dimensions[3];
-  NNADAPTER_VLOG(5) << "global_pooling = " << global_pooling;
-  // Strides
-  uint32_t strides_size =
-      input_operands[4]->length / static_cast<uint32_t>(sizeof(int32_t));
-  NNADAPTER_CHECK_EQ(strides_size, 2U);
-  auto strides_buffer = reinterpret_cast<int32_t*>(input_operands[4]->buffer);
-  auto stride_height = strides_buffer[0];
-  auto stride_width = strides_buffer[1];
-  NNADAPTER_VLOG(5) << "strides = [" << stride_height << ", " << stride_width
-                    << "]";
-  // Ceil mode
-  bool ceil_mode = *reinterpret_cast<bool*>(input_operands[5]->buffer);
-  NNADAPTER_VLOG(5) << "ceil_mode = " << ceil_mode;
-  // Count include pad(for avg_pool) or return indices(for max_pool)
-  bool flag = *reinterpret_cast<bool*>(input_operands[6]->buffer);
-  NNADAPTER_VLOG(5) << "count_include_pad/return_indices = " << flag;
-  if (operation->type == NNADAPTER_AVERAGE_POOL_2D) {
-    NNADAPTER_CHECK(!flag) << "Only support count_include_pad = false.";
-  } else if (operation->type == NNADAPTER_MAX_POOL_2D) {
-    NNADAPTER_CHECK(!flag) << "Only support return_indices = false.";
-  } else {
-    NNADAPTER_LOG(FATAL) << "Unsupported pooling operation type "
-                         << OperationTypeToString(operation->type)
-                         << " is found.";
-  }
-  // Fuse code
-  auto fuse_code = *reinterpret_cast<int32_t*>(input_operands[7]->buffer);
-  NNADAPTER_VLOG(5) << "fuse_code = " << fuse_code;
-
-  // Output
-  auto output_operand = output_operands[0];
-  NNADAPTER_VLOG(5) << "output: " << OperandToString(output_operand);
+  POOL2D_OPERATION_EXTRACT_INPUTS_OUTPUTS
 
   // Convert to Neuron operands and operations
   auto input_index = GetMappedIndex(input_operand);
