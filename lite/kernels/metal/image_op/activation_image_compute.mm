a2ebe7f1ab822c27bd211ecfa57880a808cbde02
Zhang Jun <ewalker@live.cn>
2021-08-20 21:18:55 +0800

    [metal] add swish、hard_swish、hard_sigmoid (#6636)
    * add hard_swish
    
    * update
    
    * [metal] fix sigmoid,test=develop
    
    * [metal] update,test=develop
    
    * fix bug,test=develop

--- lite/kernels/metal/image_op/activation_image_compute.mm
+++ lite/kernels/metal/image_op/activation_image_compute.mm
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include "lite/kernels/metal/image_op/sigmoid_image_compute.h"
+#include "lite/kernels/metal/image_op/activation_image_compute.h"
 #include "lite/backends/metal/metal_context_imp.h"
 #include "lite/core/op_registry.h"
 #include "lite/core/tensor.h"
@@ -25,7 +25,7 @@ namespace lite {
 namespace kernels {
 namespace metal {
 
-void SigmoidImageCompute::PrepareForRun() {
+void ActivationImageCompute::PrepareForRun() {
     auto& context = ctx_->As<MTLContext>();
     metal_context_ = (MetalContext*)context.context();
 
@@ -37,22 +37,41 @@ void SigmoidImageCompute::PrepareForRun() {
     input_buffer_ = param.X->data<MetalHalf, MetalImage>();
     output_buffer_ = param.Out->mutable_data<MetalHalf, MetalImage>(metal_context_, output_dims);
 #endif
-    function_name_ = "sigmoid";
-    if (param.has_active) {
-        if (param.active_type == lite_api::ActivationType::kSigmoid) {
+
+    int active_type = static_cast<int>(param.active_type);
+    switch (active_type) {
+        case 5:
             function_name_ = "sigmoid";
-        } else {
-             function_name_ = "hard_sigmoid";
-        } else {
-            LOG(FATAL) << "[metal] unsupported Activation type";
+            break;
+        case 7:
+            function_name_ = "swish";
+            break;
+        case 10: {
+            HardSwishMetalParam metal_param{
+                param.hard_swish_offset, param.hard_swish_threshold, param.hard_swish_scale};
+            param_buffer_ =
+                std::make_shared<MetalBuffer>(metal_context_, sizeof(metal_param), &metal_param);
         }
+            function_name_ = "hard_swish";
+            break;
+        case 14: {
+            HardSigmoidMetalParam metal_param{param.hard_sigmoid_slope, param.hard_sigmoid_offset};
+            param_buffer_ =
+                std::make_shared<MetalBuffer>(metal_context_, sizeof(metal_param), &metal_param);
+        }
+            function_name_ = "hard_sigmoid";
+            break;
+        default:
+            LOG(FATAL) << "[metal] unsupported Activation type";
+            return;
     }
+
     // pipline
     auto backend = (__bridge MetalContextImp*)metal_context_->backend();
     pipline_ = [backend pipline:function_name_];
 }
 
-void SigmoidImageCompute::Run() {
+void ActivationImageCompute::Run() {
     auto pipline = pipline_;
     auto outTexture = output_buffer_->image();
     auto backend = (__bridge MetalContextImp*)metal_context_->backend();
@@ -60,12 +79,15 @@ void SigmoidImageCompute::Run() {
     auto encoder = [backend commandEncoder];
     [encoder setTexture:input_buffer_->image() atIndex:(0)];
     [encoder setTexture:output_buffer_->image() atIndex:(1)];
+    if (function_name_ == "hard_swish" || function_name_ == "hard_sigmoid") {
+        [encoder setBuffer:param_buffer_->buffer() offset:(0) atIndex:(0)];
+    }
 
     [backend dispatchEncoder:encoder pipline:pipline outTexture:outTexture];
     [backend commit];
 }
 
-SigmoidImageCompute::~SigmoidImageCompute() {
+ActivationImageCompute::~ActivationImageCompute() {
     TargetWrapperMetal::FreeImage(output_buffer_);
 }
 
@@ -78,7 +100,7 @@ REGISTER_LITE_KERNEL(sigmoid,
     kMetal,
     kFloat,
     kMetalTexture2DArray,
-    paddle::lite::kernels::metal::SigmoidImageCompute,
+    paddle::lite::kernels::metal::ActivationImageCompute,
     def)
     .BindInput("X",
         {LiteType::GetTensorTy(TARGET(kMetal),
@@ -94,7 +116,35 @@ REGISTER_LITE_KERNEL(sigmoid,
     kMetal,
     kFP16,
     kMetalTexture2DArray,
-    paddle::lite::kernels::metal::SigmoidImageCompute,
+    paddle::lite::kernels::metal::ActivationImageCompute,
+    def)
+    .BindInput("X",
+        {LiteType::GetTensorTy(TARGET(kMetal), PRECISION(kFP16), DATALAYOUT(kMetalTexture2DArray))})
+    .BindOutput("Out",
+        {LiteType::GetTensorTy(TARGET(kMetal), PRECISION(kFP16), DATALAYOUT(kMetalTexture2DArray))})
+    .Finalize();
+
+REGISTER_LITE_KERNEL(swish,
+    kMetal,
+    kFloat,
+    kMetalTexture2DArray,
+    paddle::lite::kernels::metal::ActivationImageCompute,
+    def)
+    .BindInput("X",
+        {LiteType::GetTensorTy(TARGET(kMetal),
+            PRECISION(kFloat),
+            DATALAYOUT(kMetalTexture2DArray))})
+    .BindOutput("Out",
+        {LiteType::GetTensorTy(TARGET(kMetal),
+            PRECISION(kFloat),
+            DATALAYOUT(kMetalTexture2DArray))})
+    .Finalize();
+
+REGISTER_LITE_KERNEL(swish,
+    kMetal,
+    kFP16,
+    kMetalTexture2DArray,
+    paddle::lite::kernels::metal::ActivationImageCompute,
     def)
     .BindInput("X",
         {LiteType::GetTensorTy(TARGET(kMetal), PRECISION(kFP16), DATALAYOUT(kMetalTexture2DArray))})
@@ -106,7 +156,7 @@ REGISTER_LITE_KERNEL(hard_sigmoid,
     kMetal,
     kFloat,
     kMetalTexture2DArray,
-    paddle::lite::kernels::metal::SigmoidImageCompute,
+    paddle::lite::kernels::metal::ActivationImageCompute,
     def)
     .BindInput("X",
         {LiteType::GetTensorTy(TARGET(kMetal),
@@ -122,7 +172,35 @@ REGISTER_LITE_KERNEL(hard_sigmoid,
     kMetal,
     kFP16,
     kMetalTexture2DArray,
-    paddle::lite::kernels::metal::SigmoidImageCompute,
+    paddle::lite::kernels::metal::ActivationImageCompute,
+    def)
+    .BindInput("X",
+        {LiteType::GetTensorTy(TARGET(kMetal), PRECISION(kFP16), DATALAYOUT(kMetalTexture2DArray))})
+    .BindOutput("Out",
+        {LiteType::GetTensorTy(TARGET(kMetal), PRECISION(kFP16), DATALAYOUT(kMetalTexture2DArray))})
+    .Finalize();
+
+REGISTER_LITE_KERNEL(hard_swish,
+    kMetal,
+    kFloat,
+    kMetalTexture2DArray,
+    paddle::lite::kernels::metal::ActivationImageCompute,
+    def)
+    .BindInput("X",
+        {LiteType::GetTensorTy(TARGET(kMetal),
+            PRECISION(kFloat),
+            DATALAYOUT(kMetalTexture2DArray))})
+    .BindOutput("Out",
+        {LiteType::GetTensorTy(TARGET(kMetal),
+            PRECISION(kFloat),
+            DATALAYOUT(kMetalTexture2DArray))})
+    .Finalize();
+
+REGISTER_LITE_KERNEL(hard_swish,
+    kMetal,
+    kFP16,
+    kMetalTexture2DArray,
+    paddle::lite::kernels::metal::ActivationImageCompute,
     def)
     .BindInput("X",
         {LiteType::GetTensorTy(TARGET(kMetal), PRECISION(kFP16), DATALAYOUT(kMetalTexture2DArray))})
