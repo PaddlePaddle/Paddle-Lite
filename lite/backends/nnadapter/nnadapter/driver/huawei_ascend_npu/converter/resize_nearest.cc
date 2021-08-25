8c35454321df106d0fff98aec3ef49184528751a
csy0225 <78470701+csy0225@users.noreply.github.com>
2021-08-25 10:36:00 +0800

    [NNAdapter][HuaweiAscendNPU] Solve the problem that the output type of reduce mean operator is scalar in the 'reduce_all' case (#6687)

--- lite/backends/nnadapter/nnadapter/driver/huawei_ascend_npu/converter/resize_nearest.cc
+++ lite/backends/nnadapter/nnadapter/driver/huawei_ascend_npu/converter/resize_nearest.cc
@@ -67,8 +67,64 @@ int Program::ConvertResizeNearest(hal::Operation* operation) {
     }
     SET_INPUT(resize_nearest_op, size, shape_operator);
   } else if (scales_operand != nullptr) {
-    NNADAPTER_LOG(WARNING) << "Not support scales now.";
-    return NNADAPTER_INVALID_PARAMETER;
+    auto scale_operator = GetMappedOperator(scales_operand);
+    if (scale_operator == nullptr) {
+      scale_operator = ConvertOperand(scales_operand);
+    }
+    // Shape op
+    auto shape_op =
+        std::make_shared<ge::op::Shape>(resize_nearest_name + "/shape");
+    SET_INPUT(shape_op, x, input_operator);
+    auto shape_tensor_desc = std::make_shared<ge::TensorDesc>();
+    shape_op->update_output_desc_y(*shape_tensor_desc);
+    auto shape_operator = UpdateOperatorMap(
+        shape_operand,
+        std::make_shared<Operator>(shape_op, shape_tensor_desc, "y", -1));
+    // Slice op
+    auto slice_op =
+        std::make_shared<ge::op::Slice>(resize_nearest_name + "/slice");
+    std::vector<int> offsets{2};
+    std::vector<int> size{2};
+    auto offsets_operator = AddInt32ConstantOperator(offsets);
+    auto size_operator = AddInt32ConstantOperator(size);
+    SET_INPUT(slice_op, x, shape_operator);
+    SET_INPUT(slice_op, offsets, offsets_operator);
+    SET_INPUT(slice_op, size, size_operator);
+    auto slice_tensor_desc = std::make_shared<ge::TensorDesc>();
+    slice_op->update_output_desc_y(*slice_tensor_desc);
+    auto slice_operator = UpdateOperatorMap(
+        nullptr,
+        std::make_shared<Operator>(slice_op, slice_tensor_desc, "y", -1));
+    // Cast op
+    auto slice_cast_op =
+        std::make_shared<ge::op::Cast>(resize_nearest_name + "/slice_cast");
+    slice_cast_op->set_attr_dst_type(ge::DT_FLOAT);
+    SET_INPUT(slice_cast_op, x, slice_operator);
+    auto slice_cast_tensor_desc = std::make_shared<ge::TensorDesc>();
+    slice_cast_op->update_output_desc_y(*slice_cast_tensor_desc);
+    auto slice_cast_operator =
+        UpdateOperatorMap(nullptr,
+                          std::make_shared<Operator>(
+                              slice_cast_op, slice_cast_tensor_desc, "y", -1));
+    // Mul op
+    auto mul_op = std::make_shared<ge::op::Mul>(resize_nearest_name + "/mul");
+    SET_INPUT(mul_op, x1, slice_cast_operator);
+    SET_INPUT(mul_op, x2, scale_operator);
+    auto mul_tensor_desc = std::make_shared<ge::TensorDesc>();
+    mul_op->update_output_desc_y(*mul_tensor_desc);
+    auto mul_operator = UpdateOperatorMap(
+        nullptr, std::make_shared<Operator>(mul_op, mul_tensor_desc, "y", -1));
+    // Cast op
+    auto mul_cast_op =
+        std::make_shared<ge::op::Cast>(resize_nearest_name + "/mul_cast");
+    mul_cast_op->set_attr_dst_type(ge::DT_INT32);
+    SET_INPUT(mul_cast_op, x, mul_operator);
+    auto mul_cast_tensor_desc = std::make_shared<ge::TensorDesc>();
+    mul_cast_op->update_output_desc_y(*mul_cast_tensor_desc);
+    auto mul_cast_operator = UpdateOperatorMap(
+        nullptr,
+        std::make_shared<Operator>(mul_cast_op, mul_cast_tensor_desc, "y", -1));
+    SET_INPUT(resize_nearest_op, size, mul_cast_operator);
   } else {
     NNADAPTER_LOG(WARNING) << "Either shape_operand or scales_operand should "
                               "be set.";
