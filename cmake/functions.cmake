37d6f3c7d4c3875152c7ca8d3d2ab656232709e1
DannyIsFunny <912790387@qq.com>
2021-08-27 12:02:40 +0000

    refine kernels registry and lib strip of kernels module

--- cmake/functions.cmake
+++ cmake/functions.cmake
@@ -46,10 +46,6 @@ file(WRITE ${kernels_src_list} "") # clean
 set(fake_kernels_src_list "${CMAKE_BINARY_DIR}/fake_kernels_src_list.txt")
 file(WRITE ${fake_kernels_src_list} "") # clean
 
-if(LITE_BUILD_TAILOR)
-  set(tailored_kernels_list_path "${LITE_OPTMODEL_DIR}/.tailored_kernels_source_list")
-  file(STRINGS ${tailored_kernels_list_path} tailored_kernels_list)
-endif()
 # add a kernel for some specific device
 set(IS_FAKED_KERNEL false)
 add_library(kernels STATIC "${CMAKE_SOURCE_DIR}/lite/kernels/host/feed_compute.cc" "${CMAKE_SOURCE_DIR}/lite/kernels/host/fetch_compute.cc")
@@ -67,18 +63,40 @@ function(add_kernel TARGET device level)
         return()
     endif()
 
-    # apppend faked kernels into fake kernels source list.
+    # apppend faked kernels into fake kernels source list.(this is useful in opt tool)
     if(${IS_FAKED_KERNE})
       foreach(src ${args_SRCS})
         file(APPEND ${fake_kernels_src_list} "${CMAKE_CURRENT_SOURCE_DIR}/${src}\n")
       endforeach()
       return()
     endif()
+
+    # strp lib according to useful kernel names.
+    if(LITE_BUILD_TAILOR)
+      set(dst_file "")
+      foreach(src ${args_SRCS})
+        string(TOLOWER "${device}" device_name) # ARM => arm, Host => host
+        get_filename_component(filename ${src} NAME_WE) # conv_compute.cc => conv_compute
+        set(kernel_tailor_src_dir "${CMAKE_BINARY_DIR}/kernel_tailor_src_dir")
+        set(suffix "for_strip")
+        set(dst_file ${dst_file} "${kernel_tailor_src_dir}/${filename}_${device_name}_${suffix}.cc") # conv_compute_arm.cc
+        if("${device}" STREQUAL "METAL")
+          set(dst_file ${dst_file} "${kernel_tailor_src_dir}/${filename}_${device_name}_${suffix}.mm") # conv_compute_apple_metal_for_strip.mm
+        endif()
+        if(NOT EXISTS ${dst_file})
+          return()
+        endif()
+      endforeach()
+      target_sources(kernels PUBLIC ${dst_file})
+      set(__lite_cc_files ${__lite_cc_files} ${dst_file} CACHE INTERNAL "")
+      return()
+    endif()
+
     # compile actual kernels
     foreach(src ${args_SRCS})
         file(APPEND ${kernels_src_list} "${CMAKE_CURRENT_SOURCE_DIR}/${src}\n")
         set(KERNELS_SRC ${KERNELS_SRC} "${CMAKE_CURRENT_SOURCE_DIR}/${src}" CACHE INTERNAL "kernels source")
-        target_sources(kernels "${CMAKE_CURRENT_SOURCE_DIR}/${src}")
+        target_sources(kernels PUBLIC "${CMAKE_CURRENT_SOURCE_DIR}/${src}")
         set(__lite_cc_files ${__lite_cc_files} "${CMAKE_CURRENT_SOURCE_DIR}/${src}" CACHE INTERNAL "")
     endforeach()
 
