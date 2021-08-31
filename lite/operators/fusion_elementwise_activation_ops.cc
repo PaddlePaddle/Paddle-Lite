a1e6dd46589ad03d31502acf31e50e44ea652a51
lxw950127 <1182657559@qq.com>
2021-08-31 21:24:45 +0800

    [x86] refactor x86 elementwise op, support tanh, sigmoid, relu fusion

--- lite/operators/fusion_elementwise_activation_ops.cc
+++ lite/operators/fusion_elementwise_activation_ops.cc
@@ -106,6 +106,15 @@ REGISTER_LITE_OP(fusion_elementwise_min_activation,
 REGISTER_LITE_OP(fusion_elementwise_div_activation,
                  paddle::lite::operators::FusionElementwiseActivationOp);
 
+#ifdef LITE_WITH_X86
+REGISTER_LITE_OP(fusion_elementwise_floordiv_activation,
+                 paddle::lite::operators::FusionElementwiseActivationOp);
+REGISTER_LITE_OP(fusion_elementwise_mod_activation,
+                 paddle::lite::operators::FusionElementwiseActivationOp);
+REGISTER_LITE_OP(fusion_elementwise_pow_activation,
+                 paddle::lite::operators::FusionElementwiseActivationOp);
+#endif
+
 // #ifdef LITE_WITH_TRAIN
 // REGISTER_LITE_OP(
 //     fusion_elementwise_sub_activation_grad,
