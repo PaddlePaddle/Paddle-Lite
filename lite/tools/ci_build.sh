5f5590725b6cbe13642e27e4ef2393bf964167d8
DannyIsFunny <912790387@qq.com>
2021-09-01 13:10:53 +0000

    fix ci

--- lite/tools/ci_build.sh
+++ lite/tools/ci_build.sh
@@ -1199,7 +1199,7 @@ function test_model_optimize_tool_compile {
     cd $workspace
     cd build
     # Compile opt tool
-    cmake .. -DWITH_LITE=ON -DLITE_ON_MODEL_OPTIMIZE_TOOL=ON -DWITH_TESTING=OFF -DLITE_BUILD_EXTRA=ON
+    cmake .. -DWITH_LITE=ON -DLITE_ON_MODEL_OPTIMIZE_TOOL=ON -DWITH_TESTING=OFF -DLITE_BUILD_EXTRA=ON -DWITH_MKL=OFF
     make opt -j$NUM_CORES_FOR_COMPILE
     # Check whether opt can transform quantized mobilenetv1 successfully.
     cd lite/api && chmod +x ./opt
