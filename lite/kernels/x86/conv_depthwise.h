7a61dafde199bd13694c903cdec4c48d0640b807
chenjiaoAngel <chenjiaobuaa@126.com>
2021-09-02 17:31:36 +0800

    add x86 int8 conv framework-info. test=develop

--- lite/kernels/x86/conv_depthwise.h
+++ lite/kernels/x86/conv_depthwise.h
@@ -58,6 +58,10 @@ class DepthwiseConv : public KernelLite<TARGET(kX86), Ptype> {
   Tensor input_padding_;
   Tensor filter_pack_;
   Tensor output_pack_;
+  bool flag_trans_bias_{true};
+  std::vector<float> w_scale_;
+  Tensor weights_;
+  Tensor bias_;
 };
 
 }  // namespace x86
