fce031ba33c519fa9ec9ba4caf56312c04cdf10b
zhupengyang <zhu_py@qq.com>
2021-09-08 03:27:48 +0000

    unsqueeze

---
+++ lite/backends/nnadapter/nnadapter/driver/huawei_kirin_npu/converter/unsqueeze.cc
@@ -0,0 +1,44 @@
+// Copyright (c) 2021 PaddlePaddle Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "core/operation/unsqueeze.h"
+#include "driver/huawei_kirin_npu/converter.h"
+#include "utility/debug.h"
+#include "utility/logging.h"
+
+namespace nnadapter {
+namespace huawei_kirin_npu {
+
+int Program::ConvertReshape(hal::Operation* operation) {
+  UNSQUEEZE_OPERATION_EXTRACT_INPUTS_OUTPUTS
+
+  // Convert to GE operators
+  auto input_operator = GetMappedOperator(input_operand);
+  if (!input_operator) {
+    input_operator = ConvertOperand(input_operand);
+  }
+  auto reshape_name = GetOperatorName(output_operand);
+  auto reshape_op = std::make_shared<hiai::op::Reshape>(reshape_name);
+  auto shape_count = output_operand->type.dimension_count;
+  auto shape_data = output_operand->type.dimensions;
+  auto shape_operator = AddInt32ConstantOperator(
+      std::vector<int32_t>(shape_data, shape_data + shape_count));
+  SET_INPUT(reshape_op, x, input_operator);
+  SET_INPUT(reshape_op, shape, shape_operator);
+  MAP_OUTPUT(reshape_op, y, output_operand);
+  return NNADAPTER_NO_ERROR;
+}
+
+}  // namespace huawei_kirin_npu
+}  // namespace nnadapter
