d30121103b0b1a1150d22c9389a4292acd258af4
zhupengyang <zhu_py@qq.com>
2021-09-13 10:40:16 +0000

    test=develop

--- lite/kernels/nnadapter/converter/matmul.cc
+++ lite/kernels/nnadapter/converter/matmul.cc
@@ -12,6 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#include <cmath>
 #include "lite/kernels/nnadapter/converter/converter.h"
 
 namespace paddle {
