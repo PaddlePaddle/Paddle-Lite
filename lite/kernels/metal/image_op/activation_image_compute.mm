a681629afcef8f7ca610ed9572287fc9f3cbe46b
Zhang Jun <ewalker@live.cn>
2021-09-14 14:20:53 +0800

    [metal] add box_coder and ssd_boxes_calc_offline;  add concat (#6926)
    * [metal] update cancat
    
    * metal support ssd_boxes_calc_offline
    
    * add box_coder
    
    * add concat_4_4_normal
    
    * fix boxcoder
    
    * fix fetch
    
    * [metal] fix pooling to use MPS when global_pooling is True
    
    * fix act;
    fix io_copy;

--- lite/kernels/metal/image_op/activation_image_compute.mm
+++ lite/kernels/metal/image_op/activation_image_compute.mm
@@ -48,7 +48,7 @@ void ActivationImageCompute::setup_without_mps() {
             function_name_ = "relu";
             break;
         case 2: {
-            Relu6MetalParam metal_param{param.hard_swish_threshold};
+            Relu6MetalParam metal_param{param.Relu_clipped_coef};
             param_buffer_ =
                 std::make_shared<MetalBuffer>(metal_context_, sizeof(metal_param), &metal_param);
         }
