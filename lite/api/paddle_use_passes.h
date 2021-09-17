67a034b4123af84faae9708d09824bc9f6f28457
weihaoji <68884893+weihaoji@users.noreply.github.com>
2021-09-17 10:14:47 +0800

    [XPU] xpu detect pad zero max pooling fuse (#6958)

--- lite/api/paddle_use_passes.h
+++ lite/api/paddle_use_passes.h
@@ -119,4 +119,5 @@ USE_MIR_PASS(__xpu__bigru_fuse_pass);
 USE_MIR_PASS(__xpu__conv_pixel_shuffle_fuse_pass);
 USE_MIR_PASS(__xpu__dynamic_lstm_fuse_pass);
 USE_MIR_PASS(__xpu__multi_softmax_fuse_pass);
+USE_MIR_PASS(__xpu__max_pooling_pad_zero_detect_fuse_pass);
 USE_MIR_PASS(x86_int8_attribute_pass);
