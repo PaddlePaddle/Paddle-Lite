495778c74561e6ae84edd02bf40814ccc3c4ba96
chenyaohuang <2574638208@qq.com>
2021-09-22 10:34:37 +0800

    开发gather opencl算子,代码格式修改完成，test=develop

--- lite/kernels/opencl/gather_image_compute.cpp
+++ lite/kernels/opencl/gather_image_compute.cpp
@@ -303,12 +303,8 @@ void GatherImageCompute::Run() {
 
 namespace ocl = paddle::lite::kernels::opencl;
 
-REGISTER_LITE_KERNEL(gather,
-                     kOpenCL,
-                     kFP16,
-                     kImageDefault,
-                     ocl::GatherImageCompute,
-                     def)
+REGISTER_LITE_KERNEL(
+    gather, kOpenCL, kFP16, kImageDefault, ocl::GatherImageCompute, def)
     .BindInput("X",
                {LiteType::GetTensorTy(TARGET(kOpenCL),
                                       PRECISION(kFP16),
@@ -325,5 +321,5 @@ REGISTER_LITE_KERNEL(gather,
                 {LiteType::GetTensorTy(TARGET(kOpenCL),
                                        PRECISION(kFP16),
                                        DATALAYOUT(kImageDefault))})
-    .Finalize()
+    .Finalize();
 #define LITE_WITH_LOG
