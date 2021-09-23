2b2d51af4cc5b135fe77ac391fcc00d565b9f7c8
hong19860320 <9973393+hong19860320@users.noreply.github.com>
2021-09-23 01:54:38 +0000

    test=develop test=huawei_ascend_npu

--- lite/tests/api/test_mobilenet_v1_int8_per_layer_nnadapter.cc
+++ lite/tests/api/test_mobilenet_v1_int8_per_layer_nnadapter.cc
@@ -51,7 +51,7 @@ TEST(MobileNetV1, test_mobilenet_v1_int8_per_layer_nnadapter) {
   out_accuracy_threshold = 0.79f;
 #elif defined(NNADAPTER_WITH_MEDIATEK_APU)
   nnadapter_device_names.emplace_back("mediatek_apu");
-  out_accuracy_threshold = 0.82f;
+  out_accuracy_threshold = 0.79f;
 #elif defined(NNADAPTER_WITH_IMAGINATION_NNA)
   nnadapter_device_names.emplace_back("imagination_nna");
   out_accuracy_threshold = 0.79f;
