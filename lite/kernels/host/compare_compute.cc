b10de51cc1357081b5bd6e32b65f8830cf4e665b
wanglei91 <78581692+wanglei91@users.noreply.github.com>
2021-09-24 09:33:22 +0800

    [XPU] fix some bugs for transformer (#7014)

--- lite/kernels/host/compare_compute.cc
+++ lite/kernels/host/compare_compute.cc
@@ -520,6 +520,22 @@ REGISTER_LITE_KERNEL(greater_than, kHost, kFloat, kAny, greater_than_float, def)
     .BindPaddleOpVersion("greater_than", 1)
     .Finalize();
 
+using greater_than_bool = paddle::lite::kernels::host::CompareCompute<
+    PRECISION(kFloat),
+    paddle::lite::kernels::host::_GreaterThanFunctor<bool>>;
+REGISTER_LITE_KERNEL(greater_than, kHost, kFloat, kAny, greater_than_bool, bool)
+    .BindInput("X",
+               {LiteType::GetTensorTy(
+                   TARGET(kHost), PRECISION(kBool), DATALAYOUT(kAny), -1)})
+    .BindInput("Y",
+               {LiteType::GetTensorTy(
+                   TARGET(kHost), PRECISION(kBool), DATALAYOUT(kAny), -1)})
+    .BindOutput("Out",
+                {LiteType::GetTensorTy(
+                    TARGET(kHost), PRECISION(kBool), DATALAYOUT(kAny), -1)})
+    .BindPaddleOpVersion("greater_than", 1)
+    .Finalize();
+
 using greater_than_int32 = paddle::lite::kernels::host::CompareCompute<
     PRECISION(kInt32),
     paddle::lite::kernels::host::_GreaterThanFunctor<int32_t>>;
