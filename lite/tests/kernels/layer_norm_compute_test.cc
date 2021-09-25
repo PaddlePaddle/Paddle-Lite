16ebbf12ffc0cddc183d5cd4de2b803d0cc72183
hong19860320 <9973393+hong19860320@users.noreply.github.com>
2021-09-25 22:41:24 +0800

    [NNAdapter][HuaweiAscendNPU] Fix the unit test of layer_norm (#7064)

--- lite/tests/kernels/layer_norm_compute_test.cc
+++ lite/tests/kernels/layer_norm_compute_test.cc
@@ -150,7 +150,7 @@ TEST(LayerNorm, precision) {
 #if defined(LITE_WITH_NNADAPTER)
   place = TARGET(kNNAdapter);
 #if defined(NNADAPTER_WITH_HUAWEI_ASCEND_NPU)
-  abs_error = 1e-2;
+  abs_error = 2e-2;
 #else
   return;
 #endif
