d09b980ab2824d5c7fead30ef635f99979c3a0bc
hong19860320 <9973393+hong19860320@users.noreply.github.com>
2021-09-25 14:28:29 +0800

    [NNAdapter] Refine code for layer_norm converter (#7063)

--- lite/backends/nnadapter/nnadapter/core/operation/squeeze.cc
+++ lite/backends/nnadapter/nnadapter/core/operation/squeeze.cc
@@ -73,13 +73,10 @@ int PrepareSqueeze(hal::Operation* operation) {
                          input_type.dimensions.count);
   output_type.dimensions.count =
       input_type.dimensions.count - squeezed_dimensions_count;
-  // Dynamic dimensions
-  uint32_t dynamic_cnt_squeezed_dims = 0;
   for (uint32_t i = 0; i < input_type.dimensions.dynamic_count; i++) {
-    dynamic_cnt_squeezed_dims =
-        infer_output_shape(input_type.dimensions.dynamic_data[i],
-                           output_type.dimensions.dynamic_data[i],
-                           input_type.dimensions.count);
+    infer_output_shape(input_type.dimensions.dynamic_data[i],
+                       output_type.dimensions.dynamic_data[i],
+                       input_type.dimensions.count);
   }
   NNADAPTER_VLOG(5) << "output: " << OperandToString(output_operand);
   return NNADAPTER_NO_ERROR;
