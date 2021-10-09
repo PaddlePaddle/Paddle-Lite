4d42c07cda20d69c31e098695ae6dd8a6532f01e
zhaoyang-star <zhaoyangstar@foxmail.com>
2021-10-09 15:58:21 +0800

    fix DataLayoutCompatible test=develop

--- lite/core/type_system.h
+++ lite/core/type_system.h
@@ -194,17 +194,17 @@ static bool DataLayoutCompatibleTo(const Type& a, const Type& b) {
   return a.IsVoid() ||                 //
          (a.layout() == b.layout() ||  //
           ((b.layout() == DATALAYOUT(kAny)) &&
-           (a.layout() != DATALAYOUT(kImageDefault) ||
+           (a.layout() != DATALAYOUT(kImageDefault) &&
             a.layout() != DATALAYOUT(kImageFolder))));
 }
 static bool DataLayoutCompatible(const Type& a, const Type& b) {
   return a.IsVoid() || b.IsVoid() ||   //
          (a.layout() == b.layout() ||  //
           ((b.layout() == DATALAYOUT(kAny)) &&
-           (a.layout() != DATALAYOUT(kImageDefault) ||
+           (a.layout() != DATALAYOUT(kImageDefault) &&
             a.layout() != DATALAYOUT(kImageFolder))) ||
           ((a.layout() == DATALAYOUT(kAny)) &&
-           (b.layout() != DATALAYOUT(kImageDefault) ||
+           (b.layout() != DATALAYOUT(kImageDefault) &&
             b.layout() != DATALAYOUT(kImageFolder))));
 }
 
