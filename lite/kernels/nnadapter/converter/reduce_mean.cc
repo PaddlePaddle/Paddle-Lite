5d838581f2a546930f0ed482164b53c8cb669fa0
csy0225 <chensiyu08@baidu.com>
2021-10-18 07:34:07 +0000

    review update test=develop test=huawei_ascend_npu

--- lite/kernels/nnadapter/converter/reduce_mean.cc
+++ lite/kernels/nnadapter/converter/reduce_mean.cc
@@ -12,7 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include <iostream>
 #include "lite/kernels/nnadapter/converter/converter.h"
 
 namespace paddle {
