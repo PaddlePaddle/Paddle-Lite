46c3d1cd6b009fd1105a047444115e00e264ff27
shentanyue <34421038+shentanyue@users.noreply.github.com>
2021-10-20 14:24:08 +0800

    [NNAdapter] Add pass to eliminate assign_value op and move assgin op to converter (#7207)

--- lite/core/optimizer/optimizer.cc
+++ lite/core/optimizer/optimizer.cc
@@ -197,6 +197,7 @@ std::unique_ptr<RuntimeProgram> RunDefaultOptimizer(
        "__xpu__dynamic_lstm_fuse_pass",
        "__xpu__multi_softmax_fuse_pass",
        "ssd_boxes_calc_offline_pass",
+       "assign_value_calc_offline_pass",
        // Only for fully quantized model, infer the output scale and fix the
        // attribute 'enable_int8' for all of the quantized ops.
        "quantized_op_attributes_inference_pass",
