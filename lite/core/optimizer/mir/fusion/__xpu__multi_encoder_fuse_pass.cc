a6762144da164b438dd4b255caf47698bd8b0738
mayang002 <77949147+mayang002@users.noreply.github.com>
2021-10-25 17:57:47 +0800

    [XPU] super big ernie support (#7184)

--- lite/core/optimizer/mir/fusion/__xpu__multi_encoder_fuse_pass.cc
+++ lite/core/optimizer/mir/fusion/__xpu__multi_encoder_fuse_pass.cc
@@ -1046,7 +1046,7 @@ class XPUMultiEncoderFuser {
                                                    weight_qkv_trans_int8.get(),
                                                    max_f,
                                                    qkv_len);
-        memcpy(weight_tensor_vec[0]->mutable_data<float>(),
+        memcpy(weight_tensor_vec[0]->mutable_data<int8_t>(),
                weight_qkv_trans_int8.get(),
                qkv_len * sizeof(int8_t));
       } else {
@@ -1056,7 +1056,7 @@ class XPUMultiEncoderFuser {
             weight_qkv_trans_int16.get(),
             max_f,
             qkv_len);
-        memcpy(weight_tensor_vec[0]->mutable_data<float>(),
+        memcpy(weight_tensor_vec[0]->mutable_data<int16_t>(),
                weight_qkv_trans_int16.get(),
                qkv_len * sizeof(int16_t));
       }
