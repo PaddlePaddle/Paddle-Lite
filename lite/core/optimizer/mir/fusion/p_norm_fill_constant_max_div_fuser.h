2e1292bfe2dd549aa404244aa7ce0cd74d36edde
csy0225 <chensiyu08@baidu.com>
2021-10-26 02:47:39 +0000

    nnadapter: cast, norm, pad, dropout

--- lite/core/optimizer/mir/fusion/p_norm_fill_constant_max_div_fuser.h
+++ lite/core/optimizer/mir/fusion/p_norm_fill_constant_max_div_fuser.h
@@ -14,7 +14,25 @@
 
 #pragma once
 
-USE_SUBGRAPH_BRIDGE(fc,
-                    kNNAdapter,
-                    "rockchip_npu,mediatek_apu,huawei_kirin_npu,huawei_ascend_"
-                    "npu,amlogic_npu,imagination_nna");
+#include <memory>
+#include <string>
+#include "lite/core/optimizer/mir/pattern_matcher_high_api.h"
+
+namespace paddle {
+namespace lite {
+namespace mir {
+namespace fusion {
+
+class PNormFillConstantMaxDivFuser : public FuseBase {
+ public:
+  void BuildPattern() override;
+  void InsertNewNode(SSAGraph* graph, const key2nodes_t& matched) override;
+
+ private:
+  cpp::OpDesc GenOpDesc(const key2nodes_t& matched) override;
+};
+
+}  // namespace fusion
+}  // namespace mir
+}  // namespace lite
+}  // namespace paddle
