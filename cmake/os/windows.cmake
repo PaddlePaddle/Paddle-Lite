22f3e1938639741135831c08872219db7659882f
TangYuan-Liu <1131165566@qq.com>
2021-10-27 07:05:58 +0000

    test=develop, test=huawei_ascend_npu

--- cmake/os/windows.cmake
+++ cmake/os/windows.cmake
@@ -1,14 +1,19 @@
 # Copyright (c) 2021 PaddlePaddle Authors. All Rights Reserved.
+
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
+
 #     http://www.apache.org/licenses/LICENSE-2.0
+
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
 option(MSVC_STATIC_CRT "use static C Runtime library by default" ON)
+
 set(CMAKE_SUPPRESS_REGENERATION ON)
 set(CMAKE_STATIC_LIBRARY_PREFIX lib)
 
@@ -34,3 +39,8 @@ set(CMAKE_EXE_LINKER_FLAGS  "${CMAKE_EXE_LINKER_FLAGS} ${PADDLE_LINK_FLAGS}")
 if(MSVC)
   include(external/dirent)
 endif()
+
+# Definitions
+add_definitions("/DGOOGLE_GLOG_DLL_DECL=")
+add_definitions(-DGLOG_NO_ABBREVIATED_SEVERITIES)
+add_definitions(-DNOMINMAX)
