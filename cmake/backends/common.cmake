f084df6730c9bb4e848b837aa370533b27f040e7
TangYuan-Liu <1131165566@qq.com>
2021-10-28 07:10:54 +0000

    test=develop,test=huawei_ascend_npu

--- cmake/backends/common.cmake
+++ cmake/backends/common.cmake
@@ -12,57 +12,45 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# for opencl
+if (LITE_WITH_OPENCL)
+  include_directories("${PADDLE_SOURCE_DIR}/third-party/opencl/include")
+endif()
+
 # for mobile
 if (WITH_LITE AND LITE_WITH_LIGHT_WEIGHT_FRAMEWORK)
-    message(STATUS "Building the mobile framework")
-    include(postproject)
-    include(backends/npu) # check and prepare NPU DDK
-    include(backends/xpu) # check and prepare XPU SDK
-    include(backends/apu) # check and prepare APU SDK 
-    include(backends/huawei_ascend_npu)  # check and prepare Ascend NPU SDK 
+  message(STATUS "Building the mobile framework")
+  include(postproject)
+  include(backends/npu) # check and prepare NPU DDK
+  include(backends/xpu) # check and prepare XPU SDK
 
-    # We compile the mobile deployment library when LITE_ON_TINY_PUBLISH=ON
-    # So the following third party dependencies are not needed.
-    if (NOT LITE_ON_TINY_PUBLISH)
-        # include the necessary thirdparty dependencies
-        include(external/gflags)    # download, build, install gflags
-        # LITE_WITH_LIGHT_WEIGHT_FRAMEWORK=ON will disable glog
-        # TODO(sangoly): refine WITH_LITE and LITE_WITH_LIGHT_WEIGHT_FRAMEWORK
-        include(external/gtest)     # download, build, install gtest
-        include(ccache)             # set ccache for compilation
-        include(external/protobuf)  # download, build, install protobuf
-    endif()
+  # We compile the mobile deployment library when LITE_ON_TINY_PUBLISH=ON
+  # So the following third party dependencies are not needed.
+  if (NOT LITE_ON_TINY_PUBLISH)
+    # include the necessary thirdparty dependencies
+    include(external/gflags)    # download, build, install gflags
+    # LITE_WITH_LIGHT_WEIGHT_FRAMEWORK=ON will disable glog
+    # TODO(sangoly): refine WITH_LITE and LITE_WITH_LIGHT_WEIGHT_FRAMEWORK
+    include(external/gtest)     # download, build, install gtest
+    include(ccache)             # set ccache for compilation
+    include(external/protobuf)  # download, build, install protobuf
+  endif()
 
-    include(generic)            # simplify cmake module
-    include(configure)          # add paddle env configuration
+  include(generic)            # simplify cmake module
+  include(configure)          # add paddle env configuration
 
-    add_subdirectory(lite)
-    return()
+  return()
 endif()
 #################################  End of mobile compile ##############################
 
-set(WITH_MKLML ${WITH_MKL})
-if (NOT DEFINED WITH_MKLDNN)
-    if (WITH_MKL AND AVX2_FOUND)
-        set(WITH_MKLDNN ON)
-    else()
-        message(STATUS "Do not have AVX2 intrinsics and disabled MKL-DNN")
-        set(WITH_MKLDNN OFF)
-    endif()
-endif()
-
-########################################################################################
+#################################  Server compile  ##############################
 
 if(LITE_WITH_XPU)
-    include(backends/xpu)
+  include(backends/xpu)
 endif()
 
 if(LITE_WITH_MLU)
-    include(mlu)
-endif()
-
-if(LITE_WITH_HUAWEI_ASCEND_NPU)
-    include(backends/huawei_ascend_npu)
+  include(mlu)
 endif()
 
 include(coveralls)
@@ -100,26 +88,3 @@ endif()
 
 set(CMAKE_CXX_FLAGS_RELWITHDEBINFO "-O3 -g -DNDEBUG")
 set(CMAKE_C_FLAGS_RELWITHDEBINFO "-O3 -g -DNDEBUG")
-
-
-if(APPLE)
-    if(NOT DEFINED ENABLE_ARC)
-        # Unless specified, enable ARC support by default
-        set(ENABLE_ARC TRUE)
-        message(STATUS "Enabling ARC support by default. ENABLE_ARC not provided!")
-    endif()
-
-    set(ENABLE_ARC_INT ${ENABLE_ARC} CACHE BOOL "Whether or not to enable ARC" ${FORCE_CACHE})
-    if(ENABLE_ARC_INT)
-        set(FOBJC_ARC "-fobjc-arc")
-        set(CMAKE_XCODE_ATTRIBUTE_CLANG_ENABLE_OBJC_ARC YES CACHE INTERNAL "")
-        message(STATUS "Enabling ARC support.")
-    else()
-        set(FOBJC_ARC "-fno-objc-arc")
-        set(CMAKE_XCODE_ATTRIBUTE_CLANG_ENABLE_OBJC_ARC NO CACHE INTERNAL "")
-        message(STATUS "Disabling ARC support.")
-    endif()
-    set(CMAKE_C_FLAGS "${FOBJC_ARC} ${CMAKE_C_FLAGS}")
-    set(CMAKE_CXX_FLAGS "${FOBJC_ARC} ${CMAKE_CXX_FLAGS}")
-endif ()
-add_subdirectory(lite)
