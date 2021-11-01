7748d7e670f42a7fa26dd1c213dfe4016f933b54
chenjiaoAngel <chenjiaobuaa@126.com>
2021-11-01 14:49:15 +0800

    fix dynamic shape compute error. test=develop

--- lite/kernels/arm/conv_transpose_compute.h
+++ lite/kernels/arm/conv_transpose_compute.h
@@ -32,6 +32,29 @@ class Conv2DTransposeCompute : public KernelLite<TARGET(kARM), Ptype> {
 
   void Run() override;
 
+  virtual void ReInitWhenNeeded() {
+    auto& param = this->Param<param_t>();
+    auto x_dims = param.x->dims();
+    if (last_shape_ == x_dims) {
+      return;
+    }
+    auto w_dims = param.filter->dims();
+    auto o_dims = param.output->dims();
+    int chin = x_dims[1];
+    int hin = x_dims[2];
+    int win = x_dims[3];
+    int chout = o_dims[1];
+    int kw = w_dims[3];
+    int kh = w_dims[2];
+    int group = param.groups;
+    /* deconv weights layout: chin * chout * kh * kw*/
+    int m = chout * kw * kh / group;
+    int n = hin * win;
+    int k = chin / group;
+    workspace_size_ = group * m * n;
+    last_shape_ = x_dims;
+  }
+
   ~Conv2DTransposeCompute() = default;
 
 #ifdef LITE_WITH_PROFILE
@@ -59,6 +82,7 @@ class Conv2DTransposeCompute : public KernelLite<TARGET(kARM), Ptype> {
   bool depthwise_{false};
   bool flag_trans_bias_{false};
   std::vector<float> w_scale_;
+  DDim last_shape_;
   Tensor bias_;
 };
 
