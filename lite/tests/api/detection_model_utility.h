1852da9dba3be90f15e1abdb5e4476456d8231bc
shentanyue <shentanyue01@baidu.com>
2021-11-04 08:54:41 +0000

    add model ci

--- lite/tests/api/detection_model_utility.h
+++ lite/tests/api/detection_model_utility.h
@@ -13,6 +13,7 @@
 // limitations under the License.
 
 #pragma once
+#include <gflags/gflags.h>
 #include <algorithm>
 #include <iostream>
 #include <memory>
@@ -34,24 +35,26 @@ void SetDetectionInputV2(
     std::vector<T> raw_data,
     int input_size) {
   auto im_shape_tensor = predictor->GetInput(0);
-  auto* im_shape_data = im_shape_tensor->mutable_data<float>();
+  im_shape_tensor->Resize(std::vector<int64_t>({1, 2}));
+  auto* im_shape_data = im_shape_tensor->mutable_data<T>();
   im_shape_data[0] = input_shape[2];
   im_shape_data[1] = input_shape[3];
 
   auto input_tensor = predictor->GetInput(1);
   input_tensor->Resize(
       std::vector<int64_t>(input_shape.begin(), input_shape.end()));
-  auto* data = input_tensor->mutable_data<float>();
+  auto* data = input_tensor->mutable_data<T>();
   if (raw_data.empty()) {
     for (int i = 0; i < input_size; i++) {
       data[i] = 0.f;
     }
   } else {
-    memcpy(data, raw_data.data(), sizeof(float) * input_size);
+    memcpy(data, raw_data.data(), sizeof(T) * input_size);
   }
 
   auto scale_factor_tensor = predictor->GetInput(2);
-  auto* scale_factor_data = scale_factor_tensor->mutable_data<float>();
+  scale_factor_tensor->Resize(std::vector<int64_t>({1, 2}));
+  auto* scale_factor_data = scale_factor_tensor->mutable_data<T>();
   scale_factor_data[0] = 1;
   scale_factor_data[1] = 1;
 }
@@ -66,14 +69,102 @@ void SetDetectionInputV1(
   auto input_tensor = predictor->GetInput(0);
   input_tensor->Resize(
       std::vector<int64_t>(input_shape.begin(), input_shape.end()));
-  auto* data = input_tensor->mutable_data<float>();
+  auto* data = input_tensor->mutable_data<T>();
   if (raw_data.empty()) {
     for (int i = 0; i < input_size; i++) {
       data[i] = 0.f;
     }
   } else {
-    memcpy(data, raw_data.data(), sizeof(float) * input_size);
+    memcpy(data, raw_data.data(), sizeof(T) * input_size);
   }
+}
+
+void TestSSDDetectionModel(std::string model_dir,
+                           std::string data_dir,
+                           int32_t batch,
+                           int32_t channel,
+                           int32_t height,
+                           int32_t width,
+                           int32_t iteration,
+                           int32_t version,
+                           std::vector<std::string> nnadapter_device_names,
+                           std::string nnadapter_context_properties,
+                           std::vector<paddle::lite_api::Place> valid_places) {
+  std::shared_ptr<paddle::lite_api::PaddlePredictor> predictor = nullptr;
+  // Use the full api with CxxConfig to generate the optimized model
+  lite_api::CxxConfig cxx_config;
+  cxx_config.set_model_dir(model_dir);
+  cxx_config.set_valid_places(valid_places);
+  cxx_config.set_nnadapter_device_names(nnadapter_device_names);
+  cxx_config.set_nnadapter_context_properties(nnadapter_context_properties);
+  predictor = lite_api::CreatePaddlePredictor(cxx_config);
+  predictor->SaveOptimizedModel(model_dir,
+                                paddle::lite_api::LiteModelType::kNaiveBuffer);
+  // Use the light api with MobileConfig to load and run the optimized model
+  paddle::lite_api::MobileConfig mobile_config;
+  mobile_config.set_model_from_file(model_dir + ".nb");
+  mobile_config.set_threads(FLAGS_threads);
+  mobile_config.set_power_mode(
+      static_cast<lite_api::PowerMode>(FLAGS_power_mode));
+  mobile_config.set_nnadapter_device_names(nnadapter_device_names);
+  mobile_config.set_nnadapter_context_properties(nnadapter_context_properties);
+  predictor = paddle::lite_api::CreatePaddlePredictor(mobile_config);
+
+  std::string raw_data_dir = data_dir + std::string("/raw_data");
+  std::vector<int> input_shape{batch, channel, height, width};
+  auto raw_data = ReadRawData(raw_data_dir, input_shape, iteration);
+
+  int input_size = 1;
+  for (auto i : input_shape) {
+    input_size *= i;
+  }
+
+  for (int i = 0; i < FLAGS_warmup; ++i) {
+    if (version == 1) {
+      SetDetectionInputV1(predictor, input_shape, {}, input_size);
+    } else if (version == 2) {
+      SetDetectionInputV2(predictor, input_shape, {}, input_size);
+    } else {
+      LOG(INFO) << "Unsupported the input version!";
+      return;
+    }
+
+    predictor->Run();
+  }
+
+  std::vector<std::vector<float>> out_rets;
+  out_rets.resize(iteration);
+  double cost_time = 0;
+  for (size_t i = 0; i < raw_data.size(); ++i) {
+    if (version == 1) {
+      SetDetectionInputV1(predictor, input_shape, raw_data[i], input_size);
+    } else if (version == 2) {
+      SetDetectionInputV2(predictor, input_shape, raw_data[i], input_size);
+    } else {
+      LOG(INFO) << "Unsupported the input version!";
+      return;
+    }
+    double start = GetCurrentUS();
+    predictor->Run();
+    cost_time += GetCurrentUS() - start;
+
+    auto output_tensor = predictor->GetOutput(0);
+    auto output_shape = output_tensor->shape();
+    auto output_data = output_tensor->data<float>();
+    ASSERT_EQ(output_shape.size(), 2UL);
+    ASSERT_GT(output_shape[0], 0);
+    ASSERT_EQ(output_shape[1], 6);
+
+    int output_size = output_shape[0] * output_shape[1];
+    out_rets[i].resize(output_size);
+    memcpy(&(out_rets[i].at(0)), output_data, sizeof(float) * output_size);
+  }
+
+  LOG(INFO) << "================== Speed Report ===================";
+  LOG(INFO) << "Model: " << model_dir << ", threads num " << FLAGS_threads
+            << ", warmup: " << FLAGS_warmup << ", batch: " << batch
+            << ", iteration: " << iteration << ", spend "
+            << cost_time / iteration / 1000.0 << " ms in average.";
 }
 
 float CalBertOutAccuracy(const std::vector<std::vector<float>>& out,
---
+++ lite/tests/api/test_ssd_mobilenet_v1_voc_fp32_v2_0_nnadapter.cc
@@ -0,0 +1,71 @@
+// Copyright (c) 2019 PaddlePaddle Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <gflags/gflags.h>
+#include <gtest/gtest.h>
+#include <vector>
+#include "lite/api/paddle_api.h"
+#include "lite/api/test/lite_api_test_helper.h"
+#include "lite/api/test/test_helper.h"
+#include "lite/tests/api/PASCALVOC2012_utility.h"
+#include "lite/tests/api/detection_model_utility.h"
+
+DEFINE_string(data_dir, "", "data dir");
+DEFINE_int32(iteration, 1, "iteration times to run");
+DEFINE_int32(batch, 1, "batch of image");
+DEFINE_int32(channel, 3, "image channel");
+DEFINE_int32(height, 300, "image height");
+DEFINE_int32(width, 300, "image width");
+DEFINE_int32(model_version, 2, "model version");
+
+namespace paddle {
+namespace lite {
+
+TEST(SSDMobileNetV1VOC_V2, test_ssd_mobilenet_v1_voc_fp32_v2_0_nnadapter) {
+  std::vector<std::string> nnadapter_device_names;
+  std::string nnadapter_context_properties;
+  std::vector<paddle::lite_api::Place> valid_places;
+  valid_places.push_back(
+      lite_api::Place{TARGET(kNNAdapter), PRECISION(kFloat)});
+#if defined(LITE_WITH_ARM)
+  valid_places.push_back(lite_api::Place{TARGET(kARM), PRECISION(kFloat)});
+#elif defined(LITE_WITH_X86)
+  valid_places.push_back(lite_api::Place{TARGET(kX86), PRECISION(kFloat)});
+#else
+  LOG(INFO) << "Unsupported host arch!";
+  return;
+#endif
+#if defined(NNADAPTER_WITH_HUAWEI_ASCEND_NPU)
+  nnadapter_device_names.emplace_back("huawei_ascend_npu");
+  nnadapter_context_properties = "HUAWEI_ASCEND_NPU_SELECTED_DEVICE_IDS=0";
+#else
+  LOG(INFO) << "Unsupported NNAdapter device!";
+  return;
+#endif
+
+  TestSSDDetectionModel(FLAGS_model_dir,
+                        FLAGS_data_dir,
+                        FLAGS_batch,
+                        FLAGS_channel,
+                        FLAGS_height,
+                        FLAGS_width,
+                        FLAGS_iteration,
+                        FLAGS_model_version,
+                        nnadapter_device_names,
+                        nnadapter_context_properties,
+                        valid_places);
+}
+
+}  // namespace lite
+}  // namespace paddle
