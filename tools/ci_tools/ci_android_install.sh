91f07987f472310bfe0f1c30a45ce154a327eca9
chenjiaoAngel <chenjiaobuaa@126.com>
2021-11-08 20:18:27 +0800

    add fp16 model ut in ci. test=develop

--- tools/ci_tools/ci_android_install.sh
+++ tools/ci_tools/ci_android_install.sh
@@ -95,6 +95,83 @@ function test_model {
   adb -s ${adb_devices[$adb_index]} shell "cd /data/local/tmp/$adb_dir && export LD_LIBRARY_PATH=./ &&  ./mobilenetv1_light_api ./mobilenet_v1.nb"
 }
 
+# 3.2 Compile Android FP16 demo and upload them onto adb device to perform unit test
+function test_fp16_model {
+  cd ${WORKSPACE}
+  workspace=$PWD/$(dirname $0)
+  readonly workspace=${workspace%%lite/tools*}
+  WITH_LOG=OFF
+  WITH_CV=ON
+  WITH_EXCEPTION=ON
+  # ToolChain options: clang gcc
+  TOOL_CHAIN=clang
+  # AndroidStl options: c++_static c++_shared
+  ANDROID_STL=c++_static
+  cd $workspace
+  # step 1 build opt tool
+  if [ ! -f build.opt/lite/api/opt ]; then
+  ./lite/tools/build.sh build_optimize_tool
+  fi
+  ###  models names
+  rm -rf fp16_models &&  cp -rf ./inference_model ./fp16_models
+  models_names=$(ls fp16_models)
+  ## step 2. convert models
+  rm -rf optimized_model_fp16 && mkdir optimized_model_fp16
+  for name in $models_names
+  do
+    ./build.opt/lite/api/opt --model_dir=./models/$name --valid_targets=arm --optimize_out=./optimized_model_fp16/$name --enable_fp16=1
+  done
+
+  # step 3. compiling Android ARM lib
+  export NDK_ROOT=/opt/android-ndk-r23b/
+  if [ -f build.lite.android.armv7.clang ]; then
+    rm -rf build.lite.android.armv7.clang
+  fi
+  if [ -f build.lite.android.armv8.clang ]; then
+    rm -rf build.lite.android.armv8.clang
+  fi
+  ./lite/tools/build_android.sh --with_strip=ON --with_log=$WITH_LOG --with_cv=$WITH_CV --toolchain=$TOOL_CHAIN --with_exception=$WITH_EXCEPTION --android_stl=$ANDROID_STL --build_arm82_fp16=ON
+
+  ./lite/tools/build_android.sh --with_strip=ON --with_log=$WITH_LOG --arch=armv7 --with_cv=$WITH_CV --toolchain=$TOOL_CHAIN --with_exception=$WITH_EXCEPTION --android_stl=$ANDROID_STL --build_arm82_fp16=ON
+
+  # step 4. pack compiling results and optimized models
+  result_name=android_lib_fp16
+  rm -rf $result_name && mkdir $result_name
+  cp -rf build.lite.android.armv7.$TOOL_CHAIN/inference_lite_lib.android.armv7 $result_name/armv7_fp16.$TOOL_CHAIN
+  cp -rf build.lite.android.armv8.$TOOL_CHAIN/inference_lite_lib.android.armv8 $result_name/armv8_fp16.$TOOL_CHAIN
+  cp build.opt/lite/api/opt $result_name/
+  mv optimized_model_fp16 $result_name
+
+  # step 5. run ut
+  adb_index=$1
+  adb_dir=$2
+
+  adb_devices=($(adb devices |grep -v devices |grep device | awk -F " " '{print $1}'))
+
+  # prepare workspace directory
+  adb -s ${adb_devices[$adb_index]} shell "cd /data/local/tmp && rm -rf $adb_dir && mkdir $adb_dir"
+  # 5.1. upload optimized model
+  adb -s ${adb_devices[$adb_index]} push android_lib_fp16/optimized_model_fp16/mobilenet_v1.nb /data/local/tmp/$adb_dir
+
+  # 5.2. perform armv7 unit_test v7-ut doesn't support
+  #    5.2.1 upload armv7 lib
+  # adb -s ${adb_devices[$adb_index]} push android_lib_fp16/armv7_fp16.clang/cxx/lib/libpaddle_light_api_shared.so /data/local/tmp/$adb_dir
+  #    5.2.2 compile and upload armv7 demo
+  # cd android_lib_fp16/armv7_fp16.clang/demo/cxx/mobile_light && make && chmod +x mobilenetv1_light_api
+  # adb -s ${adb_devices[$adb_index]} push mobilenetv1_light_api /data/local/tmp/$adb_dir  && cd -
+  #    5.2.3 perform unit test
+  # adb -s ${adb_devices[$adb_index]} shell "cd /data/local/tmp/$adb_dir && export LD_LIBRARY_PATH=./ &&  ./mobilenetv1_light_api ./mobilenet_v1.nb"
+
+  # 5.3. perform armv8 unit_test
+  #    5.3.1 upload armv8 lib
+  adb -s ${adb_devices[$adb_index]} push android_lib_fp16/armv8_fp16.clang/cxx/lib/libpaddle_light_api_shared.so /data/local/tmp/$adb_dir
+  #    5.3.2 compile and upload armv8 demo
+  cd android_lib_fp16/armv8_fp16.clang/demo/cxx/mobile_light && make && chmod +x mobilenetv1_light_api
+  adb -s ${adb_devices[$adb_index]} push mobilenetv1_light_api /data/local/tmp/$adb_dir  && cd -
+  #    5.3.3 perform unit test
+  adb -s ${adb_devices[$adb_index]} shell "cd /data/local/tmp/$adb_dir && export LD_LIBRARY_PATH=./ &&  ./mobilenetv1_light_api ./mobilenet_v1.nb"
+}
+
 ####################################################################################################
 # 4. main function
 #    $1: adb device index, eg. 0 1
@@ -105,6 +182,7 @@ function main {
   prepare_models
   compile_according_to_models
   test_model $1 $2
+  test_fp16_model $1 $2
 }
 
 main $1 $2
