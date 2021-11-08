fc2ce0877cb2c8979a7e1fa1ac82c819b53ecb51
zhaoyang-star <zhaoyangstar@foxmail.com>
2021-11-08 17:12:21 +0800

    grace model_util test=develop

--- lite/utils/model_util.h
+++ lite/utils/model_util.h
@@ -14,11 +14,18 @@
 
 #pragma once
 #include <cmath>
+#include <sstream>
 #include <string>
 #include <vector>
 
 namespace paddle {
 namespace lite {
+
+std::vector<std::string> SplitString(const std::string& str_in,
+                                     const std::string& mark = ":");
+std::string ShapePrint(const std::vector<std::vector<int64_t>>& shapes);
+std::string ShapePrint(const std::vector<int64_t>& shape);
+std::vector<std::vector<int64_t>> GetShapes(const std::string& raw_shapes);
 
 template <class T>
 std::string Vector2Str(const std::vector<T>& input) {
@@ -30,22 +37,6 @@ std::string Vector2Str(const std::vector<T>& input) {
   return ss.str();
 }
 
-std::vector<std::string> SplitString(const std::string& str_in,
-                                     const std::string& mark = ":") {
-  std::vector<std::string> str_out;
-  std::string tmp_str = str_in;
-  while (!tmp_str.empty()) {
-    size_t next_offset = tmp_str.find(mark);
-    str_out.push_back(tmp_str.substr(0, next_offset));
-    if (next_offset == std::string::npos) {
-      break;
-    } else {
-      tmp_str = tmp_str.substr(next_offset + 1);
-    }
-  }
-  return str_out;
-}
-
 template <class T>
 T ShapeProduction(const std::vector<T>& shape) {
   T num = 1;
@@ -53,50 +44,6 @@ T ShapeProduction(const std::vector<T>& shape) {
     num *= i;
   }
   return num;
-}
-
-std::string ShapePrint(const std::vector<std::vector<int64_t>>& shapes) {
-  std::string shapes_str{""};
-  for (size_t shape_idx = 0; shape_idx < shapes.size(); ++shape_idx) {
-    auto shape = shapes[shape_idx];
-    std::string shape_str;
-    for (auto i : shape) {
-      shape_str += std::to_string(i) + ",";
-    }
-    shapes_str += shape_str;
-    shapes_str +=
-        (shape_idx != 0 && shape_idx == shapes.size() - 1) ? "" : " : ";
-  }
-  return shapes_str;
-}
-
-std::string ShapePrint(const std::vector<int64_t>& shape) {
-  std::string shape_str{""};
-  for (auto i : shape) {
-    shape_str += std::to_string(i) + " ";
-  }
-  return shape_str;
-}
-
-std::vector<std::vector<int64_t>> GetShapes(const std::string& raw_shapes) {
-  std::vector<std::vector<int64_t>> shapes;
-  auto str_shapes = SplitString(raw_shapes);
-  for (auto str_shape : str_shapes) {
-    std::vector<int64_t> shape;
-    std::string tmp_str = str_shape;
-    while (!tmp_str.empty()) {
-      int dim = atoi(tmp_str.data());
-      shape.push_back(dim);
-      size_t next_offset = tmp_str.find(",");
-      if (next_offset == std::string::npos) {
-        break;
-      } else {
-        tmp_str = tmp_str.substr(next_offset + 1);
-      }
-    }
-    shapes.push_back(shape);
-  }
-  return shapes;
 }
 
 template <typename T>
