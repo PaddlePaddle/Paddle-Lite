17aa23ff498c33357d4cbe213eef499a13f3674d
mjp9527 <1821582672@qq.com>
2021-11-11 19:05:07 +0800

    temporary save

--- lite/operators/op_params.h
+++ lite/operators/op_params.h
@@ -2178,6 +2178,17 @@ struct UniqueWithCountsParam : ParamBase {
   lite::Tensor* Index{};
   lite::Tensor* Count{};
 };
+
+struct GaussRandomParam : ParamBase {
+  const lite::Tensor* ShapeTensor{};
+  std::vector<const lite::Tensor*> ShapeTensorList{};
+  std::vector<int64_t> shape{};
+  lite::Tensor* Out{};
+  int seed{0};
+  int dtype{5};
+  float mean{0.f};
+};
+
 }  // namespace operators
 }  // namespace lite
 }  // namespace paddle
