6207f5f98f9d6bfa0feab63e3938ec56415174b9
mjp9527 <1821582672@qq.com>
2021-11-22 11:33:37 +0800

    reset unittest

--- lite/tests/unittest_py/rpc_service/server.py
+++ lite/tests/unittest_py/rpc_service/server.py
@@ -18,14 +18,44 @@ from paddlelite.lite import *
 
 rpyc.core.protocol.DEFAULT_CONFIG['allow_pickle'] = True
 
+def ParsePlaceInfo(place_str):
+   # todo: this func should be completed later
+   infos = ''.join(place_str.split()).split(",")
+   if len(infos) == 1 :
+       if infos[0] in TargetType.__members__:
+           return Place(eval("TargetType." + infos[0]))
+       else:
+           logging.error("Error place info: " + place_str)
+   elif len(infos) == 2 :
+       if (infos[0] in TargetType.__members__) and (infos[1] in PrecisionType.__members__):
+           return Place(eval("TargetType." + infos[0]), eval("PrecisionType." +  infos[1]))
+       else:
+           logging.error("Error place info: " + place_str)
+   elif len(infos) == 3 :
+       if (infos[0] in TargetType.__members__) and (infos[1] in PrecisionType.__members__) and (infos[2] in DataLayoutType.__members__):
+           return Place(eval("TargetType." + infos[0]), eval("PrecisionType." +  infos[1]), eval("DataLayoutType." + infos[2]))
+       else:
+           logging.error("Error place info: " + place_str)
+   else:
+       logging.error("Error place info: " + place_str)
+
+def ParsePaddleLiteConfig(self, config):
+    lite_config = CxxConfig()
+    if "valid_targets" in config:
+        valid_places = []
+        for place_str in config["valid_targets"]:
+            valid_places.append(ParsePlaceInfo(place_str))
+        lite_config.set_valid_places(valid_places)
+    if "thread" in config:
+        lite_config.set_threads(config["thread"])
+    return lite_config
+
 class RPCService(rpyc.Service):
-    def exposed_run_lite_model(self, model, params,inputs):
+    def exposed_run_lite_model(self, model, params, inputs, config_str):
         '''
         Test a single case.
         '''
-        valid_places=[Place(TargetType.ARM, PrecisionType.FP32)]
-        config = CxxConfig()
-        config.set_valid_places(valid_places)
+        config = ParsePaddleLiteConfig(self, config_str)
         config.set_model_buffer(model, len(model), params, len(params))
         predictor = create_paddle_predictor(config)
         for name in inputs:
