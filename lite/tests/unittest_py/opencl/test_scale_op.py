05596477ebdea64a62c41148ba090814baa50000
daming5432 <mingxian32@sina.com>
2021-11-24 20:02:47 +0800

    add scale opencl unittest test=develop

--- lite/tests/unittest_py/opencl/test_scale_op.py
+++ lite/tests/unittest_py/opencl/test_scale_op.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import sys
+import sys 
 sys.path.append('..')
 
 from auto_scan_test import AutoScanTest, SkipReasons
