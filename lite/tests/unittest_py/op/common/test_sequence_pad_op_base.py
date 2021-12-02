4b511158518779c6e09fc5559d47c14ce5538032
zhaoyang-star <zhaoyangstar@foxmail.com>
2021-12-02 16:31:16 +0800

    [OpTestPy] Add sequence_pad common/host utest, test=document_fix

---
+++ lite/tests/unittest_py/op/common/test_sequence_pad_op_base.py
@@ -0,0 +1,62 @@
+# Copyright (c) 2021 PaddlePaddle Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import sys
+sys.path.append('..')
+
+from program_config import TensorConfig, ProgramConfig, OpConfig, CxxConfig, TargetType, PrecisionType, DataLayoutType, Place
+import numpy as np
+from functools import partial
+from typing import Optional, List, Callable, Dict, Any, Set
+import unittest
+import hypothesis
+import hypothesis.strategies as st
+from hypothesis import assume
+
+def sample_program_configs(draw):
+
+    def generate_input(*args, **kwargs):
+        if kwargs["type"] == "int32":
+            return np.random.randint(kwargs["low"], kwargs["high"], kwargs["shape"]).astype(np.int32)
+        elif kwargs["type"] == "int64":
+            return np.random.randint(kwargs["low"], kwargs["high"], kwargs["shape"]).astype(np.int64)
+        elif kwargs["type"] == "float32":
+            return np.random.random(kwargs["shape"]).astype(np.float32)
+
+
+    input_type = draw(st.sampled_from(["int32", "int64", "float32"]))
+
+    x_shape = [12, 10] # draw(st.lists(st.integers(min_value=1, max_value=10), min_size=2, max_size=2))
+    # x_len_lod =
+    padded_length = -1
+    pad_value = np.random.random((10))
+
+    sequence_pad_op = OpConfig(
+        type = "sequence_pad",
+        inputs = {"X" : ["x_data"], "PadValue" : ["pad_value"]},
+        outputs = {"Out" : ["output_data"], "Length": ["length_data"]},
+        attrs = {"padded_length" : padded_length})
+
+    program_config = ProgramConfig(
+        ops=[sequence_pad_op],
+        weights={},
+        inputs={
+            "x_data":
+            TensorConfig(data_gen=partial(generate_input, type=input_type, low=-10, high=10, shape=x_shape), lod=[[0, 3, 4, 3]]),
+            "pad_value":
+            TensorConfig(data_gen=partial(generate_input, type=input_type, low=0, high=10, shape=[1]))
+        },
+        outputs=["output_data", "length_data"])
+
+    return program_config
