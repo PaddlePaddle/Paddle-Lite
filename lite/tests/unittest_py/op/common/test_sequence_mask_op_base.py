0a0abbab6ef5c576fcb7053346cc01dd93089c0c
zhaoyang-star <zhaoyangstar@foxmail.com>
2021-12-03 21:22:23 +0800

    [OpTestPy] Add sequence_mask common utest, test=document_fix

--- lite/tests/unittest_py/op/common/test_sequence_mask_op_base.py
+++ lite/tests/unittest_py/op/common/test_sequence_mask_op_base.py
@@ -32,30 +32,33 @@ def sample_program_configs(draw):
         elif kwargs["type"] == "int64":
             return np.random.randint(kwargs["low"], kwargs["high"], kwargs["shape"]).astype(np.int64)
         elif kwargs["type"] == "float32":
-            return np.random.random(kwargs["shape"]).astype(np.float32)
+            return (kwargs["high"] - kwargs["low"]) * np.random.random(kwargs["shape"]).astype(np.float32) + kwargs["low"]
 
-    input_type = "int32" # draw(st.sampled_from(["int32", "int64", "float32"]))
-    has_max_len_tensor = False # draw(st.booleans())
+    input_type = "float32" # draw(st.sampled_from(["int32", "int64", "float32"]))
+    has_max_len_tensor = draw(st.booleans())
 
-    x_shape = [4] # draw(st.lists(st.integers(min_value=1, max_value=10), min_size=2, max_size=3)) # [2, 3, 5]
-    maxlen = 5 # draw(st.integers(min_value=-10, max_value=10))
-    out_dtype = draw(st.sampled_from([2])) # 2, 3, 5
+    x_shape = draw(st.lists(st.integers(min_value=1, max_value=10), min_size=2, max_size=7)) # [2, 3, 5]
+    maxlen = draw(st.integers(min_value=-10, max_value=10))
+    out_dtype_dict = {"2" : np.int32,
+                      "3" : np.int64,
+                      "5" : np.float32}
+    out_dtype = draw(st.sampled_from([2, 3, 5]))
     assume(maxlen != 0 and max(x_shape) <= maxlen)
-
 
     input_dict = {"X" : ["x_data"]}
     input_data_gen_dict = {
                 "x_data":
-                TensorConfig(data_gen=partial(generate_input, type=input_type, low=1, high=3, shape=x_shape))}
+                TensorConfig(data_gen=partial(generate_input, type=input_type, low=1, high=maxlen, shape=x_shape))}
     if has_max_len_tensor:
         input_dict["MaxLenTensor"] = ["max_len_tensor"]
-        input_data_gen_dict["max_len_tensor"] = [TensorConfig(data_gen=partial(generate_input, type=input_type, low=1, high=1, shape=[maxlen]))]
+        input_data_gen_dict["max_len_tensor"] = TensorConfig(data_gen=partial(generate_input, type="int32", low=1, high=2, shape=[maxlen]))
 
     sequence_mask_op = OpConfig(
         type = "sequence_mask",
         inputs = input_dict,
         outputs = {"Y" : ["output_data"]},
-        attrs = {"maxlen" : maxlen, "out_dtype" : out_dtype})
+        attrs = {"maxlen" : maxlen, "out_dtype" : out_dtype},
+        outputs_dtype = {"output_data" : out_dtype_dict[str(out_dtype)]})
 
     program_config = ProgramConfig(
         ops=[sequence_mask_op],
