2e6b1c0a0bff732fadde6eb345ed3d5e05dfe497
zhaoyang-star <zhaoyangstar@foxmail.com>
2021-12-04 16:13:49 +0800

    v1

--- lite/tests/unittest_py/op/common/test_sequence_pad_op_base.py
+++ lite/tests/unittest_py/op/common/test_sequence_pad_op_base.py
@@ -32,20 +32,41 @@ def sample_program_configs(draw):
         elif kwargs["type"] == "int64":
             return np.random.randint(kwargs["low"], kwargs["high"], kwargs["shape"]).astype(np.int64)
         elif kwargs["type"] == "float32":
-            return np.random.random(kwargs["shape"]).astype(np.float32)
+            return (kwargs["high"] - kwargs["low"]) * np.random.random(kwargs["shape"]).astype(np.float32) + kwargs["low"]
 
+    out_dtype_dict = {"int32" : np.int32,
+                      "int64" : np.int64,
+                      "float32" : np.float32}
 
-    input_type = draw(st.sampled_from(["int32", "int64", "float32"]))
-
+    input_type = "float32" # draw(st.sampled_from(["int32", "int64", "float32"]))
     x_shape = [9, 2, 3, 4] # draw(st.lists(st.integers(min_value=1, max_value=10), min_size=2, max_size=2))
     x_len_lod = [[0, 2, 5, x_shape[0]]]
     padded_length = 4
+    pad_value_shape = [1]
+
+    # assume
+    time_step_shape = x_shape[1:]
+    assume(len(x_shape) >= 2)
+    assume(len(pad_value_shape) == 1 or pad_value_shape == time_step_shape)
+    assume(len(np.array(x_len_lod).shape) >= 2)
+
+    # assume
+    seq_num = len(x_len_lod[0]) - 1
+    max_seq_len = 0
+    for i in range(0, seq_num):
+        max_seq_len = max(max_seq_len, x_len_lod[0][i + 1] - x_len_lod[0][i])
+    real_padded_length = padded_length
+    if real_padded_length == -1:
+        real_padded_length = max_seq_len
+    assume(real_padded_length >= max_seq_len)
 
     sequence_pad_op = OpConfig(
         type = "sequence_pad",
         inputs = {"X" : ["x_data"], "PadValue" : ["pad_value"]},
         outputs = {"Out" : ["output_data"], "Length": ["length_data"]},
-        attrs = {"padded_length" : padded_length})
+        attrs = {"padded_length" : padded_length},
+        outputs_dtype = {"output_data" : out_dtype_dict[input_type],
+                         "length_data" : np.int64})
 
     program_config = ProgramConfig(
         ops=[sequence_pad_op],
@@ -54,7 +75,7 @@ def sample_program_configs(draw):
             "x_data":
             TensorConfig(data_gen=partial(generate_input, type=input_type, low=-10, high=10, shape=x_shape), lod=x_len_lod),
             "pad_value":
-            TensorConfig(data_gen=partial(generate_input, type=input_type, low=0, high=10, shape=[1]))
+            TensorConfig(data_gen=partial(generate_input, type=input_type, low=0, high=10, shape=pad_value_shape))
         },
         outputs=["output_data", "length_data"])
 
