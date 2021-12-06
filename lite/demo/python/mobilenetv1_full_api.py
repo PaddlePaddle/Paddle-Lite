9a7ee6e71cd9ba19ac1969022af6f51ab19f6d79
zhaoyang-star <zhaoyangstar@foxmail.com>
2021-12-06 13:14:01 +0800

    [OpenCL] Add python API

--- lite/demo/python/mobilenetv1_full_api.py
+++ lite/demo/python/mobilenetv1_full_api.py
@@ -33,6 +33,9 @@ parser.add_argument(
     "--param_file", default="", type=str, help="Combined model param file")
 parser.add_argument(
     "--enable_opencl", action="store_true", help="Enable OpenCL or not")
+parser.add_argument(
+    "--disable_print_results", action="store_false", help="Print results or not")
+
 
 def RunModel(args):
     # 1. Set config information
@@ -44,6 +47,7 @@ def RunModel(args):
         config.set_model_dir(args.model_dir)
 
     # For arm platform (armlinux), you can set places = [Place(TargetType.ARM, PrecisionType.FP32)]
+
     if args.enable_opencl:
         places = [Place(TargetType.OpenCL, PrecisionType.FP16, DataLayoutType.ImageDefault),
                   Place(TargetType.OpenCL, PrecisionType.FP16, DataLayoutType.ImageFolder),
@@ -54,6 +58,42 @@ def RunModel(args):
                   Place(TargetType.X86, PrecisionType.FP32),
                   Place(TargetType.ARM, PrecisionType.FP32),
                   Place(TargetType.Host, PrecisionType.FP32)]
+        '''
+        Set opencl kernel binary.
+        Large addtitional prepare time is cost due to algorithm selecting and
+        building kernel from source code.
+        Prepare time can be reduced dramitically after building algorithm file
+        and OpenCL kernel binary on the first running.
+        The 1st running time will be a bit longer due to the compiling time if
+        you don't call `set_opencl_binary_path_name` explicitly.
+        So call `set_opencl_binary_path_name` explicitly is strongly
+        recommended.
+
+        Make sure you have write permission of the binary path.
+        We strongly recommend each model has a unique binary name.
+        '''
+        bin_path = "./"
+        bin_name = "lite_opencl_kernel.bin"
+        config.set_opencl_binary_path_name(bin_path, bin_name)
+
+        '''
+        opencl tune option
+        CL_TUNE_NONE: 0
+        CL_TUNE_RAPID: 1
+        CL_TUNE_NORMAL: 2
+        CL_TUNE_EXHAUSTIVE: 3
+        '''
+        tuned_path = "./"
+        tuned_name = "lite_opencl_tuned.bin"
+        config.set_opencl_tune(CLTuneMode.CL_TUNE_NORMAL, tuned_path, tuned_name, 4)
+
+        '''
+        opencl precision option
+        CL_PRECISION_AUTO: 0, first fp16 if valid, default
+        CL_PRECISION_FP32: 1, force fp32
+        CL_PRECISION_FP16: 2, force fp16
+        '''
+        config.set_opencl_precision(CLPrecisionType.CL_PRECISION_AUTO)
     else:
         places = [Place(TargetType.X86, PrecisionType.FP32)]
 
@@ -73,7 +113,9 @@ def RunModel(args):
     # 5. Get output data
     output_tensor = predictor.get_output(0)
     output_data = output_tensor.numpy()
-    print(output_data)
+    if args.disable_print_results:
+        print(output_data)
+
 
 if __name__ == '__main__':
     args = parser.parse_args()
