ee4fc71e564c12d6573fe98640fc5f72989026c2
DannyIsFunny <912790387@qq.com>
2021-12-07 09:09:24 +0000

    add api for place require

--- lite/tests/unittest_py/program_config.py
+++ lite/tests/unittest_py/program_config.py
@@ -456,3 +456,27 @@ class CxxConfig:
         self.config["discarded_passes"].append(discarded_pass)
     def value(self):
         return self.config
+
+    def target():
+        if not self.config.has_key("valid_targets"):
+            return None
+        first_place=self.config["valid_targets"][0].split(",")
+        return eval("TargetType." + first_place[0])
+
+    def precision():
+        if not self.config.has_key("valid_targets"):
+            return None
+        first_place=''.join(self.config["valid_targets"][0]).split(",")
+        if len(first_place) < 2:
+            return PrecisionType.FP32
+        else:
+            return eval("PrecisionType." + first_place[1])
+
+    def layout():
+        if not self.config.has_key("valid_targets"):
+            return None
+        first_place=''.join(self.config["valid_targets"][0]).split(",")
+        if len(first_place) < 3:
+            return DataLayoutType.NCHW
+        else:
+            return eval("DataLayoutType." + first_place[2])
