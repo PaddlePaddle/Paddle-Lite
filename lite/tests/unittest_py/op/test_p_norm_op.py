796330f973709c465498c6e1ebeb75ff1d6e14f5
xingjing1 <2907374824@qq.com>
2021-12-08 16:32:52 +0800

    add PyTest, test=document_fix

--- lite/tests/unittest_py/op/test_p_norm_op.py
+++ lite/tests/unittest_py/op/test_p_norm_op.py
@@ -29,7 +29,7 @@ import hypothesis.strategies as st
 class TestPNormOp(AutoScanTest):
     def __init__(self, *args, **kwargs):
         AutoScanTest.__init__(self, *args, **kwargs)
-        self.enable_testing_on_place(TargetType.ARM, PrecisionType.FP32, DataLayoutType.NCHW)
+        self.enable_testing_on_place(TargetType.ARM, PrecisionType.FP32, DataLayoutType.NCHW, thread=[1,4])
 
     def is_program_valid(self, program_config: ProgramConfig , predictor_config: CxxConfig) -> bool:
         return True
