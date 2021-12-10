f2a643186af1d623971e392f164d6d82357275b5
lxw950127 <1182657559@qq.com>
2021-12-10 15:44:54 +0800

    except conv pass

---
+++ lite/tests/unittest_py/pass/test_fc_fuse_pass.py
@@ -0,0 +1,97 @@
+# Copyright (c) 2021 PaddlePaddle Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+import sys
+sys.path.append('..')
+
+from auto_scan_test import AutoScanTest, IgnoreReasons
+from program_config import TensorConfig, ProgramConfig, OpConfig, CxxConfig, TargetType, PrecisionType, DataLayoutType, Place
+import numpy as np
+from functools import partial
+from typing import Optional, List, Callable, Dict, Any, Set
+import unittest
+
+import hypothesis
+from hypothesis import given, settings, seed, example, assume, reproduce_failure
+import hypothesis.strategies as st
+
+
+class TestConvBnFuse(AutoScanTest):
+    def __init__(self, *args, **kwargs):
+        AutoScanTest.__init__(self, *args, **kwargs)
+        self.enable_testing_on_place(TargetType.X86, [PrecisionType.FP32], DataLayoutType.NCHW, thread=[1, 4])
+        self.enable_testing_on_place(TargetType.ARM, [PrecisionType.FP32], DataLayoutType.NCHW, thread=[1, 4])           
+        opencl_places = [Place(TargetType.OpenCL, PrecisionType.FP16, DataLayoutType.ImageDefault),
+                          Place(TargetType.OpenCL, PrecisionType.FP16, DataLayoutType.ImageFolder),
+                          Place(TargetType.OpenCL, PrecisionType.FP32, DataLayoutType.NCHW),
+                          Place(TargetType.OpenCL, PrecisionType.Any, DataLayoutType.ImageDefault),
+                          Place(TargetType.OpenCL, PrecisionType.Any, DataLayoutType.ImageFolder),
+                          Place(TargetType.OpenCL, PrecisionType.Any, DataLayoutType.NCHW),
+                          Place(TargetType.Host, PrecisionType.FP32)    
+                        ]
+        self.enable_testing_on_place(places=opencl_places)
+
+    def is_program_valid(self, program_config: ProgramConfig , predictor_config: CxxConfig) -> bool:
+        return True
+
+    def sample_program_configs(self, draw):
+        mul_x_in_shape=draw(st.lists(st.integers(min_value=5, max_value=10), min_size=3, max_size=5))
+        x_num_col_dims_data=len(mul_x_in_shape)
+        last=draw(st.integers(min_value=5, max_value=10))
+        mul_x_in_shape=mul_x_in_shape+[last]
+        #lite not check fuse condition : bias[0]=1 bias[1]=weight[1] 
+        add_x_data_shape= [1, draw(st.integers(min_value=5, max_value=10))]
+
+        mul_op = OpConfig(
+            type =  "mul",
+            inputs = {
+                "X": ["mul_x_data"],
+                "Y": ["mul_y_data"]},
+            outputs = {
+                "Out": ["mul_output_data"]},
+            attrs = {
+                "x_num_col_dims": x_num_col_dims_data,
+                "y_num_col_dims": 1})
+
+        elementwise_add_op = OpConfig(
+            type = "elementwise_add",
+            inputs = {"X": ["mul_output_data"], "Y": ["add_x_data"]},
+            outputs = {"Out": ["output_data"]},
+            attrs = {"axis": -1})    
+
+        ops = [mul_op, elementwise_add_op]
+        program_config = ProgramConfig(
+            ops=ops,
+            weights={
+                "add_x_data": TensorConfig(shape=add_x_data_shape)        
+            },
+            inputs={
+                "mul_x_data": TensorConfig(shape=mul_x_in_shape),
+                "mul_y_data": TensorConfig(shape=[last, add_x_data_shape[1]])
+
+            },
+            outputs=["output_data"])
+        return program_config
+    
+    def sample_predictor_configs(self):
+        config = CxxConfig()
+        return self.get_predictor_configs(), ["fc"], (1e-5, 1e-5)
+
+    def add_ignore_pass_case(self):
+        pass
+
+    def test(self, *args, **kwargs):
+        self.run_and_statis(quant=False, max_examples=25)
+
+if __name__ == "__main__":
+    unittest.main(argv=[''])
