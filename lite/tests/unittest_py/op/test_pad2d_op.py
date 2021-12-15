7716378ef3a4e874810330203b17b3af36afcf62
zhangjun <ewalker@live.cn>
2021-12-15 12:29:22 +0000

    update,test=document_fix

--- lite/tests/unittest_py/op/test_pad2d_op.py
+++ lite/tests/unittest_py/op/test_pad2d_op.py
@@ -27,23 +27,40 @@ import argparse
 import numpy as np
 from functools import partial
 
+
 class TestPad2dOp(AutoScanTest):
     def __init__(self, *args, **kwargs):
         AutoScanTest.__init__(self, *args, **kwargs)
-        self.enable_testing_on_place(TargetType.Host, PrecisionType.FP32, DataLayoutType.NCHW, thread=[1,2])
-        self.enable_testing_on_place(TargetType.X86, PrecisionType.FP32, DataLayoutType.NCHW, thread=[1,2])
+        self.enable_testing_on_place(
+            TargetType.Host,
+            PrecisionType.FP32,
+            DataLayoutType.NCHW,
+            thread=[1, 2])
+        self.enable_testing_on_place(
+            TargetType.X86,
+            PrecisionType.FP32,
+            DataLayoutType.NCHW,
+            thread=[1, 2])
 
         # metal
-        metal_places = [Place(TargetType.Metal, PrecisionType.FP32, DataLayoutType.MetalTexture2DArray),
-                        Place(TargetType.Metal, PrecisionType.FP16, DataLayoutType.MetalTexture2DArray)]
+        metal_places = [
+            Place(TargetType.Metal, PrecisionType.FP32,
+                  DataLayoutType.MetalTexture2DArray),
+            Place(TargetType.Metal, PrecisionType.FP16,
+                  DataLayoutType.MetalTexture2DArray)
+        ]
         self.enable_testing_on_place(places=metal_places)
 
-    def is_program_valid(self, program_config: ProgramConfig , predictor_config: CxxConfig) -> bool:
+    def is_program_valid(self,
+                         program_config: ProgramConfig,
+                         predictor_config: CxxConfig) -> bool:
         return True
 
     def sample_program_configs(self, draw):
-        in_shape = draw(st.lists(st.integers(
-                min_value=1, max_value=10), min_size=4, max_size=4))
+        in_shape = draw(
+            st.lists(
+                st.integers(
+                    min_value=1, max_value=10), min_size=4, max_size=4))
 
         mode = draw(st.sampled_from(["constant", "reflect", "edge"]))
         value_data = draw(st.floats(min_value=0.0, max_value=4.0))
@@ -55,15 +72,13 @@ class TestPad2dOp(AutoScanTest):
             return np.random.random(in_shape).astype(np.float32)
 
         build_ops = OpConfig(
-            type = "pad2d",
-            inputs = {
-                "X" : ["input_data"],
+            type="pad2d",
+            inputs={
+                "X": ["input_data"],
                 #"Paddings": padding_data,
-                },
-            outputs = {
-                "Out": ["output_data"],
             },
-            attrs = {
+            outputs={"Out": ["output_data"], },
+            attrs={
                 "paddings": padding_data,
                 "mode": mode,
                 "value": value_data,
@@ -73,8 +88,7 @@ class TestPad2dOp(AutoScanTest):
             ops=[build_ops],
             weights={},
             inputs={
-                "input_data":
-                TensorConfig(data_gen=partial(generate_input)),
+                "input_data": TensorConfig(data_gen=partial(generate_input)),
             },
             outputs=["output_data"])
         return program_config
@@ -87,6 +101,7 @@ class TestPad2dOp(AutoScanTest):
 
     def test(self, *args, **kwargs):
         self.run_and_statis(quant=False, max_examples=25)
+
 
 if __name__ == "__main__":
     unittest.main(argv=[''])
