348c91eee051bd4f74faf833d2fb79cee399a1df
zhaoyang-star <zhaoyangstar@foxmail.com>
2021-12-16 13:19:11 +0800

    [OpTestPy]acos opencl op ut test=document_fix

--- lite/tests/unittest_py/op/test_acos_op.py
+++ lite/tests/unittest_py/op/test_acos_op.py
@@ -85,13 +85,12 @@ class TestAcosOp(AutoScanTest):
             inputs={"X": ["input_data"]},
             outputs={"Out": ["output_data"]},
             attrs={})
-        # Make sure input data is greater than -0.9 because the output is infinite when input is close to -1
         program_config = ProgramConfig(
             ops=[acos_op],
             weights={},
             inputs={
                 "input_data": TensorConfig(data_gen=partial(
-                    generate_data, low=-0.9, high=1, shape=in_shape))
+                    generate_data, low=-1, high=1, shape=in_shape))
             },
             outputs=["output_data"])
         return program_config
