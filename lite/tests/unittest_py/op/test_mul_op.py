fd4740ed32c5e72eaa8985087742ed569edba561
huzhiqiang <912790387@qq.com>
2021-12-16 20:24:14 +0800

    Update test_mul_op.py

--- lite/tests/unittest_py/op/test_mul_op.py
+++ lite/tests/unittest_py/op/test_mul_op.py
@@ -146,5 +146,6 @@ class TestMulOp(AutoScanTest):
     def test(self, *args, **kwargs):
         self.run_and_statis(quant=False, max_examples=25)
 
+
 if __name__ == "__main__":
     unittest.main(argv=[''])
