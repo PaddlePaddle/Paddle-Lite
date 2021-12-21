463c2efc30bbd6e1bf0a478b9ae3798ce3ce65ee
csy0225 <chensiyu08@baidu.com>
2021-12-21 08:23:38 +0000

    [OpTestPy] Add elementwise op unit test, test=develop, test=document_fix

---
+++ lite/tests/unittest_py/op/test_elementwise_add_op.py
@@ -0,0 +1,188 @@
+# Copyright (c) 2021 PaddlePaddle Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import sys
+sys.path.append('../')
+
+from auto_scan_test import AutoScanTest, IgnoreReasons
+from program_config import TensorConfig, ProgramConfig, OpConfig, CxxConfig, TargetType, PrecisionType, DataLayoutType, Place
+import unittest
+
+import hypothesis
+from hypothesis import given, settings, seed, example, assume, reproduce_failure
+import hypothesis.strategies as st
+import numpy as np
+from functools import partial
+
+
+def check_broadcast(x_shape, y_shape, axis):
+    if x_shape == y_shape:
+        return True
+    else:
+        x_dims_size = len(x_shape)
+        y_dims_size = len(y_shape)
+        max_dims_size = max(x_dims_size, y_dims_size)
+        if (x_dims_size == y_dims_size):
+            # The axis should be -1 or 0 while the dimension of tensor X is equal to the dimension of tensor Y.
+            if not (axis == -1 or axis == 0):
+                return False
+        # The axis should be met [-max(x_dims_size, y_dims_size), max(x_dims_size, y_dims_size))
+        if not (axis >= (-max_dims_size) and axis < max_dims_size):
+            return False
+        if axis < 0:
+            axis = abs(x_dims_size - y_dims_size) + axis + 1
+        if not (axis >= 0 and axis < max_dims_size):
+            return False
+        # The axis should be met axis + min(x_dims_size, y_dims_size) <= max(x_dims_size, y_dims_size)
+        if axis + min(x_dims_size, y_dims_size) > max_dims_size:
+            return False
+        x_dims_array = [1 for i in range(max_dims_size)]
+        y_dims_array = [1 for i in range(max_dims_size)]
+        if x_dims_size > y_dims_size:
+            x_dims_array = x_shape
+            for i in range(y_dims_size):
+                y_dims_array[axis + i] = y_shape[i]
+        else:
+            y_dims_array = y_shape
+            for i in range(x_dims_size):
+                x_dims_array[axis + i] = x_shape[i]
+        for i in range(max_dims_size):
+            broadcast_flag = (x_dims_array[i] == y_dims_array[i]) or (
+                x_dims_array[i] <= 1) or (y_dims_array[i] <= 1)
+            if not broadcast_flag:
+                return False
+        return True
+
+
+class TestElementwiseAddOp(AutoScanTest):
+    def __init__(self, *args, **kwargs):
+        AutoScanTest.__init__(self, *args, **kwargs)
+        self.enable_testing_on_place(
+            TargetType.X86,
+            PrecisionType.FP32,
+            DataLayoutType.NCHW,
+            thread=[1, 4])
+        arm_places = [
+            Place(TargetType.ARM, PrecisionType.FP32, DataLayoutType.NCHW),
+            Place(TargetType.ARM, PrecisionType.FP16, DataLayoutType.NCHW),
+            Place(TargetType.ARM, PrecisionType.INT32, DataLayoutType.NCHW),
+            Place(TargetType.ARM, PrecisionType.INT64, DataLayoutType.NCHW)
+        ]
+        self.enable_testing_on_place(places=arm_places, thread=[1, 4])
+        opencl_valid_places = [
+            Place(TargetType.OpenCL, PrecisionType.FP16,
+                  DataLayoutType.ImageDefault), Place(
+                      TargetType.OpenCL, PrecisionType.FP16,
+                      DataLayoutType.ImageFolder),
+            Place(TargetType.OpenCL, PrecisionType.FP32, DataLayoutType.NCHW),
+            Place(TargetType.OpenCL, PrecisionType.Any,
+                  DataLayoutType.ImageDefault), Place(
+                      TargetType.OpenCL, PrecisionType.Any,
+                      DataLayoutType.ImageFolder),
+            Place(TargetType.OpenCL, PrecisionType.Any, DataLayoutType.NCHW),
+            Place(TargetType.Host, PrecisionType.FP32)
+        ]
+        self.enable_testing_on_place(places=opencl_valid_places)
+        metal_valid_places = [
+            Place(TargetType.Metal, PrecisionType.FP32,
+                  DataLayoutType.MetalTexture2DArray),
+            Place(TargetType.Metal, PrecisionType.FP16,
+                  DataLayoutType.MetalTexture2DArray)
+        ]
+        self.enable_testing_on_place(places=metal_valid_places)
+
+    def is_program_valid(self,
+                         program_config: ProgramConfig,
+                         predictor_config: CxxConfig) -> bool:
+        target_type = predictor_config.target()
+        in_x_shape = list(program_config.inputs["input_data_x"].shape)
+        in_y_shape = list(program_config.inputs["input_data_y"].shape)
+        if target_type == TargetType.Metal:
+            if in_x_shape != in_y_shape:
+                return False
+        return True
+
+    def sample_program_configs(self, draw):
+        input_data_x_shape = draw(
+            st.lists(
+                st.integers(
+                    min_value=1, max_value=20), min_size=1, max_size=6))
+        input_data_y_shape = draw(
+            st.lists(
+                st.integers(
+                    min_value=1, max_value=20), min_size=1, max_size=6))
+        axis = draw(st.integers(min_value=-6, max_value=6))
+        assume(
+            check_broadcast(input_data_x_shape, input_data_y_shape, axis) ==
+            True)
+        if axis < 0:
+            axis = abs(len(input_data_x_shape) - len(
+                input_data_y_shape)) + axis + 1
+        input_data_type = draw(
+            st.sampled_from([np.int32, np.int64, np.float32]))
+
+        def gen_input_data(*args, **kwargs):
+            return np.random.randint(
+                1, 20, size=(kwargs['shape'])).astype(kwargs['dtype'])
+
+        elementwise_add_op = OpConfig(
+            type="elementwise_add",
+            inputs={"X": ["input_data_x"],
+                    "Y": ["input_data_y"]},
+            outputs={"Out": ["output_data"]},
+            attrs={"axis": axis})
+        program_config = ProgramConfig(
+            ops=[elementwise_add_op],
+            weights={},
+            inputs={
+                "input_data_x": TensorConfig(data_gen=partial(
+                    gen_input_data,
+                    shape=input_data_x_shape,
+                    dtype=input_data_type)),
+                "input_data_y": TensorConfig(data_gen=partial(
+                    gen_input_data,
+                    shape=input_data_y_shape,
+                    dtype=input_data_type))
+            },
+            outputs=["output_data"])
+        return program_config
+
+    def sample_predictor_configs(self):
+        config = CxxConfig()
+        return self.get_predictor_configs(), ["elementwise_add"], (1e-5, 1e-5)
+
+    def add_ignore_pass_case(self):
+        err_msg = ""
+
+        def teller1(program_config, predictor_config):
+            target_type = predictor_config.target()
+            in_data_type = program_config.inputs["input_data_x"].dtype
+            if target_type in [TargetType.OpenCL, TargetType.Metal]:
+                if in_data_type != np.float32:
+                    err_msg = "Elementwise_add op on" + str(
+                        target_type
+                    ) + "backend only support input data type[float32/float16], but got" + str(
+                        in_data_type)
+                    return True
+            return False
+
+        self.add_ignore_check_case(
+            teller1, IgnoreReasons.PADDLELITE_NOT_SUPPORT, err_msg)
+
+    def test(self, *args, **kwargs):
+        self.run_and_statis(quant=False, max_examples=300)
+
+
+if __name__ == "__main__":
+    unittest.main(argv=[''])
---
+++ lite/tests/unittest_py/op/test_elementwise_mul_op.py
@@ -0,0 +1,150 @@
+# Copyright (c) 2021 PaddlePaddle Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import sys
+sys.path.append('../')
+
+from auto_scan_test import AutoScanTest, IgnoreReasons
+from program_config import TensorConfig, ProgramConfig, OpConfig, CxxConfig, TargetType, PrecisionType, DataLayoutType, Place
+import unittest
+
+import hypothesis
+from hypothesis import given, settings, seed, example, assume, reproduce_failure
+import hypothesis.strategies as st
+import numpy as np
+from functools import partial
+from test_elementwise_add_op import check_broadcast
+
+
+class TestElementwiseMulOp(AutoScanTest):
+    def __init__(self, *args, **kwargs):
+        AutoScanTest.__init__(self, *args, **kwargs)
+        self.enable_testing_on_place(
+            TargetType.X86,
+            PrecisionType.FP32,
+            DataLayoutType.NCHW,
+            thread=[1, 4])
+        arm_places = [
+            Place(TargetType.ARM, PrecisionType.FP32, DataLayoutType.NCHW),
+            Place(TargetType.ARM, PrecisionType.FP16, DataLayoutType.NCHW),
+            Place(TargetType.ARM, PrecisionType.INT32, DataLayoutType.NCHW),
+            Place(TargetType.ARM, PrecisionType.INT64, DataLayoutType.NCHW)
+        ]
+        self.enable_testing_on_place(places=arm_places, thread=[1, 4])
+        opencl_valid_places = [
+            Place(TargetType.OpenCL, PrecisionType.FP16,
+                  DataLayoutType.ImageDefault), Place(
+                      TargetType.OpenCL, PrecisionType.FP16,
+                      DataLayoutType.ImageFolder),
+            Place(TargetType.OpenCL, PrecisionType.FP32, DataLayoutType.NCHW),
+            Place(TargetType.OpenCL, PrecisionType.Any,
+                  DataLayoutType.ImageDefault), Place(
+                      TargetType.OpenCL, PrecisionType.Any,
+                      DataLayoutType.ImageFolder),
+            Place(TargetType.OpenCL, PrecisionType.Any, DataLayoutType.NCHW),
+            Place(TargetType.Host, PrecisionType.FP32)
+        ]
+        self.enable_testing_on_place(places=opencl_valid_places)
+        metal_valid_places = [
+            Place(TargetType.Metal, PrecisionType.FP32,
+                  DataLayoutType.MetalTexture2DArray),
+            Place(TargetType.Metal, PrecisionType.FP16,
+                  DataLayoutType.MetalTexture2DArray)
+        ]
+        self.enable_testing_on_place(places=metal_valid_places)
+
+    def is_program_valid(self,
+                         program_config: ProgramConfig,
+                         predictor_config: CxxConfig) -> bool:
+        target_type = predictor_config.target()
+        in_x_shape = list(program_config.inputs["input_data_x"].shape)
+        in_y_shape = list(program_config.inputs["input_data_y"].shape)
+        if target_type == TargetType.Metal:
+            if in_x_shape != in_y_shape:
+                return False
+        return True
+
+    def sample_program_configs(self, draw):
+        input_data_x_shape = draw(
+            st.lists(
+                st.integers(
+                    min_value=1, max_value=20), min_size=1, max_size=6))
+        input_data_y_shape = draw(
+            st.lists(
+                st.integers(
+                    min_value=1, max_value=20), min_size=1, max_size=6))
+        axis = draw(st.integers(min_value=-6, max_value=6))
+        assume(
+            check_broadcast(input_data_x_shape, input_data_y_shape, axis) ==
+            True)
+        if axis < 0:
+            axis = abs(len(input_data_x_shape) - len(
+                input_data_y_shape)) + axis + 1
+        input_data_type = draw(
+            st.sampled_from([np.int32, np.int64, np.float32]))
+
+        def gen_input_data(*args, **kwargs):
+            return np.random.randint(
+                1, 20, size=(kwargs['shape'])).astype(kwargs['dtype'])
+
+        elementwise_mul_op = OpConfig(
+            type="elementwise_mul",
+            inputs={"X": ["input_data_x"],
+                    "Y": ["input_data_y"]},
+            outputs={"Out": ["output_data"]},
+            attrs={"axis": axis})
+        program_config = ProgramConfig(
+            ops=[elementwise_mul_op],
+            weights={},
+            inputs={
+                "input_data_x": TensorConfig(data_gen=partial(
+                    gen_input_data,
+                    shape=input_data_x_shape,
+                    dtype=input_data_type)),
+                "input_data_y": TensorConfig(data_gen=partial(
+                    gen_input_data,
+                    shape=input_data_y_shape,
+                    dtype=input_data_type))
+            },
+            outputs=["output_data"])
+        return program_config
+
+    def sample_predictor_configs(self):
+        config = CxxConfig()
+        return self.get_predictor_configs(), ["elementwise_mul"], (1e-5, 1e-5)
+
+    def add_ignore_pass_case(self):
+        err_msg = ""
+
+        def teller1(program_config, predictor_config):
+            target_type = predictor_config.target()
+            in_data_type = program_config.inputs["input_data_x"].dtype
+            if target_type in [TargetType.OpenCL, TargetType.Metal]:
+                if in_data_type != np.float32:
+                    err_msg = "Elementwise_add op on" + str(
+                        target_type
+                    ) + "backend only support input data type[float32/float16], but got" + str(
+                        in_data_type)
+                    return True
+            return False
+
+        self.add_ignore_check_case(
+            teller1, IgnoreReasons.PADDLELITE_NOT_SUPPORT, err_msg)
+
+    def test(self, *args, **kwargs):
+        self.run_and_statis(quant=False, max_examples=300)
+
+
+if __name__ == "__main__":
+    unittest.main(argv=[''])
