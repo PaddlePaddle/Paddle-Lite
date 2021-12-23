a78daa855b78136dafad6403e86a26d11cf6fa21
xingjing1 <2907374824@qq.com>
2021-12-23 11:11:42 +0000

    add test

--- lite/tests/unittest_py/op/test_mul_op.py
+++ lite/tests/unittest_py/op/test_mul_op.py
@@ -11,7 +11,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-'''
 import sys
 sys.path.append('../')
 
@@ -109,4 +108,3 @@ class TestMulOp(AutoScanTest):
 
 if __name__ == "__main__":
     unittest.main(argv=[''])
-'''
