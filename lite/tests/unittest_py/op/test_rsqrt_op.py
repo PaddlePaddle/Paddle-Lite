c4653757f8cddf7f32db012fbdb86c57a2b193bf
zhaoyang-star <zhaoyangstar@foxmail.com>
2021-12-24 12:27:16 +0800

    [OpenCL] Add scale/rsqrt (#7946)

--- lite/tests/unittest_py/op/test_rsqrt_op.py
+++ lite/tests/unittest_py/op/test_rsqrt_op.py
@@ -62,9 +62,9 @@ class TestRsqrtOp(AutoScanTest):
     def is_program_valid(self,
                          program_config: ProgramConfig,
                          predictor_config: CxxConfig) -> bool:
-        return False  # fix arm_opencl ci error
         in_shape = list(program_config.inputs["input_data"].shape)
-        if predictor_config.target() == TargetType.OpenCL:
+        target = predictor_config.target()
+        if target == TargetType.OpenCL:
             if len(in_shape) != 4:
                 return False
         return True
