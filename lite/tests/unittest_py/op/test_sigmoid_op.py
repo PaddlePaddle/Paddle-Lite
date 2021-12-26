f0636e522f76092faaab6229502fd5933fc976e2
xiaoxiaohehe001 <hiteezsf@163.com>
2021-12-26 17:49:39 +0800

    test=document_fix

--- lite/tests/unittest_py/op/test_sigmoid_op.py
+++ lite/tests/unittest_py/op/test_sigmoid_op.py
@@ -70,7 +70,7 @@ class TestSigmoidOp(AutoScanTest):
                          predictor_config: CxxConfig) -> bool:
         x_shape = list(program_config.inputs["input_data"].shape)
         if predictor_config.target() == TargetType.Metal:
-            if len(x_shape) <= 3:
+            if len(x_shape) != 4:
                 return False
         return True
 
@@ -100,13 +100,23 @@ class TestSigmoidOp(AutoScanTest):
         return program_config
 
     def sample_predictor_configs(self):
-        return self.get_predictor_configs(), ["sigmoid"], (5e-4, 5e-4)
+        atol, rtol = 1e-5, 1e-5
+        config_lists = self.get_predictor_configs()
+        for config in config_lists:
+            if config.target() in [TargetType.Metal]:
+                atol, rtol = 5e-4, 5e-4
+
+        return self.get_predictor_configs(), ["sigmoid"], (atol, rtol)
 
     def add_ignore_pass_case(self):
         pass
 
     def test(self, *args, **kwargs):
-        self.run_and_statis(quant=False, max_examples=60)
+        target_str = self.get_target()
+        if target_str == "Metal":
+            self.run_and_statis(quant=False, max_examples=60)
+        else:
+            self.run_and_statis(quant=False, max_examples=25)
 
 
 if __name__ == "__main__":
