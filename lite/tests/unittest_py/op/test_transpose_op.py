1cf02998f1682bbe696859dac288049669ccf7bd
xiaoxiaohehe001 <hiteezsf@163.com>
2021-12-28 20:42:24 +0800

    test=document_fix

--- lite/tests/unittest_py/op/test_transpose_op.py
+++ lite/tests/unittest_py/op/test_transpose_op.py
@@ -56,21 +56,23 @@ class TestTransposeOp(AutoScanTest):
         # ]
         # self.enable_testing_on_place(places=opencl_places)
 
-        # metal having diffs but only small diff withing 1e-2 but not 1e-5
-        # but it will run erros at some case
-        # metal_places = [
-        #     Place(TargetType.Metal, PrecisionType.FP32,
-        #           DataLayoutType.MetalTexture2DArray),
-        #     Place(TargetType.Metal, PrecisionType.FP16,
-        #           DataLayoutType.MetalTexture2DArray),
-        #     Place(TargetType.ARM, PrecisionType.FP32),
-        #     Place(TargetType.Host, PrecisionType.FP32)
-        # ]
-        # self.enable_testing_on_place(places=metal_places)
+        metal_places = [
+            Place(TargetType.Metal, PrecisionType.FP32,
+                  DataLayoutType.MetalTexture2DArray),
+            Place(TargetType.Metal, PrecisionType.FP16,
+                  DataLayoutType.MetalTexture2DArray),
+            Place(TargetType.ARM, PrecisionType.FP32),
+            Place(TargetType.Host, PrecisionType.FP32)
+        ]
+        self.enable_testing_on_place(places=metal_places)
 
     def is_program_valid(self,
                          program_config: ProgramConfig,
                          predictor_config: CxxConfig) -> bool:
+        x_shape = list(program_config.inputs["X_data"].shape)
+        if predictor_config.target() == TargetType.Metal:
+            if x_shape[0] != 1 or x_shape[1] > 64:
+                return False
         return True
 
     def sample_program_configs(self, draw):
@@ -117,13 +119,21 @@ class TestTransposeOp(AutoScanTest):
         return program_config
 
     def sample_predictor_configs(self):
-        return self.get_predictor_configs(), [""], (1e-5, 1e-5)
+        atol, rtol = 1e-5, 1e-5
+        target_str = self.get_target()
+        if target_str == "Metal":
+            atol, rtol = 5e-4, 5e-4
+        return self.get_predictor_configs(), ["transpose"], (atol, rtol)
 
     def add_ignore_pass_case(self):
         pass
 
     def test(self, *args, **kwargs):
-        self.run_and_statis(quant=False, max_examples=25)
+        target_str = self.get_target()
+        if target_str == "Metal":
+            self.run_and_statis(quant=False, max_examples=300)
+        else:
+            self.run_and_statis(quant=False, max_examples=25)
 
 
 if __name__ == "__main__":
