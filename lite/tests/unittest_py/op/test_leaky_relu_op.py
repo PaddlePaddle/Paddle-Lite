f35fb6cd60a3eca5756fbb39a8fe99b42616c0c4
xiaoxiaohehe001 <hiteezsf@163.com>
2021-12-28 21:06:02 +0800

    test=document_fix

--- lite/tests/unittest_py/op/test_leaky_relu_op.py
+++ lite/tests/unittest_py/op/test_leaky_relu_op.py
@@ -45,6 +45,15 @@ class TestLeakyReluOp(AutoScanTest):
             PrecisionType.FP32,
             DataLayoutType.NCHW,
             thread=[1, 2])
+        metal_places = [
+            Place(TargetType.Metal, PrecisionType.FP32,
+                  DataLayoutType.MetalTexture2DArray),
+            Place(TargetType.Metal, PrecisionType.FP16,
+                  DataLayoutType.MetalTexture2DArray),
+            Place(TargetType.ARM, PrecisionType.FP32),
+            Place(TargetType.Host, PrecisionType.FP32)
+        ]
+        self.enable_testing_on_place(places=metal_places)
 
     def is_program_valid(self,
                          program_config: ProgramConfig,
@@ -76,7 +85,11 @@ class TestLeakyReluOp(AutoScanTest):
         return program_config
 
     def sample_predictor_configs(self):
-        return self.get_predictor_configs(), ["leaky_relu"], (1e-5, 1e-5)
+        atol, rtol = 1e-5, 1e-5
+        target_str = self.get_target()
+        if target_str == "Metal":
+            atol, rtol = 2e-4, 2e-4
+        return self.get_predictor_configs(), ["leaky_relu"], (atol, rtol)
 
     def add_ignore_pass_case(self):
         pass
