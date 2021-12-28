275cc81accfa433fe1fc80ca98e1c2a0e84f1486
devchai123 <986828121@qq.com>
2021-12-28 16:19:35 +0800

    第一次提交pr

---
+++ tools/ci_tools/ci_chai_paddle.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+
+
+# coding=UTF-8
+filename = 'test_text.txt'
+def func(filename):
+    with open(filename, 'w') as file_object:
+        file_object.write("Add a word")
+if __name__ == '__main__':
+    func(filenaiime)
