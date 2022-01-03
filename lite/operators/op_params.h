8f44cc0a53678120b10b787072c39be8111d9954
csy0225 <78470701+csy0225@users.noreply.github.com>
2022-01-03 16:39:41 +0800

    [OpTestPy] Add crf_decoding distribute_fpn_proposals equal op unit test (#8076)

--- lite/operators/op_params.h
+++ lite/operators/op_params.h
@@ -1540,6 +1540,7 @@ struct DistributeFpnProposalsParam : ParamBase {
   int max_level{};
   int refer_level{};
   int refer_scale{};
+  bool pixel_offset{true};
 };
 
 /// --------------------- instance_norm operators --------------------
