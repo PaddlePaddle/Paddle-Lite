9388443a2a3f2a65dab2aa396736ac07546b3091
shentanyue <34421038+shentanyue@users.noreply.github.com>
2022-01-08 13:12:02 +0800

    [NNAdapter] Enhance the dynamic shape inference (#8166)

--- lite/backends/nnadapter/nnadapter/driver/kunlunxin_xtcl/converter/reshape.cc
+++ lite/backends/nnadapter/nnadapter/driver/kunlunxin_xtcl/converter/reshape.cc
@@ -15,7 +15,9 @@
 #include "core/operation/reshape.h"
 #include "driver/kunlunxin_xtcl/converter/converter.h"
 #include "utility/debug.h"
+#include "utility/hints.h"
 #include "utility/logging.h"
+#include "utility/modeling.h"
 
 namespace nnadapter {
 namespace kunlunxin_xtcl {
@@ -27,6 +29,36 @@ int ConvertReshape(Converter* converter, hal::Operation* operation) {
   auto input_expr = converter->GetMappedExpr(input_operand);
   if (!input_expr.defined()) {
     input_expr = converter->ConvertOperand(input_operand);
+  }
+  uint32_t shape_count = 0;
+  int32_t* shape_data = nullptr;
+  if (IsTemporaryShapeOperand(shape_operand)) {
+    if (IsOperandWithDynamicShape(shape_operand)) {
+      NNADAPTER_LOG(FATAL) << "Unsupported dynamic shape for reshape operation";
+    } else {
+      auto& temporary_shape = *(GetTemporaryShape(shape_operand));
+      shape_count = temporary_shape.count;
+      shape_data = temporary_shape.data;
+      for (uint32_t i = 0; i < shape_count; i++) {
+        if (shape_data[i] == 0) {
+          shape_data[i] = input_operand->type.dimensions.data[i];
+        }
+      }
+    }
+  } else if (IsConstantOperand(shape_operand)) {
+    shape_count = shape_operand->length / sizeof(int32_t);
+    shape_data = reinterpret_cast<int32_t*>(shape_operand->buffer);
+    for (uint32_t i = 0; i < shape_count; i++) {
+      if (shape_data[i] == 0 &&
+          input_operand->type.dimensions.data[i] != NNADAPTER_UNKNOWN) {
+        shape_data[i] = input_operand->type.dimensions.data[i];
+      }
+    }
+  } else {
+    NNADAPTER_LOG(FATAL) << "Unsupported shape lifetime: "
+                         << OperandLifetimeCodeToString(
+                                shape_operand->type.lifetime);
+    return NNADAPTER_INVALID_PARAMETER;
   }
   auto reshape_expr = converter->builder()->CreateReshape(
       input_expr, ConvertToXTCLArray<xtcl::Integer>(shape_data, shape_count));
