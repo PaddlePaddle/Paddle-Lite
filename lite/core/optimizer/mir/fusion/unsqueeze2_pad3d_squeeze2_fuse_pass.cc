96690457da2991d16e0d626ca5d1369f22e1604a
zhaoyang-star <zhaoyangstar@foxmail.com>
2022-01-17 22:00:27 +0800

    fix kirin device failure

--- lite/core/optimizer/mir/fusion/unsqueeze2_pad3d_squeeze2_fuse_pass.cc
+++ lite/core/optimizer/mir/fusion/unsqueeze2_pad3d_squeeze2_fuse_pass.cc
@@ -12,10 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-//
-// Created by chenyaohuang on 2021/12/17.
-//
-
 #include "lite/core/optimizer/mir/fusion/unsqueeze2_pad3d_squeeze2_fuse.h"
 #include <memory>
 #include <vector>
@@ -28,10 +24,8 @@ namespace mir {
 
 void Unsqueeze2Pad3dSqueeze2FusePass::Apply(
     const std::unique_ptr<SSAGraph>& graph) {
-  VLOG(4) << "start";
   fusion::Unsqueeze2Pad3dSqueeze2Fuser fuser("unsqueeze2", "pad3d", "squeeze2");
   fuser(graph.get());
-  VLOG(4) << "end";
 }
 }  // namespace mir
 }  // namespace lite
@@ -40,4 +34,4 @@ void Unsqueeze2Pad3dSqueeze2FusePass::Apply(
 REGISTER_MIR_PASS(lite_unsqueeze2_pad3d_squeeze2_fuse_pass,
                   paddle::lite::mir::Unsqueeze2Pad3dSqueeze2FusePass)
     .BindTargets({TARGET(kAny)})
-    .ExcludeTargets({TARGET(kXPU), TARGET(kBM)});
+    .ExcludeTargets({TARGET(kXPU), TARGET(kBM), TARGET(kHuaweiAscendNPU)});
