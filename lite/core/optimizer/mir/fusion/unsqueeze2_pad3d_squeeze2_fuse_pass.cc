5a7cfc2d3335f186f30eab857331a78cc9b60fa3
zhaoyang-star <zhaoyangstar@foxmail.com>
2022-01-18 10:58:15 +0800

    [Pass] fix kirin device failure

--- lite/core/optimizer/mir/fusion/unsqueeze2_pad3d_squeeze2_fuse_pass.cc
+++ lite/core/optimizer/mir/fusion/unsqueeze2_pad3d_squeeze2_fuse_pass.cc
@@ -24,10 +24,8 @@ namespace mir {
 
 void Unsqueeze2Pad3dSqueeze2FusePass::Apply(
     const std::unique_ptr<SSAGraph>& graph) {
-  //  VLOG(4) << "start";
   fusion::Unsqueeze2Pad3dSqueeze2Fuser fuser("unsqueeze2", "pad3d", "squeeze2");
   fuser(graph.get());
-  //  VLOG(4) << "end";
 }
 }  // namespace mir
 }  // namespace lite
