863fe223795d51b1742f2ed3d6012897d9978079
zhaoyang-star <zhaoyangstar@foxmail.com>
2022-01-18 11:09:16 +0800

    [Pass] fix kirin device failure

--- lite/core/optimizer/mir/fusion/unsqueeze2_pad3d_squeeze2_fuse_pass.cc
+++ lite/core/optimizer/mir/fusion/unsqueeze2_pad3d_squeeze2_fuse_pass.cc
@@ -34,4 +34,4 @@ void Unsqueeze2Pad3dSqueeze2FusePass::Apply(
 REGISTER_MIR_PASS(lite_unsqueeze2_pad3d_squeeze2_fuse_pass,
                   paddle::lite::mir::Unsqueeze2Pad3dSqueeze2FusePass)
     .BindTargets({TARGET(kAny)})
-    .ExcludeTargets({TARGET(kXPU), TARGET(kBM)});
+    .ExcludeTargets({TARGET(kXPU), TARGET(kBM), TARGET(kHuaweiAscendNPU)});
