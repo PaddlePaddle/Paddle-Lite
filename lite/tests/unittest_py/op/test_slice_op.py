4d185e5a96504d34a3385fb206a689ede8c56a4a
zhenlin-work <wuzhenlin456@163.com>
2022-01-20 12:01:38 +0000

    fix_slice_ut_bug test=develop, test=document_fix

--- lite/tests/unittest_py/op/test_slice_op.py
+++ lite/tests/unittest_py/op/test_slice_op.py
@@ -182,7 +182,7 @@ class TestSliceOp(AutoScanTest):
         pass
 
     def test(self, *args, **kwargs):
-        self.run_and_statis(quant=False, max_examples=100)
+        self.run_and_statis(quant=False, max_examples=150)
 
 
 if __name__ == "__main__":
