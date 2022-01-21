94121f791c386c61bcc8f6a9a457f44509bf5c31
zhaoyang-star <zhaoyangstar@foxmail.com>
2022-01-21 14:04:52 +0800

    [Pass] Add keepdims_convert_pass

--- lite/core/optimizer/optimizer.cc
+++ lite/core/optimizer/optimizer.cc
@@ -180,6 +180,7 @@ std::unique_ptr<RuntimeProgram> RunDefaultOptimizer(
        "unsqueeze_calc_offline_pass",
        "identity_dropout_eliminate_pass",
        "sparse_conv_detect_pass",
+       "keepdims_convert_pass",
        "__xpu__max_pooling_pad_zero_detect_fuse_pass",
        "__xpu__graph_dedup_pass",
        "__xpu__resnet_fuse_pass",
