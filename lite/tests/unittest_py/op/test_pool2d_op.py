0b175ada810ed16761d3fb4545b06d17c53ab46c
zhaoyang-star <zhaoyangstar@foxmail.com>
2022-01-29 13:07:00 +0800

    fix pool2d test=document_fix

--- lite/tests/unittest_py/op/test_pool2d_op.py
+++ lite/tests/unittest_py/op/test_pool2d_op.py
@@ -155,24 +155,12 @@ class TestPool2dOp(AutoScanTest):
                     or program_config.ops[0].attrs["pooling_type"] == "avg" :
                     return True
             if predictor_config.target() == TargetType.OpenCL:
-                if program_config.ops[0].attrs[
-                        "adaptive"] == True or program_config.ops[0].attrs[
-                            "padding_algorithm"] == "SAME":
+                if program_config.ops[0].attrs["adaptive"] == True:
                     return True
-
-        def teller2(program_config, predictor_config):
-            strides = program_config.ops[0].attrs["strides"]
-            if program_config.ops[0].attrs["ceil_mode"] == True \
-                and strides[0] != strides[1]:
-                return True
 
         self.add_ignore_check_case(
             teller1, IgnoreReasons.ACCURACY_ERROR,
             "The op output has diff in a specific case. We need to fix it as soon as possible."
-        )
-        self.add_ignore_check_case(
-            teller2, IgnoreReasons.ACCURACY_ERROR,
-            "The op output has diff when ceil_model==True and strides[0] is not equal to strides[1] because the output of paddle is abnormal. We need to wait paddle's bugfix."
         )
 
         def teller2(program_config, predictor_config):
@@ -186,7 +174,7 @@ class TestPool2dOp(AutoScanTest):
 
         self.add_ignore_check_case(
             teller2, IgnoreReasons.PADDLELITE_NOT_SUPPORT,
-            "Lite does not support this op in a specific case on opencl. We need to fix it as soon as possible."
+            "Lite does not support this op in a specific case on metal. We need to fix it as soon as possible."
         )
 
     def test(self, *args, **kwargs):
