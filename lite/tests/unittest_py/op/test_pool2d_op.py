70d9c7f193176f8c36c6df495b82bd4d203a626a
xingjing1 <2907374824@qq.com>
2022-02-11 04:20:07 +0000

    fix ARM bugs

--- lite/tests/unittest_py/op/test_pool2d_op.py
+++ lite/tests/unittest_py/op/test_pool2d_op.py
@@ -114,10 +114,8 @@ class TestPool2dOp(AutoScanTest):
             assume(in_shape[2] / ksize[0] == strides[0])
             assume(in_shape[3] / ksize[1] == strides[1])
 
-        #if both paddle and lite have invalid output, so it is an invalid input.
-        #also it is unreasonable when ceil_mode == true with paddings = [0, 0]
-        if paddings == [0, 0] or paddings == [0, 0, 0, 0]:
-            assume(ceil_mode == False)
+        if padding_algorithm == "SAME":
+            assume(paddings == [0, 0])
 
         build_ops = OpConfig(
             type="pool2d",
@@ -191,7 +189,11 @@ class TestPool2dOp(AutoScanTest):
 
         def teller3(program_config, predictor_config):
             if predictor_config.target() == TargetType.ARM:
-                # This is an paddle error, when padding_algorithm == "SAME" with exclusive is True
+                # This is an paddle error, when padding_algorithm == "Valid" with exclusive is False
+                if program_config.ops[0].attrs[
+                        "ceil_mode"] == True and program_config.ops[0].attrs[
+                            "exclusive"] == False:
+                    return True
                 if program_config.ops[0].attrs[
                         "padding_algorithm"] == "VALID" and program_config.ops[
                             0].attrs["exclusive"] == False:
