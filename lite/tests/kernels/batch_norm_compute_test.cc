4737b4f1b04c8bc4a08efc469decf71e20763a1f
Mr.SY <1131165566@qq.com>
2022-02-11 23:15:10 +0800

    [NNAdapter][HuaweiKirinNPU] Add batch_norm, gather, p_norm and tile (#8423)

--- lite/tests/kernels/batch_norm_compute_test.cc
+++ lite/tests/kernels/batch_norm_compute_test.cc
@@ -168,6 +168,8 @@ TEST(BatchNorm, precision) {
   return;
 #elif defined(NNADAPTER_WITH_VERISILICON_TIMVX)
   abs_error = 1e-2;
+#elif defined(NNADAPTER_WITH_HUAWEI_KIRIN_NPU)
+  abs_error = 1e-2;
 #else
   return;
 #endif
