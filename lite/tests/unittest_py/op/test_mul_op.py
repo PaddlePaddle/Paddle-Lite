6e3a8cf5d4909e6cee69e7f9abf63540e67f8688
chenjiaoAngel <chenjiaobuaa@126.com>
2022-02-16 15:17:13 +0800

    add mul int8 op_ut.

--- lite/tests/unittest_py/op/test_mul_op.py
+++ lite/tests/unittest_py/op/test_mul_op.py
@@ -36,6 +36,12 @@ class TestMulOp(AutoScanTest):
             PrecisionType.FP16,
             DataLayoutType.NCHW,
             thread=[1, 4])
+        arm_valid_places = [
+            Place(TargetType.ARM, PrecisionType.INT8, DataLayoutType.NCHW),
+            Place(TargetType.ARM, PrecisionType.FP32, DataLayoutType.NCHW)
+        ]
+        self.enable_testing_on_place(places=arm_valid_places, thread=[1, 4])
+
         self.enable_testing_on_place(
             TargetType.X86,
             PrecisionType.FP32,
