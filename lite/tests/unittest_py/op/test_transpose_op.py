4609e9cf946f2d9ae1cb7beae296a1c1d9f74fdc
MXX <54735487+mjp9527@users.noreply.github.com>
2022-02-28 10:27:03 +0800

    [ut_fix_bug] add fp16 pytest for scale and transpose on ARM (#8417)

--- lite/tests/unittest_py/op/test_transpose_op.py
+++ lite/tests/unittest_py/op/test_transpose_op.py
@@ -65,6 +65,11 @@ class TestTransposeOp(AutoScanTest):
             Place(TargetType.Host, PrecisionType.FP32)
         ]
         self.enable_testing_on_place(places=metal_places)
+        self.enable_testing_on_place(
+            TargetType.ARM,
+            PrecisionType.FP16,
+            DataLayoutType.NCHW,
+            thread=[1, 4])
         self.enable_testing_on_place(TargetType.NNAdapter, PrecisionType.FP32)
         self.enable_devices_on_nnadapter(device_names=["kunlunxin_xtcl"])
 
