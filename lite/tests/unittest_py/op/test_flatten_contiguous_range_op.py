20fc4dd4afe5ae1547a2abaeb96ee4e01308300f
lxw950127 <1182657559@qq.com>
2022-03-17 13:54:37 +0000

    fix, test=nvidia_tensorrt

--- lite/tests/unittest_py/op/test_flatten_contiguous_range_op.py
+++ lite/tests/unittest_py/op/test_flatten_contiguous_range_op.py
@@ -36,9 +36,8 @@ class TestFlattenContiguousRangeOp(AutoScanTest):
             DataLayoutType.Any,
             thread=[1, 4])
         self.enable_testing_on_place(TargetType.NNAdapter, PrecisionType.FP32)
-        self.enable_devices_on_nnadapter(device_names=[
-            "kunlunxin_xtcl", "cambricon_mlu", "nvidia_tensorrt"
-        ])
+        self.enable_devices_on_nnadapter(
+            device_names=["kunlunxin_xtcl", "cambricon_mlu"])
 
     def is_program_valid(self,
                          program_config: ProgramConfig,
