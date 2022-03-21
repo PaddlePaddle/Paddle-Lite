757926c6106c0ae3036f954f9bf53ebf97749e86
mjp9527 <1821582672@qq.com>
2022-03-21 15:02:28 +0800

    fix pytest, test=nvidia_tensorrt

--- lite/tests/unittest_py/op/test_arg_max_op.py
+++ lite/tests/unittest_py/op/test_arg_max_op.py
@@ -111,12 +111,13 @@ class TestArgMaxOp(AutoScanTest):
                         return True
 
         def _teller2(program_config, predictor_config):
+            set_dtype = program_config.ops[0].attrs["dtype"]
             in_shape = list(program_config.inputs["input_data"].shape)
             axis = program_config.ops[0].attrs["axis"]
             keep_dims = program_config.ops[0].attrs["keepdims"]
             if predictor_config.target() == TargetType.Metal:
                 if len(in_shape) != 4 or in_shape[
-                        0] != 1 or axis != 1 or keep_dims == False:
+                        0] != 1 or axis != 1 or keep_dims == False or set_dtype == 2:
                     return True
 
         def _teller3(program_config, predictor_config):
