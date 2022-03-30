6258526283056eca607bb485815485f7dc45be8f
MXX <54735487+mjp9527@users.noreply.github.com>
2022-03-30 16:12:26 +0800

    [NNADAPTER] add batch_norm for tensorrt, test=nvidia_tensorrt (#8717)
    * [NNADAPTER] add batch_norm for tensorrt, test=nvidia_tensorrt

--- lite/kernels/nnadapter/converter/all.h
+++ lite/kernels/nnadapter/converter/all.h
@@ -15,10 +15,11 @@
 #ifndef __NNADAPTER_CONVERTER_ALL_H__  // NOLINT
 #define __NNADAPTER_CONVERTER_ALL_H__
 
-REGISTER_CONVERTER(batch_norm,
-                   ConvertBatchNorm,
-                   "huawei_ascend_npu,verisilicon_"
-                   "timvx,cambricon_mlu,huawei_kirin_npu,intel_openvino");
+REGISTER_CONVERTER(
+    batch_norm,
+    ConvertBatchNorm,
+    "huawei_ascend_npu,verisilicon_"
+    "timvx,cambricon_mlu,huawei_kirin_npu,intel_openvino,nvidia_tensorrt");
 REGISTER_CONVERTER(
     cast,
     ConvertCast,
