73e5e77190a79ec9e2ff1342337224247b4d40df
HappyAngel <chenjiaobuaa@126.com>
2022-04-13 19:18:59 +0800

    add sa8155 support (#8815)

--- lite/core/device_info.cc
+++ lite/core/device_info.cc
@@ -693,8 +693,20 @@ bool DeviceInfo::SetCPUInfoByName() {
     SetFP16Info(1, 1);
     SetDotInfo(2, 1, 1);
     return true;
-  }
-  if (dev_name_.find("KONA") != std::string::npos) {  // 865
+  } else if (dev_name_.find("SA8155") != std::string::npos) {  // sa8155
+    core_num_ = 8;
+    core_ids_ = {0, 1, 2, 3, 4, 5, 6, 7};
+    big_core_ids_ = {4, 5, 6, 7};
+    little_core_ids_ = {0, 1, 2, 3};
+    cluster_ids_ = {1, 1, 1, 1, 0, 0, 0, 0};
+    SetArchInfo(2, kA76, kA55);
+    SetCacheInfo(0, 2, 192 * 1024, 256 * 1024);
+    SetCacheInfo(1, 2, 512 * 1024, 128 * 1024);
+    SetCacheInfo(2, 1, 4 * 1024 * 1024);
+    SetFP16Info(1, 1);
+    SetDotInfo(2, 1, 1);
+    return true;
+  } else if (dev_name_.find("KONA") != std::string::npos) {  // 865
     core_num_ = 8;
     core_ids_ = {0, 1, 2, 3, 4, 5, 6, 7};
     big_core_ids_ = {4, 5, 6, 7};
@@ -707,8 +719,7 @@ bool DeviceInfo::SetCPUInfoByName() {
     SetFP16Info(1, 1);
     SetDotInfo(2, 1, 1);
     return true;
-  }
-  if (dev_name_.find("SM8150") != std::string::npos) {  // 855
+  } else if (dev_name_.find("SM8150") != std::string::npos) {  // 855
     core_num_ = 8;
     core_ids_ = {0, 1, 2, 3, 4, 5, 6, 7};
     big_core_ids_ = {4, 5, 6, 7};
