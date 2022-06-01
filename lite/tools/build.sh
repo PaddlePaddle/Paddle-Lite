6f0060561a6c583241819ce8cc403a3ca9e97a36
mjp9527 <1821582672@qq.com>
2022-06-01 20:39:58 +0800

    tmpsave3

--- lite/tools/build.sh
+++ lite/tools/build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -e
+export NDK_ROOT=/opt/android-ndk-r23b && export PATH=$NDK_ROOT:$PATH
 
 readonly CMAKE_COMMON_OPTIONS="-DWITH_GPU=OFF \
                                -DWITH_MKL=OFF \
