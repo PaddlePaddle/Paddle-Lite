60e9cea00d92b5a37b398606537edcfe1917cadf
liuyiqing <liuyiqing01@baidu.com>
2022-06-08 09:54:00 +0000

    [NNAdapter][IntelOpenvino] Add sqrt op, test=intel_openvino

--- lite/tests/unittest_py/op/test_sqrt_op.py
+++ lite/tests/unittest_py/op/test_sqrt_op.py
@@ -50,6 +50,8 @@ class TestSqrtOp(AutoScanTest):
             Place(TargetType.Host, PrecisionType.FP32)
         ]
         self.enable_testing_on_place(places=opencl_places)
+        self.enable_testing_on_place(TargetType.NNAdapter, PrecisionType.FP32)
+        self.enable_devices_on_nnadapter(device_names=["intel_openvino"])
 
     def is_program_valid(self,
                          program_config: ProgramConfig,
