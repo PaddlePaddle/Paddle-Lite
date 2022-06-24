1040c0dc809468e2dd9013b792287af6fc9780ab
“quwei03” <quwei03@baidu.com>
2022-06-24 20:37:12 +0800

    update

--- lite/core/optimizer/mir/__xpu__static_kernel_pick_pass.cc
+++ lite/core/optimizer/mir/__xpu__static_kernel_pick_pass.cc
@@ -21,7 +21,6 @@
 #include <vector>
 #include "lite/core/optimizer/mir/graph_visualize_pass.h"
 #include "lite/core/optimizer/mir/pass_registry.h"
-
 #ifdef LITE_WITH_XPU
 #include "lite/backends/xpu/target_wrapper.h"
 namespace paddle {
