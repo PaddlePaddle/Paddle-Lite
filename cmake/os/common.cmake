c38e1bf96216f78fa8793918d0f7d0238a1fdce4
zhupengyang <zhupengyang@baidu.com>
2022-06-27 02:47:18 +0000

    test=develop

--- cmake/os/common.cmake
+++ cmake/os/common.cmake
@@ -58,7 +58,7 @@ if(LITE_WITH_ARM)
   endif()
 
   # Toolchain config
-  if (LITE_ON_TINY_PUBLISH OR LITE_WITH_LTO)
+  if ((LITE_ON_TINY_PUBLISH OR LITE_WITH_LTO) AND NOT ARM_TARGET_OS STREQUAL "qnx")
     if(ARM_TARGET_LANG STREQUAL "gcc")
       set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -flto")
     else()
