f80063aeefde3bb2e4ad6fd9c1ab5ab293bd2427
lyq <30404405+affectionlu@users.noreply.github.com>
2022-06-29 16:07:16 +0800

    [NNAdapter][IntelOpenVINO] Add bmm op and converter (#9164)

---
+++ lite/operators/bmm_op.h
@@ -0,0 +1,52 @@
+// Copyright (c) 2019 PaddlePaddle Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#pragma once
+#include <string>
+#include <vector>
+#include "lite/core/kernel.h"
+#include "lite/core/op_lite.h"
+#include "lite/core/scope.h"
+#include "lite/operators/op_params.h"
+#include "lite/utils/all.h"
+
+namespace paddle {
+namespace lite {
+namespace operators {
+
+class BmmOpLite : public OpLite {
+ public:
+  BmmOpLite() {}
+
+  explicit BmmOpLite(const std::string &type) : OpLite(type) {}
+
+  bool CheckShape() const override;
+
+  bool InferShapeImpl() const override;
+
+  bool InferShapeWithCache() const override { return true; }
+
+  void AttachKernel(KernelBase *kernel) override { kernel->SetParam(param_); }
+
+  bool AttachImpl(const cpp::OpDesc &op_desc, lite::Scope *scope) override;
+
+  std::string DebugString() const override { return "bmm"; }
+
+ private:
+  mutable BmmParam param_;
+};
+
+}  // namespace operators
+}  // namespace lite
+}  // namespace paddle
