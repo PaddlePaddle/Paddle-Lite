46cd113f3a54887e83138fb5c399ad3a63ff2606
zhupengyang <zhu_py@qq.com>
2022-07-29 10:26:22 +0800

    [QualcommQnn] add tanh, unsqueeze, equal, slice, cast (#9296)

--- lite/backends/nnadapter/nnadapter/include/nnadapter/optimizer/convert_datalayout_nchw_to_nhwc.h
+++ lite/backends/nnadapter/nnadapter/include/nnadapter/optimizer/convert_datalayout_nchw_to_nhwc.h
@@ -39,6 +39,7 @@ class NCHW2NHWCDataLayoutConverter {
   void ConvertBatchNormalization(core::Operation* operation);
   void ConvertCast(core::Operation* operation);
   void ConvertClip(core::Operation* operation);
+  void ConvertComparisons(core::Operation* operation);
   void ConvertConv2DTranspose(core::Operation* operation);
   void ConvertCumSum(core::Operation* operation);
   void ConvertElementwise(core::Operation* operation);
@@ -59,12 +60,14 @@ class NCHW2NHWCDataLayoutConverter {
   void ConvertResizeNearest(core::Operation* operation);
   void ConvertResizeLinear(core::Operation* operation);
   void ConvertShape(core::Operation* operation);
+  void ConvertSlice(core::Operation* operation);
   void ConvertSoftmax(core::Operation* operation);
   void ConvertSplit(core::Operation* operation);
   void ConvertSqueeze(core::Operation* operation);
   void ConvertStack(core::Operation* operation);
   void ConvertTranspose(core::Operation* operation);
   void ConvertMatMul(core::Operation* operation);
+  void ConvertUnsqueeze(core::Operation* operation);
 
  private:
   core::Model* model_{nullptr};
