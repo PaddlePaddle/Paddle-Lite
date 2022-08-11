ea6753f0f14f2097e3be8e3a96a990b48f26ad2c
hong19860320 <9973393+hong19860320@users.noreply.github.com>
2022-08-11 04:35:08 +0000

    test=huawei_ascend_npu
    Change-Id: I3e959f4d48779c567779e93d9a155c9be364531f

--- lite/tests/kernels/activation_compute_test.cc
+++ lite/tests/kernels/activation_compute_test.cc
@@ -1313,6 +1313,8 @@ TEST(Activation_abs, precision) {
   abs_error = 5e-2;
 #elif defined(NNADAPTER_WITH_HUAWEI_KIRIN_NPU)
   abs_error = 5e-2;
+#elif defined(NNADAPTER_WITH_QUALCOMM_QNN)
+  abs_error = 1e-2;
 #else
   return;
 #endif
