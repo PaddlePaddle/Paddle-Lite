f0feb16c9d6115e922a3f998711314f1620a35bf
zhupengyang <zhu_py@qq.com>
2022-08-26 09:49:36 +0800

    test

--- lite/tests/api/test_darknet53_fp32_v2_3_nnadapter.cc
+++ lite/tests/api/test_darknet53_fp32_v2_3_nnadapter.cc
@@ -55,9 +55,8 @@ TEST(DarkNet, test_darknet53_fp32_v2_3_nnadapter) {
   out_accuracy_threshold = 0.75f;
 #elif defined(NNADAPTER_WITH_QUALCOMM_QNN)
   nnadapter_device_names.emplace_back("qualcomm_qnn");
-  FLAGS_iteration = 1;
-  // TODO(hong19860320) Fix precision
-  out_accuracy_threshold = 0.f;
+  FLAGS_iteration = 5;
+  out_accuracy_threshold = 0.6f;
 #else
   LOG(INFO) << "Unsupported NNAdapter device!";
   return;
