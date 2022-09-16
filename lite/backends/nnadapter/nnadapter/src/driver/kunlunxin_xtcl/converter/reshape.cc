94558889e8a347253cad266838fb332f3f167eef
daydayup-lh <daydayuplih@outlook.com>
2022-09-16 03:36:53 +0000

    [NNAdapter][KunlunxinXTCL] fix kunlunxin_xtcl ops error and add unit test test=xtcl test=develop

--- lite/backends/nnadapter/nnadapter/src/driver/kunlunxin_xtcl/converter/reshape.cc
+++ lite/backends/nnadapter/nnadapter/src/driver/kunlunxin_xtcl/converter/reshape.cc
@@ -18,12 +18,22 @@
 #include "utility/hints.h"
 #include "utility/logging.h"
 #include "utility/modeling.h"
+#include "utility/utility.h"
 
 namespace nnadapter {
 namespace kunlunxin_xtcl {
 
 int ConvertReshape(Converter* converter, core::Operation* operation) {
   RESHAPE_OPERATION_EXTRACT_INPUTS_OUTPUTS
+  auto input_size =
+      ProductionOfDimensions(input_operand->type.dimensions.data,
+                             input_operand->type.dimensions.count);
+  auto output_size =
+      ProductionOfDimensions(output_operand->type.dimensions.data,
+                             output_operand->type.dimensions.count);
+  NNADAPTER_CHECK_EQ(input_size, output_size)
+      << "Expect input_size == output_size, but receive: " << input_size << ", "
+      << output_size;
 
   // Convert to XTCL exprs
   auto input_expr = converter->GetMappedExpr(input_operand);
