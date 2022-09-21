cf3051369170e0fdbd462e70ae91efb9e30f4133
Aurelius84 <zhangliujie@baidu.com>
2022-09-21 02:21:14 +0000

    move into core

--- lite/core/int_array_test.cc
+++ lite/core/int_array_test.cc
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include "lite/operators/int_array.h"
+#include "lite/core/int_array.h"
 #include <gtest/gtest.h>
 #include <vector>
 
