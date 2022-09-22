d93e45444d7c1102c53edf48dafa5094d03cfd45
zhupengyang <zhu_py@qq.com>
2022-09-22 16:28:56 +0800

    [xpu] fix performance

--- cmake/backends/xpu.cmake
+++ cmake/backends/xpu.cmake
@@ -23,7 +23,7 @@ set (XPU_DOWNLOAD_DIR ${XPU_SOURCE_DIR}/download)
 set (XPU_INSTALL_DIR ${THIRD_PARTY_PATH}/install)
 
 if (NOT XPU_SDK_URL)
-  set (XPU_SDK_URL "https://baidu-kunlun-product.su.bcebos.com/KL-SDK/klsdk-dev/20220919")
+  set (XPU_SDK_URL "https://baidu-kunlun-product.su.bcebos.com/KL-SDK/klsdk-dev/20220923")
 endif ()
 
 if (NOT XPU_SDK_ENV)
