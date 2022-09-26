349f2c030f83d070e06e5bd4eaa45434c86c2277
pangyoki <pangyoki@126.com>
2022-09-26 11:05:25 +0000

    upgrade protobuf version to 3.19.0

--- cmake/external/protobuf.cmake
+++ cmake/external/protobuf.cmake
@@ -179,7 +179,7 @@ FUNCTION(build_protobuf TARGET_NAME BUILD_FOR_HOST)
 
     # https://github.com/protocolbuffers/protobuf.git
     SET(PROTOBUF_REPO "")
-    SET(PROTOBUF_TAG "9f75c5aa851cd877fb0d93ccc31b8567a6706546")
+    SET(PROTOBUF_TAG "v3.19.0")
     SET(OPTIONAL_CACHE_ARGS "")
     SET(OPTIONAL_ARGS "")
     SET(SOURCE_DIR "${PADDLE_SOURCE_DIR}/third-party/protobuf-host")
