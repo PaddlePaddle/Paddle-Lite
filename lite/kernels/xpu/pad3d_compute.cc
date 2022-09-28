8451541b478165c757b7521a95f528346a71d91e
wbn520 <2565033196@qq.com>
2022-09-28 09:31:00 +0800

    Modified code based on comment.

--- lite/kernels/xpu/pad3d_compute.cc
+++ lite/kernels/xpu/pad3d_compute.cc
@@ -37,14 +37,25 @@ void Pad3dCompute<T>::Run() {
   auto* out = param.Out;
   T* out_data = out->template mutable_data<T>(TARGET(kXPU));
   bool is_ncdhw;
+  int n, c, d, h, w;
   if (data_format == "NCDHW") {
     is_ncdhw = true;
+    n = in_dims[0];
+    c = in_dims[1];
+    d = in_dims[2];
+    h = in_dims[3];
+    w = in_dims[4];
   } else if (data_format == "NDHWC") {
     is_ncdhw = false;
+    n = in_dims[0];
+    c = in_dims[4];
+    d = in_dims[1];
+    h = in_dims[2];
+    w = in_dims[3];
   } else {
     LOG(FATAL) << "xpu unsupport data_format: " << data_format;
   }
-  // trans format
+  // trans pad format
   std::vector<int> padding(6);
   padding[0] = pads[4];
   padding[1] = pads[5];
@@ -54,96 +65,41 @@ void Pad3dCompute<T>::Run() {
   padding[5] = pads[1];
 
   if (mode == "constant") {
-    if (is_ncdhw) {
-      std::vector<int> pad_left = {0, 0, pads[4], pads[2], pads[0]};
-      std::vector<int> pad_right = {0, 0, pads[5], pads[3], pads[1]};
-
-      int n_shape = in_dims[0];
-      int c_shape = in_dims[1];
-      int d_shape = in_dims[2];
-      int h_shape = in_dims[3];
-      int w_shape = in_dims[4];
-
-      std::vector<int> xshape = {n_shape, c_shape, d_shape, h_shape, w_shape};
-      int r = xdnn::pad<T>(ctx.GetRawContext(),
-                           in_data,
-                           out_data,
-                           xshape,
-                           pad_left,
-                           pad_right,
-                           value);
-      CHECK_EQ(r, 0);
-    } else {
-      std::vector<int> pad_left = {0, pads[4], pads[2], pads[0], 0};
-      std::vector<int> pad_right = {0, pads[5], pads[3], pads[1], 0};
-
-      int n_shape = in_dims[0];
-      int d_shape = in_dims[1];
-      int h_shape = in_dims[2];
-      int w_shape = in_dims[3];
-      int c_shape = in_dims[4];
-      std::vector<int> xshape = {n_shape, d_shape, h_shape, w_shape, c_shape};
-
-      int r = xdnn::pad<T>(ctx.GetRawContext(),
-                           in_data,
-                           out_data,
-                           xshape,
-                           pad_left,
-                           pad_right,
-                           value);
-      CHECK_EQ(r, 0);
-    }
+    int r = xdnn::constant_pad3d<T>(ctx.GetRawContext(),
+                                    in_data,
+                                    out_data,
+                                    n,
+                                    c,
+                                    d,
+                                    h,
+                                    w,
+                                    padding,
+                                    value,
+                                    is_ncdhw);
+    CHECK_EQ(r, 0);
   } else if (mode == "reflect") {
-    int r = 0;
-    if (is_ncdhw) {
-      int r = xdnn::reflection_pad3d<T>(ctx.GetRawContext(),
-                                        in_data,
-                                        out_data,
-                                        in_dims[0],
-                                        in_dims[1],
-                                        in_dims[2],
-                                        in_dims[3],
-                                        in_dims[4],
-                                        padding,
-                                        is_ncdhw);
-    } else {
-      int r = xdnn::reflection_pad3d<T>(ctx.GetRawContext(),
-                                        in_data,
-                                        out_data,
-                                        in_dims[0],
-                                        in_dims[4],
-                                        in_dims[1],
-                                        in_dims[2],
-                                        in_dims[3],
-                                        padding,
-                                        is_ncdhw);
-    }
+    int r = xdnn::reflection_pad3d<T>(ctx.GetRawContext(),
+                                      in_data,
+                                      out_data,
+                                      n,
+                                      c,
+                                      d,
+                                      h,
+                                      w,
+                                      padding,
+                                      is_ncdhw);
     CHECK_EQ(r, 0);
   } else if (mode == "replicate") {
-    int r = 0;
-    if (is_ncdhw) {
-      int r = xdnn::replication_pad3d<T>(ctx.GetRawContext(),
-                                         in_data,
-                                         out_data,
-                                         in_dims[0],
-                                         in_dims[1],
-                                         in_dims[2],
-                                         in_dims[3],
-                                         in_dims[4],
-                                         padding,
-                                         is_ncdhw);
-    } else {
-      int r = xdnn::replication_pad3d<T>(ctx.GetRawContext(),
-                                         in_data,
-                                         out_data,
-                                         in_dims[0],
-                                         in_dims[4],
-                                         in_dims[1],
-                                         in_dims[2],
-                                         in_dims[3],
-                                         padding,
-                                         is_ncdhw);
-    }
+    int r = xdnn::replication_pad3d<T>(ctx.GetRawContext(),
+                                       in_data,
+                                       out_data,
+                                       n,
+                                       c,
+                                       d,
+                                       h,
+                                       w,
+                                       padding,
+                                       is_ncdhw);
     CHECK_EQ(r, 0);
   } else {
     LOG(FATAL) << "xpu unsupport mode: " << mode;
