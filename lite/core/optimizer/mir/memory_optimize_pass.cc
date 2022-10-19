a385f458193560ce9766a00508d4559abbac72e1
csy0225 <chensiyu08@baidu.com>
2022-10-19 02:08:21 +0000

    update comment test=huawei_ascend_npu,test=cambricon_mlu,test=rockchip_npu

--- lite/core/optimizer/mir/memory_optimize_pass.cc
+++ lite/core/optimizer/mir/memory_optimize_pass.cc
@@ -345,7 +345,5 @@ void MemoryOptimizePass::Apply(const std::unique_ptr<SSAGraph>& graph) {
 
 REGISTER_MIR_PASS(memory_optimize_pass, paddle::lite::mir::MemoryOptimizePass)
     .BindTargets({TARGET(kARM), TARGET(kOpenCL)})
-    .ExcludeTargets({TARGET(kBM),
-                     TARGET(kXPU),
-                     TARGET(kMetal),
-                     TARGET(kNNAdapter)});
+    .ExcludeTargets(
+        {TARGET(kBM), TARGET(kXPU), TARGET(kMetal), TARGET(kNNAdapter)});
