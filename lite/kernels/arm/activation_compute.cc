663ed164119867b1678049ecc320ec61d33f8116
wz1qqx <55830058+wz1qqx@users.noreply.github.com>
2022-11-17 12:41:29 +0800

    [ARM] Add silu op  (#9280)
    * add silu op and python unitest

--- lite/kernels/arm/activation_compute.cc
+++ lite/kernels/arm/activation_compute.cc
@@ -169,6 +169,16 @@ void EluCompute::Run() {
       x_data, output_data, x_dims.production(), alpha, ctx.threads());
 }
 
+void SiluCompute::Run() {
+  auto& param = this->Param<param_t>();
+  auto& ctx = this->ctx_->template As<ARMContext>();
+  auto x_dims = param.X->dims();
+  auto x_data = param.X->data<float>();
+  auto output_data = param.Out->mutable_data<float>();
+  lite::arm::math::act_silu<float>(
+      x_data, output_data, x_dims.production(), ctx.threads());
+}
+
 }  // namespace arm
 }  // namespace kernels
 }  // namespace lite
@@ -273,6 +283,11 @@ REGISTER_LITE_KERNEL(thresholded_relu,
     .Finalize();
 REGISTER_LITE_KERNEL(
     elu, kARM, kFloat, kNCHW, paddle::lite::kernels::arm::EluCompute, def)
+    .BindInput("X", {LiteType::GetTensorTy(TARGET(kARM))})
+    .BindOutput("Out", {LiteType::GetTensorTy(TARGET(kARM))})
+    .Finalize();
+REGISTER_LITE_KERNEL(
+    silu, kARM, kFloat, kNCHW, paddle::lite::kernels::arm::SiluCompute, def)
     .BindInput("X", {LiteType::GetTensorTy(TARGET(kARM))})
     .BindOutput("Out", {LiteType::GetTensorTy(TARGET(kARM))})
     .Finalize();
--- lite/tests/kernels/activation_compute_test.cc
+++ lite/tests/kernels/activation_compute_test.cc
@@ -29,6 +29,7 @@ enum activation_type_test {
   RELU_CLIPPED,
   PRELU,
   SIGMOID,
+  SILU,
   TANH,
   SWISH,
   RELU6,
@@ -162,6 +163,12 @@ class ActivationComputeTester : public arena::TestCase {
       case SIGMOID: {
         for (int i = 0; i < dims_.production(); i++) {
           output_data[i] = 1.f / (1.f + std::exp(-x_data[i]));
+        }
+        break;
+      }
+      case SILU: {
+        for (int i = 0; i < dims_.production(); i++) {
+          output_data[i] = x_data[i] / (1.f + std::exp(-x_data[i]));
         }
         break;
       }
@@ -670,6 +677,32 @@ TEST(Activation_sigmoid, precision) {
             DDim(dims),
             "sigmoid",
             SIGMOID,
+            abs_error);
+  }
+}
+
+TEST(Activation_silu, precision) {
+  Place place;
+  float abs_error = 2e-5;
+  std::vector<std::vector<int64_t>> test_dims{
+      {1, 3, 2, 4}, {2, 3, 4}, {5, 4}, {8}};
+#if defined(LITE_WITH_ARM)
+  place = TARGET(kARM);
+#else
+  return;
+#endif
+
+  for (auto dims : test_dims) {
+    TestAct(place,
+            "def",
+            0.01,
+            6.,
+            "all",
+            0.,
+            1.0,
+            DDim(dims),
+            "silu",
+            SILU,
             abs_error);
   }
 }
