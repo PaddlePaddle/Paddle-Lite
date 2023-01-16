58e3741feb38240df0d1aa40db5d60dc4ebaf6a2
shentanyue <shentanyue01@baidu.com>
2023-01-16 13:43:55 +0800

    fix meshgrid of ascend test=huawei_ascend_npu

--- lite/backends/nnadapter/nnadapter/src/operation/meshgrid.cc
+++ lite/backends/nnadapter/nnadapter/src/operation/meshgrid.cc
@@ -15,6 +15,7 @@
 #include "operation/meshgrid.h"
 #include <vector>
 #include "core/types.h"
+#include "operation/math/meshgrid.h"
 #include "utility/debug.h"
 #include "utility/logging.h"
 #include "utility/micros.h"
@@ -31,9 +32,9 @@ NNADAPTER_EXPORT bool ValidateMeshgrid(const core::Operation* operation) {
 NNADAPTER_EXPORT int PrepareMeshgrid(core::Operation* operation) {
   MESHGRID_OPERATION_EXTRACT_INPUTS_OUTPUTS
   for (auto output_operand : output_operands) {
-    output_operand->type.dimensions.count = input_count;
     CopyOperandTypeExceptQuantParams(&output_operand->type,
                                      input_operands[0]->type);
+    output_operand->type.dimensions.count = input_count;
   }
 
   // Infer the shape and type of output operands
@@ -72,6 +73,34 @@ NNADAPTER_EXPORT int PrepareMeshgrid(core::Operation* operation) {
 }
 
 NNADAPTER_EXPORT int ExecuteMeshgrid(core::Operation* operation) {
+  // SOFTMAX_OPERATION_EXTRACT_INPUTS_OUTPUTS
+
+  // // Allocate and calculate the output operands
+  // int status = -1;
+  // auto& input_type = input_operand->type;
+  // auto input_shape = std::vector<int32_t>(
+  //     input_type.dimensions.data,
+  //     input_type.dimensions.data + input_type.dimensions.count);
+  // const auto input_buffer = input_operand->buffer;
+  // NNADAPTER_CHECK(input_buffer);
+  // auto& output_type = output_operand->type;
+  // auto output_buffer = AllocateOperand(output_operand);
+  // NNADAPTER_CHECK_EQ(input_type.precision, output_type.precision);
+  // if (input_type.precision == NNADAPTER_FLOAT32) {
+  //   const auto input_data = reinterpret_cast<const float*>(input_buffer);
+  //   auto output_data = reinterpret_cast<float*>(output_buffer);
+  //   status = math::softmax<float>(input_data, input_shape, axis,
+  //   output_data);
+  // } else {
+  //   NNADAPTER_LOG(FATAL) << "Unsupported precision code("
+  //                        <<
+  //                        OperandPrecisionCodeToString(input_type.precision)
+  //                        << ") for " <<
+  //                        OperationTypeToString(operation->type)
+  //                        << " is found!";
+  // }
+  // NNADAPTER_CHECK_EQ(status, 0);
+  // return NNADAPTER_NO_ERROR;
   return NNADAPTER_FEATURE_NOT_SUPPORTED;
 }
 
