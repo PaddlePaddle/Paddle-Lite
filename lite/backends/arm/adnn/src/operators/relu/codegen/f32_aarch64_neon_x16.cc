ff32e47c5b034d0b4dfda047c102a314794c248c
hong19860320 <9973393+hong19860320@users.noreply.github.com>
2023-02-23 03:07:33 +0000

    refine
    Change-Id: Id5abd4f349e7f054c4544cbdd03c5b7c3e0cccd2

--- lite/backends/arm/adnn/src/operators/relu/codegen/f32_aarch64_neon_x16.cc
+++ lite/backends/arm/adnn/src/operators/relu/codegen/f32_aarch64_neon_x16.cc
@@ -12,21 +12,10 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// Auto-generated file from src/operators/relu/fp32_neon.cc.in, Don't edit it!
-// Copyright (c) 2023 PaddlePaddle Authors. All Rights Reserved.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
+// This file was automatically generated, Do not edit it!
+// Template file: src/operators/relu/fp32_neon.cc.in
+// Output file: src/operators/relu/codegen/f32_aarch64_neon_x16.cc
+// Command args:  -D BATCH_TILE=16 -D ARCH=aarch64
 #include <arm_neon.h>
 #include <assert.h>
 #include "adnn/core/types.h"
--- lite/backends/arm/adnn/tools/codegen.py
+++ lite/backends/arm/adnn/tools/codegen.py
@@ -35,7 +35,7 @@ def key_value(argument):
 
 
 parser = argparse.ArgumentParser()
-parser.add_argument("input_file")
+parser.add_argument("template_file")
 parser.add_argument("output_file")
 parser.add_argument(
     "-D", dest="defines", nargs="*", type=key_value, action="append")
@@ -63,11 +63,22 @@ def escape(line):
 
 
 def main(args):
+    command_line = args[0]
+    command_args = ""
+    for i in range(1, len(args)):
+        command_line = command_line + " " + args[i]
+    for i in range(3, len(args)):
+        command_args = command_args + " " + args[i]
+    print(command_line)
     args = parser.parse_args()
-    print(args)
-    input_defines = dict(chain(*args.defines))
-    input_text = codecs.open(args.input_file, "r", encoding="utf-8").read()
-    input_lines = input_text.splitlines()
+    template_defines = dict(chain(*args.defines))
+    template_defines["TEMPLATE_FILE"] = args.template_file
+    template_defines["OUTPUT_FILE"] = args.output_file
+    template_defines["COMMAND_LINE"] = command_line
+    template_defines["COMMAND_ARGS"] = command_args
+    template_text = codecs.open(
+        args.template_file, "r", encoding="utf-8").read()
+    template_lines = template_text.splitlines()
     python_lines = []
     blank_lines = 0
     last_line = ""
@@ -77,62 +88,62 @@ def main(args):
     # Indicates whether this is the first line inside Python
     # code block (i.e. for, while, if, elif, else)
     python_block_start = True
-    for i, input_line in enumerate(input_lines):
-        if input_line == "":
+    for i, template_line in enumerate(template_lines):
+        if template_line == "":
             blank_lines += 1
             continue
         # Skip lint markers.
-        #if 'LINT' in input_line:
+        #if 'LINT' in template_line:
         #  continue
 
-        input_indent = extract_leading_whitespace(input_line)
+        template_indent = extract_leading_whitespace(template_line)
         if python_block_start:
-            assert input_indent.startswith(last_indent)
-            extra_python_indent = input_indent[len(last_indent):]
+            assert template_indent.startswith(last_indent)
+            extra_python_indent = template_indent[len(last_indent):]
             python_indent = indent_stack[-1][1] + extra_python_indent
-            indent_stack.append((input_indent, python_indent))
-            assert input_indent.startswith(indent_stack[-1][0])
+            indent_stack.append((template_indent, python_indent))
+            assert template_indent.startswith(indent_stack[-1][0])
         else:
-            while not input_indent.startswith(indent_stack[-1][0]):
+            while not template_indent.startswith(indent_stack[-1][0]):
                 del indent_stack[-1]
         python_block_start = False
 
         python_indent = indent_stack[-1][1]
-        stripped_input_line = input_line.strip()
-        if stripped_input_line.startswith(
-                "$") and not stripped_input_line.startswith("${"):
-            if stripped_input_line.endswith(":"):
+        stripped_template_line = template_line.strip()
+        if stripped_template_line.startswith(
+                "$") and not stripped_template_line.startswith("${"):
+            if stripped_template_line.endswith(":"):
                 python_block_start = True
             while blank_lines != 0:
                 python_lines.append(python_indent + "print(file=OUT_STREAM)")
                 blank_lines -= 1
-            python_lines.append(python_indent + stripped_input_line.replace(
+            python_lines.append(python_indent + stripped_template_line.replace(
                 "$", ""))
         else:
-            assert input_line.startswith(python_indent)
+            assert template_line.startswith(python_indent)
             while blank_lines != 0:
                 python_lines.append(python_indent + "print(file=OUT_STREAM)")
                 blank_lines -= 1
             python_lines.append(python_indent + "print(%s, file=OUT_STREAM)" %
-                                escape(input_line[len(python_indent):]))
-        last_line = input_line
-        last_indent = input_indent
+                                escape(template_line[len(python_indent):]))
+        last_line = template_line
+        last_indent = template_indent
 
     while blank_lines != 0:
         python_lines.append(python_indent + "print(file=OUT_STREAM)")
         blank_lines -= 1
 
-    exec_globals = dict(input_defines)
+    exec_globals = dict(template_defines)
     if sys.version_info > (3, 0):
         output_stream = io.StringIO()
     else:
         output_stream = io.BytesIO()
     exec_globals["OUT_STREAM"] = output_stream
-    python_bytecode = compile("\n".join(python_lines), args.input_file, 'exec')
+    python_bytecode = compile("\n".join(python_lines), args.template_file,
+                              'exec')
     exec(python_bytecode, exec_globals)
     with codecs.open(args.output_file, "w", encoding="utf-8") as output_file:
-        output_file.write("// Auto-generated file from " + args.input_file +
-                          ", Don't edit it!\n" + output_stream.getvalue())
+        output_file.write(output_stream.getvalue())
 
 
 if __name__ == "__main__":
