cfb2ac7dda67982184fd953f265e9eaff845acc2
yunyipower <zhouyouisme@163.com>
2023-02-27 11:44:04 +0000

    adjust unit test for softplus, XPU1 no implement.

--- lite/tests/kernels/activation_compute_test.cc
+++ lite/tests/kernels/activation_compute_test.cc
@@ -1163,8 +1163,6 @@ TEST(Activation_softplus, precision) {
 #else
   return;
 #endif
-#elif defined(LITE_WITH_XPU)
-  place = TARGET(kXPU);
 #elif defined(LITE_WITH_X86)
   place = TARGET(kHost);
 #elif defined(LITE_WITH_ARM)
