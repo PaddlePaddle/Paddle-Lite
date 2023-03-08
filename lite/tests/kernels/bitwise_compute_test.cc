2a1e50a68b0c2d04f0ce022f1c6b9ecf88e27ffe
wz1qqx <2997223960@qq.com>
2023-03-08 09:42:30 +0000

    fix ci,test=develop

--- lite/tests/kernels/bitwise_compute_test.cc
+++ lite/tests/kernels/bitwise_compute_test.cc
@@ -1,3 +1,17 @@
+// Copyright (c) 2023 PaddlePaddle Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 #include <gtest/gtest.h>
 #include <algorithm>
 
@@ -62,7 +76,7 @@ T not_op(T a) {
 
 template <>
 bool not_op(bool a) {
-  return ~a;
+  return !a;
 }
 
 template <class T = int64_t>
@@ -91,52 +105,53 @@ class BitwiseComputeTester : public arena::TestCase {
         axis_(axis) {}
 
   void RunBaseline(Scope* scope) override {
-    if (axis_ < 0) {
-      axis_ = x_dims_.size() - y_dims_.size();
-    }
-    auto x_shape = x_dims_.Vectorize();
-    while (x_shape.size() < 4) {
-      x_shape.push_back(1);
-    }
-    auto y_shape = y_dims_.Vectorize();
-    y_shape.insert(y_shape.begin(), axis_, 1);
-    while (y_shape.size() < 4) {
-      y_shape.push_back(1);
-    }
-    CHECK_EQ(x_shape.size(), 4);
-    CHECK_EQ(y_shape.size(), 4);
-
     auto x = scope->FindTensor(x_);
-    auto y = scope->FindTensor(y_);
     auto x_data = x->template data<T>();
-    auto y_data = y->template data<T>();
     auto out = scope->NewTensor(out_);
     out->Resize(x_dims_);
     auto out_data = out->template mutable_data<T>();
     memcpy(out_data, x_data, sizeof(T) * x_dims_.production());
 
-    int xn = x_shape[0];
-    int xc = x_shape[1];
-    int xh = x_shape[2];
-    int xw = x_shape[3];
-
-    int yn = y_shape[0];
-    int yc = y_shape[1];
-    int yh = y_shape[2];
-    int yw = y_shape[3];
-    if (bitwise_type_ == "and") {
-      BITWISE(and_op);
-    } else if (bitwise_type_ == "or") {
-      BITWISE(or_op);
-    } else if (bitwise_type_ == "xor") {
-      BITWISE(xor_op);
-    } else if (bitwise_type_ == "not") {
+    if (bitwise_type_ == "not") {
       auto numel = x->numel();
       for (int i = 0; i < numel; ++i) {
         out_data[i] = not_op(x_data[i]);
       }
     } else {
-      LOG(FATAL) << "unsupported bitwise_op";
+      if (axis_ < 0) {
+        axis_ = x_dims_.size() - y_dims_.size();
+      }
+      auto x_shape = x_dims_.Vectorize();
+      while (x_shape.size() < 4) {
+        x_shape.push_back(1);
+      }
+      auto y_shape = y_dims_.Vectorize();
+      y_shape.insert(y_shape.begin(), axis_, 1);
+      while (y_shape.size() < 4) {
+        y_shape.push_back(1);
+      }
+      CHECK_EQ(x_shape.size(), 4);
+      CHECK_EQ(y_shape.size(), 4);
+
+      auto y = scope->FindTensor(y_);
+      auto y_data = y->template data<T>();
+
+      int xn = x_shape[0];
+      int xc = x_shape[1];
+      int xh = x_shape[2];
+      int xw = x_shape[3];
+
+      int yn = y_shape[0];
+      int yc = y_shape[1];
+      int yh = y_shape[2];
+      int yw = y_shape[3];
+      if (bitwise_type_ == "and") {
+        BITWISE(and_op);
+      } else if (bitwise_type_ == "or") {
+        BITWISE(or_op);
+      } else if (bitwise_type_ == "xor") {
+        BITWISE(xor_op);
+      }
     }
   }
 
@@ -144,7 +159,9 @@ class BitwiseComputeTester : public arena::TestCase {
     std::string op_type = "bitwise_" + bitwise_type_;
     op_desc->SetType(op_type);
     op_desc->SetInput("X", {x_});
-    op_desc->SetInput("Y", {y_});
+    if (bitwise_type_ != "not") {
+      op_desc->SetInput("Y", {y_});
+    }
     op_desc->SetOutput("Out", {out_});
     op_desc->SetAttr("axis_", axis_);
   }
@@ -159,12 +176,15 @@ void BitwiseComputeTester<T>::PrepareData() {
     dx[i] = static_cast<T>(i % 128);
   }
   SetCommonTensor(x_, x_dims_, dx.data());
-
-  std::vector<T> dy(y_dims_.production());
-  for (int i = 0; i < y_dims_.production(); i++) {
-    dy[i] = static_cast<T>(i % 128);
+  if (bitwise_type_ == "not") {
+    return;
+  } else {
+    std::vector<T> dy(y_dims_.production());
+    for (int i = 0; i < y_dims_.production(); i++) {
+      dy[i] = static_cast<T>(i % 128);
+    }
+    SetCommonTensor(y_, y_dims_, dy.data());
   }
-  SetCommonTensor(y_, y_dims_, dy.data());
 }
 
 template <>
@@ -174,12 +194,15 @@ void BitwiseComputeTester<bool>::PrepareData() {
     x_data[i] = static_cast<uint8_t>(i % 2);
   }
   SetCommonTensor(x_, x_dims_, reinterpret_cast<bool*>(x_data.data()));
-
-  std::vector<uint8_t> y_data(y_dims_.production());
-  for (int i = 0; i < y_dims_.production(); i++) {
-    y_data[i] = static_cast<uint8_t>(i % 2);
+  if (bitwise_type_ == "not") {
+    return;
+  } else {
+    std::vector<uint8_t> y_data(y_dims_.production());
+    for (int i = 0; i < y_dims_.production(); i++) {
+      y_data[i] = static_cast<uint8_t>(i % 2);
+    }
+    SetCommonTensor(y_, y_dims_, reinterpret_cast<bool*>(y_data.data()));
   }
-  SetCommonTensor(y_, y_dims_, reinterpret_cast<bool*>(y_data.data()));
 }
 
 template <class T = int64_t>
@@ -188,42 +211,35 @@ void TestPre(const Place& place,
              std::vector<int64_t> x_shape,
              std::vector<int64_t> y_shape,
              std::string bitwise_type,
-             int axis,
              const std::string& alias = "int64") {
   std::unique_ptr<arena::TestCase> tester(new BitwiseComputeTester<T>(
-      place, alias, x_shape, y_shape, bitwise_type, axis));
+      place, alias, x_shape, y_shape, bitwise_type));
   arena::Arena arena(std::move(tester), place, abs_error);
   arena.TestPrecision();
 }
 
-template <class T = int64_t>
+template <class T = bool>
 void TestPer(const Place& place,
              float abs_error,
              std::vector<int64_t> x_shape,
              std::vector<int64_t> y_shape,
              std::string bitwise_type,
-             int axis,
-             const std::string& alias = "def") {
+             const std::string& alias = "int64") {
   std::unique_ptr<arena::TestCase> tester(new BitwiseComputeTester<T>(
-      place, alias, x_shape, y_shape, bitwise_type, axis));
+      place, alias, x_shape, y_shape, bitwise_type));
   arena::Arena arena(std::move(tester), place, abs_error);
   arena.TestPerformance();
 }
 
 void TestBitDims(const Place& place, float abs_error) {
   for (auto bit_type : std::vector<std::string>{"and", "not"}) {
-    TestPre(place, abs_error, {2, 3, 4, 5}, {2, 3, 4, 5}, bit_type, 0);
-    TestPre(place, abs_error, {2, 3, 4}, {2, 3, 4}, bit_type, 0);
-    TestPre(place, abs_error, {2, 3, 4}, {2, 3}, bit_type, 0);
-    TestPre(place, abs_error, {2, 3}, {2}, bit_type, 0);
-    TestPre(place, abs_error, {2, 3, 4, 5}, {3, 4}, bit_type, 1);
-    TestPre(place, abs_error, {2, 3, 4}, {3, 4}, bit_type, 1);
-    TestPre(place, abs_error, {2, 3}, {3}, bit_type, 1);
-    TestPre(place, abs_error, {2, 3, 4, 5}, {4, 5}, bit_type, 2);
-    TestPre(place, abs_error, {2, 3, 4}, {4}, bit_type, 2);
-    TestPre(place, abs_error, {2, 3, 4, 5}, {5}, bit_type, 3);
-    TestPre(place, abs_error, {2, 3, 4, 5}, {3, 4, 5}, bit_type, -1);
-    TestPre(place, abs_error, {2, 3, 4}, {3, 4}, bit_type, -1);
+    if (bit_type != "not") {
+      TestPre(place, abs_error, {2, 3, 4, 5}, {3, 4, 5}, bit_type);
+      TestPre(place, abs_error, {2, 3, 4}, {3, 4}, bit_type);
+      TestPre(place, abs_error, {2, 3}, {2, 1}, bit_type);
+    } else {
+      TestPre(place, abs_error, {2}, {}, bit_type);
+    }
   }
 }
 
@@ -232,22 +248,29 @@ void TestBitTypes(const Place& place, float abs_error) {
   Place host_int32_place(TARGET(kHost), PRECISION(kInt32));
   Place host_int64_place(TARGET(kHost), PRECISION(kInt64));
   for (auto bit_type : std::vector<std::string>{"and", "not"}) {
-    TestPre<bool>(host_bool_place,
-                  abs_error,
-                  {2, 3, 4, 5},
-                  {2, 3, 4, 5},
-                  bit_type,
-                  0,
-                  "bl");
-    TestPre<int32_t>(host_int32_place,
-                     abs_error,
-                     {2, 3, 4, 5},
-                     {2, 3, 4, 5},
-                     bit_type,
-                     -1,
-                     "int32");
-    TestPre<int64_t>(
-        host_int64_place, abs_error, {2, 3, 4, 5}, {3}, bit_type, 1, "int64");
+    if (bit_type == "not") {
+      TestPre<bool>(
+          host_bool_place, abs_error, {2, 3, 4, 5}, {}, bit_type, "bl");
+      TestPre<int32_t>(
+          host_int32_place, abs_error, {2, 3, 4, 5}, {}, bit_type, "int32");
+      TestPre<int64_t>(
+          host_int64_place, abs_error, {2, 3, 4, 5}, {}, bit_type, "int64");
+    } else {
+      TestPre<bool>(host_bool_place,
+                    abs_error,
+                    {2, 3, 4, 5},
+                    {2, 3, 4, 5},
+                    bit_type,
+                    "bl");
+      TestPre<int32_t>(host_int32_place,
+                       abs_error,
+                       {2, 3, 4, 5},
+                       {3, 4, 5},
+                       bit_type,
+                       "int32");
+      TestPre<int64_t>(
+          host_int64_place, abs_error, {2, 3, 4, 5}, {4, 1}, bit_type, "int64");
+    }
   }
 }
 
