e2a9e90a091e3f2863915a1c0033050d9961dd76
mjp9527 <54735487+mjp9527@users.noreply.github.com>
2023-03-16 18:32:17 +0800

    [Cherry-pick] Add viterbi decode for arm (#10108)

--- lite/operators/op_params.h
+++ lite/operators/op_params.h
@@ -2337,6 +2337,15 @@ struct RoundParam : ParamBase {
   lite::Tensor* Out{};
 };
 
+struct ViterbiDecodeParam : ParamBase {
+  const lite::Tensor* input{};
+  const lite::Tensor* length{};
+  const lite::Tensor* transition{};
+  lite::Tensor* path{};
+  lite::Tensor* scores{};
+  bool include_bos_eos_tag{};
+};
+
 }  // namespace operators
 }  // namespace lite
 }  // namespace paddle
