0d5a36a4a95665b83bece93efaea83dd18249f3d
wasupandceacar <wasupandceacar@gmail.com>
2023-04-04 14:04:27 +0800

    fix var desc update error when doing conv-conv fuse

--- lite/core/program.cc
+++ lite/core/program.cc
@@ -41,9 +41,9 @@ void CheckProgramDescValidity(std::shared_ptr<cpp::ProgramDesc> program_desc,
   CHECK_GT(block_size, 0) << "No block exists in current program_desc";
   // TODD(hong19860320) Only support updating the block desc which already
   // exists in the origin program desc
-  CHECK_LE(block_size, inst_block_size) << "Invalid block size, expected (0,"
-                                        << inst_block_size << "] but got "
-                                        << block_size;
+  CHECK_LE(block_size, inst_block_size)
+      << "Invalid block size, expected (0," << inst_block_size << "] but got "
+      << block_size;
 }
 
 std::map<std::string, cpp::VarDesc> ClearBlockDescInfo(
@@ -75,7 +75,8 @@ void UpdatePersistableVarDesc(cpp::VarDesc* var,
     }
   }
   if (var_name != "feed" && var_name != "fetch") {
-    var->SetShape(previous_var_desc.GetShape());
+    auto tensor = scope->FindVar(var_name)->GetMutable<Tensor>();
+    var->SetShape(tensor->dims().data());
     var->SetDataType(previous_var_desc.GetDataType());
   }
 }
