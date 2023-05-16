b7c31ab9cfb32348545829130894b79a7c8971ef
lishicheng1996 <43111799+lishicheng1996@users.noreply.github.com>
2023-05-16 10:25:03 +0800

    Metal backend support 0d tensor (#10265)

--- lite/backends/metal/metal_image.mm
+++ lite/backends/metal/metal_image.mm
@@ -71,6 +71,8 @@ void MetalImage::InitTexture() {
                 dim.emplace_back(pad_to_four_dim_[i]);
             });
         } break;
+        case 0:
+            break;
         default:
             LOG(FATAL) << "metal_image: Dim size is error";
     }
@@ -98,6 +100,8 @@ void MetalImage::InitTexture() {
                 desc_.height = static_cast<NSUInteger>(dim[1]);
                 desc_.arrayLength = static_cast<NSUInteger>(((dim[3] + 3) / 4) * (dim[0]));
                 break;
+            case 0:
+                break;
             default:
                 LOG(FATAL) << "metal_image: Dim size is error";
         }
@@ -114,6 +118,8 @@ void MetalImage::InitTexture() {
                 desc_.height = static_cast<NSUInteger>(dim[1]);
                 desc_.arrayLength = static_cast<NSUInteger>(((dim[3] + 3) / 4) * (dim[0]));
             } break;
+            case 0:
+                break;
             default:
                 LOG(FATAL) << "metal_image: Dim size is error";
         }
