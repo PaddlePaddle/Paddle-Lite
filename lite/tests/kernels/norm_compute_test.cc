6570a95d304089e6d16e63a5d94b619a042a584c
mjp9527 <54735487+mjp9527@users.noreply.github.com>
2023-05-29 10:55:10 +0800

    [ARM] move norm_compute to Host (#10274)
    * [ARM] move norm_compute to Host

--- lite/tests/kernels/norm_compute_test.cc
+++ lite/tests/kernels/norm_compute_test.cc
@@ -112,7 +112,7 @@ TEST(Norm, precision) {
 #elif defined(LITE_WITH_XPU)
   place = TARGET(kXPU);
 #elif defined(LITE_WITH_ARM)
-  place = TARGET(kARM);
+  place = TARGET(kHost);
 #elif defined(LITE_WITH_X86)
   place = TARGET(kHost);
 #else
